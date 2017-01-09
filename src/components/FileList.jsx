25a28434b57af4a3f776f88fe285a044e463a170
GoOz <gooz@foojin.com>
2017-01-09 16:32:26 +0100

    [Feat] Add Empty folder view

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -3,6 +3,7 @@ import styles from '../styles/table'
 import React from 'react'
 import { translate } from '../lib/I18n'
 
+import Empty from '../components/Empty'
 import File from '../components/File'
 
 const FileList = ({ t, f, files = [], onFileEdit, isFetching }) => {
@@ -10,23 +11,26 @@ const FileList = ({ t, f, files = [], onFileEdit, isFetching }) => {
     return <p>Loading</p>
   }
   return (
-    <table class={styles['fil-content-table']} role='contentinfo'>
-      <thead>
-        <tr>
-          <th class={styles['fil-content-file']}>{ t('table.head_name') }</th>
-          <th>{ t('table.head_update') }</th>
-          <th>{ t('table.head_size') }</th>
-          <th>{ t('table.head_status') }</th>
-        </tr>
-      </thead>
-      <tbody>
-        {files.map((file, idx) => (
-          <File
-            onEdit={onFileEdit}
-            attributes={file} />
-        ))}
-      </tbody>
-    </table>
+    <div role='contentinfo'>
+      <table class={styles['fil-content-table']}>
+        <thead>
+          <tr>
+            <th class={styles['fil-content-file']}>{ t('table.head_name') }</th>
+            <th>{ t('table.head_update') }</th>
+            <th>{ t('table.head_size') }</th>
+            <th>{ t('table.head_status') }</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files.map((file, idx) => (
+            <File
+              onEdit={onFileEdit}
+              attributes={file} />
+          ))}
+        </tbody>
+      </table>
+      {files.length === 0 && <Empty />}
+    </div>
   )
 }
 
