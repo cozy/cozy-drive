5ae3d90f375c7927520f93572ee060f3607c73c6
Grégory Le Garec <gregory.legarec@gmail.com>
2017-01-12 17:29:13 +0100

    [feat] Handle FETCH_FILES_FAILURE in UI reducer

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -2,17 +2,21 @@ import { combineReducers } from 'redux'
 
 import {
   FETCH_FILES,
+  FETCH_FILES_FAILURE,
   RECEIVE_FILES,
   CREATE_FOLDER,
   CREATE_FOLDER_SUCCESS,
   UPLOAD_FILE,
-  UPLOAD_FILE_SUCCESS
+  UPLOAD_FILE_SUCCESS,
+  RELOAD_PAGE
 } from '../actions'
 
 const isFetching = (state = false, action) => {
   switch (action.type) {
     case FETCH_FILES:
       return action.folderId
+    case FETCH_FILES_FAILURE:
+      return false
     case RECEIVE_FILES:
       return false
     default:
@@ -51,8 +55,23 @@ const updating = (state = [], action) => {
   }
 }
 
+const error = (state = null, action) => {
+  switch (action.type) {
+    case FETCH_FILES_FAILURE:
+      console.debug('FETCH_FILES_FAILURE')
+      return {
+        message: 'error.fetch_files',
+        cause: action.error,
+        critical: true
+      }
+    default:
+      return state
+  }
+}
+
 export default combineReducers({
   isFetching,
   isWorking,
-  updating
+  updating,
+  error
 })
