b6fb96e9b5da9a055bf333c138c834b7156b69dc
Enguerran <enguerran@ticabri.com>
2017-01-18 09:55:12 +0100

    update translation key

--- mobile/src/containers/OnBoarding.jsx
+++ mobile/src/containers/OnBoarding.jsx
@@ -9,10 +9,10 @@ export const OnBoarding = ({t, isLoggedIn, onClick, children}) => {
   if (!isLoggedIn) {
     return (
       <div class={classnames(styles['wizard'])}>
-        <p>{t('wizard.cozy_address')}</p>
-        <input type='text' placeholder={t('wizard.cozy_address_placeholder')} />
-        <p>{t('wizard.description')}</p>
-        <button role='button' className='coz-btn coz-btn--regular' onClick={onClick}>{t('wizard.next')}</button>
+        <p>{t('mobile.wizard.cozy_address')}</p>
+        <input type='text' placeholder={t('mobile.wizard.cozy_address_placeholder')} />
+        <p>{t('mobile.wizard.description')}</p>
+        <button role='button' className='coz-btn coz-btn--regular' onClick={onClick}>{t('mobile.wizard.next')}</button>
       </div>)
   }
   return children
