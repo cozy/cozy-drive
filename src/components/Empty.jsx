a448cdc812fc750f4addf990a1e34ef32ee06d3d
CPatchane <code@patchane.com>
2017-01-21 21:55:13 +0100

    [feat] Add empty view component

---
+++ src/components/Empty.jsx
@@ -0,0 +1,25 @@
+import styles from '../styles/empty'
+
+import React from 'react'
+import { translate } from '../lib/I18n'
+
+const Empty = ({ t, emptyType }) => {
+  return (
+    <div>
+      {emptyType === 'photos' &&
+        <div class={styles['pho-empty']}>
+          <h2>{ t('Empty.photos_title') }</h2>
+          <p>{ t('Empty.photos_text')}</p>
+        </div>
+      }
+      {emptyType === 'albums' &&
+        <div class={styles['pho-empty']}>
+          <h2>{ t('Empty.albums_title') }</h2>
+          <p>{ t('Empty.albums_text')}</p>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default translate()(Empty)
