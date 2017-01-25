49445504080271c117e0df7a4e167e24035374a8
Raphaël Rougeron <goldoraf@gmail.com>
2017-01-25 12:07:26 +0100

    [feat] Added responsive action menu based on bosonic's menu

--- src/containers/Toolbar.jsx
+++ src/containers/Toolbar.jsx
@@ -5,19 +5,39 @@ import { connect } from 'react-redux'
 import { translate } from '../lib/I18n'
 
 import UploadButton from './UploadButton'
+import Menu, { MenuButton, Item } from 'react-bosonic/lib/Menu'
 
 import { addFolder } from '../actions'
 
 const Toolbar = ({ t, addFolder }) => (
-  <div className={styles['fil-content-toolbar']} role='toolbar'>
+  <div className={styles['fil-toolbar']} role='toolbar'>
     <UploadButton />
-    <button
-      role='button'
-      className='coz-btn coz-btn--more'
-      onClick={() => addFolder()}
-    >
-      <span class='coz-hidden'>{ t('toolbar.item_more') }</span>
-    </button>
+    <MenuButton>
+      <button
+        role='button'
+        className='coz-btn coz-btn--more'
+      >
+        <span class='coz-hidden'>{ t('toolbar.item_more') }</span>
+      </button>
+      <Menu className={styles['fil-toolbar-menu']}>
+        <Item>
+          <a className={styles['fil-action-upload']}>
+            {t('toolbar.menu_upload')}
+          </a>
+        </Item>
+        <Item>
+          <a className={styles['fil-action-newfolder']} onClick={addFolder}>
+            {t('toolbar.menu_new_folder')}
+          </a>
+        </Item>
+        <hr />
+        <Item>
+          <a className={styles['fil-action-select']}>
+            {t('toolbar.menu_select')}
+          </a>
+        </Item>
+      </Menu>
+    </MenuButton>
   </div>
 )
 
