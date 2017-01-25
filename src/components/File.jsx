c0277bcba2ff94557dbf7a5bdda06aa06ae6232f
Raphaël Rougeron <goldoraf@gmail.com>
2017-01-25 17:48:31 +0100

    [fix] Made folders not selectable (for the moment)

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -61,7 +61,9 @@ class File extends Component {
               disabled={isDir(attributes)}
               checked={attributes.selected}
              />
-            <label onClick={e => this.toggle(e)} />
+             {isDir(attributes)
+               ? <label />
+               : <label onClick={e => this.toggle(e)} />}
           </span>
         </td>
         {this.renderFilenameCell(attributes, onOpen, editing)}
