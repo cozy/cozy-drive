351bf3232e5902b87cb1d6a9a4c06d51b19ca2dc
Enguerran <enguerran@ticabri.com>
2017-01-26 17:44:10 +0100

    fix: SET_URL

--- mobile/src/reducers/mobile.js
+++ mobile/src/reducers/mobile.js
@@ -2,13 +2,13 @@ import { SETUP, SET_URL } from '../actions'
 
 const initialState = {
   isSetup: false,
-  serverUrl: ''
+  serverUrl: 'https://'
 }
 
 export const mobile = (state = initialState, action) => {
   switch (action.type) {
     case SET_URL:
-      return Object.assign({}, state, { serverUrl: `http://${action.url}` })
+      return Object.assign({}, state, { serverUrl: action.url })
     case SETUP:
       return Object.assign({}, state, { isSetup: true })
   }
