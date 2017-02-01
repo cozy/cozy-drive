2ba17270daf5a9880ea2028543cce2e94f4e5e7d
Yannick Lohse <contact@yannick-lohse.fr>
2017-02-01 15:57:05 +0100

    [feat] added translations

--- src/containers/DeleteConfirmation.jsx
+++ src/containers/DeleteConfirmation.jsx
@@ -12,29 +12,29 @@ const DeleteConfirmation = ({ t, selected, onConfirm, onDismiss }) => (
     <div className={styles['coz-overlay']}>
       <div className={styles['coz-modal']}>
         <h2 className={styles['coz-modal-title']}>
-          Delete this File?
+          {t('deleteconfirmation.title')}
         </h2>
         <button
           className={classNames('coz-btn', 'coz-btn--close', styles['coz-modal-close'])}
           onClick={() => onDismiss(selected)}
           >
-          <span className='coz-hidden'>close</span>
+          <span className='coz-hidden'>{t('deleteconfirmation.close')}</span>
         </button>
         <p className={classNames(styles['fil-deleteconfirmation-text'], styles['icon-trash'])}>
-          Your folder will be trashed
+          {t('deleteconfirmation.trash')}
         </p>
         <p className={classNames(styles['fil-deleteconfirmation-text'], styles['icon-restore'])}>
-          restore
+          {t('deleteconfirmation.restore')}
         </p>
         <p className={classNames(styles['fil-deleteconfirmation-text'], styles['icon-shared'])}>
-          shared
+          {t('deleteconfirmation.shared')}
         </p>
         <div className={styles['fil-deleteconfirmation-buttons']}>
           <button className={styles['secondary']} onClick={() => onDismiss(selected)}>
-            cancel
+            {t('deleteconfirmation.cancel')}
           </button>
           <button className={styles['danger']} onClick={() => onConfirm(selected)}>
-            delete
+            {t('deleteconfirmation.delete')}
           </button>
         </div>
       </div>
