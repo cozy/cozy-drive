1d1397cdf8b0c15f198aed4afb13e0ed5e21519e
Enguerran <enguerran@ticabri.com>
2017-02-01 14:45:52 +0100

    populate settings page :family:

--- mobile/src/containers/Settings.jsx
+++ mobile/src/containers/Settings.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { translate } from '../../../src/lib/I18n'
 
-export default (props) => {
-  <h1>Settings</h1>
-}
+import styles from '../styles/settings'
+
+const Settings = ({t, version, serverUrl}) => (
+  <div className={styles['settings']}>
+    <h3 className={styles['settings__category']}>{t('mobile.settings.about')}</h3>
+    <h4 className={styles['settings__subcategory']}>{t('mobile.settings.app_version')}</h4>
+    <p className={styles['settings__item']}>{version}</p>
+    <h4 className={styles['settings__subcategory']}>{t('mobile.settings.account')}</h4>
+    <p className={styles['settings__item']}>{serverUrl}</p>
+  </div>
+)
+
+const mapStateToProps = (state, ownProps) => ({
+  version: window.navigator.appInfo ? window.navigator.appInfo.version : 'dev (run in cordova to get the config.xml version)',
+  serverUrl: state.mobile.serverUrl
+})
+
+export default translate()(connect(mapStateToProps)(Settings))
