ded598dba300e116d214785015fe5d395f042e22
Yannick Lohse <contact@yannick-lohse.fr>
2017-02-01 13:35:07 +0100

    [feat] added and styled modal content

--- src/containers/DeleteConfirmation.jsx
+++ src/containers/DeleteConfirmation.jsx
@@ -1,4 +1,5 @@
-//import styles from '../styles/alerter'
+import styles from '../styles/deleteconfirmation'
+import classNames from 'classnames'
 
 import React from 'react'
 import { connect } from 'react-redux'
@@ -7,8 +8,34 @@ import { translate } from '../lib/I18n'
 //import { downloadSelection, hideSelectionBar, showDeleteConfirmation } from '../actions'
 
 const DeleteConfirmation = ({ t }) => (
-  <div>
-    delete me?
+  <div className={styles['fil-deleteconfirmation']}>
+    <div className={styles['coz-overlay']}>
+      <div className={styles['coz-modal']}>
+        <h2 className={styles['coz-modal-title']}>
+          Delete this File?
+        </h2>
+        <button className={classNames('coz-btn', 'coz-btn--close', styles['coz-modal-close'])}>
+          <span className='coz-hidden'>close</span>
+        </button>
+        <p className={classNames(styles['fil-deleteconfirmation-text'], styles['icon-trash'])}>
+          Your folder will be trashed
+        </p>
+        <p className={classNames(styles['fil-deleteconfirmation-text'], styles['icon-restore'])}>
+          restore
+        </p>
+        <p className={classNames(styles['fil-deleteconfirmation-text'], styles['icon-shared'])}>
+          shared
+        </p>
+        <div className={styles['fil-deleteconfirmation-buttons']}>
+          <button className={styles['secondary']}>
+            cancel
+          </button>
+          <button className={styles['danger']}>
+            delete
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
