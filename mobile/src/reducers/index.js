4346680cf4d069abd94113c088da76f8265d0793
Enguerran <enguerran@ticabri.com>
2017-02-08 18:01:58 +0100

    feature: a 'backup images' setting :camera:

--- mobile/src/reducers/index.js
+++ mobile/src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux'
 
 import {folder, files} from '../../../src/reducers/folder'
 import ui from '../../../src/reducers/ui'
-import {mobile} from './mobile'
+import { mobile } from './mobile'
 
 const filesApp = combineReducers({
   folder,
