82fa145639654a68ce5c00c46097f5fd354879ca
Enguerran <enguerran@ticabri.com>
2017-02-08 09:27:00 +0100

    inject onRegister

--- mobile/src/actions/index.js
+++ mobile/src/actions/index.js
@@ -44,11 +44,45 @@ export const setUrl = (url) => {
   }
 }
 
+const onRegistered = (dispatch) => {
+  const { cordova } = window
+  return (client, url) => {
+    if (cordova && cordova.InAppBrowser) {
+      const { InAppBrowser } = cordova
+      const target = '_blank'
+      const options = 'location=yes,hidden=no'
+      const inAppBrowser = InAppBrowser.open(url, target, options)
+
+      return new Promise((resolve) => {
+        inAppBrowser.addEventListener('loadstart', ({url}) => {
+          const accessCode = /\?access_code=(.+)$/.test(url)
+          const state = /\?state=(.+)$/.test(url)
+
+          if (accessCode || state) {
+            resolve(url)
+          }
+        })
+      })
+      .then(
+        (url) => {
+          inAppBrowser.close()
+          return url
+        },
+        (err) => {
+          inAppBrowser.close()
+          dispatch(error())
+          throw err
+        }
+      )
+    }
+  }
+}
+
 // TODO need to refactor this braces hell
 export const registerDevice = (router, location) => {
   return async (dispatch, getState) => {
     await dispatch(setUrl(getState().mobile.serverUrl))
-    await init(getState().mobile.serverUrl)
+    await init(getState().mobile.serverUrl, onRegistered(dispatch))
     try {
       await cozy.authorize()
       await cozy.offline.replicateFromCozy('io.cozy.files')
--- mobile/src/lib/cozy_init.js
+++ mobile/src/lib/cozy_init.js
@@ -10,50 +10,17 @@ const getClientParams = () => ({
   scopes: ['io.cozy.files:GET']
 })
 
-const getOnRegistered = () => {
-  return (client, url) => {
-    if (window.cordova && window.cordova.InAppBrowser) {
-      const { InAppBrowser } = window.cordova
-      const target = '_blank'
-      const options = 'location=yes,hidden=no'
-      const inAppBrowser = InAppBrowser.open(url, target, options)
-
-      return new Promise((resolve) => {
-        inAppBrowser.addEventListener('loadstart', ({url}) => {
-          const accessCode = /\?access_code=(.+)$/.test(url)
-          const state = /\?state=(.+)$/.test(url)
-
-          if (accessCode || state) {
-            resolve(url)
-          }
-        })
-      })
-      .then(
-        (url) => {
-          inAppBrowser.close()
-          return url
-        },
-        (err) => {
-          inAppBrowser.close()
-          dispatch(error())
-          throw err
-        }
-      )
-    }
-  }
-}
-
-const getAuth = () => ({
+const getAuth = (onRegister) => ({
   storage: getStorage(),
   clientParams: getClientParams(),
-  onRegistered: getOnRegistered()
+  onRegistered: onRegister
 })
 
-export const init = (url) => {
-  console.log('init', url)
+export const init = (url, onRegister) => {
+  console.log(`Cozy Client initializes a connection with ${url}`)
   cozy.init({
     cozyURL: url,
     offline: {doctypes: ['io.cozy.files'], timer: 15},
-    oauth: getAuth()
+    oauth: getAuth(onRegister)
   })
 }
