b8ac3ab53263fdeaee9120623dfe51158cbcb3a1
Enguerran <enguerran@ticabri.com>
2017-02-09 18:38:46 +0100

    fix: spread op for reducers and beyond

--- mobile/src/reducers/index.js
+++ mobile/src/reducers/index.js
@@ -1,14 +1,9 @@
 import {combineReducers} from 'redux'
 
-import {folder, files} from '../../../src/reducers/folder'
-import ui from '../../../src/reducers/ui'
+import filesApp from '../../../src/reducers'
 import { mobile } from './mobile'
 
-const filesApp = combineReducers({
-  folder,
-  files,
-  ui,
+export default combineReducers({
+  ...filesApp,
   mobile
 })
-
-export default filesApp
