9c8ce9c3621419b3ec42399bf4ba82450093c7b0
m4dz <code@m4dz.net>
2017-02-10 15:16:16 +0100

    [chore] make config filenames more expressive

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,20 @@ const build = /:production$/.test(process.env.NODE_ENV)
 const target = process.env.NODE_ENV.match(/^(\w+):/)[1]
 
 const common = merge(
-  require('./config/webpack.base.config'),
-  require('./config/webpack.disable-contexts.config'),
-  require('./config/webpack.preact.config'),
-  require('./config/webpack.cozy-ui.config'),
-  require('./config/webpack.pictures.config'),
-  require('./config/webpack.vendors.config'),
-  require('./config/webpack.manifest.config'),
-  require(`./config/webpack.${target}.target`)
+  require('./config/webpack.config.base'),
+  require('./config/webpack.config.disable-contexts'),
+  require('./config/webpack.config.preact'),
+  require('./config/webpack.config.cozy-ui'),
+  require('./config/webpack.config.pictures'),
+  require('./config/webpack.config.vendors'),
+  require('./config/webpack.config.manifest'),
+  require(`./config/webpack.target.${target}`)
 )
 
 if (build) {
   module.exports = merge(
     common,
-    require('./config/webpack.prod.config')
+    require('./config/webpack.config.prod')
   )
 } else {
   module.exports = common
