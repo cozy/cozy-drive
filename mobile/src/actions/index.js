09bac036149f51deea79e7652143591b84a2d14b
Enguerran <enguerran@ticabri.com>
2017-02-13 19:15:28 +0100

    hack: let you use oauth with the browser
    this is for dev purpose.

--- mobile/src/actions/index.js
+++ mobile/src/actions/index.js
@@ -92,6 +92,24 @@ const onRegistered = dispatch => (client, url) => {
         throw err
       }
     )
+  } else {
+    /**
+     * for dev purpose:
+     * In oauth workflow, the server displays an authorization page
+     * User must accept to give permission then the server gives an url
+     * with query parameters used by cozy-client-js to initialize itself.
+     *
+     * This hack let developers open the authorization page in a new tab
+     * then get the "access_code" url and paste it in the prompt to let the
+     * application initialize and redirect to other pages.
+     */
+    console.log(url)
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const token = prompt('Paste the url here:')
+        resolve(token)
+      }, 10000)
+    })
   }
 }
 
