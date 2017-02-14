a59f2cb1eb9ac7b6304a3c81079f2c95e0994a64
Simon Constans <simonc@linux.com>
2017-02-14 15:16:12 +0100

    Don't upload image which is already uploaded :umbrella:

--- mobile/src/actions/media_backup.js
+++ mobile/src/actions/media_backup.js
@@ -3,19 +3,25 @@ import { getPhotos, getBlob } from '../lib/media'
 
 export const MEDIA_UPLOAD_START = 'MEDIA_UPLOAD_START'
 export const MEDIA_UPLOAD_END = 'MEDIA_UPLOAD_END'
+export const IMAGE_UPLOAD_SUCCESS = 'IMAGE_UPLOAD_SUCCESS'
 
-export const mediaBackup = () => async dispatch => {
+export const mediaBackup = () => async (dispatch, getState) => {
   dispatch({ type: MEDIA_UPLOAD_START })
   let photos = await getPhotos()
+  const alreadyUploaded = getState().mediaBackup.uploaded
   for (let photo of photos) {
-    const blob = await getBlob(photo)
-    const options = {
-      dirID: 'io.cozy.files.root-dir',
-      name: photo.filename
+    if (!alreadyUploaded.includes(photo.id)) {
+      const blob = await getBlob(photo)
+      const options = {
+        dirID: 'io.cozy.files.root-dir',
+        name: photo.filename
+      }
+      await cozy.files.create(blob, options).then(() => {
+        dispatch({ type: IMAGE_UPLOAD_SUCCESS, id: photo.id })
+      }).catch(err => {
+        console.log(err)
+      })
     }
-    await cozy.files.create(blob, options).catch(err => {
-      console.log(err)
-    })
   }
   dispatch({ type: MEDIA_UPLOAD_END })
 }
