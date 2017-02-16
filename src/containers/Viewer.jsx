76b38b47128bc8bee88a670ad10649e6e1ad0e4f
Yannick Lohse <contact@yannick-lohse.fr>
2017-02-16 14:38:30 +0100

    [feat] Added keyboard navigation

--- src/containers/Viewer.jsx
+++ src/containers/Viewer.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/viewer'
 
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 
@@ -8,19 +8,38 @@ import { STACK_FILES_DOWNLOAD_PATH } from '../constants/config'
 
 import ViewerToolbar from '../containers/ViewerToolbar'
 
-const Viewer = ({ current, previous, next, navigateToPhoto }) => {
-  return (<div className={styles['pho-viewer-wrapper']} role='viewer'>
-    <ViewerToolbar />
-    <div className={styles['pho-viewer-content']}>
-      <a role='button' className={styles['photo-viewer-nav-previous']} onClick={() => navigateToPhoto(previous)} />
-      <div className={styles['pho-viewer-photo']}>
-        <img
-          src={`${STACK_FILES_DOWNLOAD_PATH}/${current}`}
-        />
+const KEY_CODE_LEFT = 37
+const KEY_CODE_RIGHT = 39
+
+class Viewer extends Component{
+  componentWillMount () {
+    this.props.onKeyDownCallback = this.onKeyDown.bind(this)
+    document.addEventListener('keydown', this.props.onKeyDownCallback, false)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.props.onKeyDownCallback, false)
+  }
+
+  onKeyDown (e) {
+    if (e.keyCode === KEY_CODE_LEFT) this.props.navigateToPhoto(this.props.previous)
+    else if (e.keyCode === KEY_CODE_RIGHT) this.props.navigateToPhoto(this.props.next)
+  }
+
+  render ({ current, previous, next, navigateToPhoto }) {
+    return (<div className={styles['pho-viewer-wrapper']} role='viewer'>
+      <ViewerToolbar />
+      <div className={styles['pho-viewer-content']}>
+        <a role='button' className={styles['photo-viewer-nav-previous']} onClick={() => navigateToPhoto(previous)} />
+        <div className={styles['pho-viewer-photo']}>
+          <img
+            src={`${STACK_FILES_DOWNLOAD_PATH}/${current}`}
+          />
+        </div>
+        <a role='button' className={styles['photo-viewer-nav-next']} onClick={() => navigateToPhoto(next)} />
       </div>
-      <a role='button' className={styles['photo-viewer-nav-next']} onClick={() => navigateToPhoto(next)} />
-    </div>
-  </div>)
+    </div>)
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
