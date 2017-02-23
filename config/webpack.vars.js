bcc70ea7853ab9a174fb2b45c15736a0890488f2
m4dz <code@m4dz.net>
2017-02-23 11:16:25 +0100

    [chore] fix production build

--- config/webpack.vars.js
+++ config/webpack.vars.js
@@ -2,7 +2,7 @@
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const production = /:production$/.test(process.env.NODE_ENV)
+const production = process.env.NODE_ENV === 'production'
 
 module.exports = {
   production: production,
