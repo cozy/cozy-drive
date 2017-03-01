6c5a6b4624208929f57261372fbb3c434b3af039
Yannick Lohse <contact@yannick-lohse.fr>
2017-03-01 14:48:21 +0100

    [refactor] Refactored breadcrumb

--- src/containers/Breadcrumb.jsx
+++ src/containers/Breadcrumb.jsx
@@ -13,20 +13,43 @@ import Spinner from '../components/Spinner'
 import { isBrowsingTrash } from '../reducers'
 
 const Breadcrumb = ({ t, router, folder, opening, toggle, isBrowsingTrash, goToFolder }) => {
-  const isRoot = folder.id === ROOT_DIR_ID
-  const isTrash = folder.id === TRASH_DIR_ID
+  // reconstruct the whole path to the current folder (first element is the root, the last is the current folder)
+  let path = [folder]
 
-  const isInRoot = folder.parent && folder.parent.id === ROOT_DIR_ID
-  const isInTrash = folder.parent && folder.parent.id === TRASH_DIR_ID
+  // does the folder have parents to display? The trash folder has the root folder as parent, but we don't want to show that.
+  let parent = folder.parent
+  if (parent && parent.id && !(isBrowsingTrash && parent.id === ROOT_DIR_ID)) {
+    path.unshift(parent)
 
-  const showParentFolder = !isRoot && !isTrash && !isInRoot && !isInTrash && folder.parent
+    // has the parent a parent too?
+    if (parent.dir_id && !(isBrowsingTrash && parent.dir_id === ROOT_DIR_ID)) {
+      // since we don't have *actually* any information about the parent's parent, we have to fake it
+      path.unshift({ id: parent.dir_id })
+    }
+  }
 
-  const isLevel2Root = folder.parent && folder.parent.dir_id && folder.parent.dir_id !== ROOT_DIR_ID
-  const isLevel2Trash = folder.parent && folder.parent.dir_id && folder.parent.dir_id !== TRASH_DIR_ID && !isInTrash
+  // finally, we need to make sure we have the root level folder, which can be either the root, or the trash folder. While we're at it, we also rename the folders when we need to.
+  let hasRootFolder = false
+  path.forEach(folder => {
+    if (folder.id === ROOT_DIR_ID) {
+      folder.name = t('breadcrumb.title_files')
+      hasRootFolder = true
+    }
+    else if (folder.id === TRASH_DIR_ID) {
+      folder.name = t('breadcrumb.title_trash')
+      if (isBrowsingTrash) hasRootFolder = true
+    }
 
-  const showEllipsis = (isBrowsingTrash && isLevel2Trash) || (!isBrowsingTrash && isLevel2Root)
+    if (!folder.name) folder.name = '…'
+  })
 
-  const topLevelTitle = isBrowsingTrash ? 'breadcrumb.title_trash' : 'breadcrumb.title_files'
+  if (!hasRootFolder) {
+    //if we don't have one, we add it manually
+    path.unshift({
+      id: isBrowsingTrash ? TRASH_DIR_ID : ROOT_DIR_ID,
+      name: isBrowsingTrash ? t('breadcrumb.title_trash') : t('breadcrumb.title_files')
+    })
+  }
 
   return (
     <div>
@@ -34,43 +57,25 @@ const Breadcrumb = ({ t, router, folder, opening, toggle, isBrowsingTrash, goToF
       </button>
       <h2 className={styles['fil-content-title']}>
 
-        { (isRoot || isTrash) && // Displays the non-interactive root folder
-          <span>{ t(topLevelTitle) }</span> }
-
-        { !isRoot && !isTrash && // show the interactive root folder
-          <span
-            className={classNames(styles['fil-inside-path'], styles['fil-path-hidden'])}
-            onClick={() => {
-              toggle()
-              goToFolder(isBrowsingTrash ? TRASH_DIR_ID : ROOT_DIR_ID).then(() => toggle())
-            }}
-          >
-            <a>{ t(topLevelTitle) }</a>
-            <span className={styles['separator']}>/</span>
-          </span>
-        }
-
-        { showEllipsis && // show an ellipsis if there are more than 2 levels
-          <span className={classNames(styles['fil-inside-path'], styles['fil-path-hidden'])}>
-            …
-            <span className={styles['separator']}>/</span>
-          </span> }
-
-        { showParentFolder && // Displays the parent folder
-          <span
-            className={classNames(styles['fil-inside-path'], styles['fil-path-hidden'])}
-            onClick={() => {
-              toggle()
-              goToFolder(folder.parent.id).then(() => toggle())
-            }}
-          >
-            <a>{folder.parent.name}</a>
-            <span className={styles['separator']}>/</span>
-          </span>
-        }
-
-        { !isRoot && !isTrash && // Displays the current folder
-          <span>{folder.name}</span>}
+        { path.map((folder, index) => {
+          if (index < path.length - 1) return (
+            <span
+              className={classNames(styles['fil-inside-path'], styles['fil-path-hidden'])}
+              onClick={() => {
+                toggle()
+                goToFolder(folder.id).then(() => toggle())
+              }}
+            >
+              <a>
+                { folder.name }
+              </a>
+              <span className={styles['separator']}>/</span>
+            </span>
+          )
+          else return (
+            <span>{folder.name}</span>
+          )
+        }) }
 
         { opening && <Spinner /> }
 
