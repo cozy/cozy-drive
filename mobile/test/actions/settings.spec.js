903ef54ba29dfcb374a6ecdef37c0bce93f0cf3b
CPatchane <code@patchane.com>
2017-03-02 15:39:51 +0100

    [test] Update tests

--- mobile/test/actions/settings.spec.js
+++ mobile/test/actions/settings.spec.js
@@ -38,30 +38,25 @@ describe('url actions creators', () => {
     const store = mockStore()
 
     store.dispatch(checkURL('https://localhost'))
-      .then(() => {
-        expect(store.getActions()).toEqual([{ type: SET_URL, url: 'https://localhost' }])
-      })
+    expect(store.getActions()).toEqual([{ type: SET_URL, url: 'https://localhost' }])
   })
 
   it('should not accept http://', () => {
     const store = mockStore()
 
-    store.dispatch(checkURL('http://localhost'))
-      .then(() => {
-      })
-      .catch((err) => {
-        console.log('don\'t know why err is not instanceof OnBoardingError', err instanceof OnBoardingError)
-        expect(err.name).toEqual('OnBoardingError')
-      })
+    try {
+      store.dispatch(checkURL('http://localhost'))
+    } catch (err) {
+      console.log('don\'t know why err is not instanceof OnBoardingError', err instanceof OnBoardingError)
+      expect(err.name).toEqual('OnBoardingError')
+    }
   })
 
   it('should accept url without scheme://', () => {
     const store = mockStore()
 
     store.dispatch(checkURL('localhost'))
-      .then(() => {
-        expect(store.getActions()).toEqual([{ type: SET_URL, url: 'https://localhost' }])
-      })
+    expect(store.getActions()).toEqual([{ type: SET_URL, url: 'https://localhost' }])
   })
 
   it('should create SET_CLIENT action', () => {
