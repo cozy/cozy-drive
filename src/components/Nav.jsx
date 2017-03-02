38520aad5133379d956345a52125b53099e6c1b6
GoOz <gooz@foojin.com>
2017-03-02 14:49:38 +0100

    [Chore] use Nav style from Cozy-UI instead of the app

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,35 +12,35 @@ const Nav = ({ t, router }) => {
   let isBrowsingTrash = router.location.pathname.match(/^\/trash/) !== null
   return (
     <nav>
-      <ul class={styles['fil-nav']}>
-        <li class={styles['fil-nav-item']}>
-          <Link to='/files' class={classNames(styles['fil-cat-files'], { [styles['active']]: isBrowsingFiles })}>
+      <ul class={styles['coz-nav']}>
+        <li class={styles['coz-nav-item']}>
+          <Link to='/files' class={classNames(styles['coz-nav-link'], styles['fil-cat-files'], { [styles['active']]: isBrowsingFiles })}>
             { t('nav.item_files') }
           </Link>
         </li>
-        <li class={styles['fil-nav-item']}>
-          <Link to='/recent' class={styles['fil-cat-recent']} activeClassName={styles['active']}>
+        <li class={styles['coz-nav-item']}>
+          <Link to='/recent' class={classNames(styles['coz-nav-link'], styles['fil-cat-recent'])} activeClassName={styles['active']}>
             { t('nav.item_recent') }
           </Link>
         </li>
-        <li class={styles['fil-nav-item']}>
-          <Link to='/shared' class={styles['fil-cat-shared']} activeClassName={styles['active']}>
+        <li class={styles['coz-nav-item']}>
+          <Link to='/shared' class={classNames(styles['coz-nav-link'], styles['fil-cat-shared'])} activeClassName={styles['active']}>
             { t('nav.item_shared') }
           </Link>
         </li>
-        <li class={styles['fil-nav-item']}>
-          <Link to='/activity' class={styles['fil-cat-activity']} activeClassName={styles['active']}>
+        <li class={styles['coz-nav-item']}>
+          <Link to='/activity' class={classNames(styles['coz-nav-link'], styles['fil-cat-activity'])} activeClassName={styles['active']}>
             { t('nav.item_activity') }
           </Link>
         </li>
-        <li class={styles['fil-nav-item']}>
-          <Link to='/trash' class={classNames(styles['fil-cat-trash'], { [styles['active']]: isBrowsingTrash })}>
+        <li class={styles['coz-nav-item']}>
+          <Link to='/trash' class={classNames(styles['coz-nav-link'], styles['fil-cat-trash'], { [styles['active']]: isBrowsingTrash })}>
             { t('nav.item_trash') }
           </Link>
         </li>
         {__TARGET__ === 'mobile' &&
-        <li class={styles['fil-nav-item']}>
-          <Link to='/settings' class={styles['fil-cat-settings']} activeClassName={styles['active']}>
+        <li class={styles['coz-nav-item']}>
+          <Link to='/settings' class={classNames(styles['coz-nav-link'], styles['fil-cat-settings'])} activeClassName={styles['active']}>
             { t('nav.item_settings') }
           </Link>
         </li>
