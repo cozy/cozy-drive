91372b48097c22462eee61b35b0fe7f85eaadd65
CPatchane <code@patchane.com>
2017-03-20 11:56:56 +0100

    [fix] Better try catch block for albums photos fetching

--- src/actions/albums.js
+++ src/actions/albums.js
@@ -50,22 +50,16 @@ export const fetchAlbums = (mangoIndex) => {
 export const fetchAlbumPhotosStatsById = (albumId) => {
   return async (dispatch) => {
     try {
-      return await cozy.client.data.find(ALBUM_DOCTYPE, albumId)
-        .then(async album => {
-          return await cozy.client.data.listReferencedFiles(album)
-            .then(async photosIds => {
-              let fetchedPhotos = []
-              for (let index in photosIds) {
-                fetchedPhotos.push(
-                  await cozy.client.files.statById(photosIds[index])
-                    .then(photo => photo)
-                )
-              }
-              album.photos = fetchedPhotos
-              dispatch({type: FETCH_CURRENT_ALBUM_PHOTOS_SUCCESS, album})
-              return fetchedPhotos
-            })
-        })
+      let album = await cozy.client.data.find(ALBUM_DOCTYPE, albumId)
+      const photosIds = await cozy.client.data.listReferencedFiles(album)
+      let fetchedPhotos = []
+      for (let index in photosIds) {
+        const photo = await cozy.client.files.statById(photosIds[index])
+        fetchedPhotos.push(photo)
+      }
+      album.photos = fetchedPhotos
+      dispatch({type: FETCH_CURRENT_ALBUM_PHOTOS_SUCCESS, album})
+      return fetchedPhotos
     } catch (fetchError) {
       throw new Error(fetchError.response
         ? fetchError.response.statusText
