e99034a3a8b513052c0fd8d1c61a13924d0fb99a
Grégory Le Garec <gregory.legarec@gmail.com>
2017-03-21 11:47:07 +0100

    [feat] Transform PhotoList to Component and use containerWidth :sparkles:

--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/photoList'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Dimensions from 'react-dimensions'
 
 import Empty from './Empty'
@@ -8,24 +8,29 @@ import Photo from './Photo'
 
 import classNames from 'classnames'
 
-export const PhotoList = ({ key, title, photos, selected, onPhotoToggle }) => {
-  return (
-    <div
-      className={classNames(styles['pho-section'], selected.length && styles['pho-section--has-selection'])}
-      key={key}
-      >
-      {!!title && <h3>{title}</h3>}
-      {photos.map(photo =>
-        <Photo
-          photo={photo}
-          key={photo._id}
-          selected={selected.indexOf(photo._id) !== -1}
-          onToggle={onPhotoToggle}
-        />
-      )}
-      {photos.length === 0 && <Empty emptyType='photos' />}
-    </div>
-  )
+export class PhotoList extends Component {
+  render () {
+    const { key, title, photos, selected, onPhotoToggle, containerWidth } = this.props
+    return (
+      <div
+        className={classNames(styles['pho-section'], selected.length && styles['pho-section--has-selection'])}
+        key={key}
+        // Specify the width for making justified layout work.
+        style={containerWidth ? `width:${containerWidth}px` : ''}
+        >
+        {!!title && <h3>{title}</h3>}
+        {photos.map((photo, index) =>
+          <Photo
+            photo={photo}
+            key={photo._id}
+            selected={selected.indexOf(photo._id) !== -1}
+            onToggle={onPhotoToggle}
+          />
+        )}
+        {photos.length === 0 && <Empty emptyType='photos' />}
+      </div>
+    )
+  }
 }
 
 export default Dimensions()(PhotoList)
