65810d018a218c7a0712d64090270668b5de1610
CPatchane <code@patchane.com>
2017-03-23 12:51:36 +0100

    feat: correctly handle error if album cover photo fetching failed

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -5,13 +5,13 @@ import React from 'react'
 import Empty from '../components/Empty'
 import AlbumItem from '../components/AlbumItem'
 
-export const AlbumsList = ({ t, f, albums }) => {
+export const AlbumsList = ({ t, f, albums, onServerError }) => {
   if (albums.length === 0) {
     return <Empty emptyType='albums' />
   } else {
     return (
       <div className={styles['pho-album-list']}>
-        {albums.map((a) => <AlbumItem album={a} key={a._id} />)}
+        {albums.map((a) => <AlbumItem album={a} key={a._id} onServerError={onServerError} />)}
       </div>
     )
   }
