4d2d3cbae05f5a69a097ce1af2c2ffa51d0b3149
Simon Constans <simonc@linux.com>
2017-03-27 12:00:34 +0200

    Refactor: Use OnBoarding component in BackupPhotosVideos :hammer:

--- mobile/src/containers/onboarding/BackupPhotosVideos.jsx
+++ mobile/src/containers/onboarding/BackupPhotosVideos.jsx
@@ -1,39 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import classNames from 'classnames'
-
-import { translate } from '../../../../src/lib/I18n'
-
-import Breadcrumb from '../../components/Breadcrumb'
-
-import styles from '../../styles/onboarding'
-
+import OnBoarding from '../../components/OnBoarding'
 import { setBackupImages } from '../../actions/settings'
 
-export const BackupPhotosVideos = ({ t, onActivate, onSkip }) =>
+export const BackupPhotosVideos = ({ onActivate, onSkip }) =>
 (
-  <div className={classNames(styles['wizard'], styles['photos-backup'])}>
-    <header className={styles['wizard-header']}>
-      <a className={styles['skipLink']} onClick={onSkip}>
-        {t('mobile.onboarding.step.skip')}
-      </a>
-    </header>
-    <div className={styles['wizard-main']}>
-      <div className={classNames(styles['illustration'], styles['illustration-photos'])} />
-      <h1 className={styles['title']}>{t('mobile.onboarding.photos.title')}</h1>
-      <p className={styles['description']}>{t('mobile.onboarding.photos.description')}</p>
-    </div>
-    <footer className={styles['wizard-footer']}>
-      <button
-        role='button'
-        className={'coz-btn coz-btn--regular'}
-        onClick={onActivate}
-      >
-        {t('mobile.onboarding.step.button')}
-      </button>
-      <Breadcrumb currentStep={2} totalSteps={3} />
-    </footer>
-  </div>
+  <OnBoarding onActivate={onActivate} onSkip={onSkip} nameStep='photos' currentStep={2} totalSteps={3} />
 )
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
@@ -47,4 +19,4 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   }
 })
 
-export default connect(null, mapDispatchToProps)(translate()(BackupPhotosVideos))
+export default connect(null, mapDispatchToProps)(BackupPhotosVideos)
