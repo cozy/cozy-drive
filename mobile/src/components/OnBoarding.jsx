b51470f10263d725acc8fdca19501c0742b66ac5
Simon Constans <simonc@linux.com>
2017-03-27 11:59:55 +0200

    refactor: Create OnBoarding components to mutualise code :hammer:

---
+++ mobile/src/components/OnBoarding.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import classNames from 'classnames'
+
+import { translate } from '../../../src/lib/I18n'
+import Breadcrumb from './Breadcrumb'
+import styles from '../styles/onboarding'
+
+export const OnBoarding = ({ t, onActivate, onSkip, nameStep, currentStep, totalSteps }) =>
+(
+  <div className={classNames(styles['wizard'], styles['photos-backup'])}>
+    <header className={styles['wizard-header']}>
+      {onSkip && <a className={styles['skipLink']} onClick={onSkip}>
+        {t('mobile.onboarding.step.skip')}
+      </a>}
+    </header>
+    <div className={styles['wizard-main']}>
+      <div className={classNames(styles['illustration'], styles['illustration-' + nameStep])} />
+      <h1 className={styles['title']}>{t('mobile.onboarding.' + nameStep + '.title')}</h1>
+      <p className={styles['description']}>{t('mobile.onboarding.' + nameStep + '.description')}</p>
+    </div>
+    <footer className={styles['wizard-footer']}>
+      <button
+        role='button'
+        className={'coz-btn coz-btn--regular'}
+        onClick={onActivate}
+      >
+        {onSkip && t('mobile.onboarding.step.button')}
+        {!onSkip && t('mobile.onboarding.step.next')}
+      </button>
+      <Breadcrumb currentStep={currentStep} totalSteps={totalSteps} />
+    </footer>
+  </div>
+)
+
+export default translate()(OnBoarding)
