3f1d290038e7bf3a1d92f12e111bd21a91e49531
Simon Constans <simonc@linux.com>
2017-03-27 16:18:07 +0200

    test: Add OnBoarding component :white_check_mark:

--- mobile/test/onboarding.spec.js
+++ mobile/test/onboarding.spec.js
@@ -6,8 +6,6 @@ import renderer from 'react-test-renderer'
 import Wizard from '../../mobile/src/components/Wizard'
 import { SelectServer } from '../../mobile/src/containers/onboarding/SelectServer'
 import { Welcome } from '../../mobile/src/containers/onboarding/Welcome'
-import { BackupPhotosVideos } from '../../mobile/src/containers/onboarding/BackupPhotosVideos'
-import { Analytics } from '../../mobile/src/containers/onboarding/Analytics'
 
 // used for ref issue in jest tests using react-test-renderer https://facebook.github.io/react/blog/2016/11/16/react-v15.4.0.html#mocking-refs-for-snapshot-testing
 function createNodeMock (element) {
@@ -47,20 +45,6 @@ describe('Onboarding', () => {
   it('should render the SelectServer screen', () => {
     const options = {createNodeMock}
     const component = renderer.create(<SelectServer t={() => {}} />, options)
-
-    let tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-
-  it('should render the BackupPhotosVideos screen', () => {
-    const component = renderer.create(<BackupPhotosVideos t={() => {}} />)
-
-    let tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-
-  it('should render the Analytics screen', () => {
-    const component = renderer.create(<Analytics t={() => {}} />)
 
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
