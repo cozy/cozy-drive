600ad9c0c163399111c3b19f4ddc50ac43af78cc
Enguerran <enguerran@ticabri.com>
2017-05-04 14:52:20 +0200

    feat: remove from album :heavy_minus_sign:

--- src/containers/SelectionBar.jsx
+++ src/containers/SelectionBar.jsx
@@ -2,13 +2,16 @@ import styles from '../styles/selectionbar'
 
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import classNames from 'classnames'
 import { translate } from '../lib/I18n'
 
+import Alerter from '../components/Alerter'
 import { hideSelectionBar } from '../actions/selection'
-import { addToAlbum } from '../ducks/albums'
 
-const SelectionBar = ({ t, selected, selectedCount, onHide, onAddToAlbum }) => (
+import { addToAlbum, removeFromAlbum, getCurrentAlbum } from '../ducks/albums'
+
+const SelectionBar = ({ t, selected, selectedCount, album, onHide, onAddToAlbum, onRemoveFromAlbum, router }) => (
   <div
     className={classNames(styles['coz-selectionbar'], {
       [styles['coz-selectionbar--active']]: selectedCount !== 0
@@ -26,6 +29,15 @@ const SelectionBar = ({ t, selected, selectedCount, onHide, onAddToAlbum }) => (
     >
       {t('SelectionBar.add_to_album')}
     </button>
+    {router.location.pathname.startsWith('/albums') &&
+    <button
+      disabled={selectedCount === 0}
+      className={styles['pho-action-album-remove']}
+      onClick={() => onRemoveFromAlbum(selected, album)}
+    >
+      {t('SelectionBar.remove_from_album')}
+    </button>
+    }
     <button className={styles['coz-action-close']} onClick={onHide}>
       {t('SelectionBar.close')}
     </button>
@@ -34,19 +46,19 @@ const SelectionBar = ({ t, selected, selectedCount, onHide, onAddToAlbum }) => (
 
 const mapStateToProps = (state, ownProps) => ({
   selected: state.ui.selected,
-  selectedCount: state.ui.selected.length
+  selectedCount: state.ui.selected.length,
+  album: getCurrentAlbum(state.albums)
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onHide: () => {
-    dispatch(hideSelectionBar())
-  },
-  onAddToAlbum: (selected) => {
-    dispatch(addToAlbum(selected))
-  }
+  onHide: () => dispatch(hideSelectionBar()),
+  onAddToAlbum: selected => dispatch(addToAlbum(selected)),
+  onRemoveFromAlbum: (selected, album) =>
+  dispatch(removeFromAlbum(selected, album))
+  .catch(err => Alerter.error((err.message)))
 })
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(translate()(SelectionBar))
+)(translate()(withRouter(SelectionBar)))
