9719e8c4f6a1b5d745b91ca44aa54b4d73a8afff
GoOz <gooz@foojin.com>
2017-05-22 18:19:51 +0200

    feat: add button to download desktop client. Desktop only [WIP]

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,17 @@
 import styles from '../styles/sidebar'
 
+import { translate } from '../lib/I18n'
+
 import React from 'react'
+import classNames from 'classnames'
 
 import Nav from '../containers/Nav'
 
-const Sidebar = () => (
+const Sidebar = ({ t }) => (
   <aside class={styles['fil-sidebar']}>
     <Nav />
+    <a href={t('nav.link-client')} className={classNames('coz-btn', 'coz-btn--secondary', styles['coz-btn--client'])}><span>{t('nav.btn-client')}</span></a>
   </aside>
 )
 
-export default Sidebar
+export default translate()(Sidebar)
