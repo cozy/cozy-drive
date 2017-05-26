8798483c3578cdd2f129928b9e4666810a6f2bab
Yannick Lohse <contact@yannick-lohse.fr>
2017-05-26 11:09:07 +0200

    feat: hide albums when there is none to pick from
    Also refactor: made the SelectAlbumsForm component dumber

--- src/components/SelectAlbumsForm.jsx
+++ src/components/SelectAlbumsForm.jsx
@@ -7,10 +7,7 @@ import classNames from 'classnames'
 import { withError } from '../components/ErrorComponent'
 import { withEmpty } from '../components/Empty'
 
-import Loading from '../components/Loading'
 import AlbumItem from '../components/AlbumItem'
-
-import { fetchAlbums, getAlbumsList } from '../ducks/albums'
 
 const DumbAlbumsList = props => (
   <div className={classNames(styles['pho-album-list'], styles['pho-album-list--thumbnails'], styles['pho-album-list--selectable'])}>
@@ -28,33 +25,13 @@ const DumbAlbumsView = props => (
 
 const ErrorAlbumsView = withError(props => props.error, 'albums', DumbAlbumsView)
 
-export class AlbumsView extends Component {
-  componentWillMount () {
-    this.props.fetchAlbums()
+export const AlbumsView = (props) => {
+  if (!props.albums) {
+    return null
   }
-
-  render () {
-    if (!this.props.albums) {
-      return null
-    }
-    const { fetchingStatus } = this.props.albums
-    const isFetching = fetchingStatus === 'pending' || fetchingStatus === 'loading'
-    const error = fetchingStatus === 'failed'
-    return isFetching
-      ? <Loading loadingType='albums_fetching' />
-      : <ErrorAlbumsView error={error} onServerError={() => this.handleError(error)} {...this.props} />
-  }
+  const { fetchingStatus } = props.albums
+  const error = fetchingStatus === 'failed'
+  return <ErrorAlbumsView error={error} onServerError={() => this.handleError(error)} {...props} />
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  albums: getAlbumsList(state)
-})
-
-export const mapDispatchToProps = (dispatch, ownProps) => ({
-  fetchAlbums: () => dispatch(fetchAlbums())
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AlbumsView)
+export default AlbumsView
