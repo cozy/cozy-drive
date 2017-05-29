0e4ac03312f1990f84ea4b7c5a1b9ea3e80a1467
Yannick Lohse <contact@yannick-lohse.fr>
2017-05-29 18:23:41 +0200

    refactor: Topbar as a regular component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,65 +1,73 @@
 import styles from '../styles/topbar'
 
-import React from 'react'
+import React, { Component } from 'react'
 import { translate } from '../lib/I18n'
 import { withRouter } from 'react-router'
 
 const KEYCODE_ENTER = 13
 const KEYCODE_ESC = 27
 
-export const Topbar = ({ t, children, router, viewName, albumName = '', editing = false, onEdit = ()=>{} }) => {
-  const backToAlbums = () => {
+class Topbar extends Component{
+  constructor (props) {
+    super(props)
+
+    this.ignoreBlurEvent = false
+    this.backToAlbums = this.backToAlbums.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+  }
+  backToAlbums () {
     // go to parent
-    let url = router.location.pathname
-    router.push(url.substring(0, url.lastIndexOf('/')))
+    let url = this.props.router.location.pathname
+    this.props.router.push(url.substring(0, url.lastIndexOf('/')))
   }
-
-  let ignoreBlurEvent = false // we'll ignore blur events if they are triggered by pressing enter or escape, to prevent `onEdit` from firing twice
-
-  const handleBlur = e => {
-    if (!ignoreBlurEvent) onEdit(e.target.value.trim() !== '' ? e.target.value : albumName)
+  handleBlur (e) {
+    if (!this.ignoreBlurEvent && typeof this.props.onEdit === 'function') this.props.onEdit(e.target.value.trim() !== '' ? e.target.value : albumName)
   }
-
-  const handleKeyDown = e => {
+  handleKeyDown (e) {
     if (e.keyCode === KEYCODE_ENTER) {
-      ignoreBlurEvent = true
-      onEdit(e.target.value)
+      this.ignoreBlurEvent = true
+      this.props.onEdit(e.target.value)
     }
     else if (e.keyCode === KEYCODE_ESC) {
-      ignoreBlurEvent = true
-      onEdit(albumName)
+      this.ignoreBlurEvent = true
+      this.props.onEdit(albumName)
     }
   }
 
-  const focusInput = elem => {
-    if (!elem) return
-    setTimeout(() => {
-      elem.focus()
-      elem.select()
-    })
-  }
+  render ({ t, children, router, viewName, albumName = '', editing = false, onEdit = ()=>{} }) {
+    let ignoreBlurEvent = false // we'll ignore blur events if they are triggered by pressing enter or escape, to prevent `onEdit` from firing twice
 
-  return (
-    <div className={styles['pho-content-header']}>
-      {viewName === 'albumContent' &&
-        <div
-          role='button'
-          className={styles['pho-content-album-previous']}
-          onClick={backToAlbums}
-        />
-      }
-      <h2 className={styles['pho-content-title']}>
-        {
-          viewName !== 'albumContent'
-          ? t(`Nav.${viewName}`)
-          : editing
-          ? <input type='text' value={albumName} onKeyDown={handleKeyDown} onBlur={handleBlur} ref={focusInput} />
-          : albumName
+    const focusInput = elem => {
+      if (!elem) return
+      setTimeout(() => {
+        elem.focus()
+        elem.select()
+      })
+    }
+
+    return (
+      <div className={styles['pho-content-header']}>
+        {viewName === 'albumContent' &&
+          <div
+            role='button'
+            className={styles['pho-content-album-previous']}
+            onClick={this.backToAlbums}
+          />
         }
-      </h2>
-      {children}
-    </div>
-  )
+        <h2 className={styles['pho-content-title']}>
+          {
+            viewName !== 'albumContent'
+            ? t(`Nav.${viewName}`)
+            : editing
+            ? <input type='text' value={albumName} onKeyDown={this.handleKeyDown} onBlur={this.handleBlur} ref={focusInput} />
+            : albumName
+          }
+        </h2>
+        {children}
+      </div>
+    )
+  }
 }
 
 export default translate()(withRouter(Topbar))
