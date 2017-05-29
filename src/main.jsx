70e034e124d59c8104917dbeac3dcf120e456154
Yannick Lohse <contact@yannick-lohse.fr>
2017-05-29 09:39:56 +0200

    feat: conditionally use piwik

--- src/main.jsx
+++ src/main.jsx
@@ -58,22 +58,25 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   let history = hashHistory
-  try {
-    var PiwikReactRouter = require('piwik-react-router')
-    const piwikTracker = (Piwik.getTracker(), PiwikReactRouter({
-      url: __PIWIK_TRACKER_URL__,
-      siteId: __PIWIK_SITEID__,
-      injectScript: false
-    }))
-    piwikTracker.push(['enableHeartBeatTimer'])
-    let userId = data.cozyDomain
-    let indexOfPort = userId.indexOf(':')
-    if (indexOfPort >= 0) userId = userId.substring(0, indexOfPort)
-    piwikTracker.push(['setUserId', userId])
-    piwikTracker.push(['setCustomDimension', __PIWIK_DIMENSION_ID_APP__, data.cozyAppName])
 
-    history = piwikTracker.connectToHistory(hashHistory)
-  } catch (err) {}
+  if (data.cozyTracking === 'true' || data.cozyTracking === ''){
+    try {
+      var PiwikReactRouter = require('piwik-react-router')
+      const piwikTracker = (Piwik.getTracker(), PiwikReactRouter({
+        url: __PIWIK_TRACKER_URL__,
+        siteId: __PIWIK_SITEID__,
+        injectScript: false
+      }))
+      piwikTracker.push(['enableHeartBeatTimer'])
+      let userId = data.cozyDomain
+      let indexOfPort = userId.indexOf(':')
+      if (indexOfPort >= 0) userId = userId.substring(0, indexOfPort)
+      piwikTracker.push(['setUserId', userId])
+      piwikTracker.push(['setCustomDimension', __PIWIK_DIMENSION_ID_APP__, data.cozyAppName])
+
+      history = piwikTracker.connectToHistory(hashHistory)
+    } catch (err) {}
+  }
 
   render((
     <I18n context={context} lang={data.cozyLocale}>
