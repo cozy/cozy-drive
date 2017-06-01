68709bc0997e046090aa958ef40aa85e6b5d3563
Simon Constans <simonc@linux.com>
2017-06-01 16:47:01 +0200

    fix: Refresh directory on mobile app :pray:

--- mobile/src/actions/settings.js
+++ mobile/src/actions/settings.js
@@ -90,7 +90,7 @@ export const registerDevice = () => async (dispatch, getState) => {
 
 export const startReplication = (dispatch, getState) => {
   const firstReplication = getState().settings.firstReplication
-  const refreshFolder = () => { dispatch(openFolder(getState().folder.id)) }
+  const refreshFolder = () => { dispatch(openFolder(getState().view.openedFolderId)) }
   const revokeClient = () => { dispatch(reduxRevokeClient()) }
   const firstReplicationFinished = () => { dispatch(setFirstReplication(true)) }
 
