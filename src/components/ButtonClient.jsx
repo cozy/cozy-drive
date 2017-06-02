d0a7fcd8608f5be1d9df037f8a17b94e4efaa5e7
GoOz <gooz@foojin.com>
2017-06-02 12:44:55 +0200

    feat: add target blank on download app button

--- src/components/ButtonClient.jsx
+++ src/components/ButtonClient.jsx
@@ -5,7 +5,7 @@ import { translate } from '../lib/I18n'
 import React from 'react'
 
 const ButtonClient = ({ t }) => (
-  <a href={t('Nav.link-client')} className={styles['coz-btn-client']}><span>{t('Nav.btn-client')}</span></a>
+  <a href={t('Nav.link-client')} target='_blank' className={styles['coz-btn-client']}><span>{t('Nav.btn-client')}</span></a>
 )
 
 export default translate()(ButtonClient)
