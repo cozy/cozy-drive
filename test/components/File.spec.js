d8e4a8f0a0ead78b70e0883aef8fdec0a630f8b9
Simon Constans <simonc@linux.com>
2017-06-09 10:58:34 +0200

    chore: Upgrade cozy-ui :arrow_up:

--- test/components/File.spec.js
+++ test/components/File.spec.js
@@ -15,7 +15,7 @@ describe('File component', () => {
       created_at: '2017-01-03T08:52:37.093020313Z'
     }
     const component = renderer.create(
-      <I18n lang='en'>
+      <I18n lang='en' dictRequire={() => ('')}>
         <File attributes={attrs} />
       </I18n>
     )
