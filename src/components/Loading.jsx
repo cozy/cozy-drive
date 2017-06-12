2a69401eebb321d8258fa253161a0bee257339a0
GoOz <gooz@foojin.com>
2017-06-12 11:48:45 +0200

    feat: upgrade loading component so it can change its color

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,14 +2,17 @@ import styles from '../styles/loading'
 
 import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
+import classNames from 'classnames'
 
-export const Loading = ({ t, loadingType, noMargin }) => {
+export const Loading = ({ t, loadingType, noMargin, color }) => {
   return (
     <div
-      className={noMargin
-        ? styles['pho-loading--no-margin']
-        : styles['pho-loading']
-      }
+      className={classNames(
+        styles['pho-loading'], {
+          [styles['pho-loading--no-margin']]: noMargin,
+          [styles[`pho-loading--${color}`]]: color
+        }
+      )}
     >
       {loadingType && <p>{t(`Loading.${loadingType}`)}</p>}
     </div>
