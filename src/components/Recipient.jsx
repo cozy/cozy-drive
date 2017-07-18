260e0ae14206bf909977f7bc157ea6263053d765
Enguerran <enguerran@ticabri.com>
2017-07-18 12:12:54 +0200

    refactor: remove unnecessary LoC :lock:

--- src/components/Recipient.jsx
+++ src/components/Recipient.jsx
@@ -2,16 +2,15 @@ import styles from '../styles/recipient'
 
 import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
-import classNames from 'classnames'
 
 export const Recipient = ({ t, name, url, status }) => {
   return (
-    <div className={classNames(styles['pho-recipient'])}>
-      <div className={classNames(styles['pho-recipient-idents'])}>
-        <div className={classNames(styles['pho-recipient-user'])}>{name}</div>
-        <div className={classNames(styles['pho-recipient-url'])}>{url}</div>
+    <div className={styles['pho-recipient']}>
+      <div className={styles['pho-recipient-idents']}>
+        <div className={styles['pho-recipient-user']}>{name}</div>
+        <div className={styles['pho-recipient-url']}>{url}</div>
       </div>
-      <div className={classNames(styles['pho-recipient-status'])}>{status}</div>
+      <div className={styles['pho-recipient-status']}>{status}</div>
     </div>
   )
 }
