9a2788b9aa089afabe4e2e716e8a281a60388025
Enguerran <enguerran@ticabri.com>
2017-07-25 18:53:30 +0200

    feat: download folder in public view

---
+++ src/components/DownloadButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import classnames from 'classnames'
+
+const DownloadButton = ({ label, disabled = false, onDownload, className }) => (
+  <button
+    role='button'
+    disabled={disabled}
+    className={classnames(className, 'coz-btn', 'coz-btn--regular', 'coz-btn--download')}
+    onClick={() => onDownload()}
+  >
+    {label}
+  </button>
+)
+
+export default DownloadButton
