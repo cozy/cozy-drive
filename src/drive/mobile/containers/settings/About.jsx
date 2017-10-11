7b52d683faa5321a66fcbf9f6bb4d27aacf1bbe8
GoOz <gooz@foojin.com>
2017-10-11 17:06:59 +0200

    🚑  Fix the complicated triple tap to show the support button on iOS
    Use Hammer to handle tap gesture instead of basic onClick

--- src/drive/mobile/containers/settings/About.jsx
+++ src/drive/mobile/containers/settings/About.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { translate } from 'cozy-ui/react/I18n'
 import SettingCategory, { ELEMENT_TEXT } from '../../components/SettingCategory'
+import Hammer from 'hammerjs'
 
-export const About = ({ t, version, serverUrl, onClick }) => (
-  <SettingCategory
-    title={t('mobile.settings.about.title')}
-    elements={[
-      {
-        type: ELEMENT_TEXT,
-        label: t('mobile.settings.about.account'),
-        value: <a href="{serverUrl}">{serverUrl}</a>
-      },
-      {
-        type: ELEMENT_TEXT,
-        label: t('mobile.settings.about.app_version'),
-        value: version,
-        onClick
-      }
-    ]}
-  />
-)
+class About extends Component {
+  componentDidMount() {
+    this.gesturesHandler = new Hammer.Manager(this.versionNumber)
+    this.gesturesHandler.add(new Hammer.Tap({ event: 'singletap' }))
+    this.gesturesHandler.on('singletap', this.props.onTap)
+  }
+
+  componentWillUnmount() {
+    this.gesturesHandler.destroy()
+  }
+
+  render({ t, version, serverUrl }) {
+    return (
+      <SettingCategory
+        title={t('mobile.settings.about.title')}
+        elements={[
+          {
+            type: ELEMENT_TEXT,
+            label: t('mobile.settings.about.account'),
+            value: <a href="{serverUrl}">{serverUrl}</a>
+          },
+          {
+            type: ELEMENT_TEXT,
+            label: t('mobile.settings.about.app_version'),
+            value: (
+              <span
+                ref={versionNumber => {
+                  this.versionNumber = versionNumber
+                }}
+              >
+                {version}
+              </span>
+            )
+          }
+        ]}
+      />
+    )
+  }
+}
 
 const mapStateToProps = state => ({
   version: window.navigator.appInfo ? window.navigator.appInfo.version : 'dev',
