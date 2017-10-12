691a871787e7801c2ce66667b7b2601a12b04d5c
Patrick Browne <pt.browne@gmail.com>
2017-10-12 18:01:15 +0200

    feat: add fallback text for mobiles which download the file instead of displaying it

--- src/drive/ducks/files/FileOpener.js
+++ src/drive/ducks/files/FileOpener.js
@@ -1,9 +1,12 @@
 /* global cozy */
 
-import { getFileDownloadUrl } from '../../actions'
-import { Spinner, Modal } from 'cozy-ui/react'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { flowRight as compose } from 'lodash'
+
+import { Spinner, Modal, translate } from 'cozy-ui/react'
+
+import { getFileDownloadUrl } from '../../actions'
 import styles from './styles'
 
 const withAlert = Wrapped =>
@@ -11,54 +14,62 @@ const withAlert = Wrapped =>
     alert: data => dispatch({ type: 'ALERT', alert: data })
   }))(Wrapped)
 
-export default withAlert(
-  class extends Component {
-    state = { url: null, loading: false, closing: false }
+class FileOpener extends Component {
+  state = { url: null, loading: false, closing: false }
 
-    componentWillMount() {
-      this.openFile()
-    }
+  componentWillMount() {
+    this.openFile()
+  }
 
-    async openFile() {
-      const { router, params: { fileId } } = this.props
-      try {
-        const url = await getFileDownloadUrl(fileId)
-        this.setState({ url })
-      } catch (e) {
-        router.push('/')
-        alert({
-          message: 'alert.could_not_open_file'
-        })
-      } finally {
-        this.setState({ loading: false })
-      }
-    }
-
-    handleClose = async () => {
-      this.setState({ closing: true })
-      const { router, params: { fileId } } = this.props
-      const { relationships: { parent } } = await cozy.client.files.statById(
-        fileId
-      )
-
-      // Go to the parent folder
-      router.push(`/files/${parent.data.id}`)
-    }
-
-    render() {
-      const { closing, loading, url } = this.state
-      return (
-        <div className={styles.fileOpener}>
-          {loading || closing ? (
-            <Spinner size="xxlarge" loadingType="message" middle="true" />
-          ) : null}
-          {url && !closing ? (
-            <Modal overflowHidden withCross secondaryAction={this.handleClose}>
-              <iframe frameBorder="0" src={url} />
-            </Modal>
-          ) : null}
-        </div>
-      )
+  async openFile() {
+    const { router, params: { fileId }, alert } = this.props
+    try {
+      const url = await getFileDownloadUrl(fileId)
+      this.setState({ url })
+    } catch (e) {
+      router.push('/')
+      alert({
+        message: 'alert.could_not_open_file'
+      })
+    } finally {
+      this.setState({ loading: false })
     }
   }
-)
+
+  handleClose = async () => {
+    this.setState({ closing: true })
+    const { router, params: { fileId } } = this.props
+    const { relationships: { parent } } = await cozy.client.files.statById(
+      fileId
+    )
+
+    // Go to the parent folder
+    router.push(`/files/${parent.data.id}`)
+  }
+
+  render() {
+    const { t } = this.props
+    const { closing, loading, url } = this.state
+    return (
+      <div className={styles.fileOpener}>
+        {loading || closing ? (
+          <Spinner size="xxlarge" loadingType="message" middle="true" />
+        ) : null}
+        {url && !closing ? (
+          <Modal overflowHidden withCross secondaryAction={this.handleClose}>
+            <p className={styles['fileOpener__fallback']}>
+              {t('Files.viewer-fallback')}
+            </p>
+            <iframe
+              className={styles['fileOpener__iframe']}
+              frameBorder="0"
+              src={url}
+            />
+          </Modal>
+        ) : null}
+      </div>
+    )
+  }
+}
+
+export default compose(withAlert, translate())(FileOpener)
