f6fadaa20361e5c8e2ba88e3946b8ca4375b9855
Raphaël Rougeron <goldoraf@gmail.com>
2017-11-21 17:27:25 +0100

    [fix] Quickfixed reporting error on web

--- src/drive/mobile/lib/reporter.js
+++ src/drive/mobile/lib/reporter.js
@@ -1,13 +1,18 @@
 /* global __SENTRY_TOKEN__, __DEVELOPMENT__ */
 import Raven from 'raven-js'
 
-const PROD_ANALYTICS_URL = `https://${__SENTRY_TOKEN__}@sentry.cozycloud.cc/6`
-const DEV_ANALYTICS_URL = `https://${__SENTRY_TOKEN__}@sentry.cozycloud.cc/2`
-export const ANALYTICS_URL = __DEVELOPMENT__
-  ? DEV_ANALYTICS_URL
-  : PROD_ANALYTICS_URL
-
 let isEnabled = false
+
+const getAnalyticsUrl = () => {
+  if (window.__SENTRY_TOKEN__ === undefined) {
+    return ''
+  }
+  const PROD_ANALYTICS_URL = `https://${__SENTRY_TOKEN__}@sentry.cozycloud.cc/6`
+  const DEV_ANALYTICS_URL = `https://${__SENTRY_TOKEN__}@sentry.cozycloud.cc/2`
+  return __DEVELOPMENT__ ? DEV_ANALYTICS_URL : PROD_ANALYTICS_URL
+}
+
+export const ANALYTICS_URL = getAnalyticsUrl()
 
 export const getReporterConfiguration = () => ({
   shouldSendCallback: () => isEnabled,
