62d6a80c450a164a1717398a1bd08b5f709cd266
Raphaël Rougeron <goldoraf@gmail.com>
2017-11-23 14:46:58 +0100

    [fix] Fixed required permissions for the mobile app

--- src/drive/mobile/lib/cozy-helper.js
+++ src/drive/mobile/lib/cozy-helper.js
@@ -12,6 +12,7 @@ export const getLang = () =>
 export const initClient = url => {
   const offline = { doctypes: ['io.cozy.files'] }
   if (isIos()) offline.options = { adapter: 'cordova-sqlite' }
+  console.log('test')
   return new CozyClient({
     cozyURL: url,
     oauth: {
@@ -29,6 +30,7 @@ export const initClient = url => {
         scopes: [
           'io.cozy.files',
           'io.cozy.contacts',
+          'io.cozy.apps',
           'io.cozy.jobs:POST:sendmail:worker'
         ]
       }
