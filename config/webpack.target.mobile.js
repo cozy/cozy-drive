b87ec25e56731ea18cd40e124a87ba17c8f873ca
Simon <github@fafaru.com>
2017-11-29 17:48:55 +0100

    chore: Use the production build of cozy libs
    Il faut utiliser les libs en min qui ont été optimisé pour la production.
    Sinon on a un warning pour redux par exemple avec la cozy-bar.

--- config/webpack.target.mobile.js
+++ config/webpack.target.mobile.js
@@ -28,8 +28,8 @@ module.exports = function(production, app) {
         PouchDB: 'pouchdb',
         pouchdbFind: 'pouchdb-find',
         pouchdbAdapterCordovaSqlite: 'pouchdb-adapter-cordova-sqlite',
-        'cozy.client': 'cozy-client-js/dist/cozy-client.js',
-        'cozy.bar': 'cozy-bar/dist/cozy-bar.mobile.js'
+        'cozy.client': production ? 'cozy-client-js/dist/cozy-client.min.js' : 'cozy-client-js/dist/cozy-client.js',
+        'cozy.bar': production ? 'cozy-bar/dist/cozy-bar.mobile.min.js' : 'cozy-bar/dist/cozy-bar.mobile.js'
       }),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, `../targets/${app}/web/index.ejs`),
