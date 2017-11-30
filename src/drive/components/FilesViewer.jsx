9331691799e65bbff2697812198fb4bd8493b5ba
Raphaël Rougeron <goldoraf@gmail.com>
2017-11-30 14:51:33 +0100

    [fix] Kind of fixed the direct opening of a file in the viewer (from a link) when the file is not in the first 5O files fetched

--- src/drive/components/FilesViewer.jsx
+++ src/drive/components/FilesViewer.jsx
@@ -14,12 +14,22 @@ const getParentPath = router => {
 class FilesViewer extends Component {
   render() {
     if (this.props.files.length === 0) return null
+    // TODO: temp fix for thumbnail links on mobile
     if (isCordova() && !this.props.filesWithLinks) return null
     const files = isCordova()
       ? this.props.filesWithLinks.filter(f => f.type !== 'directory')
       : this.props.files.filter(f => f.type !== 'directory')
     const { params, router } = this.props
     const currentIndex = files.findIndex(f => f.id === params.fileId)
+    // TODO: if we can't find the file, that's probably because the user is trying to open
+    // a direct link to a file that wasn't in the first 50 files of the containing folder
+    // (it comes from a fetchMore...)
+    if (currentIndex === -1) {
+      router.push({
+        pathname: getFolderUrl(params.folderId, router.location)
+      })
+      return null
+    }
     return (
       <Viewer
         files={files}
