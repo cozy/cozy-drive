d42475b406a359faee80da0cdd05b88703eb0165
Raphaël Rougeron <goldoraf@gmail.com>
2017-12-06 16:00:52 +0100

    [feat] Added loading timeout and no network handling to all viewers
    [fix] Code style

--- src/viewer/VideoViewer.jsx
+++ src/viewer/VideoViewer.jsx
@@ -1,33 +1,13 @@
-import React, { Component } from 'react'
-import { getDownloadLink } from 'cozy-client'
-import Spinner from 'cozy-ui/react/Spinner'
+import React from 'react'
 
+import withFileUrl from './withFileUrl'
 import styles from './styles'
 
-export default class VideoViewer extends Component {
-  state = {
-    fileDownloadUrl: null
-  }
+const VideoViewer = ({ file, url }) => (
+  <div className={styles['pho-viewer-videoviewer']}>
+    <video src={url} controls="controls" />
+    <p className={styles['pho-viewer-filename']}>{file.name}</p>
+  </div>
+)
 
-  componentWillMount() {
-    getDownloadLink(this.props.file).then(url =>
-      this.setState({ fileDownloadUrl: url })
-    )
-  }
-
-  render() {
-    const { file } = this.props
-    const { fileDownloadUrl } = this.state
-    return (
-      <div className={styles['pho-viewer-videoviewer']}>
-        {fileDownloadUrl && <video src={fileDownloadUrl} controls="controls" />}
-        {fileDownloadUrl && (
-          <p className={styles['pho-viewer-filename']}>{file.name}</p>
-        )}
-        {!fileDownloadUrl && (
-          <Spinner size="xxlarge" middle="true" noMargin color="white" />
-        )}
-      </div>
-    )
-  }
-}
+export default withFileUrl(VideoViewer)
