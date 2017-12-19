d4e355829c9982dca9f4bb83c178b7488b2c14e6
Yannick Lohse <contact@yannick-lohse.fr>
2017-12-19 17:29:10 +0100

    fix: cozy link in settings

--- src/drive/mobile/containers/settings/About.jsx
+++ src/drive/mobile/containers/settings/About.jsx
@@ -23,7 +23,7 @@ class About extends Component {
           {
             type: ELEMENT_TEXT,
             label: t('mobile.settings.about.account'),
-            value: <a href="{serverUrl}">{serverUrl}</a>
+            value: <a href={serverUrl}>{serverUrl}</a>
           },
           {
             type: ELEMENT_TEXT,
