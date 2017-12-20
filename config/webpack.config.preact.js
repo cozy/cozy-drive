3b09c1cedba07deee95eb6ac2732b8148551f7c1
Yannick Lohse <contact@yannick-lohse.fr>
2017-12-20 16:04:41 +0100

    chore: alias create-react-class with preact

--- config/webpack.config.preact.js
+++ config/webpack.config.preact.js
@@ -14,7 +14,8 @@ module.exports = {
     extensions: ['.jsx'],
     alias: {
       react: 'preact-compat',
-      'react-dom': 'preact-compat'
+      'react-dom': 'preact-compat',
+      'create-react-class': 'preact-compat/lib/create-react-class'
     }
   }
 }
