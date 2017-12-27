32742f517d0500c3e4d8292a5090729c136c94f3
Enguerran <enguerran@ticabri.com>
2017-12-27 18:12:36 +0100

    feat: add sharing special case
    but still need a real implementation

--- src/sharing/ShareModal.jsx
+++ src/sharing/ShareModal.jsx
@@ -1,6 +1,7 @@
 import styles from './share.styl'
 
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
   cozyConnect,
   fetchSharings,
@@ -61,15 +62,17 @@ export class ShareModal extends Component {
     return (
       <Modal title={t(`${documentType}.share.title`)} dismissAction={onClose}>
         <div className={styles['share-modal-content']}>
-          <ShareByEmailComingSoon
-            document={document}
-            documentType={documentType}
-            recipients={sharing.recipients}
-            contacts={contacts.data}
-            sharingDesc={sharingDesc}
-            onShare={share}
-            onUnshare={unshare}
-          />
+          {withSharingCheck(document, documentType, t)(
+            <ShareByEmailComingSoon
+              document={document}
+              documentType={documentType}
+              recipients={sharing.recipients}
+              contacts={contacts.data}
+              sharingDesc={sharingDesc}
+              onShare={share}
+              onUnshare={unshare}
+            />
+          )}
           <hr className={styles['divider']} />
           <ShareByLink
             document={this.props.document}
@@ -83,6 +86,65 @@ export class ShareModal extends Component {
       </Modal>
     )
   }
+}
+
+// TODO: implements
+const isSearchParam = value => {
+  const searchParam = new URL(window.location).searchParams.get(
+    'shareSpecialCase'
+  )
+  return searchParam ? searchParam.includes(value) : false
+}
+
+const hasSharedFolder = id => isSearchParam('hasSharedFolder')
+const isInSharedFolder = id => isSearchParam('isInSharedFolder')
+const getSpecialCase = id => {
+  const specialCase1 = new URL(window.location)
+  specialCase1.search = '?shareSpecialCase=isInSharedFolder'
+  const specialCase2 = new URL(window.location)
+  specialCase2.search = '?shareSpecialCase=hasSharedFolder'
+  console.warn(
+    `Check the sharing special cases with a query parameter, for instance: ${
+      specialCase1.href
+    } or ${specialCase2.href}`
+  )
+  if (hasSharedFolder(id)) {
+    return 'hasSharedFolder'
+  }
+  if (isInSharedFolder(id)) {
+    return 'isInSharedFolder'
+  }
+}
+
+const ShareSpecialCase = ({ documentType, type, specialCase, t }) => (
+  <div style={{ margin: 0, backgroundColor: '#f5f6f7' }}>
+    <hr className={styles['divider']} />
+    <div style={{ padding: '8px 1.5em 1.5em' }}>
+      {t(`${documentType}.share.specialCase.base`, { type })}{' '}
+      <b>{t(`${documentType}.share.specialCase.${specialCase}`)}</b>
+    </div>
+  </div>
+)
+
+ShareSpecialCase.propTypes = {
+  documentType: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  specialCase: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired
+}
+
+const withSharingCheck = ({ id, type }, documentType, t) => BaseComponent => {
+  const specialCase = getSpecialCase(id)
+  return specialCase ? (
+    <ShareSpecialCase
+      documentType={documentType}
+      type={type}
+      specialCase={specialCase}
+      t={t}
+    />
+  ) : (
+    BaseComponent
+  )
 }
 
 export default cozyConnect(
