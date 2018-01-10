5bf0b8683bee85f4a7f81d99e05e17e4dc6fa686
Yannick Lohse <contact@yannick-lohse.fr>
2018-01-10 15:27:26 +0100

    feat: make Avatar a standalone component

---
+++ src/sharing/components/Avatar.jsx
@@ -0,0 +1,16 @@
+import ColorHash from './colorhash'
+import styles from './recipient.styl'
+
+export const Avatar = ({ name, className }) => {
+  const initial = name.charAt(0)
+  const bg = ColorHash().getColor(name)
+  const style = {
+    'background-color': bg
+  }
+  return (
+    <div className={styles['pho-recipient-avatar']} style={style}>
+      <span>{initial}</span>
+    </div>
+  )
+}
+export default Avatar
