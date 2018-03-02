17c308f9de3eb17dbe8b64f4b4502ed2987c744d
Travis CI User <travis@example.org>
2018-03-02 11:20:14 +0000

    publish: Merge 958fbf87e1d8582baeec35c94e36e930d9e98596 into 6ca3edaee73f19b284add0cf4d65ea18d2d00357
    generated from commit b997c88fd4654753291dc67a563d30198e825bc0

--- publish_registry.sh
+++ publish_registry.sh
@@ -49,4 +49,4 @@ curl -sS --fail -X POST \
     -H "Content-Type: application/json" \
     -H "Authorization: Token ${REGISTRY_TOKEN}" \
     -d "{\"editor\": \"${REGISTRY_EDITOR}\", \"version\": \"${COZY_APP_VERSION}\", \"url\": \"${COZY_BUILD_URL}\", \"sha256\": \"${shasum}\", \"type\": \"webapp\"}" \
-    "https://int-registry.cozycloud.cc/registry/${COZY_APP_SLUG}"
+    "https://staging-apps-registry.cozycloud.cc/registry/${COZY_APP_SLUG}"
