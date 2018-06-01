f2b371cd40b58a08a46d9cd23cc62fb21d2c7cf5
Raphaël Rougeron <goldoraf@gmail.com>
2018-06-01 10:41:28 +0200

    fix: Fixed various Drive intents issues: missing icons in the search suggestions and the usage of the old cozy-client

--- src/drive/ducks/files/FileOpenerExternal.jsx
+++ src/drive/ducks/files/FileOpenerExternal.jsx
@@ -1,3 +1,4 @@
+/* global cozy */
 /**
  * This component was previously named FileOpener
  * It has been renamed since it is used in :
@@ -7,14 +8,11 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { flowRight as compose } from 'lodash'
 
 import { Spinner, translate } from 'cozy-ui/react'
 import util from 'cozy-ui/stylus/utilities/text'
 import styles from './styles'
 import Viewer from 'viewer'
-import { fetchDocument, getDocument } from 'cozy-client'
 
 const doNothing = () => {}
 
@@ -25,33 +23,26 @@ const FileNotFoundError = translate()(({ t }) => (
 ))
 
 class FileOpener extends Component {
-  state = { loading: true }
+  state = {
+    loading: true,
+    file: null
+  }
   componentWillMount() {
     this.loadFileInfo()
   }
 
   async loadFileInfo() {
-    const { alert } = this.props
     try {
       this.setState({ fileNotFound: false })
-
-      const res = await this.props.fetchFile()
-
-      if (!res) {
-        this.setState({ fileNotFound: true })
-      }
+      const file = await cozy.client.files.statById(getFileId(this.props))
+      this.setState({ file, loading: false })
     } catch (e) {
-      alert({
-        message: 'alert.could_not_open_file'
-      })
-    } finally {
-      this.setState({ loading: false })
+      this.setState({ fileNotFound: true, loading: false })
     }
   }
 
   render() {
-    const { file } = this.props
-    const { loading, fileNotFound } = this.state
+    const { file, loading, fileNotFound } = this.state
 
     return (
       <div className={styles.fileOpener}>
@@ -75,17 +66,6 @@ const getFileId = ownProps => {
   return ownProps.fileId || ownProps.router.params.fileId
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  alert: data => dispatch({ type: 'ALERT', alert: data }),
-  fetchFile: async () => {
-    return dispatch(fetchDocument('io.cozy.files', getFileId(ownProps)))
-  }
-})
-
-const mapStateToProps = (state, ownProps) => ({
-  file: getDocument(state, 'io.cozy.files', getFileId(ownProps))
-})
-
 FileOpener.PropTypes = {
   router: PropTypes.shape({
     params: PropTypes.shape({
@@ -95,7 +75,4 @@ FileOpener.PropTypes = {
   fileId: PropTypes.number
 }
 
-export default compose(
-  translate(),
-  connect(mapStateToProps, mapDispatchToProps)
-)(FileOpener)
+export default translate()(FileOpener)
