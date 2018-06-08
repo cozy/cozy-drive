f4fbfb4404b457f733c45ef87bc71c850b21bdc8
Yannick Lohse <contact@yannick-lohse.fr>
2018-06-08 12:23:38 +0200

    fix: support transition from previous token storage

--- targets/drive/mobile/main.jsx
+++ targets/drive/mobile/main.jsx
@@ -26,11 +26,12 @@ import { backupImages } from 'drive/mobile/ducks/mediaBackup'
 import { getTranslateFunction } from 'drive/mobile/lib/i18n'
 import { scheduleNotification } from 'drive/mobile/lib/notification'
 import { isIos } from 'drive/mobile/lib/device'
-import { getLang, initClient, initBar, restoreCozyClientJs } from 'drive/mobile/lib/cozy-helper'
+import { getLang, initClient, initBar, restoreCozyClientJs, resetClient } from 'drive/mobile/lib/cozy-helper'
 import { revokeClient } from 'drive/mobile/actions/authorization'
-import { startReplication } from 'drive/mobile/actions/settings'
+import { startReplication, setToken } from 'drive/mobile/actions/settings'
 import { configureReporter } from 'drive/mobile/lib/reporter'
 import { intentHandlerAndroid, intentHandlerIOS } from 'drive/mobile/lib/intents'
+import { LocalStorage as Storage } from 'cozy-client-js'
 
 if (__DEVELOPMENT__) {
   // Enables React dev tools for Preact
@@ -43,16 +44,29 @@ window.handleOpenURL = require('drive/mobile/lib/handleDeepLink').default(
   hashHistory
 )
 
+const getPreviousToken = async store => {
+  if (store.getState().mobile.settings.token) return store.getState().mobile.settings.token
+  else {
+    // transition from version 1.5.3 to the next one, we recover the token stored by cozy-client-js
+    const cozyClientStorage = new Storage()
+    const { token } = await cozyClientStorage.load('creds')
+    store.dispatch(setToken(token))
+    return token
+  }
+}
+
 const startApplication = async function(store, client) {
   configureReporter()
-  const { client: clientInfos } = store.getState().settings
-    const { token } = store.getState().mobile.settings
-  if (clientInfos && token) {
-    const oauthClient = client.getOrCreateStackClient()
-    oauthClient.setOAuthOptions(clientInfos);
-    oauthClient.setCredentials(token)
 
-    try {
+  try {
+    const { client: clientInfos } = store.getState().settings
+    const token = await getPreviousToken(store)
+
+    if (clientInfos && token) {
+      const oauthClient = client.getOrCreateStackClient()
+      oauthClient.setOAuthOptions(clientInfos);
+      oauthClient.setCredentials(token)
+
       await oauthClient.fetchInformation()
 
       await restoreCozyClientJs(client.options.uri, clientInfos, token)
@@ -60,10 +74,12 @@ const startApplication = async function(store, client) {
       startReplication(store.dispatch, store.getState) // don't like to pass `store.dispatch` and `store.getState` as parameters, big coupling
       initBar(client)
     }
-    catch (e) {
-      console.warn('Your device is not connected to your server anymore')
-      store.dispatch(revokeClient())
-    }
+  }
+  catch (e) {
+    console.warn(e)
+    console.warn('Your device is not connected to your server anymore')
+    store.dispatch(revokeClient())
+    resetClient(client)
   }
 
   useHistoryForTracker(hashHistory)
