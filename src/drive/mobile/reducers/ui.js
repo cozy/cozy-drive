f3dcf186576e1373d2857e25dfa517621734a702
Yannick Lohse <contact@yannick-lohse.fr>
2018-07-02 17:27:22 +0200

    fix: hide unlink confirmation after unlink

--- src/drive/mobile/reducers/ui.js
+++ src/drive/mobile/reducers/ui.js
@@ -1,6 +1,7 @@
 import {
   SHOW_UNLINK_CONFIRMATION,
-  HIDE_UNLINK_CONFIRMATION
+  HIDE_UNLINK_CONFIRMATION,
+  UNLINK
 } from '../actions/unlink'
 
 export const initialState = {
@@ -12,6 +13,7 @@ export const ui = (state = initialState, action) => {
     case SHOW_UNLINK_CONFIRMATION:
       return { ...state, displayUnlinkConfirmation: true }
     case HIDE_UNLINK_CONFIRMATION:
+    case UNLINK:
       return { ...state, displayUnlinkConfirmation: false }
     default:
       return state
