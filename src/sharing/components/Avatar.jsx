3eba0a9b1da12b5dee06f4b38c02bfbd7c968433
Yannick Lohse <contact@yannick-lohse.fr>
2018-07-19 10:04:05 +0200

    feat: Placeholder avatar when more then 3 recipients

---
+++ src/sharing/components/Avatar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ColorHash from './colorhash'
+import styles from './recipient.styl'
+import PropTypes from 'prop-types'
+
+export const Avatar = ({ name = '' }) => {
+  const initial = name.charAt(0)
+  const bg = ColorHash().getColor(name)
+  const style = {
+    'background-color': bg
+  }
+  return (
+    <div className={styles['pho-recipient-avatar']} style={style}>
+      <span>{initial}</span>
+    </div>
+  )
+}
+
+Avatar.propTypes = {
+  name: PropTypes.string
+}
+
+Avatar.defaultProps = {
+  name: ''
+}
+
+export const AvatarPlusX = ({ count = 1 }) => (
+  <div className={styles['pho-recipient-avatar']}>
+    <span className={styles['pho-recipient-avatar-plusx']}>+{count}</span>
+  </div>
+)
+
+AvatarPlusX.propTypes = {
+  count: PropTypes.number
+}
+
+AvatarPlusX.defaultProps = {
+  count: 1
+}
+
+export const AvatarLink = () => (
+  <div className={styles['pho-recipient-avatar']}>
+    <span className={styles['pho-recipient-avatar-link']} />
+  </div>
+)
