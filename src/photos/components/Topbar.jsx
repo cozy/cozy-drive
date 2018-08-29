5ee816dccdb8cd8c8a58974da7149ba619e6b4ab
Yannick Lohse <contact@yannick-lohse.fr>
2018-08-29 09:18:02 +0200

    fix: Include page title in cozy bar

--- src/photos/components/Topbar.jsx
+++ src/photos/components/Topbar.jsx
@@ -1,46 +1,39 @@
+/* global cozy  */
 import styles from '../styles/topbar'
 
 import React, { Component } from 'react'
 import { translate } from 'cozy-ui/react/I18n'
+import { withBreakpoints } from 'cozy-ui/react'
 import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
+
+const { BarCenter } = cozy.bar
 
 const KEYCODE_ENTER = 13
 const KEYCODE_ESC = 27
 
-class Topbar extends Component {
+class EditableAlbumName extends Component {
   constructor(props) {
     super(props)
-
     this.ignoreBlurEvent = false // we'll ignore blur events if they are triggered by pressing enter or escape, to prevent `onEdit` from firing twice
     this.inputElement = null
-
-    this.backToAlbums = this.backToAlbums.bind(this)
-    this.handleBlur = this.handleBlur.bind(this)
-    this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
-  componentDidUpdate() {
+  componentDidMount() {
     if (this.inputElement !== null) {
       this.inputElement.focus()
       this.inputElement.select()
     }
   }
 
-  backToAlbums() {
-    // go to parent
-    let url = this.props.router.location.pathname
-    this.props.router.push(url.substring(0, url.lastIndexOf('/')))
-  }
-
-  handleBlur(e) {
+  handleBlur = e => {
     if (!this.ignoreBlurEvent && this.props.onEdit)
       this.props.onEdit(
         e.target.value.trim() !== '' ? e.target.value : this.props.albumName
       )
   }
 
-  handleKeyDown(e) {
+  handleKeyDown = e => {
     if (e.keyCode === KEYCODE_ENTER && this.props.onEdit) {
       this.ignoreBlurEvent = true
       this.props.onEdit(e.target.value)
@@ -50,33 +43,64 @@ class Topbar extends Component {
     }
   }
 
-  render({ t, children, viewName, albumName = '', editing = false }) {
+  render() {
+    const { albumName } = this.props
+    return (
+      <input
+        type="text"
+        value={albumName}
+        onKeyDown={this.handleKeyDown}
+        onBlur={this.handleBlur}
+        ref={elem => {
+          this.inputElement = elem
+        }}
+      />
+    )
+  }
+}
+
+EditableAlbumName.propTypes = {
+  albumName: PropTypes.string,
+  onEdit: PropTypes.func.isRequired
+}
+
+const TopbarTitle = ({ children }) => (
+  <h2 className={styles['pho-content-title']}>{children}</h2>
+)
+
+class Topbar extends Component {
+  backToAlbums = () => {
+    // go to parent
+    let url = this.props.router.location.pathname
+    this.props.router.push(url.substring(0, url.lastIndexOf('/')))
+  }
+
+  renderTitle() {
+    const { t, viewName, albumName = '', onEdit, editing = false } = this.props
+    const isAlbumContent = viewName === 'albumContent'
+
+    if (!isAlbumContent) return t(`Nav.${viewName}`)
+    else if (editing)
+      return <EditableAlbumName albumName={albumName} onEdit={onEdit} />
+    else return albumName
+  }
+
+  render() {
+    const { children, viewName, breakpoints: { isMobile } } = this.props
+    const isAlbumContent = viewName === 'albumContent'
+    const title = <TopbarTitle>{this.renderTitle()}</TopbarTitle>
+    const responsiveTitle = isMobile ? <BarCenter>{title}</BarCenter> : title
+
     return (
       <div className={styles['pho-content-header']}>
-        {viewName === 'albumContent' && (
+        {isAlbumContent && (
           <div
             role="button"
             className={styles['pho-content-album-previous']}
             onClick={this.backToAlbums}
           />
         )}
-        <h2 className={styles['pho-content-title']}>
-          {viewName !== 'albumContent' ? (
-            t(`Nav.${viewName}`)
-          ) : editing ? (
-            <input
-              type="text"
-              value={albumName}
-              onKeyDown={this.handleKeyDown}
-              onBlur={this.handleBlur}
-              ref={elem => {
-                this.inputElement = elem
-              }}
-            />
-          ) : (
-            albumName
-          )}
-        </h2>
+        {responsiveTitle}
         {children}
       </div>
     )
@@ -88,7 +112,10 @@ Topbar.propTypes = {
   albumName: PropTypes.string,
   t: PropTypes.func.isRequired,
   editing: PropTypes.bool.isRequired,
-  onEdit: PropTypes.func.isRequired
+  onEdit: PropTypes.func.isRequired,
+  router: PropTypes.object.isRequired,
+  breakpoints: PropTypes.object.isRequired,
+  children: PropTypes.node
 }
 
-export default translate()(withRouter(Topbar))
+export default withBreakpoints()(translate()(withRouter(Topbar)))
