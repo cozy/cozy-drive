ca16438cdedef8e8c78855c2613ba26116501923
Yannick Lohse <contact@yannick-lohse.fr>
2018-09-05 18:28:06 +0200

    fix: Add folder buton on mobile

--- src/drive/web/modules/services/components/Picker/AddFolderButton.jsx
+++ src/drive/web/modules/services/components/Picker/AddFolderButton.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Button } from 'cozy-ui/react'
+import { Button, ButtonAction, withBreakpoints, Icon } from 'cozy-ui/react'
+import IconFolderAdd from 'drive/assets/icons/icon-folder-add.svg'
+import { showNewFolderInput } from 'drive/web/modules/filelist/duck'
 
-const AddFolderButton = ({ addFolder }) => (
-  <Button onClick={addFolder}>Nouveau dossier</Button>
-)
+const AddFolderButton = ({ addFolder, breakpoints: { isMobile } }) => {
+  if (isMobile)
+    return (
+      <ButtonAction
+        compact
+        rightIcon={<Icon icon={IconFolderAdd} color="coolGrey" />}
+        onClick={addFolder}
+        label={'Nouveau dossier'}
+      />
+    )
+  else
+    return (
+      <Button icon={IconFolderAdd} onClick={addFolder}>
+        Nouveau dossier
+      </Button>
+    )
+}
 
 const mapDispatchToPropsButton = (dispatch, ownProps) => ({
   addFolder: () => dispatch(showNewFolderInput())
 })
 
-export default connect(null, mapDispatchToPropsButton)(AddFolderButton)
+export default connect(null, mapDispatchToPropsButton)(
+  withBreakpoints()(AddFolderButton)
+)
