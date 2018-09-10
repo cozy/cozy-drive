29423b92d319006fba701f0bd441c83d1b669183
Yannick Lohse <contact@yannick-lohse.fr>
2018-09-10 11:51:38 +0200

    feat: New add to my cozy button

--- src/drive/web/modules/public/PublicToolbar.jsx
+++ src/drive/web/modules/public/PublicToolbar.jsx
@@ -2,15 +2,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classnames from 'classnames'
-import { withBreakpoints } from 'cozy-ui/react'
+import {
+  ButtonLink,
+  withBreakpoints,
+  Menu,
+  MenuItem,
+  Icon
+} from 'cozy-ui/react'
 import { MoreButton } from 'components/Button'
-import Menu, { Item } from 'components/Menu'
 import DownloadButton from './DownloadButton'
-import toolbarstyles from 'drive/styles/toolbar'
 import { downloadFiles } from 'drive/web/modules/navigation/duck'
-import CozyHomeLink, { getHomeLinkHref } from 'components/Button/CozyHomeLink'
+import toolbarstyles from 'drive/styles/toolbar'
+import { getQueryParameter } from 'react-cozy-helpers'
+
+import CloudIcon from 'drive/assets/icons/icon-cloud-open.svg'
+import DownloadIcon from 'drive/assets/icons/icon-download-16.svg'
 
 const { BarRight } = cozy.bar
+
+const OpenInCozyButton = ({ t, size = 'normal', href = '' }) => (
+  <ButtonLink
+    href={href}
+    size={size}
+    label={t('toolbar.menu_open_cozy')}
+    icon={CloudIcon}
+  />
+)
 
 const DownloadFilesButton = ({ t, onDownload, size = 'normal' }) => (
   <DownloadButton
@@ -29,57 +46,89 @@ const MoreMenu = ({ onDownload, t }) => (
       toolbarstyles['fil-toolbar-menu--public']
     )}
     buttonClassName={toolbarstyles['fil-toolbar-more-btn']}
-    button={<MoreButton>{t('Toolbar.more')}</MoreButton>}
+    component={<MoreButton>{t('Toolbar.more')}</MoreButton>}
+    position="right"
   >
-    <Item>
-      <a
-        className={toolbarstyles['fil-action-create-cozy']}
-        href={getHomeLinkHref('sharing-drive')}
-      >
-        {t('Share.create-cozy')}
-      </a>
-    </Item>
-    <Item>
-      <a className={toolbarstyles['fil-action-download']} onClick={onDownload}>
-        {t('toolbar.menu_download_folder')}
-      </a>
-    </Item>
+    <MenuItem icon={<Icon icon={CloudIcon} />}>
+      {t('toolbar.menu_open_cozy')}
+    </MenuItem>
+    <MenuItem onSelect={onDownload} icon={<Icon icon={DownloadIcon} />}>
+      {t('toolbar.menu_download_folder')}
+    </MenuItem>
   </Menu>
 )
 
-const PublicToolbar = (
-  { files, onDownload, breakpoints: { isMobile }, renderInBar = false },
-  { t }
-) => {
-  if (isMobile) {
-    return (
-      <BarRight>
-        <MoreMenu t={t} onDownload={() => onDownload(files)} />
-      </BarRight>
-    )
-  } else if (renderInBar) {
-    return (
-      <BarRight>
-        <div className={toolbarstyles['toolbar-inside-bar']}>
-          <CozyHomeLink from="sharing-drive" />
-          <DownloadFilesButton
-            t={t}
-            onDownload={() => onDownload(files)}
-            size="small"
-          />
-        </div>
-      </BarRight>
-    )
-  } else {
-    return (
-      <div className={toolbarstyles['fil-toolbar-files']} role="toolbar">
-        <CozyHomeLink from="sharing-drive" />
-        <DownloadFilesButton t={t} onDownload={() => onDownload(files)} />
+class PublicToolbar extends React.Component {
+  state = {
+    discoveryLink: null
+  }
+
+  componentDidMount() {
+    this.loadSharingDiscoveryLink()
+  }
+
+  async loadSharingDiscoveryLink() {
+    try {
+      const { client } = this.context
+      const response = await client
+        .collection('io.cozy.permissions')
+        .getOwnPermissions()
+      const sourceId = response.data.attributes.source_id
+      const sharingId = sourceId.split('/')[1]
+
+      const { sharecode } = getQueryParameter()
+
+      const link = client
+        .collection('io.cozy.sharings')
+        .getDiscoveryLink(sharingId, sharecode)
+      this.setState({ discoveryLink: link })
+    } catch (err) {
+      console.warn('Failed to load sharing discovery link', err)
+    }
+  }
+
+  render() {
+    const {
+      files,
+      onDownload,
+      breakpoints: { isMobile },
+      renderInBar = false
+    } = this.props
+    const { t } = this.context
+    const { discoveryLink } = this.state
+
+    if (isMobile) {
+      return (
         <BarRight>
-          <div />
+          <MoreMenu t={t} onDownload={() => onDownload(files)} />
         </BarRight>
-      </div>
-    )
+      )
+    } else if (renderInBar) {
+      return (
+        <BarRight>
+          <div className={toolbarstyles['toolbar-inside-bar']}>
+            {discoveryLink && (
+              <OpenInCozyButton href={discoveryLink} t={t} size="small" />
+            )}
+            <DownloadFilesButton
+              t={t}
+              onDownload={() => onDownload(files)}
+              size="small"
+            />
+          </div>
+        </BarRight>
+      )
+    } else {
+      return (
+        <div className={toolbarstyles['fil-toolbar-files']} role="toolbar">
+          {discoveryLink && <OpenInCozyButton href={discoveryLink} t={t} />}
+          <DownloadFilesButton t={t} onDownload={() => onDownload(files)} />
+          <BarRight>
+            <div />
+          </BarRight>
+        </div>
+      )
+    }
   }
 }
 
