96d069fa90429872acc9958af533127faab7b79d
Yannick Lohse <contact@yannick-lohse.fr>
2018-09-10 15:28:53 +0200

    fix: Re-establish previous button for sharings by link

--- src/drive/web/modules/public/PublicToolbar.jsx
+++ src/drive/web/modules/public/PublicToolbar.jsx
@@ -14,8 +14,10 @@ import DownloadButton from './DownloadButton'
 import { downloadFiles } from 'drive/web/modules/navigation/duck'
 import toolbarstyles from 'drive/styles/toolbar'
 import { getQueryParameter } from 'react-cozy-helpers'
+import CozyHomeLink, { getHomeLinkHref } from 'components/Button/CozyHomeLink'
 
 import CloudIcon from 'drive/assets/icons/icon-cloud-open.svg'
+import CloudNegative from 'drive/assets/icons/icon-cloud-negative.svg'
 import DownloadIcon from 'drive/assets/icons/icon-download-16.svg'
 
 const { BarRight } = cozy.bar
@@ -38,7 +40,7 @@ const DownloadFilesButton = ({ t, onDownload, size = 'normal' }) => (
     size={size}
   />
 )
-const MoreMenu = ({ onDownload, t }) => (
+const MoreMenu = ({ t, onDownload, onOpenInCozy, onCreateCozy }) => (
   <Menu
     title={t('toolbar.item_more')}
     className={classnames(
@@ -49,9 +51,16 @@ const MoreMenu = ({ onDownload, t }) => (
     component={<MoreButton>{t('Toolbar.more')}</MoreButton>}
     position="right"
   >
-    <MenuItem icon={<Icon icon={CloudIcon} />}>
-      {t('toolbar.menu_open_cozy')}
-    </MenuItem>
+    {onOpenInCozy && (
+      <MenuItem onSelect={onOpenInCozy} icon={<Icon icon={CloudIcon} />}>
+        {t('toolbar.menu_open_cozy')}
+      </MenuItem>
+    )}
+    {onCreateCozy && (
+      <MenuItem onSelect={onCreateCozy} icon={<Icon icon={CloudNegative} />}>
+        {t('Share.create-cozy')}
+      </MenuItem>
+    )}
     <MenuItem onSelect={onDownload} icon={<Icon icon={DownloadIcon} />}>
       {t('toolbar.menu_download_folder')}
     </MenuItem>
@@ -64,7 +73,7 @@ class PublicToolbar extends React.Component {
   }
 
   componentDidMount() {
-    this.loadSharingDiscoveryLink()
+    if (window.location.pathname === '/preview') this.loadSharingDiscoveryLink()
   }
 
   async loadSharingDiscoveryLink() {
@@ -87,6 +96,10 @@ class PublicToolbar extends React.Component {
     }
   }
 
+  redirectTo(url) {
+    window.location.href = url
+  }
+
   render() {
     const {
       files,
@@ -100,15 +113,28 @@ class PublicToolbar extends React.Component {
     if (isMobile) {
       return (
         <BarRight>
-          <MoreMenu t={t} onDownload={() => onDownload(files)} />
+          <MoreMenu
+            t={t}
+            onDownload={() => onDownload(files)}
+            onOpenInCozy={
+              discoveryLink ? () => this.redirectTo(discoveryLink) : false
+            }
+            onCreateCozy={
+              discoveryLink
+                ? false
+                : () => this.redirectTo(getHomeLinkHref('sharing-drive'))
+            }
+          />
         </BarRight>
       )
     } else if (renderInBar) {
       return (
         <BarRight>
           <div className={toolbarstyles['toolbar-inside-bar']}>
-            {discoveryLink && (
+            {discoveryLink ? (
               <OpenInCozyButton href={discoveryLink} t={t} size="small" />
+            ) : (
+              <CozyHomeLink from="sharing-drive" />
             )}
             <DownloadFilesButton
               t={t}
@@ -121,7 +147,11 @@ class PublicToolbar extends React.Component {
     } else {
       return (
         <div className={toolbarstyles['fil-toolbar-files']} role="toolbar">
-          {discoveryLink && <OpenInCozyButton href={discoveryLink} t={t} />}
+          {discoveryLink ? (
+            <OpenInCozyButton href={discoveryLink} t={t} />
+          ) : (
+            <CozyHomeLink from="sharing-drive" />
+          )}
           <DownloadFilesButton t={t} onDownload={() => onDownload(files)} />
           <BarRight>
             <div />
