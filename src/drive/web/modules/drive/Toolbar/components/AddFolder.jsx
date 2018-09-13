64eeb5d237dca7d17d4d5a811faa3239c8aa1b92
Quentin Valmori <quentin.valmori@gmail.com>
2018-09-13 17:40:43 +0200

    UploadButton refacto

---
+++ src/drive/web/modules/drive/Toolbar/components/AddFolder.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { translate } from 'cozy-ui/react/I18n'
+import styles from 'drive/styles/toolbar'
+import { showNewFolderInput } from 'drive/web/modules/filelist/duck'
+
+const AddFolder = translate()(({ t, addFolder }) => {
+  return (
+    <a className={styles['fil-action-newfolder']} onClick={addFolder}>
+      {t('toolbar.menu_new_folder')}
+    </a>
+  )
+})
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  addFolder: () => dispatch(showNewFolderInput())
+})
+
+export default connect(null, mapDispatchToProps)(AddFolder)
