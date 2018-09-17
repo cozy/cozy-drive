9bbf54bd92b7bce7c32edc496b443e47009b2590
Quentin Valmori <quentin.valmori@gmail.com>
2018-09-17 14:41:20 +0200

    Delete container has to be aware of the router

--- src/drive/web/modules/drive/Toolbar/containers/delete.jsx
+++ src/drive/web/modules/drive/Toolbar/containers/delete.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { showModal } from 'react-cozy-helpers'
 import DeleteConfirm from '../../DeleteConfirm'
-
+import { withRouter } from 'react-router'
 import { trashFiles } from 'drive/web/modules/navigation/duck'
 import toolbarContainer from './toolbar'
 const mapStateToProps = (state, ownProps) => ({})
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch, ownProps) => ({
   trashFolder: folder =>
     dispatch(
       showModal(
         <DeleteConfirm
           files={[folder]}
-          onConfirm={() =>
-            dispatch(trashFiles([folder])).then(() => {
-              console.log('should be redirected')
-              // ownProps.router.push(`/folder/${folder.parent.id}`)
-            })
-          }
+          onConfirm={async () => {
+            await dispatch(trashFiles([folder]))
+            ownProps.router.push(`/folder/${folder.parent.id}`)
+          }}
         />
       )
     )
 })
 
 const deleteContainer = component =>
-  toolbarContainer(connect(mapStateToProps, mapDispatchToProps)(component))
+  toolbarContainer(
+    withRouter(connect(mapStateToProps, mapDispatchToProps)(component))
+  )
 
 export default deleteContainer
