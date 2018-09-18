619480ed7aee3787777382006cb489d4132893a9
Quentin Valmori <quentin.valmori@gmail.com>
2018-09-18 17:52:38 +0200

    typo

--- src/drive/web/modules/drive/Toolbar/components/SelectableItem.jsx
+++ src/drive/web/modules/drive/Toolbar/components/SelectableItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import selectableContainer from '../containers/selectable'
 const SelectableItem = props => {
-  return React.cloneElement(props.children, { onCLick: props.showSelectionBar })
+  return React.cloneElement(props.children, { onClick: props.showSelectionBar })
 }
 
 export default selectableContainer(SelectableItem)
