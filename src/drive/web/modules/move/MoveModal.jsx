4743b55591712087d1c7008d7561bee5391ab3ce
Yannick Lohse <contact@yannick-lohse.fr>
2018-09-25 10:55:14 +0200

    feat: Display a basic file list

--- src/drive/web/modules/move/MoveModal.jsx
+++ src/drive/web/modules/move/MoveModal.jsx
@@ -1,22 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  Modal,
-  ModalDescription,
-  ModalHeader,
-  ModalFooter,
-  Button
-} from 'cozy-ui/react'
+import { Modal, Button } from 'cozy-ui/react'
+import { Query } from 'cozy-client'
+import FileList from 'drive/web/modules/filelist/FileList'
+import Topbar from 'drive/web/modules/layout/Topbar'
+import { ROOT_DIR_ID, TRASH_DIR_ID } from 'drive/constants/config'
 
 class MoveModal extends React.Component {
+  state = {
+    folderId: ROOT_DIR_ID
+  }
+
+  navigateTo = folder => {
+    this.setState({ folderId: folder.id })
+  }
+
+  sortData = data => {
+    const folders = data.filter(
+      entry => entry.type === 'directory' && entry.id !== TRASH_DIR_ID
+    )
+    const files = data.filter(entry => entry.type !== 'directory')
+
+    return folders.concat(files)
+  }
+
   render() {
+    const { client } = this.context
+    const { folderId } = this.state
+    const query = client =>
+      client
+        .find('io.cozy.files')
+        .where({ dir_id: folderId })
+        .sortBy({ name: 'asc' })
+
     return (
-      <Modal>
-        <ModalDescription>Actual content goes here</ModalDescription>
-        <ModalFooter>
+      <Modal size={'xlarge'} closable={false} overflowHidden={true}>
+        <Topbar>topbar content</Topbar>
+        <Query query={query}>
+          {({ data, fetchStatus }) => {
+            return (
+              <FileList
+                withSelectionCheckbox={false}
+                canSort={false}
+                fileActions={[]}
+                files={this.sortData(data)}
+                selectionModeActive={false}
+                actionMenuActive={false}
+                onFolderOpen={id =>
+                  this.navigateTo(data.find(f => f.id === id))
+                }
+              />
+            )
+          }}
+        </Query>
+        <div>
           <Button>Déplacer</Button>
           <Button theme="secondary">Annuler</Button>
-        </ModalFooter>
+        </div>
       </Modal>
     )
   }
