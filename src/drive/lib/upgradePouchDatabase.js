c378d7718c5ae212ff3c17cf1c10cc1ff687f13d
Yannick Lohse <contact@yannick-lohse.fr>
2018-10-04 14:11:39 +0200

    fix: Recreate indexes after migration

--- src/drive/lib/upgradePouchDatabase.js
+++ src/drive/lib/upgradePouchDatabase.js
@@ -4,7 +4,10 @@ const upgradePouchDatabase = async dbName => {
   const db = cozy.client.offline.getDatabase(dbName)
 
   const infos = await db.info()
-  if (infos.adapter === 'websql') cozy.client.offline.migrateDatabase(dbName)
+  if (infos.adapter === 'websql') {
+    await cozy.client.offline.migrateDatabase(dbName)
+    return true
+  } else return false
 }
 
 export default upgradePouchDatabase
