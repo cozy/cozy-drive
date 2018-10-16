7e9b9b1ca611b35dc687bda2986a8de1accbd230
Yannick Lohse <contact@yannick-lohse.fr>
2018-10-16 18:00:10 +0200

    chore: Configure sentry token for all targets

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,6 +2,9 @@
 
 const { DefinePlugin, ProvidePlugin } = require('webpack')
 
+const path = require('path')
+const pkg = require(path.resolve(__dirname, '../package.json'))
+
 module.exports = {
   devtool: 'cheap-module-source-map',
   externals: ['cozy'],
@@ -20,7 +23,9 @@ module.exports = {
       __PIWIK_SITEID__: 8,
       __PIWIK_SITEID_MOBILE__: 12,
       __PIWIK_DIMENSION_ID_APP__: 1,
-      __PIWIK_TRACKER_URL__: JSON.stringify('https://piwik.cozycloud.cc')
+      __PIWIK_TRACKER_URL__: JSON.stringify('https://piwik.cozycloud.cc'),
+      __SENTRY_TOKEN__: JSON.stringify('29bd1255b6d544a1b65435a634c9ff67:ba312a96643d4f98aee26c6378c74212'),
+      __APP_VERSION__: JSON.stringify(pkg.version)
     }),
     new ProvidePlugin({
       'cozy.client': 'cozy-client-js/dist/cozy-client.js',
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -2,6 +2,9 @@
 
 const { DefinePlugin, optimize } = require('webpack')
 
+const path = require('path')
+const pkg = require(path.resolve(__dirname, '../package.json'))
+
 module.exports = {
   output: {
     filename: '[name].[hash].min.js'
@@ -23,7 +26,9 @@ module.exports = {
       __PIWIK_SITEID__: 8,
       __PIWIK_SITEID_MOBILE__: 12,
       __PIWIK_DIMENSION_ID_APP__: 1,
-      __PIWIK_TRACKER_URL__: JSON.stringify('https://piwik.cozycloud.cc')
+      __PIWIK_TRACKER_URL__: JSON.stringify('https://piwik.cozycloud.cc'),
+      __SENTRY_TOKEN__: JSON.stringify('9259817fbb44484b8b7a0a817d968ae4:171a3bcb3095448484aa3e709ea47e9b'),
+      __APP_VERSION__: JSON.stringify(pkg.version)
     })
   ]
 }
