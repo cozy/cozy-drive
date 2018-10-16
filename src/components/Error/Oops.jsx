8948b8baa21f8321670ade94b85c221e4604c5f3
Yannick Lohse <contact@yannick-lohse.fr>
2018-10-16 16:58:16 +0200

    fix(ErrorView): Enable pointer events

--- src/components/Error/Oops.jsx
+++ src/components/Error/Oops.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { translate } from 'cozy-ui/react/I18n'
 import { Empty, Button } from 'cozy-ui/react'
 import EmptyIcon from '../../drive/assets/icons/icon-folder-broken.svg'
+import styles from './oops.styl'
 
 const reload = () => {
   window.location.reload()
 }
 
 const Oops = ({ t }) => (
-  <Empty title={t('error.open_folder')} icon={EmptyIcon}>
+  <Empty
+    title={t('error.open_folder')}
+    icon={EmptyIcon}
+    className={styles['oops']}
+  >
     <Button onClick={reload} label={t('error.button.reload')} />
   </Empty>
 )
