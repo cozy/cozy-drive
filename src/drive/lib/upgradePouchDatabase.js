d278af346e433cf7c879feee47170f9f2f0fafa2
Yannick Lohse <contact@yannick-lohse.fr>
2018-10-16 16:37:25 +0200

    fix: Check for missing indexes

--- src/drive/lib/upgradePouchDatabase.js
+++ src/drive/lib/upgradePouchDatabase.js
@@ -1,13 +1,27 @@
 /* global cozy */
 
-const upgradePouchDatabase = async dbName => {
+const upgradePouchDatabase = async (dbName, existingIndexes = []) => {
   const db = cozy.client.offline.getDatabase(dbName)
 
   const infos = await db.info()
-  if (infos.adapter === 'websql') {
-    await cozy.client.offline.migrateDatabase(dbName)
-    return true
-  } else return false
+  const isAdapterOutdated = infos.adapter === 'websql'
+
+  if (isAdapterOutdated) {
+    try {
+      await cozy.client.offline.migrateDatabase(dbName)
+      return true
+    } catch (err) {
+      console.warn(err)
+      return false
+    }
+  } else {
+    const { indexes } = await db.getIndexes()
+    const missingIndexes = existingIndexes.filter(
+      existingIndex => !indexes.find(index => index.ddoc === existingIndex)
+    )
+
+    return missingIndexes.length > 0 ? true : false
+  }
 }
 
 export default upgradePouchDatabase
