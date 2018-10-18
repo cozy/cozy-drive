ab142e67badef2ce39439c0e3ab27a16b88f60bd
Yannick Lohse <contact@yannick-lohse.fr>
2018-10-18 15:54:36 +0200

    fix(PouchMigration): Correct import statement

--- targets/drive/web/services.jsx
+++ targets/drive/web/services.jsx
@@ -9,7 +9,7 @@ import IntentHandler from 'drive/web/modules/services'
 import CozyClient, { CozyProvider } from 'cozy-client'
 import { I18n } from 'cozy-ui/react/I18n'
 import { getQueryParameter } from 'react-cozy-helpers'
-import upgradePouchDatabase from 'drive/lib/upgradePouchDatabase'
+import { upgradePouchDatabase } from 'drive/lib/upgradePouchDatabase'
 import { schema } from '../doctypes'
 if (__DEVELOPMENT__) {
   // Enables React dev tools for Preact
