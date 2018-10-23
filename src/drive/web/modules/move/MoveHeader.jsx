c0b4ce46138265766bbfbe350db89892e0bdd06f
Yannick Lohse <contact@yannick-lohse.fr>
2018-10-23 14:58:40 +0200

    feat(Move): Multiple elements header

--- src/drive/web/modules/move/MoveHeader.jsx
+++ src/drive/web/modules/move/MoveHeader.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ContextHeader } from 'cozy-ui/react'
+import {
+  Well,
+  Media,
+  Img,
+  Bd,
+  Circle,
+  Counter,
+  Icon,
+  Button,
+  Caption,
+  Bold
+} from 'cozy-ui/react'
+import palette from 'cozy-ui/stylus/settings/palette.json'
 import DriveIcon from 'drive/assets/icons/icon-drive.svg'
 
+const HeaderIlustration = ({ count }) => {
+  return count > 1 ? (
+    <Circle>
+      <Counter count={count} />
+    </Circle>
+  ) : (
+    <Icon icon={DriveIcon} size={32} />
+  )
+}
+
 const MoveHeader = ({ entries, onClose }, { t }) => (
-  <ContextHeader
-    title={entries[0].name}
-    text={t('Move.to')}
-    icon={DriveIcon}
-    onClose={onClose}
-  />
+  <Well>
+    <Media>
+      <Img className="u-mr-1">
+        <HeaderIlustration count={entries.length} />
+      </Img>
+      <Bd>
+        <Bold ellipsis>
+          {entries.length > 1
+            ? t('Move.title', { smart_count: entries.length })
+            : entries[0].name}
+        </Bold>
+        <Caption ellipsis>{t('Move.to')}</Caption>
+      </Bd>
+      <Button theme="close" onClick={onClose} extension="narrow" type="button">
+        <Icon icon="cross" width="14" height="14" color={palette['coolGrey']} />
+      </Button>
+    </Media>
+  </Well>
 )
 
 MoveHeader.propTypes = {
