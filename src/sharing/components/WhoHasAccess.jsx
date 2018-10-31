a5bd36aac6e4a23a2e5071a8121cfe0e7c124e2c
Quentin Valmori <quentin.valmori@gmail.com>
2018-10-31 12:12:52 +0100

    refacto: PureComponent 🚀

--- src/sharing/components/WhoHasAccess.jsx
+++ src/sharing/components/WhoHasAccess.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Recipient from './Recipient'
 import { SubTitle } from 'cozy-ui/react'
 
-const WhoHasAccess = (
-  { isOwner = false, recipients, document, documentType, onRevoke, className },
-  { t }
-) => (
-  <div className={className}>
-    {recipients.length > 1 && (
-      <SubTitle>
-        {t(`${documentType}.share.whoHasAccess.title`, {
-          smart_count: recipients.length
-        })}
-      </SubTitle>
-    )}
-    {recipients.map(recipient => (
-      <Recipient
-        {...recipient}
-        isOwner={isOwner}
-        document={document}
-        documentType={documentType}
-        onRevoke={onRevoke}
-      />
-    ))}
-  </div>
-)
+class WhoHasAccess extends PureComponent {
+  render() {
+    const {
+      isOwner = false,
+      recipients,
+      document,
+      documentType,
+      onRevoke,
+      className
+    } = this.props
+    const { t } = this.context
+
+    return (
+      <div className={className}>
+        {recipients.length > 1 && (
+          <SubTitle>
+            {t(`${documentType}.share.whoHasAccess.title`, {
+              smart_count: recipients.length
+            })}
+          </SubTitle>
+        )}
+        {recipients.map(recipient => (
+          <Recipient
+            {...recipient}
+            isOwner={isOwner}
+            document={document}
+            documentType={documentType}
+            onRevoke={onRevoke}
+          />
+        ))}
+      </div>
+    )
+  }
+}
 
 export default WhoHasAccess
