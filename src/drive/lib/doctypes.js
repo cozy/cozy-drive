639f08cfc23bb75aa4510c9387d4a7259f6f399c
Gara64 <paul@cozycloud.cc>
2018-11-14 12:12:09 +0100

    feat: Add photos settings doctype

--- src/drive/lib/doctypes.js
+++ src/drive/lib/doctypes.js
@@ -1,6 +1,7 @@
 export const DOCTYPE_FILES = 'io.cozy.files'
 export const DOCTYPE_CONTACTS = 'io.cozy.contacts'
 export const DOCTYPE_ALBUMS = 'io.cozy.photos.albums'
+export const DOCTYPE_PHOTOS_SETTINGS = 'io.cozy.photos.settings'
 export const DOCTYPE_APPS = 'io.cozy.apps'
 
 export const schema = {
