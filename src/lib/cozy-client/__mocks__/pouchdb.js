59455e23973b766cd28f220974bd4b5f0e83010d
Quentin Valmori <quentin.valmori@gmail.com>
2018-11-14 11:43:34 +0100

    refacto: Remove old cozy-client stuff

--- src/lib/cozy-client/__mocks__/pouchdb.js
+++
@@ -1,4 +0,0 @@
-var PouchDB = jest.fn()
-PouchDB.plugin = jest.fn()
-
-module.exports = PouchDB
--- src/lib/cozy-client/withClient.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-//eslint-disable-next-line
-const withClient = WrappedComponent => (props, context) => (
-  <WrappedComponent {...props} client={context.client} />
-)
-
-export default withClient
