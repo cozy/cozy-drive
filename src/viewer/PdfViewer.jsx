4285a1344e40a0e58937209f163f3899defbf2f2
Quentin Valmori <quentin.valmori@gmail.com>
2018-12-04 16:04:29 +0100

    fix(Viewer): We must provide a background color to PDF on Safari Mobile

--- src/viewer/PdfViewer.jsx
+++ src/viewer/PdfViewer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NoViewer from './NoViewer'
-
+import cx from 'classnames'
 import withFileUrl from './withFileUrl'
 import styles from './styles'
+import { isIOS } from 'cozy-device-helper'
 
 const PdfViewer = ({ file, url }) => (
-  <div className={styles['pho-viewer-pdfviewer']}>
+  <div
+    className={cx(styles['pho-viewer-pdfviewer'], {
+      [styles['pho-viewer-pdfviewer-ios']]: isIOS()
+    })}
+  >
     <object
       data={url}
       type="application/pdf"
