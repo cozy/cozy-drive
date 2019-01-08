ae81da3b7b43a4f41cff43710cdcabea597ec9c9
CozyKourai <julie.clement@cozycloud.cc>
2019-01-08 16:43:48 +0100

    test: Travis config and tests optimization   modified:   .gitignore 	modified:   .travis.yml   modified:   package.json 	modified:   testcafe/config.json 	new file:   testcafe/runner-drive.js 	new file:   testcafe/runner-photos.js 	modified:   testcafe/tests/helpers/roles.js 	new file:   testcafe/tests/pages/login-model.js 	modified:   testcafe/tests/pages/photos-model.js 	modified:   testcafe/tests/photos_crud.js 	modified:   yarn.lock

---
+++ testcafe/tests/tmp.js
@@ -0,0 +1,58 @@
+import { Selector, Role } from "testcafe"; //import testcafe function
+import { regularUser } from "./helpers/roles"; //import roles for login
+import config from "../config"; //import url & psswd config
+//import { generateRandomInteger } from "./helpers/utils.js";
+const _ = require("lodash");
+
+import Page from "./pages/photos-model";
+
+const page = new Page();
+
+fixture`PHOTOS - CRUD`.page`${config.photosUrl}`.beforeEach(async t => {
+  await t.useRole(regularUser);
+  await t.resizeWindow(1280, 1024); // No upload button on mobile view, beware of the size!
+
+  t.ctx.allPhotosStartCount = await page.allPhotos.count; //Pics count at test start
+  console.log("beforeEach > allPhotosStartCount " + t.ctx.allPhotosStartCount);
+});
+
+test("Uploading 1 pic from Photos view", async t => {
+  //new pic shows up
+
+  await t
+    .setFilesToUpload(page.btnUpload, ["../data/IMG0.jpg"])
+    .expect(page.divUpload.visible)
+    .ok()
+    .expect(page.modalUpload.exists)
+    .ok({ timeout: 50000 })
+    .expect(page.divUpload.child("h4").innerText)
+    .contains("Uploaded 1 out of 1 successfully");
+  await t.takeScreenshot("upload_successfull1-1.png");
+
+  const allPhotosEndCount = await page.allPhotos.count; //Pics count at the end
+  console.log("allPhotosEndCount " + allPhotosEndCount);
+  await t.expect(allPhotosEndCount).eql(t.ctx.allPhotosStartCount + 1);
+});
+
+test("Uploading 3 pcis from Photos view", async t => {
+  //new pics show up
+
+  await t
+    .setFilesToUpload(page.btnUpload, [
+      "../data/IMG-JPG.jpg",
+      "../data/IMG-PNG.png",
+      "../data/IMG-GIF.gif"
+    ])
+    .expect(page.divUpload.visible)
+    .ok()
+    .expect(page.modalUpload.exists)
+    .ok({ timeout: 50000 })
+    .expect(page.divUpload.child("h4").innerText)
+    .contains("Uploaded 3 out of 3 successfully");
+  await t.takeScreenshot("upload_successfull3-3.png");
+
+  const allPhotosEndCount = await page.allPhotos.count; //Pics count at the end
+  console.log("allPhotosEndCount " + allPhotosEndCount);
+
+  await t.expect(allPhotosEndCount).eql(t.ctx.allPhotosStartCount + 3);
+});
