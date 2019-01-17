efe79f5a4bf5d2626b5b75ec7e12c247d6f8ad55
CozyKourai <julie.clement@cozycloud.cc>
2019-01-17 15:14:18 +0100

    refactor: New timeout management
    refactor: Function in pages models
    test: Add comment in .Ok()

--- testcafe/tests/helpers/utils.js
+++ testcafe/tests/helpers/utils.js
@@ -1,7 +1,49 @@
-import { ClientFunction } from 'testcafe'
+import { ClientFunction, t } from 'testcafe'
 
 export const TESTCAFE_PHOTOS_URL = process.env.TESTCAFE_PHOTOS_URL
 export const TESTCAFE_USER_PASSWORD = process.env.TESTCAFE_USER_PASSWORD
 
 //Returns the URL of the current web page
 export const getPageUrl = ClientFunction(() => window.location.href)
+
+export function wait(timeout) {
+  //@param {number} timeout : amount of time in milliseconds during which a selector property must be stable
+  return {
+    until: selector => {
+      return {
+        // exists property of selector
+        exists: {
+          // wait until exists property of selector is stable
+          isStable: async () => {
+            const interval = 100
+            const elapsedTimeMaxValue = timeout * 3
+            let stabilityElapsedTime = 0
+            let previousValue = ''
+            let elapsedTime = 0
+            while (true) {
+              await t.wait(interval)
+              const currentValue = (await selector.exists).toString()
+              //console.log(`Selector exists property evaluates to '${currentValue}'`)
+              if (currentValue !== previousValue) {
+                stabilityElapsedTime = 0
+              }
+              if (currentValue === previousValue) {
+                stabilityElapsedTime += interval
+              }
+              previousValue = currentValue
+              elapsedTime += interval
+              if (stabilityElapsedTime > timeout) {
+                //  console.log(`Now it is safe to check if selector exists`)
+                return
+              }
+              if (elapsedTime > elapsedTimeMaxValue) {
+                console.log(`Selector is unstable`)
+                return
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- testcafe/tests/photos_crud.js
+++ testcafe/tests/photos_crud.js
@@ -1,6 +1,6 @@
 import { Selector, Role } from 'testcafe' //import testcafe function
 import { regularUser } from './helpers/roles' //import roles for login
-import { getPageUrl, TESTCAFE_PHOTOS_URL } from './helpers/utils'
+import { getPageUrl, TESTCAFE_PHOTOS_URL, wait } from './helpers/utils'
 import random from 'lodash/random'
 
 import Page from './pages/photos-model'
@@ -9,22 +9,14 @@ const page = new Page()
 
 fixture`PHOTOS - CRUD`.page`${TESTCAFE_PHOTOS_URL}/`.beforeEach(async t => {
   await t.useRole(regularUser)
-
-  await t.resizeWindow(1280, 1024) // No upload button on mobile view, beware of the size!
-
-  t.ctx.allPhotosStartCount = await page.allPhotos.count //Pics count at test start
-  console.log(
-    'Number of pictures on page (Before test): ' + t.ctx.allPhotosStartCount
-  )
+  t.ctx.allPhotosStartCount = await page.getPhotosCount('Before') //Pics count at test start
 })
 
 test('Uploading 1 pic from Photos view', async t => {
-  //new pic shows up
   await page.uploadPhotos(['../data/IMG0.jpg'])
 })
 
 test('Uploading 3 pcis from Photos view', async t => {
-  //new pics show up
   await page.uploadPhotos([
     '../data/IMG-JPG.jpg',
     '../data/IMG-PNG.png',
@@ -34,109 +26,37 @@ test('Uploading 3 pcis from Photos view', async t => {
 
 test('Select 1 pic from Photos view', async t => {
   //Selection bar shows up. It includes AddtoAlbun, Download and Delete buttons
-  await t
-    .hover(page.photoThumb(0))
-    .click(page.photoCheckbox.nth(0)) //Index
-    .expect(page.barPhoto.visible)
-    .ok()
-
-    .expect(page.barPhotoBtnAddtoalbum.visible)
-    .ok()
-    .expect(page.barPhotoBtnDl.visible)
-    .ok()
-    .expect(page.barPhotoBtnDelete.visible)
-    .ok()
-
-  // TODO - Add check on label text ??
+  await page.selectPhotos(1)
+  await page.checkPhotobar()
 })
 
 test('Select 3 pic from Photos view', async t => {
   //Selection bar shows up. It includes AddtoAlbun, Download and Delete buttons
-  await t
-    .hover(page.photoThumb(0)) //Only one 'hover' as all checkbox should be visible once the 1st checkbox is checked
-    .click(page.photoCheckbox.nth(0)) //Index
-    .click(page.photoCheckbox.nth(1)) //Index
-    .click(page.photoCheckbox.nth(2)) //Index
-    .expect(page.barPhoto.visible)
-    .ok()
-
-    .expect(page.barPhotoBtnAddtoalbum.visible)
-    .ok()
-    .expect(page.barPhotoBtnDl.visible)
-    .ok()
-    .expect(page.barPhotoBtnDelete.visible)
-    .ok()
-
-  // TODO - Add check on label text ??
+  await page.selectPhotos(3)
+  await page.checkPhotobar()
 })
 
 test('Open 1st pic', async t => {
   //Right arrow shows up. Navigatio to other pics is OK, Closing pic (X or 'esc') is Ok
+  await page.openPhotoFullscreen(0)
+  await page.navigateToNextPhoto(0)
+  await page.closePhotoFullscreenX()
 
-  await t
-    .click(page.photoThumb(0))
-    .expect(page.photoFull.visible)
-    .ok()
-    .takeScreenshot()
-
-  const photo1src = await page.photoFull.getAttribute('src')
-
-  await t
-    .expect(page.photoNavPrevious.exists)
-    .notOk() //1st photo, so previous button does not exist
-    .hover(page.photoNavNext) //1st photo, so next button should exists
-    .expect(page.photoNavNextBtn.visible)
-    .ok() //Next arrow is shown
-    .click(page.photoNavNextBtn)
-
-  const photo2src = await page.photoFull.getAttribute('src')
-  await t
-    .expect(photo1src)
-    .notEql(photo2src) //Photo has change, so src is different
-    .click(page.photoBtnClose) //Pic closed using Button
-    .expect(page.photoFull.exists)
-    .notOk()
-
-    .click(page.photoThumb(0)) //re-open for closing using 'esc'
-    .expect(page.photoFull.visible)
-    .ok()
-    .pressKey('esc') //Pic closed using 'esc'
-    .expect(page.photoFull.exists)
-    .notOk()
+  await page.openPhotoFullscreen(0)
+  await page.navigateToPrevPhoto(0)
+  await page.closePhotoFullscreenEsc()
 })
 
 test('Open Last pic', async t => {
   //Left arrow shows up. Navigatio to other pics is OK, Closing pic (X or 'esc') is Ok
 
-  await t
-    .click(page.photoThumb(t.ctx.allPhotosStartCount - 1))
-    .expect(page.photoFull.visible)
-    .ok()
+  await page.openPhotoFullscreen(t.ctx.allPhotosStartCount - 1)
+  await page.navigateToNextPhoto(t.ctx.allPhotosStartCount - 1)
+  await page.closePhotoFullscreenX()
 
-  const photo1src = await page.photoFull.getAttribute('src')
-
-  await t
-    .expect(page.photoNavNext.exists)
-    .notOk() //last photo, so next button does not exist
-    .hover(page.photoNavPrevious) //1st photo, so prev button should exists
-    .expect(page.photoNavPreviousBtn.visible)
-    .ok() //prev arrow is shown
-    .click(page.photoNavPreviousBtn)
-
-  const photo2src = await page.photoFull.getAttribute('src')
-  await t
-    .expect(photo1src)
-    .notEql(photo2src) //Photo has change, so src is different
-    .click(page.photoBtnClose) //Pic closed using Button
-    .expect(page.photoFull.exists)
-    .notOk()
-
-    .click(page.photoThumb(t.ctx.allPhotosStartCount - 1)) //re-open for closing using 'esc'
-    .expect(page.photoFull.visible)
-    .ok()
-    .pressKey('esc') //Pic closed using 'esc'
-    .expect(page.photoFull.exists)
-    .notOk()
+  await page.openPhotoFullscreen(t.ctx.allPhotosStartCount - 1)
+  await page.navigateToPrevPhoto(t.ctx.allPhotosStartCount - 1)
+  await page.closePhotoFullscreenEsc()
 })
 
 test('Open a random pic (not first nor last)', async t => {
@@ -146,85 +66,20 @@ test('Open a random pic (not first nor last)', async t => {
   const photoIndex = random(1, t.ctx.allPhotosStartCount - 2)
 
   console.log('Open random pic  > photoIndex ' + photoIndex)
+  await page.openPhotoFullscreen(photoIndex)
+  await page.navigateToNextPhoto(photoIndex)
+  await page.closePhotoFullscreenX()
 
-  //Photo cannot be the first, and cannot be the last (hence -2)
-  await t
-    .click(page.photoThumb(photoIndex))
-    .expect(page.photoFull.visible)
-    .ok()
-
-  const photo1src = await page.photoFull.getAttribute('src')
-
-  await t
-    .hover(page.photoNavNext) //photo in between, so next button should exists
-    .expect(page.photoNavNextBtn.visible)
-    .ok() //Next arrow is shown
-    .hover(page.photoNavPrevious) //photo in between, so next button should exists
-    .expect(page.photoNavPreviousBtn.visible)
-    .ok() //Next arrow is shown
-
-    .click(page.photoNavPreviousBtn) //Go to photo-1
-    .hover(page.photoNavNext) //photo-1 has a next button to go back to photo
-    .click(page.photoNavNextBtn) //Go back to photo
-    .hover(page.photoNavNext)
-    .click(page.photoNavNextBtn) //Got to photo+1
-    .hover(page.photoNavPrevious) //photo+1 has a previous button to go back to photo
-    .click(page.photoNavPreviousBtn) //Go Back to photo
-
-  const photo2src = await page.photoFull.getAttribute('src')
-  await t
-    .expect(photo1src)
-    .eql(photo2src) //In the end we are back to photo, so both src are the same
-    .click(page.photoBtnClose) //Pic closed using Button
-    .expect(page.photoFull.exists)
-    .notOk()
-
-    .click(page.photoThumb(photoIndex)) //re-open for closing using 'esc'
-    .expect(page.photoFull.visible)
-    .ok()
-    .pressKey('esc') //Pic closed using 'esc'
-    .expect(page.photoFull.exists)
-    .notOk()
+  await page.openPhotoFullscreen(photoIndex)
+  await page.navigateToPrevPhoto(photoIndex)
 })
 
 test('Deleting 1st pic in Photo view : Open up a modal, and confirm', async t => {
   //pic is removed
-  await t
-    .hover(page.photoThumb(0))
-    .click(page.photoCheckbox.nth(0)) //Index
-    .expect(page.barPhoto.visible)
-    .ok()
-
-    .click(page.barPhotoBtnDelete)
-    .expect(page.modalDelete.visible)
-    .ok()
-    .click(page.modalDeleteBtnDelete)
-  await t.takeScreenshot()
-
-  const allPhotosEndCount = await page.allPhotos.count //Pics count at the end
-  console.log('allPhotosEndCount ' + allPhotosEndCount)
-
-  await t.expect(allPhotosEndCount).eql(t.ctx.allPhotosStartCount - 1)
+  await page.deletePhotos(1)
 })
 
 test('Deleting the 1st 3 pics in Photo view : Open up a modal, and confirm', async t => {
   //pics are removed
-  await t
-    .hover(page.photoThumb(0)) //Only one 'hover' as all checkbox should be visible once the 1st checkbox is checked
-    .click(page.photoCheckbox.nth(0)) //Index
-    .click(page.photoCheckbox.nth(1)) //Index
-    .click(page.photoCheckbox.nth(2)) //Index
-    .expect(page.barPhoto.visible)
-    .ok()
-
-    .click(page.barPhotoBtnDelete)
-    .expect(page.modalDelete.visible)
-    .ok()
-    .click(page.modalDeleteBtnDelete)
-  await t.takeScreenshot()
-
-  const allPhotosEndCount = await page.allPhotos.count //Pics count at the end
-  console.log('allPhotosEndCount ' + allPhotosEndCount)
-
-  await t.expect(allPhotosEndCount).eql(t.ctx.allPhotosStartCount - 3)
+  await page.deletePhotos(3)
 })
