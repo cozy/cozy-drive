00bc169399c1334af50c43fc28686ff959e7ce22
Quentin Valmori <quentin.valmori@gmail.com>
2019-01-24 14:52:46 +0100

    feat(Onboarding): We can use COZY_FLAGS=beta-onboarding yarn build:drive:mobile to build with the beta url

--- src/authentication/src/steps/ButtonLinkRegistration.jsx
+++ src/authentication/src/steps/ButtonLinkRegistration.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getPlatform } from 'cozy-device-helper'
 import { Button } from 'cozy-ui/react'
+import flag from 'cozy-flags'
 
 import { nativeLinkOpen } from '../LinkManager'
 export const ButtonLinkRegistration = ({
@@ -10,8 +11,11 @@ export const ButtonLinkRegistration = ({
   subtle = false,
   type = 'submit'
 }) => {
-  const url = `https://manager.cozycloud.cc/cozy/create?pk_campaign=drive-${getPlatform() ||
-    'browser'}`
+  const url = flag('beta-onboarding')
+    ? `https://staging-manager.cozycloud.cc/cozy/create?domain=cozy.works?pk_campaign=drive-${getPlatform() ||
+        'browser'}`
+    : `https://manager.cozycloud.cc/cozy/create?pk_campaign=drive-${getPlatform() ||
+        'browser'}`
   return (
     <Button
       onClick={() => {
