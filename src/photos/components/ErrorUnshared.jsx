d1abbb70fc65cc9108c7263285adfe0c255c199f
Quentin Valmori <quentin.valmori@gmail.com>
2019-01-24 10:17:56 +0100

    fix(Photos): Sharing view error when link is revoked

---
+++ src/photos/components/ErrorUnshared.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ErrorShare from 'components/Error/ErrorShare'
+import { Main } from 'cozy-ui/react/Layout'
+import styles from '../targets/public/index.styl'
+import classNames from 'classnames'
+
+const ErrorUnsharedComponent = () => (
+  <div
+    className={classNames(
+      styles['pho-public-layout'],
+      styles['pho-public-layout--full'],
+      'u-pt-3'
+    )}
+  >
+    <Main className="u-pt-1-half">
+      <ErrorShare errorType={`public_album_unshared`} />
+    </Main>
+  </div>
+)
+
+export default ErrorUnsharedComponent
