5850fc63c316d49a102f4f637c8c0565aeecb2b9
Quentin Valmori <quentin.valmori@gmail.com>
2019-01-31 14:04:12 +0100

    fix(LinkCreation): Link to stating manager

--- src/authentication/src/steps/ButtonLinkRegistration.jsx
+++ src/authentication/src/steps/ButtonLinkRegistration.jsx
@@ -12,7 +12,7 @@ export const ButtonLinkRegistration = ({
   type = 'submit'
 }) => {
   const url = flag('beta-onboarding')
-    ? `https://staging-manager.cozycloud.cc/cozy/create?domain=cozy.works?pk_campaign=drive-${getPlatform() ||
+    ? `https://staging-manager.cozycloud.cc/cozy/create?domain=cozy.works&pk_campaign=drive-${getPlatform() ||
         'browser'}`
     : `https://manager.cozycloud.cc/cozy/create?pk_campaign=drive-${getPlatform() ||
         'browser'}`
