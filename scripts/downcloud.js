0a036016371e9442430374d5d09ae7543247deec
Yannick Lohse <contact@yannick-lohse.fr>
2019-02-06 11:27:22 +0100

    chore: Upload web builds to downcloud

--- scripts/downcloud.js
+++ scripts/downcloud.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs-extra')
 const path = require('path')
+const tar = require('tar')
 const { spawn } = require('child_process')
 
 const DOWNCLOUD_UPLOAD_DIR = 'www-upload/'
@@ -25,9 +26,29 @@ const launchCmd = (cmd, params, options) => {
   })
 }
 
-const pushArchive = async (fileName, parentFolder, options) => {
+const createArchive = async (folderName, archiveFileName) => {
+  console.log(`↳ ℹ️  Creating archive ${folderName}/${archiveFileName}`)
+  const fileList = await fs.readdir(folderName)
+  const options = {
+    gzip: true,
+    cwd: folderName,
+    file: folderName + '/' + archiveFileName
+  }
+  try {
+    await tar.c(options, fileList)
+  } catch (error) {
+    console.error(
+      `↳ ❌ Unable to generate app archive. Is tar installed as a dependency ? Error : ${
+        error.message
+      }`
+    )
+    throw new Error('Unable to generate archive')
+  }
+}
+
+const pushArtifact = async (fileName, parentFolder, options) => {
   const { appSlug, appVersion, buildCommit } = options
-  console.log(`↳ ℹ️  Sending archive to downcloud`)
+  console.log(`↳ ℹ️  Sending artifact to downcloud`)
   const folder = `${appSlug}/mobile/${appVersion}${
     buildCommit ? `-${buildCommit}` : ''
   }/`
@@ -67,34 +88,38 @@ const pushArchive = async (fileName, parentFolder, options) => {
   }
 }
 
+const getUploadTarget = async () => {
+  const uploadTarget = process.argv[2]
+
+  if (!fs.existsSync(uploadTarget)) {
+    throw new Error(`❌ ${uploadTarget} does not exist.`)
+  }
+  else if (fs.lstatSync(uploadTarget).isDirectory()) {
+    const appName = process.env.COZY_APP_SLUG || 'app'
+    const archiveFileName = `${appName.toLowerCase()}.tar.gz`
+    await createArchive(uploadTarget, archiveFileName)
+
+    return [uploadTarget, archiveFileName]
+  }
+  else {
+    const uploadDir = path.dirname(uploadTarget)
+    const uploadFile = path.basename(uploadTarget)
+
+    return [uploadDir, uploadFile]
+  }
+}
+
 const run = (async () => {
   try {
-    const uploadTarget = process.argv[2]
+    const [uploadDir, uploadFile] = await getUploadTarget()
+    const { version } = require('../package.json')
 
-    if (!fs.existsSync(uploadTarget)) {
-      console.error(`↳ ❌ ${uploadTarget} does not exist.`)
-      process.exit(1)
-    }
-    else {
-      if (fs.lstatSync(uploadTarget).isDirectory()) {
-        console.error(`↳ ❌ ${uploadTarget} is a directory.`)
-        //@TODO: turn the directory into an archive and upload it, see https://github.com/cozy/cozy-libs/blob/master/packages/cozy-app-publish/lib/hooks/pre/downcloud.js#L113
-        process.exit(1)
-      }
-      const uploadDir = path.dirname(uploadTarget)
-      const uploadFile = path.basename(uploadTarget)
-
-      const { version } = require('../package.json')
-
-      const { appBuildUrl } = await pushArchive(uploadFile, uploadDir, {
-        appSlug: process.env.COZY_APP_SLUG,
-        appVersion: version,
-        buildCommit: process.env.TRAVIS_COMMIT
-      })
-      console.log(`↳ ✅ Upload complete, APK available at ${appBuildUrl}`)
-
-    }
-
+    const { appBuildUrl } = await pushArtifact(uploadFile, uploadDir, {
+      appSlug: process.env.COZY_APP_SLUG,
+      appVersion: version,
+      buildCommit: process.env.TRAVIS_COMMIT
+    })
+    console.log(`↳ ✅ Upload complete, artifact available at ${appBuildUrl}`)
   }
   catch (error) {
     console.error(`↳ ❌  Error while uploading: ${error.message}`)
