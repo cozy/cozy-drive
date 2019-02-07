093420382f9333dd4455d122964efefc3dee4a1f
Yannick Lohse <contact@yannick-lohse.fr>
2019-02-07 18:35:54 +0100

    feat: Support markdown

--- src/viewer/TextViewer.jsx
+++ src/viewer/TextViewer.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
-
-import withFileUrl from './withFileUrl'
-import styles from './styles'
+import ReactMarkdown from 'react-markdown'
 import cx from 'classnames'
 import Spinner from 'cozy-ui/react/Spinner'
+import withFileUrl from './withFileUrl'
+import styles from './styles'
 import NoViewer from './NoViewer'
+
+const MarkdownRenderer = ({ text }) => (
+  <ReactMarkdown
+    className={cx(styles['pho-viewer-textviewer-content'], 'u-p-1')}
+    source={text}
+  />
+)
+
+const PlainTextRenderer = ({ text }) => (
+  <pre
+    className={cx(
+      styles['pho-viewer-textviewer-content'],
+      'u-mh-auto',
+      'u-mv-2'
+    )}
+  >
+    {text}
+  </pre>
+)
+
+const Loader = () => (
+  <div className={styles['pho-viewer-textviewer']}>
+    <Spinner size="xxlarge" middle noMargin color="white" />
+  </div>
+)
 
 class TextViewer extends React.Component {
   state = {
     text: '',
+    isMarkdown: false,
     loading: true,
     error: null
   }
@@ -18,17 +44,21 @@ class TextViewer extends React.Component {
   }
 
   async loadFile() {
-    const { url } = this.props
+    const { url, file } = this.props
     try {
       const parsedURL = new URL(url)
       const client = this.context.client.getClient()
       const response = await client.fetch('GET', parsedURL.pathname)
       const text = await response.text()
+      const isMarkdown = file.mime === 'text/markdown' || /.md$/.test(file.name)
+
       this.setState({
         text,
+        isMarkdown,
         loading: false
       })
     } catch (error) {
+      console.warn(error)
       this.setState({
         loading: false,
         error
@@ -37,15 +67,10 @@ class TextViewer extends React.Component {
   }
 
   render() {
-    const { loading, error, text } = this.state
+    const { loading, error, text, isMarkdown } = this.state
     const { url, file } = this.props
 
-    if (loading)
-      return (
-        <div className={styles['pho-viewer-textviewer']}>
-          <Spinner size="xxlarge" middle noMargin color="white" />
-        </div>
-      )
+    if (loading) return <Loader />
     else if (error) return <NoViewer file={file} fallbackUrl={url} />
     else
       return (
@@ -53,7 +78,11 @@ class TextViewer extends React.Component {
           <h2 className={cx(styles['pho-viewer-filename'], 'u-mt-2', 'u-mb-1')}>
             {file.name}
           </h2>
-          <pre className="u-mh-auto u-mv-2">{text}</pre>
+          {isMarkdown ? (
+            <MarkdownRenderer text={text} />
+          ) : (
+            <PlainTextRenderer text={text} />
+          )}
         </div>
       )
   }
