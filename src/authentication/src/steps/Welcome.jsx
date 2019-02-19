7441cf4cb3b836909f3b0bdd6f9114e6a12c4519
Yannick Lohse <contact@yannick-lohse.fr>
2019-02-19 12:16:11 +0100

    fix: Upgrade cozy-ui

--- src/authentication/src/steps/Welcome.jsx
+++ src/authentication/src/steps/Welcome.jsx
@@ -43,7 +43,13 @@ export class Welcome extends Component {
       appIcon
     } = this.props
     return (
-      <div className={classNames(styles['wizard'], styles['wizard--welcome'])}>
+      <div
+        className={classNames(
+          styles['wizard'],
+          styles['wizard--welcome'],
+          styles['wizard-wrapper--center']
+        )}
+      >
         <div className={styles['wizard-wrapper']}>
           <div className={styles['wizard-main']}>
             <div className={styles['wizard-logo']}>
