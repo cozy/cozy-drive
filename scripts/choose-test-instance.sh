15e69ec95d154903a2d11576af548cd0b4ccbc90
CozyKourai <julie.clement@cozycloud.cc>
2019-02-22 14:43:14 +0100

    fix(travis) : change modulo to manage 0

--- scripts/choose-test-instance.sh
+++ scripts/choose-test-instance.sh
@@ -4,7 +4,7 @@ set -eu
 INSTANCE_TESTCAFE="None"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
- let INSTANCE_NUM=$TRAVIS_PULL_REQUEST%4;
+ INSTANCE_NUM=$(($TRAVIS_PULL_REQUEST%4));
  INSTANCE_TESTCAFE="testcafe"$INSTANCE_NUM".cozy.rocks";
 fi
 echo "export INSTANCE_TESTCAFE=$INSTANCE_TESTCAFE" > setinstance.sh
