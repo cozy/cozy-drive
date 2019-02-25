b52cb9317fc68902bd4776ea5eee61812cb3f76a
Bruno Michel <bmichel@menfin.info>
2019-02-25 13:52:31 +0100

    feat: Link the mobile and web Drive applications (#1465)
    See https://docs.cozy.io/en/cozy-stack/auth/#linked-applications

--- src/drive/mobile/lib/constants.js
+++ src/drive/mobile/lib/constants.js
@@ -1,3 +1,4 @@
-export const SOFTWARE_ID = 'io.cozy.drive.mobile'
+export const SOFTWARE_ID = 'registry://drive'
 export const SOFTWARE_NAME = 'Cozy Drive'
 export const APP_STORE_ID = '1224102389'
+export const APP_MARKET_ID = 'market://details?id=io.cozy.drive.mobile'
