04c0969b929af4238555e747c3a77099cbbaaff0
Quentin Valmori <quentin.valmori@gmail.com>
2019-02-26 17:56:46 +0100

    refactor: Some cleaning

--- src/drive/mobile/modules/authorization/sagas.js
+++ src/drive/mobile/modules/authorization/sagas.js
@@ -18,36 +18,8 @@ export const saveCredentials = (client, token) => dispatch => {
 export const renewAuthorization = client => async dispatch => {
   const url = client.options.uri
   const { infos, token } = await client.renewAuthorization(url)
-  await restoreCozyClientJs(url, infos, token)
+  restoreCozyClientJs(url, infos, token)
   updateBarAccessToken(token)
   dispatch(setUrl(url))
   dispatch(saveCredentials(infos, token))
 }
-/*
-export const setOauthClient = (client) => dispatch => {
-  oauthClient.setOAuthOptions(realOauthOptions)
-    oauthClient.setCredentials(token)
-    await restoreCozyClientJs(client.options.uri, realOauthOptions, token)
-    oauthClient.onTokenRefresh = token => {
-      updateBarAccessToken(token.accessToken)
-      restoreCozyClientJs(client.options.uri, realOauthOptions, token)
-      store.dispatch(setToken(token))
-    }
-    await oauthClient.fetchInformation()
-}
-
-const clientInfos = getClientSettings(store.getState())
-    console.log('clientInfo index.js', clientInfos)
-    
-    realOauthOptions =
-      clientInfos !== null ? { ...clientInfos, ...getOauthOptions() } : null
-    const token = getToken(store.getState())
-    const stackClient = client.getStackClient()
-    stackClient.setOAuthOptions(realOauthOptions)
-    stackClient.setCredentials(token)
-    await restoreCozyClientJs(client.options.uri, realOauthOptions, token)
-    stackClient.onTokenRefresh = token => {
-      updateBarAccessToken(token.accessToken)
-      restoreCozyClientJs(client.options.uri, realOauthOptions, token)
-      store.dispatch(setToken(token))
-    }*/
