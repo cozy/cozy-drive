35108e31832d39d049d71a6c83cd9bd6db117ee7
Yannick Lohse <contact@yannick-lohse.fr>
2019-03-07 12:35:28 +0100

    feat: Update unlink process

--- src/drive/mobile/modules/settings/components/Unlink.jsx
+++ src/drive/mobile/modules/settings/components/Unlink.jsx
@@ -3,28 +3,14 @@ import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import { translate } from 'cozy-ui/react/I18n'
 import SettingCategory, { ELEMENT_BUTTON } from './SettingCategory'
-import Modal from 'cozy-ui/react/Modal'
 import {
   unlink,
   getClientSettings
 } from 'drive/mobile/modules/authorization/duck'
 
 class Unlink extends Component {
-  state = {
-    showConfirmation: false
-  }
-
-  showConfirmation = () => {
-    this.setState({ showConfirmation: true })
-  }
-
-  hideConfirmation = () => {
-    this.setState({ showConfirmation: false })
-  }
-
   render() {
     const { t, unlink, clientSettings } = this.props
-    const { showConfirmation } = this.state
     const { client } = this.context
     return (
       <div>
@@ -36,22 +22,10 @@ class Unlink extends Component {
               description: t('mobile.settings.unlink.description'),
               text: t('mobile.settings.unlink.button'),
               theme: 'danger-outline',
-              onClick: this.showConfirmation
+              onClick: () => unlink(client, clientSettings)
             }
           ]}
         />
-        {showConfirmation && (
-          <Modal
-            title={t('mobile.settings.unlink.confirmation.title')}
-            description={t('mobile.settings.unlink.confirmation.description')}
-            secondaryType="secondary"
-            secondaryText={t('mobile.settings.unlink.confirmation.cancel')}
-            secondaryAction={this.hideConfirmation}
-            primaryType="danger"
-            primaryText={t('mobile.settings.unlink.confirmation.unlink')}
-            primaryAction={() => unlink(client, clientSettings)}
-          />
-        )}
       </div>
     )
   }
