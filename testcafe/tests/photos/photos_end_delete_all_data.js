696c9d8e3c909441f685bd6969d3cc0f95e62fc0
CozyKourai <julie.clement@cozycloud.cc>
2019-03-13 11:20:38 +0100

    fix: merge conflicts

--- testcafe/tests/photos/photos_end_delete_all_data.js
+++ testcafe/tests/photos/photos_end_delete_all_data.js
@@ -1,4 +1,4 @@
-import { photosUser } from '../helpers/roles' 
+import { photosUser } from '../helpers/roles'
 import { TESTCAFE_PHOTOS_URL } from '../helpers/utils'
 import Page from '../pages/photos-model'
 import { IMG0, IMG1, IMG2, IMG3, IMG4 } from '../helpers/data'
@@ -23,7 +23,7 @@ fixture`Delete all photos`.page`${TESTCAFE_PHOTOS_URL}/`
     await ctx.vr.checkVr()
   })
 
-test('Deleting 1st pic on Timeline : Open up a modal, and confirm', async () => {
+test('Deleting 1st pic on Timeline : Open up a modal, and confirm', async t => {
   await page.selectPhotosByName([IMG0])
   //pic is removed
   await page.deletePhotos(1)
@@ -31,7 +31,7 @@ test('Deleting 1st pic on Timeline : Open up a modal, and confirm', async () => 
   await t.fixtureCtx.vr.takeScreenshotAndReview('delete-1-pic.png')
 })
 
-test('Deleting 4 pics on Timeline : Open up a modal, and confirm', async () => {
+test('Deleting 4 pics on Timeline : Open up a modal, and confirm', async t => {
   await page.selectPhotosByName([IMG1, IMG2, IMG3, IMG4])
   //pics are removed, there are no more pictures on  page
   await page.deletePhotos(4, true)
