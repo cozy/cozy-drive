5a87fd1d46849f90a69bd68c9879c6cab203a44e
Yannick Lohse <contact@yannick-lohse.fr>
2019-03-26 10:25:41 +0000

    fix: Return to previous app after deep link

--- src/drive/mobile/lib/handleDeepLink.js
+++ src/drive/mobile/lib/handleDeepLink.js
@@ -5,7 +5,7 @@ import { setOnboarding } from '../modules/authorization/duck'
 export const handleDeeplink = (history, store, url) => {
   const stripped = url.replace(RX, '')
 
-  history.push('/' + stripped)
+  history.replace('/' + stripped)
   if (stripped.includes('auth?')) {
     const currentLocation = history.getCurrentLocation()
     const { code, state, cozy_url } = currentLocation.query
