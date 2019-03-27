3b5a81649eb9edb9b7a4da48f0e3c3dc7fe05454
Yannick Lohse <contact@yannick-lohse.fr>
2019-03-27 10:58:33 +0000

    refactor: Using icon components when possible

--- src/viewer/NoNetworkViewer.jsx
+++ src/viewer/NoNetworkViewer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Button } from 'cozy-ui/react/Button'
+import { Button, Icon } from 'cozy-ui/transpiled/react'
 import { translate } from 'cozy-ui/react/I18n'
 
 import styles from './styles'
+import IconCloud from './icons/icon-cloud-wrong.svg'
 
 const NoNetworkViewer = ({ t, onReload }) => (
   <div className={styles['pho-viewer-canceled']}>
+    <Icon icon={IconCloud} width={160} height={140} />
     <h2>{t('Viewer.loading.error')}</h2>
     <Button onClick={onReload} label={t('Viewer.loading.retry')} />
   </div>
