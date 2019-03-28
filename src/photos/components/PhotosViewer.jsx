52d5159fe045087104cdc04210a2104b8630bfdb
Yannick Lohse <contact@yannick-lohse.fr>
2019-03-28 13:54:54 +0000

    fix: Photo viewer with overlay
    [APK]

--- src/photos/components/PhotosViewer.jsx
+++ src/photos/components/PhotosViewer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import { Overlay } from 'cozy-ui/transpiled/react'
 import Viewer from 'viewer'
 
 const getParentPath = router => {
@@ -12,23 +13,25 @@ class PhotosViewer extends Component {
     const { photos, params, router } = this.props
     const currentIndex = photos.findIndex(p => p.id === params.photoId)
     return (
-      <Viewer
-        files={photos}
-        currentIndex={currentIndex}
-        onChange={nextPhoto =>
-          router.push({
-            pathname: `${getParentPath(router)}/${nextPhoto.id}`,
-            query: router.location.query
-          })
-        }
-        onClose={() =>
-          router.push({
-            pathname: getParentPath(router),
-            query: router.location.query
-          })
-        }
-        {...this.props}
-      />
+      <Overlay>
+        <Viewer
+          files={photos}
+          currentIndex={currentIndex}
+          onChange={nextPhoto =>
+            router.push({
+              pathname: `${getParentPath(router)}/${nextPhoto.id}`,
+              query: router.location.query
+            })
+          }
+          onClose={() =>
+            router.push({
+              pathname: getParentPath(router),
+              query: router.location.query
+            })
+          }
+          {...this.props}
+        />
+      </Overlay>
     )
   }
 }
