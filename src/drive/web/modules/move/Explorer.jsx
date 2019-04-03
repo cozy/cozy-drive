7855549b1b51f0a93928256d60744f7a7e1498a7
CozyKourai <julie.clement@cozycloud.cc>
2019-04-03 16:57:02 +0200

    refactor(drive): add data-test-id for testcafe

--- src/drive/web/modules/move/Explorer.jsx
+++ src/drive/web/modules/move/Explorer.jsx
@@ -6,7 +6,12 @@ import fileListStyles from 'drive/styles/filelist'
 
 const Explorer = ({ children }) => (
   <FileList canSort={false} fileActions={null} withSelectionCheckbox={false}>
-    <div className={fileListStyles['fil-content-body']}>{children}</div>
+    <div
+      data-test-id="fil-content-modal"
+      className={fileListStyles['fil-content-body']}
+    >
+      {children}
+    </div>
   </FileList>
 )
 
