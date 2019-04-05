669deab930f57752a076cc06a0bc3c888784f1a7
Quentin Valmori <quentin.valmori@gmail.com>
2019-04-05 10:40:25 +0200

    fix: Better Personalization

--- src/components/Button/CozyHomeLink.jsx
+++ src/components/Button/CozyHomeLink.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import { ButtonLink } from 'cozy-ui/react'
 import styles from 'components/Button/index.styl'
 import getHomeLinkHref from 'components/Button/getHomeLinkHref'
-
+import CozyHomeLinkIcon from 'components/Button/CozyHomeLinkIcon'
 const CozyHomeLink = ({ from, embedInCozyBar = false, t, size, className }) => (
   <ButtonLink
     label={t('Share.create-cozy')}
-    icon="cozy-negative"
+    icon={CozyHomeLinkIcon}
     className={embedInCozyBar ? styles['bar-homelink'] : className}
     href={getHomeLinkHref(from)}
     size={size}
