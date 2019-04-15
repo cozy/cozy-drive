516701eabf0e77e13016bcf3a1b5615ec15f6ae0
CozyKourai <julie.clement@cozycloud.cc>
2019-04-15 17:29:58 +0200

    refactor: navigateToPrevPhoto to navigateToPrevFile (also for "next")

--- testcafe/tests/photos/photos_crud.js
+++ testcafe/tests/photos/photos_crud.js
@@ -35,12 +35,12 @@ test('Open 1st pic', async () => {
   console.group('↳ ℹ️  Open 1st pic')
   //Right arrow shows up. Navigatio to other pics is OK, Closing pic (X or 'esc') is Ok
   await photoViewer.openPhotoFullscreen(0)
-  await photoViewer.navigateToNextPhoto(0)
+  await photoViewer.navigateToNextFile(0)
   await photoViewer.closeViewer({
     exitWithEsc: true
   })
   await photoViewer.openPhotoFullscreen(0)
-  await photoViewer.navigateToPrevPhoto(0)
+  await photoViewer.navigateToPrevFile(0)
   await photoViewer.closeViewer({
     exitWithEsc: false
   })
@@ -50,13 +50,13 @@ test('Open 1st pic', async () => {
 test('Open Last pic', async t => {
   console.group('↳ ℹ️  Open Last pic')
   //Left arrow shows up. Navigatio to other pics is OK, Closing pic (X or 'esc') is Ok
-  await photoViewer.openPhotoFullscreen(t.ctx.allPhotosStartCount - 1)
-  await photoViewer.navigateToNextPhoto(t.ctx.allPhotosStartCount - 1)
+  await photoViewer.openPhotoFullscreen(t.ctx.totalFilesCount - 1)
+  await photoViewer.navigateToNextFile(t.ctx.totalFilesCount - 1)
   await photoViewer.closeViewer({
     exitWithEsc: true
   })
-  await photoViewer.openPhotoFullscreen(t.ctx.allPhotosStartCount - 1)
-  await photoViewer.navigateToPrevPhoto(t.ctx.allPhotosStartCount - 1)
+  await photoViewer.openPhotoFullscreen(t.ctx.totalFilesCount - 1)
+  await photoViewer.navigateToPrevFile(t.ctx.totalFilesCount - 1)
   await photoViewer.closeViewer({
     exitWithEsc: false
   })
@@ -67,16 +67,16 @@ test('Open a random pic (not first nor last)', async t => {
   console.group('↳ ℹ️  Open a random pic (not first nor last)')
   //Both arrows show up. Navigatio to other pics is OK, Closing pic (X or 'esc') is Ok
   // We need at least 3 pics in our cozy for this test to pass
-  const photoIndex = random(1, t.ctx.allPhotosStartCount - 2)
+  const photoIndex = random(1, t.ctx.totalFilesCount - 2)
 
   console.log('Open random pic  > photoIndex ' + photoIndex)
   await photoViewer.openPhotoFullscreen(photoIndex)
-  await photoViewer.navigateToNextPhoto(photoIndex)
+  await photoViewer.navigateToNextFile(photoIndex)
   await photoViewer.closeViewer({
     exitWithEsc: true
   })
   await photoViewer.openPhotoFullscreen(photoIndex)
-  await photoViewer.navigateToPrevPhoto(photoIndex)
+  await photoViewer.navigateToPrevFile(photoIndex)
   await photoViewer.closeViewer({
     exitWithEsc: false
   })
