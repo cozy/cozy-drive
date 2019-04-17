656a709b2f7bbec7b473161044aea77b0dba542c
CozyKourai <julie.clement@cozycloud.cc>
2019-04-17 10:18:42 +0200

    fix: remove --disable-dev-shm-usage as it didnt fix the clientfunction bug

--- testcafe/runner-drive.js
+++ testcafe/runner-drive.js
@@ -27,9 +27,7 @@ async function runRunner() {
       'testcafe/tests/drive/public-viewer-feature.js'
     ])
     //emulation:cdpPort=9222 is used to set the download folder in headless mode
-    .browsers([
-      'chrome:headless:emulation:cdpPort=9222 --start-maximized --disable-dev-shm-usage'
-    ])
+    .browsers(['chrome:headless:emulation:cdpPort=9222 --start-maximized'])
 
     .screenshots(
       'reports/',
--- testcafe/runner-photos.js
+++ testcafe/runner-photos.js
@@ -28,9 +28,7 @@ async function runRunner() {
       'testcafe/tests/photos/photos_end_delete_all_data.js'
     ])
     //emulation:cdpPort=9222 is used to set the download folder in headless mode
-    .browsers([
-      'chrome:headless:emulation:cdpPort=9222 --start-maximized --disable-dev-shm-usage'
-    ])
+    .browsers(['chrome:headless:emulation:cdpPort=9222 --start-maximized'])
 
     .screenshots(
       'reports/',
