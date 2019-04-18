f4817f1fa43cae25300322f656b4dbdee98cc1b5
CozyKourai <julie.clement@cozycloud.cc>
2019-04-18 14:47:22 +0200

    fix: remove toast from screenshots

---
+++ testcafe/tests/pages/commons.js
@@ -0,0 +1,17 @@
+import { t } from 'testcafe'
+import * as selectors from './selectors'
+import { isExistingAndVisibile } from '../helpers/utils'
+
+//this files contains function that can be use both in photos and drive
+
+//Check that the alerte toast exist, and wait for it to disappear
+export async function checkToastAppearsAndDisappears(toastText) {
+  await t
+  isExistingAndVisibile(
+    selectors.alertWrapper.withText(toastText),
+    `Toast Alert`
+  )
+  await t
+    .expect(selectors.alertWrapper.withText(toastText).exists)
+    .notOk('Toast still exists')
+}
