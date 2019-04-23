7002befeea62dd262426f03c9599e3caf355ef8d
Quentin Valmori <quentin.valmori@gmail.com>
2019-04-23 19:02:31 +0200

    feat: Redirect universalLink to handleOpenUrl

--- src/drive/mobile/lib/handleDeepLink.js
+++ src/drive/mobile/lib/handleDeepLink.js
@@ -1,10 +1,10 @@
 const PROTOCOL = 'cozydrive://'
 const RX = new RegExp('^' + PROTOCOL)
+const RX2 = new RegExp('^' + 'http://universal-link.cozycloud.cc/drive/')
 import { setOnboarding } from '../modules/authorization/duck'
 
 export const handleDeeplink = (history, store, url) => {
-  const stripped = url.replace(RX, '')
-
+  const stripped = url.replace(RX, '').replace(RX2, '')
   history.replace('/' + stripped)
   if (stripped.includes('auth?')) {
     const currentLocation = history.getCurrentLocation()
