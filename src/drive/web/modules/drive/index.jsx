ad8aa9c7e1ea8ad4a7d589ab09af1dbe49a998f5
Yannick Lohse <contact@yannick-lohse.fr>
2019-04-25 14:21:12 +0200

    refactor: Moved FileOpenerExternal component

--- src/drive/web/modules/drive/index.jsx
+++ src/drive/web/modules/drive/index.jsx
@@ -1,4 +1,3 @@
-export { default as FileOpenerExternal } from './FileOpenerExternal.jsx'
 export { default as FolderContainer } from './FolderContainer.jsx'
 export { default as RecentContainer } from './RecentContainer.jsx'
 export { default as SharingsContainer } from './SharingsContainer.jsx'
--- src/drive/web/modules/navigation/AppRoute.jsx
+++ src/drive/web/modules/navigation/AppRoute.jsx
@@ -8,11 +8,11 @@ import OnBoarding from 'drive/mobile/modules/onboarding/OnBoarding'
 import Layout from 'drive/web/modules/layout/Layout'
 import FileExplorer from './FileExplorer'
 import FilesViewer from 'drive/web/modules/viewer/FilesViewer'
+import FileOpenerExternal from 'drive/web/modules/viewer/FileOpenerExternal'
 
 import {
   FolderContainer as Folder,
   RecentContainer as Recent,
-  FileOpenerExternal,
   SharingsContainer as Sharings
 } from 'drive/web/modules/drive'
 import { Container as Trash } from 'drive/web/modules/trash'
