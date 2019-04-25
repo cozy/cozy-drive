4647505ade99a036dc7af24f9cd1fa49761a3340
Quentin Valmori <quentin.valmori@gmail.com>
2019-04-25 16:31:05 +0200

    tmp

--- src/drive/mobile/lib/handleDeepLink.js
+++ src/drive/mobile/lib/handleDeepLink.js
@@ -2,28 +2,13 @@ const PROTOCOL = 'cozydrive://'
 const UNIVERSALLINK_URL = 'https://universal-link.cozycloud.cc/'
 const CustomSchemeRegex = new RegExp('^' + PROTOCOL)
 const UniversalLinkRegex = new RegExp(`^${UNIVERSALLINK_URL}drive/`)
+require('url-polyfill')
 import { setOnboarding } from '../modules/authorization/duck'
 
 export const handleDeeplink = (history, store, url) => {
-  /*
-  let's remove redirect params. It's only needed for universal link
-  if we arrive at this stage, we don't need it anymore
-  */
-  let route = new URL(url)
-
-  if (route.searchParams.has('redirect')) {
-    route.searchParams.delete('redirect')
-  }
-  const stripped = route.toString().replace(RX, '')
-  let stripped = ''
-  if (url.startsWith('http')) {
-    stripped = url.replace(UniversalLinkRegex, '')
-  } else {
-    stripped = url.replace(CustomSchemeRegex, '')
-  }
-
-  history.replace('/' + stripped)
-  if (stripped.includes('auth?')) {
+  const path = generateRoute(url)
+  history.replace('/' + path)
+  if (path.includes('auth?')) {
     const currentLocation = history.getCurrentLocation()
     const { code, state, cozy_url } = currentLocation.query
     store.dispatch(
@@ -34,4 +19,20 @@ export const handleDeeplink = (history, store, url) => {
       })
     )
   }
+}
+
+const generateRoute = url => {
+  const urlObj = new URL(url)
+  //remove fallback params
+  if (urlObj.searchParams.has('fallback')) {
+    urlObj.searchParams.delete('fallback')
+  }
+  let newUrl = urlObj.toString()
+  let path = ''
+  if (newUrl.startsWith('http')) {
+    path = newUrl.replace(UniversalLinkRegex, '')
+  } else {
+    path = newUrl.replace(CustomSchemeRegex, '')
+  }
+  return path
 }
