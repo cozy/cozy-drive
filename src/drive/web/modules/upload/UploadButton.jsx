7e5274be469ec8a8c7cc3d7134df0c94b40a5eb3
Yannick Lohse <contact@yannick-lohse.fr>
2019-04-25 14:21:12 +0200

    fix: Upload button takes whole width

--- src/drive/web/modules/upload/UploadButton.jsx
+++ src/drive/web/modules/upload/UploadButton.jsx
@@ -24,7 +24,7 @@ const UploadButton = ({ label, disabled, onUpload, className }) => (
     className={className}
     style={styles.parent}
   >
-    <span>
+    <span className="u-flex u-flex-items-center">
       <Icon icon="upload" />
       <span>{label}</span>
       <input
