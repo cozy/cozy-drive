831aa8cafa713ed96112b2cce0c13add8571d7b2
CozyKourai <julie.clement@cozycloud.cc>
2019-04-29 14:12:51 +0200

    fix: change tests order to avoid screenshots changes

--- testcafe/runner-photos.js
+++ testcafe/runner-photos.js
@@ -20,9 +20,9 @@ async function runRunner() {
       'testcafe/tests/photos/photos_start_upload_photos.js',
 
       'testcafe/tests/photos/photos_crud.js',
-      'testcafe/tests/photos/create_full_album_scenario.js',
-      'testcafe/tests/photos/create_empty_album_scenario.js',
       'testcafe/tests/photos/album_sharing_scenario.js',
+      'testcafe/tests/photos/create_empty_album_scenario.js',
+      'testcafe/tests/photos/create_full_album_scenario.js',
 
       //Scenario that just delete photos, so we don't need to do it in every test.
       'testcafe/tests/photos/photos_end_delete_all_data.js'
