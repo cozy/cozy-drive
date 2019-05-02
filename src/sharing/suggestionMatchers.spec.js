0576f4b183f2c49090cc56a3e24f221c977f8bae
Cédric Messiant <cedric.messiant@gmail.com>
2019-05-02 16:19:17 +0200

    fix: cozyUrlMatch and add a test

--- src/sharing/suggestionMatchers.spec.js
+++ src/sharing/suggestionMatchers.spec.js
@@ -1,6 +1,49 @@
 import * as matchers from './suggestionMatchers'
 
 describe('Suggestion matchers', () => {
+  describe('cozyUrlMatch', () => {
+    it('should return true if cozy url matches', () => {
+      const contact = {
+        fullname: 'Jon Snow',
+        cozy: [
+          {
+            url: 'https://jon.mycozy.cloud'
+          }
+        ]
+      }
+      const result = matchers.cozyUrlMatch('https://jon.mycozy.cloud', contact)
+      expect(result).toBe(true)
+    })
+
+    it("should return false if cozy url doesn't match", () => {
+      const contact = {
+        fullname: 'Jon Snow',
+        cozy: [
+          {
+            url: 'https://jon.mycozy.cloud'
+          }
+        ]
+      }
+      const result = matchers.emailMatch(
+        'https://jonsnow.mycozy.cloud',
+        contact
+      )
+      expect(result).toBe(false)
+    })
+
+    it('should return false if contact has no cozy', () => {
+      const contact = {
+        fullname: 'Jon Snow',
+        cozy: []
+      }
+      const result = matchers.emailMatch(
+        'https://jonsnow.mycozy.cloud',
+        contact
+      )
+      expect(result).toBe(false)
+    })
+  })
+
   describe('emailMatch', () => {
     it('should return true if one email matches', () => {
       const contact = {
