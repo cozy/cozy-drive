5cd022d3772a54f7586c32d11ae3b791b812fcea
Cédric Messiant <cedric.messiant@gmail.com>
2019-05-06 17:44:04 +0200

    feat: Use contact's doctype version

--- src/drive/lib/doctypes.js
+++ src/drive/lib/doctypes.js
@@ -4,10 +4,13 @@ export const DOCTYPE_FILES = 'io.cozy.files'
 export const DOCTYPE_ALBUMS = 'io.cozy.photos.albums'
 export const DOCTYPE_PHOTOS_SETTINGS = 'io.cozy.photos.settings'
 export const DOCTYPE_APPS = 'io.cozy.apps'
-const DOCTYPE_CONTACTS_VERSION = 2
+export const DOCTYPE_CONTACTS_VERSION = 2
 
 export const schema = {
   files: { doctype: DOCTYPE_FILES },
-  contacts: { doctype: Contact.doctype },
+  contacts: {
+    doctype: Contact.doctype,
+    doctypeVersion: DOCTYPE_CONTACTS_VERSION
+  },
   groups: { doctype: Group.doctype }
 }
