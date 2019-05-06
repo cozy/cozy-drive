607831e8cc3487c903ebd425342a86bfe55e16e0
Cédric Messiant <cedric.messiant@gmail.com>
2019-05-06 16:40:21 +0200

    feat: Add appMetadata and contacts doctype version to CozyClient

--- src/drive/lib/doctypes.js
+++ src/drive/lib/doctypes.js
@@ -4,6 +4,7 @@ export const DOCTYPE_FILES = 'io.cozy.files'
 export const DOCTYPE_ALBUMS = 'io.cozy.photos.albums'
 export const DOCTYPE_PHOTOS_SETTINGS = 'io.cozy.photos.settings'
 export const DOCTYPE_APPS = 'io.cozy.apps'
+const DOCTYPE_CONTACTS_VERSION = 2
 
 export const schema = {
   files: { doctype: DOCTYPE_FILES },
