496e35e58373bf15e59db1804ea55cd81a8b3885
CozyKourai <julie.clement@cozycloud.cc>
2019-05-09 11:47:56 +0200

    chore: Add timestamp in testcafe log

--- testcafe/tests/helpers/logger.js
+++ testcafe/tests/helpers/logger.js
@@ -1,3 +1,6 @@
+require('log-prefix')(function() {
+  return '%s [' + new Date().toTimeString() + ']'
+})
 const logger = require('minilog')('testcafe')
 require('minilog').enable()
 require('minilog').suggest.deny('testcafe', 'debug')
