9b9d68fcd230def33888c787003b3d850d2fa689
Cédric Messiant <cedric.messiant@gmail.com>
2019-05-16 15:02:42 +0200

    feat: Put handle conflicts feature under a flag

---
+++ src/lib/flags.js
@@ -0,0 +1,22 @@
+import flag from 'cozy-flags'
+
+export const initFlags = () => {
+  let activateFlags = flag('switcher') === true ? true : false
+  if (process.env.NODE_ENV !== 'production' && flag('switcher') === null) {
+    activateFlags = true
+  }
+
+  const searchParams = new URL(window.location).searchParams
+  if (!activateFlags && searchParams.get('flags') !== null) {
+    activateFlags = true
+  }
+
+  if (activateFlags) {
+    flagsList()
+  }
+}
+
+const flagsList = () => {
+  flag('switcher', true)
+  flag('handle-conflicts', false)
+}
