1437776f50418703fca0b9ffeb31d17084cfdcb5
Quentin Valmori <quentin.valmori@gmail.com>
2019-05-20 19:10:01 +0200

    feat: Add __APP_SLUG__ global var

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,8 +1,10 @@
+/* global __APP_SLUG__ */
 import minilog from 'minilog'
 
-const logger = minilog('cozy-drive')
+const logger = minilog(`cozy-${__APP_SLUG__}`)
 minilog.enable()
 
-minilog.suggest.allow('cozy-drive', 'log')
-minilog.suggest.allow('cozy-drive', 'info')
+minilog.suggest.allow(`cozy-${__APP_SLUG__}`, 'log')
+minilog.suggest.allow(`cozy-${__APP_SLUG__}`, 'info')
+
 export default logger
