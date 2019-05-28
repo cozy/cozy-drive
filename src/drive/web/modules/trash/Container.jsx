44748ed318bd5dd29d98bdd104a48b94e5610e87
Quentin Valmori <quentin.valmori@gmail.com>
2019-05-28 11:20:20 +0200

    feat: Trash as its own openFolder action

--- src/drive/web/modules/trash/Container.jsx
+++ src/drive/web/modules/trash/Container.jsx
@@ -7,8 +7,14 @@ import FolderView from 'drive/web/modules/layout/FolderView'
 import DestroyConfirm from './components/DestroyConfirm'
 import Toolbar from './Toolbar'
 
-import { restoreFiles, destroyFiles } from './actions'
-
+import {
+  restoreFiles,
+  destroyFiles,
+  OPEN_FOLDER_FROM_TRASH,
+  OPEN_FOLDER_FROM_TRASH_SUCCESS,
+  OPEN_FOLDER_FROM_TRASH_FAILURE
+} from './actions'
+import { openFolder, getFolderUrl } from 'drive/web/modules/navigation/duck'
 //!TODO WTF? inital state ?
 const mapStateToProps = () => ({
   isTrashContext: true,
@@ -36,8 +42,29 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
           )
       }
     }
-  })
+  }),
+  onFolderOpen: folderId => {
+    dispatch(
+      openFolder(
+        folderId,
+        OPEN_FOLDER_FROM_TRASH,
+        OPEN_FOLDER_FROM_TRASH_SUCCESS,
+        OPEN_FOLDER_FROM_TRASH_FAILURE
+      )
+    )
+    ownProps.router.push(getFolderUrl(folderId, ownProps.location))
+  }
 })
+
+/*
+
+navigateToFolder = async folderId => {
+    await this.props.fetchFolder(folderId)
+    this.props.router.push(getFolderUrl(folderId, this.props.location))
+  }
+  fetchFolder: folderId => dispatch(openFolder(folderId)),
+
+*/
 
 export default translate()(
   connect(
