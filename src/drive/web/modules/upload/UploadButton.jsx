8dc4ba24f986e3278fd837f27b6eef4c56626eb1
Cédric Messiant <cedric.messiant@gmail.com>
2019-05-31 17:40:53 +0200

    feat: Use FileInput from cozy-ui

--- src/drive/web/modules/upload/UploadButton.jsx
+++ src/drive/web/modules/upload/UploadButton.jsx
@@ -2,51 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { Icon } from 'cozy-ui/react'
+import { Icon, FileInput } from 'cozy-ui/react'
 
 import withSharingState from 'sharing/hoc/withSharingState'
 import { uploadFiles } from 'drive/web/modules/navigation/duck'
 
-const styles = {
-  parent: {
-    position: 'relative',
-    overflow: 'hidden'
-  },
-  input: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    opacity: 0,
-    width: '100%',
-    height: '100%',
-    zIndex: 1
-  }
-}
-
 const UploadButton = ({ label, disabled, onUpload, className }) => (
-  <label
-    role="button"
-    disabled={disabled}
+  <FileInput
     className={className}
-    style={styles.parent}
+    data-test-id="upload-btn"
+    label={label}
+    disabled={disabled}
+    multiple
+    onChange={onUpload}
   >
     <span className="u-flex u-flex-items-center">
       <Icon icon="upload" />
       <span>{label}</span>
-      <input
-        data-test-id="upload-btn"
-        type="file"
-        multiple
-        style={styles.input}
-        disabled={disabled}
-        onChange={e => {
-          if (e.target.files) {
-            onUpload(Array.from(e.target.files))
-          }
-        }}
-      />
     </span>
-  </label>
+  </FileInput>
 )
 
 UploadButton.propTypes = {
