28aa4d77fd3f8bb2165f8b00dd7f001b21c8e690
Quentin Valmori <quentin.valmori@gmail.com>
2019-06-11 13:59:42 +0200

    refactor: Remove unsued global pouch

--- webpack/mobile.config.js
+++ webpack/mobile.config.js
@@ -1,13 +1,7 @@
-const { ProvidePlugin } = require('webpack')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   plugins: [
-    new ProvidePlugin({
-      PouchDB: 'pouchdb',
-      pouchdbFind: 'pouchdb-find',
-      pouchdbAdapterCordovaSqlite: 'pouchdb-adapter-cordova-sqlite'
-    }),
     new CopyPlugin([
       {
         from: 'src/drive/mobile/assets/fonts/',
