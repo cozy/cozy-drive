b4a2a6ee4714c4e8bf0f33eee177fb4922f8f379
Gara64 <paul@cozycloud.cc>
2019-09-06 12:16:58 +0200

    test: mock pickInstance

--- src/photos/targets/services/onPhotoUpload.spec.js
+++ src/photos/targets/services/onPhotoUpload.spec.js
@@ -1,6 +1,9 @@
 import { onPhotoUpload } from './onPhotoUpload'
 import { readSetting } from 'photos/ducks/clustering/settings'
-import { convertDurationInMilliseconds } from 'photos/ducks/clustering/utils'
+import {
+  convertDurationInMilliseconds,
+  pickInstance
+} from 'photos/ducks/clustering/utils'
 import CozyClient from 'cozy-client'
 
 CozyClient.fromEnv = jest.fn()
@@ -8,7 +11,8 @@ jest.mock('photos/ducks/clustering/settings', () => ({
   readSetting: jest.fn()
 }))
 jest.mock('photos/ducks/clustering/utils', () => ({
-  convertDurationInMilliseconds: jest.fn()
+  convertDurationInMilliseconds: jest.fn(),
+  pickInstance: jest.fn()
 }))
 jest.mock('cozy-client')
 const client = new CozyClient({})
@@ -17,6 +21,7 @@ client.save = jest.fn(() => Promise.resolve({ data: {} }))
 describe('onPhotoUpload', () => {
   beforeEach(() => {
     jest.spyOn(Date, 'now').mockImplementation(() => 1000)
+    pickInstance.mockReturnValue(true)
   })
   afterEach(() => {
     jest.restoreAllMocks()
