661fab9a449e66baec4855164d06168096d8ba7e
Quentin Valmori <quentin.valmori@gmail.com>
2019-09-17 13:57:43 +0200

    refactor: Simpler API

--- src/drive/web/modules/drive/Toolbar/components/PortaledQueue.jsx
+++ src/drive/web/modules/drive/Toolbar/components/PortaledQueue.jsx
@@ -3,39 +3,16 @@ import ReactDOM from 'react-dom'
 import { UploadQueue } from 'drive/web/modules/upload/UploadQueue'
 import { translate } from 'cozy-ui/transpiled/react'
 class PortaledQueue extends Component {
-  state = {
-    display: true
-  }
-  /**
-   *
-   * This is the only way to re-display the Queue
-   * after a scann if it was previously manually hidden
-   */
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (
-      nextProps.doneCount !== nextProps.successCount &&
-      prevState.display === false
-    ) {
-      return {
-        display: true
-      }
-    }
-    return prevState
-  }
-
   render() {
-    const { file, doneCount, successCount, t } = this.props
-    const { display } = this.state
-    if (!display && doneCount === successCount) return null
+    const { file, doneCount, successCount, t, onClear } = this.props
+
     return ReactDOM.createPortal(
       <UploadQueue
         queue={file ? [file] : []}
         doneCount={doneCount}
         successCount={successCount}
         t={t}
-        purgeQueue={() => {
-          this.setState({ display: false })
-        }}
+        purgeQueue={onClear}
       />,
       document.querySelector('[role=application]')
     )
