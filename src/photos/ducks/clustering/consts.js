686e436e42f9e8dcdc8311b276d453bdf0daec87
Gara64 <paul@cozycloud.cc>
2019-09-30 10:31:45 +0200

    feat: Increase percent of instances having clustering

--- src/photos/ducks/clustering/consts.js
+++ src/photos/ducks/clustering/consts.js
@@ -12,7 +12,7 @@ export const COARSE_COEFFICIENT = 1
 export const EVALUATION_THRESHOLD = 500
 export const CHANGES_RUN_LIMIT = 1000
 export const TRIGGER_ELAPSED = '20m'
-export const PERCENT_INSTANCES = 25
+export const PERCENT_INSTANCES = 66
 
 export const DEFAULT_SETTING = {
   type: SETTING_TYPE,
