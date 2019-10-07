0e8eef0fac23f2c5a604e262e491d7b087cbec24
Quentin Valmori <quentin.valmori@gmail.com>
2019-10-07 11:48:55 +0200

    fix: Better UX on desktop

--- src/drive/web/modules/drive/Toolbar/components/NewModal.jsx
+++ src/drive/web/modules/drive/Toolbar/components/NewModal.jsx
@@ -8,7 +8,7 @@ import Modal, {
 } from 'cozy-ui/react/Modal'
 import { Button, Icon } from 'cozy-ui/react'
 import { translate } from 'cozy-ui/react/I18n'
-
+import withBreakpoints from 'cozy-ui/react/helpers/withBreakpoints'
 import classNames from 'classnames'
 import styles from './styles.styl'
 
@@ -22,26 +22,44 @@ class NewModal extends Component {
       t,
       secondaryText,
       secondaryAction,
-      description
+      description,
+      breakpoints: { isMobile }
     } = this.props
     //!TODO Move this new kind of Modal to UI
     return (
-      <Modal mobileFullscreen closable={false}>
-        <ModalHeader className={classNames(styles['modal-header'])}>
-          <h2>{title}</h2>
-          <Button
-            icon={<Icon icon={'cross'} size={'16'} />}
-            onClick={() => dismissAction()}
-            iconOnly
-            label={t('close')}
-            subtle
-            theme={'secondary'}
-          />
-        </ModalHeader>
-        <ModalContent className="u-flex-grow-1 u-ph-1">
+      <Modal
+        mobileFullscreen
+        closable={isMobile ? false : true}
+        size="large"
+        title={isMobile ? '' : title}
+        dismissAction={dismissAction}
+      >
+        {isMobile && (
+          <ModalHeader className={classNames(styles['modal-header'])}>
+            <h2>{title}</h2>
+
+            <Button
+              icon={<Icon icon={'cross'} size={'16'} />}
+              onClick={() => dismissAction()}
+              iconOnly
+              label={t('close')}
+              subtle
+              theme={'secondary'}
+            />
+          </ModalHeader>
+        )}
+        <ModalContent
+          className={classNames({
+            ['u-flex-grow-1 u-ph-1']: isMobile
+          })}
+        >
           {description}
         </ModalContent>
-        <ModalFooter className={classNames(styles['modal-footer'])}>
+        <ModalFooter
+          className={classNames({
+            [styles['modal-footer']]: isMobile
+          })}
+        >
           <ModalButtons
             primaryText={primaryText}
             primaryAction={primaryAction}
@@ -56,4 +74,4 @@ class NewModal extends Component {
   }
 }
 
-export default translate()(NewModal)
+export default withBreakpoints()(translate()(NewModal))
