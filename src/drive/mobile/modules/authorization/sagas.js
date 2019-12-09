38c5268511ea88aa4285cc14b70f573a7fde9230
Quentin Valmori <quentin.valmori@gmail.com>
2019-12-09 14:59:20 +0100

    refactor: Rm code again

--- src/drive/mobile/modules/authorization/sagas.js
+++ src/drive/mobile/modules/authorization/sagas.js
@@ -1,5 +1,4 @@
-import { restoreCozyClientJs } from 'drive/mobile/lib/cozy-helper'
-import { setUrl, setOffline } from 'drive/mobile/modules/settings/duck'
+import { setOffline } from 'drive/mobile/modules/settings/duck'
 import { startReplication } from 'drive/mobile/modules/replication/sagas'
 import { setClient, setToken } from './duck'
 
@@ -10,12 +9,4 @@ export const saveCredentials = (client, token) => dispatch => {
   // TODO: not ideal to startReplication here, we should be explicit when we start it
   // but we need to restart it when renewing authorization (see below)
   dispatch(startReplication())
-}
-
-export const renewAuthorization = client => async dispatch => {
-  const url = client.getStackClient().uri
-  const { infos, token } = await client.renewAuthorization(url)
-  restoreCozyClientJs(url, infos, token)
-  dispatch(setUrl(url))
-  dispatch(saveCredentials(infos, token))
 }
