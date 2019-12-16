c0485d53d5623ac0b6acf1bdb51c8c96449a6213
Quentin Valmori <quentin.valmori@gmail.com>
2019-12-16 12:59:09 +0100

    feat: Add isNote helper

--- src/drive/web/modules/drive/files.js
+++ src/drive/web/modules/drive/files.js
@@ -1,8 +1,25 @@
+//!TODO Put this to cozy-client/modiles/files
 const FILE_TYPE = 'file'
 const DIR_TYPE = 'directory'
 export const ALBUMS_DOCTYPE = 'io.cozy.photos.albums'
 export const isFile = file => file && file.type === FILE_TYPE
 export const isDirectory = file => file && file.type === DIR_TYPE
+export const isNote = file => {
+  if (
+    file &&
+    file.name &&
+    file.name.endsWith('.cozy-note') &&
+    file.type === FILE_TYPE &&
+    file.metadata &&
+    file.metadata.content &&
+    file.metadata.schema &&
+    file.metadata.title &&
+    file.metadata.version
+  )
+    return true
+  return false
+}
+
 export const isReferencedByAlbum = file => {
   if (
     file.relationships &&
