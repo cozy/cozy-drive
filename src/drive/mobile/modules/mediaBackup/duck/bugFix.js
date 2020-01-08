7adeddc69b4650fcf2f12bd15c531fa3c37498ac
Quentin Valmori <quentin.valmori@gmail.com>
2020-01-08 16:20:04 +0100

    fix: WIP bugFix

--- src/drive/mobile/modules/mediaBackup/duck/bugFix.js
+++ src/drive/mobile/modules/mediaBackup/duck/bugFix.js
@@ -1,206 +1,92 @@
 import { getTranslateFunction } from 'drive/mobile/lib/i18n'
-import {
-  getReferencedFolders,
-  REF_PHOTOS,
-  getOrCreateFolderWithReference
-} from 'folder-references'
-import { CozyFile } from 'models'
+import { REF_PHOTOS, getOrCreateFolderWithReference } from 'folder-references'
+import { ROOT_DIR_ID } from 'drive/constants/config'
+import { handleConflictOnMove } from './handleConflictOnMove'
+/**
+- eventuellement créer le dossier photos
+- supprimer /photos/t(backup) si vide
+- renommage dossier backup
+- déplacer t(backup) folder avec gestion conflits
+- supprimer la ref de media_folder
+- mettre la ref sur photosFolder
+- on supprime pas l'ancien media_foldr au cas ou il rete des choses dedans
+*/
 
 export const fixMagicFolderName = async (client, savedFromMyDeviceFolder) => {
   //LET'S FIX the bug introduced in 1.18.18. Used in 1.18.24 release
-  const t = getTranslateFunction()
   if (
-    savedFromMyDeviceFolder.name ===
+    savedFromMyDeviceFolder.name !==
     'mobile.settings.media_backup.backup_folder'
-  ) {
-    const photosRootPath = `/${t('mobile.settings.media_backup.media_folder')}`
-    const photosRootPathWithoutTranslation =
-      '/mobile.settings.media_backup.media_folder'
-    const newNameForSavedFromMyDevice = t(
-      'mobile.settings.media_backup.backup_folder'
-    )
-    let photosRootFolder
-    try {
-      photosRootFolder = await client
-        .collection('io.cozy.files')
-        .statByPath(photosRootPath)
-    } catch (e) {}
-    //If `/Photos` doesn't exist, we rename folders and that it
-    if (!photosRootFolder) {
+  )
+    return null // early exit if nothing to do
+
+  const t = getTranslateFunction()
+  const photosRootPath = `/${t('mobile.settings.media_backup.media_folder')}`
+  const photosRootPathWithoutTranslation =
+    '/mobile.settings.media_backup.media_folder'
+  const newNameForSavedFromMyDevice = t(
+    'mobile.settings.media_backup.backup_folder'
+  )
+  const newNamePhotoFolder = t('mobile.settings.media_backup.media_folder')
+
+  let photosRootFolder
+  try {
+    photosRootFolder = await client
+      .collection('io.cozy.files')
+      .statByPath(photosRootPath)
+  } catch (e) {
+    // marche pas
+    photosRootFolder = await client
+      .collection('io.cozy.files')
+      .createDirectory({ dirId: ROOT_DIR_ID, name: newNamePhotoFolder })
+  }
+
+  try {
+    const pathSavedFromMobileFolder = `${photosRootPath}/${newNameForSavedFromMyDevice}`
+    const folderSyncPhoto = await client
+      .collection('io.cozy.files')
+      .statByPath(pathSavedFromMobileFolder)
+    console.log('folderSyncPhoto', folderSyncPhoto)
+
+    const hasEmptyRightFolder =
+      folderSyncPhoto.included && folderSyncPhoto.included.length === 0
+        ? true
+        : false
+
+    if (hasEmptyRightFolder) {
       await client
         .collection('io.cozy.files')
-        .updateAttributes(savedFromMyDeviceFolder.id, {
-          name: newNameForSavedFromMyDevice
-        })
-
-      const photoBackupFolder = await getReferencedFolders(client, REF_PHOTOS)
-      const newNamePhotoBackupFolder = t(
-        'mobile.settings.media_backup.media_folder'
-      )
-      //If referenced folder exist, let's rename it
-      if (
-        photoBackupFolder &&
-        photoBackupFolder[0] &&
-        photoBackupFolder[0].name ===
-          'mobile.settings.media_backup.media_folder'
-      ) {
-        await renameFolder(
-          client,
-          photoBackupFolder[0],
-          newNamePhotoBackupFolder
-        )
-        //Else we check if  folder if /mobile.settings.media_backup.media_folder exist and rename it
-      } else {
-        try {
-          const photosRootFolderWithoutTranslation = await client
-            .collection('io.cozy.files')
-            .statByPath(photosRootPathWithoutTranslation)
-          if (
-            photosRootFolderWithoutTranslation.data &&
-            photosRootFolderWithoutTranslation.data.id
-          ) {
-            await client
-              .collection('io.cozy.files')
-              .updateAttributes(photosRootFolderWithoutTranslation.data.id, {
-                name: newNamePhotoBackupFolder
-              })
-          }
-        } catch (e) {
-          console.log('error', e)
-          //I don't know what to do here
-        }
-      }
-    } else {
-      const pathSavedFromMobileFolder = `/${t(
-        'mobile.settings.media_backup.media_folder'
-      )}/${t('mobile.settings.media_backup.backup_folder')}`
-      let folderSyncPhoto
-      try {
-        folderSyncPhoto = await client
-          .collection('io.cozy.files')
-          .statByPath(pathSavedFromMobileFolder)
-      } catch (e) {
-        //
-      }
-
-      const hasAlreadyPhotoInRightFolder =
-        folderSyncPhoto &&
-        folderSyncPhoto.included &&
-        folderSyncPhoto.included.length > 0
-          ? true
-          : false
-      const newCreatedFromMyDeviceFolder = await client
-        .collection('io.cozy.files')
-        .updateAttributes(savedFromMyDeviceFolder.id, {
-          name: newNameForSavedFromMyDevice
-        })
-      if (!hasAlreadyPhotoInRightFolder) {
-        //We delete the previous folder at the right path, we rename the referenced folder
-        //we move the referenced folder
-        if (folderSyncPhoto) {
-          await client
-            .collection('io.cozy.files')
-            .deleteFilePermanently(folderSyncPhoto.data.id)
-        }
-
-        try {
-          await CozyFile.move(savedFromMyDeviceFolder.id, {
-            folderId: photosRootFolder.data.id
-          })
-        } catch (e) {
-          console.log('move err', e)
-        }
-      } else {
-        try {
-          await handleConflictOnMove(
-            client,
-            newCreatedFromMyDeviceFolder.data,
-            photosRootFolder,
-            newNameForSavedFromMyDevice
-          )
-        } catch (e) {
-          console.log('e', e)
-        }
-      }
-      //to be sure that /Photos is referenced
-      if (photosRootFolder) {
-        await getOrCreateFolderWithReference(
-          client,
-          `${photosRootPath}`,
-          REF_PHOTOS
-        )
-        try {
-          let oldphotosRootFolder = await client
-            .collection('io.cozy.files')
-            .statByPath('/mobile.settings.media_backup.media_folder')
-          await client
-            .collection('io.cozy.files')
-            .destroy(oldphotosRootFolder.data)
-        } catch (e) {}
-      }
+        .deleteFilePermanently(folderSyncPhoto.data.id)
     }
-  }
-  //END OF BUG FIX
-}
-
-//Fix bug for 1.18.18 release. Used for 1.18.24 release
-const generateNewFolderNameOnConflict = folderName => {
-  //Check if the string ends by _1
-  const regex = new RegExp('(_)([0-9]+)$')
-  const matches = folderName.match(regex)
-  if (matches) {
-    let versionNumber = parseInt(matches[2])
-    //increment versionNumber
-    versionNumber++
-    const newfolderName = folderName.replace(
-      new RegExp('(_)([0-9]+)$'),
-      `_${versionNumber}`
-    )
-    return newfolderName
-  } else {
-    return `${folderName}_1`
-  }
-}
-
-const renameFolder = async (client, folderToRename, newName) => {
-  try {
-    return await client
-      .collection('io.cozy.files')
-      .updateAttributes(folderToRename.id, { name: newName })
   } catch (e) {
-    if (/Conflict/.test(e.message)) {
-      const newNameWithNum = generateNewFolderNameOnConflict(newName)
-      return renameFolder(client, folderToRename, newNameWithNum)
-    }
-    throw e
+    console.warn('error', e)
   }
-}
 
-const handleConflictOnMove = async (
-  client,
-  savedFromMyDeviceFolder,
-  photosFolder,
-  newName
-) => {
-  try {
-    return await CozyFile.move(savedFromMyDeviceFolder.id, {
-      folderId: photosFolder.data.id
+  const newCreatedFromMyDeviceFolder = await client
+    .collection('io.cozy.files')
+    .updateAttributes(savedFromMyDeviceFolder.id, {
+      name: newNameForSavedFromMyDevice
     })
+
+  await handleConflictOnMove(
+    client,
+    newCreatedFromMyDeviceFolder.data,
+    photosRootFolder,
+    newNameForSavedFromMyDevice
+  )
+
+  try {
+    const oldMediaFolder = await client
+      .collection('io.cozy.files')
+      .statByPath(photosRootPathWithoutTranslation)
+    await client
+      .collection('io.cozy.files')
+      .removeReferencedBy(oldMediaFolder.data, [
+        { _type: 'io.cozy.apps', _id: REF_PHOTOS }
+      ])
   } catch (e) {
-    if (/Conflict/.test(e.message)) {
-      const newNameWithNum = generateNewFolderNameOnConflict(newName)
-      const newFolder = await renameFolder(
-        client,
-        savedFromMyDeviceFolder,
-        newNameWithNum
-      )
-      return handleConflictOnMove(
-        client,
-        newFolder,
-        photosFolder,
-        newNameWithNum
-      )
-    }
-    throw e
+    console.log('error stat remove ref', e)
   }
+
+  await getOrCreateFolderWithReference(client, photosRootPath, REF_PHOTOS)
 }
