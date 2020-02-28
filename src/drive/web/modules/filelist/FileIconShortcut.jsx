fbaee793c35b340ae5eac6a0576e491046b6aeca
Quentin Valmori <quentin.valmori@gmail.com>
2020-02-28 13:04:25 +0100

    feat: Use new StackIcon

--- src/drive/web/modules/filelist/FileIconShortcut.jsx
+++ src/drive/web/modules/filelist/FileIconShortcut.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
 import { withClient } from 'cozy-client'
 import useFetchShortcut from './useFetchShortcut'
-import Badge from 'cozy-ui/transpiled/react/Badge'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconStack from 'cozy-ui/transpiled/react/IconStack'
+import palette from 'cozy-ui/transpiled/react/palette'
+
 const FileIconShortcut = ({ file, size, client }) => {
-  const { fetchStatus, shortcutInfos, shortcurtImg } = useFetchShortcut(
-    client,
-    file
-  )
+  const { shortcurtImg, shouldDisplayImg } = useFetchShortcut(client, file)
 
   return (
-    <Badge content={<Icon icon="share" />} type="normal">
-      <img src={shortcurtImg} width={size} height={size} />
-    </Badge>
+    <IconStack
+      align="bottom-right"
+      background={
+        <>
+          <div style={{ display: shouldDisplayImg ? 'block' : 'none' }}>
+            <img src={shortcurtImg} width={size} height={size} />
+          </div>
+          <div
+            style={{
+              display: !shouldDisplayImg ? 'block' : 'none'
+            }}
+          >
+            <Icon icon="globe" size={size} color={palette.coolGrey} />
+          </div>
+        </>
+      }
+      foreground={
+        <div
+          style={{
+            borderRadius: '12px',
+            width: '20px',
+            height: '20px',
+            border: 'solid 1px var(--silver)',
+            backgroundColor: '#ffffff',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            boxSizing: 'border-box'
+          }}
+        >
+          <Icon icon="link" height={10} width={10} />
+        </div>
+      }
+    />
   )
 }
 
