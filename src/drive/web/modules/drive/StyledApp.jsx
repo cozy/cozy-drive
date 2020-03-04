6119966935c7a12d9446edc35bc78e9752e15454
Quentin Valmori <quentin.valmori@gmail.com>
2020-03-04 07:51:56 +0100

    feat: Use now Dialog and add MuiCozyTheme

--- src/drive/web/modules/drive/StyledApp.jsx
+++ src/drive/web/modules/drive/StyledApp.jsx
@@ -1,7 +1,10 @@
+import React from 'react'
 import 'cozy-ui/transpiled/react/stylesheet.css'
 import 'cozy-sharing/dist/stylesheet.css'
 //eslint-disable-next-line
 import mainStyles from 'drive/styles/main.styl'
-
-const StyleApp = ({ children }) => children
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
+const StyleApp = ({ children }) => {
+  return <MuiCozyTheme>{children}</MuiCozyTheme>
+}
 export default StyleApp
