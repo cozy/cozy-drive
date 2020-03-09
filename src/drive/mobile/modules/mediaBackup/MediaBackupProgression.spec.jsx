abc10a6269a08dd47e7f1574984c32db548a860d
Yannick Lohse <contact@yannick-lohse.fr>
2020-03-09 17:49:05 +0100

    test: Fix failing test

--- src/drive/mobile/modules/mediaBackup/MediaBackupProgression.spec.jsx
+++ src/drive/mobile/modules/mediaBackup/MediaBackupProgression.spec.jsx
@@ -1,42 +1,10 @@
 import React from 'react'
-import configureStore from 'redux-mock-store'
 import { shallow } from 'enzyme'
-import { Provider } from 'react-redux'
-import { I18n } from 'cozy-ui/transpiled/react/I18n'
-const locales = { helloworld: 'Hello World !' }
+import { UploadStatus } from './MediaBackupProgression'
 
-const render = () => {
-  const MediaBackupProgression = require('./MediaBackupProgression')
-    .connectedUploadStatus
-  const wrapperProvider = ({ children }) => (
-    <Provider store={configureStore()({})}>
-      <I18n lang={'en'} dictRequire={() => locales}>
-        {children}
-      </I18n>
-    </Provider>
-  )
-
-  const wrapper = shallow(<MediaBackupProgression t={x => x} />, {
-    wrappingComponent: wrapperProvider
-  })
-  // we need to dive into the connect()ed and the translate()ed wrappers
-  return wrapper.dive()
-}
-
-const mockSelectors = (providedSelectors = {}) => {
-  const { isImagesBackupOn = () => true, ...selectors } = providedSelectors
-  jest.doMock('drive/mobile/modules/settings/duck', () => ({
-    isImagesBackupOn,
-    getServerUrl: () => 'http://cozy.tools:8080'
-  }))
-  jest.doMock('./duck/reducer', () => ({
-    isPreparingBackup: () => false,
-    isUploading: () => false,
-    isAborted: () => false,
-    isQuotaReached: () => false,
-    getUploadStatus: () => ({}),
-    ...selectors
-  }))
+const render = props => {
+  const wrapper = shallow(<UploadStatus t={x => x} {...props} />)
+  return wrapper
 }
 
 describe('MediaBackupProgression', () => {
@@ -45,30 +13,27 @@ describe('MediaBackupProgression', () => {
   })
 
   it('should not render anything when the backup is disabled', () => {
-    mockSelectors({
-      isImagesBackupOn: () => false
-    })
-    expect(render().type()).toBe(null)
+    expect(render({ isEnabled: false }).type()).toBe(null)
   })
 
   it('should render stg when the backup is in preparation', () => {
-    mockSelectors({
-      isPreparingBackup: () => true
-    })
-    expect(render().find('UploadPreparing')).toHaveLength(1)
+    expect(
+      render({ isEnabled: true, isPreparing: true }).find('UploadPreparing')
+    ).toHaveLength(1)
   })
 
   it('should render the progression when uploading', () => {
-    mockSelectors({
-      isUploading: () => true,
-      getUploadStatus: () => ({ current: 5, total: 10 })
+    const comp = render({
+      isEnabled: true,
+      isUploading: true,
+      current: 5,
+      total: 10
     })
-    expect(render().find('UploadProgression')).toHaveLength(1)
-    expect(render().find({ current: 5, total: 10 })).toHaveLength(1)
+    expect(comp.find('UploadProgression')).toHaveLength(1)
+    expect(comp.find({ current: 5, total: 10 })).toHaveLength(1)
   })
 
   it('should render stg when uptodate', () => {
-    mockSelectors()
-    expect(render().find('UploadUptodate')).toHaveLength(1)
+    expect(render({ isEnabled: true }).find('UploadUptodate')).toHaveLength(1)
   })
 })
