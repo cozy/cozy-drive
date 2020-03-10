5ffae514ab674b33121402818e0c236b061eadac
Quentin Valmori <quentin.valmori@gmail.com>
2020-03-10 16:55:03 +0100

    feat: Display an error message if something went wrong during the redirection

--- src/drive/web/modules/navigation/ExternalRedirect.jsx
+++ src/drive/web/modules/navigation/ExternalRedirect.jsx
@@ -3,9 +3,11 @@ import { useClient, useFetchShortcut } from 'cozy-client'
 import Empty from 'cozy-ui/transpiled/react/Empty'
 import IconSprite from 'cozy-ui/transpiled/react/Icon/Sprite'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
+import EmptyIcon from 'assets/icons/icon-folder-broken.svg'
+
 const ExternalRedirect = ({ params: { fileId }, t }) => {
   const client = useClient()
-  const { shortcutInfos } = useFetchShortcut(client, fileId)
+  const { shortcutInfos, fetchStatus } = useFetchShortcut(client, fileId)
   if (shortcutInfos) {
     window.location.href = shortcutInfos.data.attributes.url
   }
@@ -13,12 +15,22 @@ const ExternalRedirect = ({ params: { fileId }, t }) => {
   return (
     <>
       <IconSprite />
-      <Empty
-        data-test-id="empty-share"
-        icon={'globe'}
-        title={t('External.redirection.title')}
-        text={t('External.redirection.text')}
-      />
+      {fetchStatus === 'failed' && (
+        <Empty
+          data-test-id="empty-share"
+          icon={EmptyIcon}
+          title={t('External.redirection.title')}
+          text={t('External.redirection.error')}
+        />
+      )}
+      {fetchStatus !== 'failed' && (
+        <Empty
+          data-test-id="empty-share"
+          icon={'globe'}
+          title={t('External.redirection.title')}
+          text={t('External.redirection.text')}
+        />
+      )}
     </>
   )
 }
