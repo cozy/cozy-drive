403133158ccdbd6ec2e88d2e94e8b1d40fad4633
Yannick Lohse <contact@yannick-lohse.fr>
2020-03-10 14:30:06 +0100

    feat: Added SharingProvider

--- src/drive/web/modules/views/DriveView.jsx
+++ src/drive/web/modules/views/DriveView.jsx
@@ -1,6 +1,7 @@
 /* global __TARGET__ */
 import React, { useCallback } from 'react'
 import { useQuery, Q } from 'cozy-client'
+import SharingProvider from 'cozy-sharing'
 
 import Breadcrumb from 'drive/web/modules/navigation/Breadcrumb'
 import SelectionBar from 'drive/web/modules/selection/SelectionBar'
@@ -54,60 +55,62 @@ const DriveView = ({ params, router }) => {
   })
 
   return (
-    <Main>
-      <Topbar>
-        {false && <Breadcrumb onFolderOpen={null} />}
-        {false && (
-          <Toolbar
-            folderId={null}
-            canUpload={true}
-            canCreateFolder={true}
-            disabled={false}
-          />
-        )}
-      </Topbar>
-      <Dropzone
-        role="main"
-        disabled={__TARGET__ === 'mobile'}
-        displayedFolder={null}
-      >
-        {false && <SelectionBar actions={[]} />}
-        <FileListv2>
-          <MobileFileListHeader canSort={true} />
-          <FileListHeader canSort={true} />
-          <FileListBodyV2 selectionModeActive={false}>
-            <AddFolder />
-            {fetchStatus === 'loading' && <FileListRowsPlaceholder />}
-            {fetchStatus === 'error' && <Oops />}
-            {fetchStatus === 'loaded' &&
-              data.length === 0 && <EmptyDrive canUpload={true} />}
-            {fetchStatus === 'loaded' &&
-              data.length > 0 && (
-                <div className={isMobileApp() ? 'u-ov-hidden' : ''}>
-                  {data.map(file => (
-                    <File
-                      key={file._id}
-                      attributes={file}
-                      displayedFolder={null}
-                      actions={{}}
-                      isRenaming={false}
-                      onFolderOpen={navigateToFolder}
-                      onFileOpen={navigateToFile}
-                      withSelectionCheckbox={true}
-                      withFilePath={false}
-                      withSharedBadge={true}
-                      isFlatDomain={true}
-                    />
-                  ))}
-                  {hasMore && (
-                    <LoadMore onClick={fetchMore} isLoading={false} />
-                  )}
-                </div>
-              )}
-          </FileListBodyV2>
-        </FileListv2>
-      </Dropzone>
-    </Main>
+    <SharingProvider doctype="io.cozy.files" documentType="Files">
+      <Main>
+        <Topbar>
+          {false && <Breadcrumb onFolderOpen={null} />}
+          {false && (
+            <Toolbar
+              folderId={null}
+              canUpload={true}
+              canCreateFolder={true}
+              disabled={false}
+            />
+          )}
+        </Topbar>
+        <Dropzone
+          role="main"
+          disabled={__TARGET__ === 'mobile'}
+          displayedFolder={null}
+        >
+          {false && <SelectionBar actions={[]} />}
+          <FileListv2>
+            <MobileFileListHeader canSort={true} />
+            <FileListHeader canSort={true} />
+            <FileListBodyV2 selectionModeActive={false}>
+              <AddFolder />
+              {fetchStatus === 'loading' && <FileListRowsPlaceholder />}
+              {fetchStatus === 'error' && <Oops />}
+              {fetchStatus === 'loaded' &&
+                data.length === 0 && <EmptyDrive canUpload={true} />}
+              {fetchStatus === 'loaded' &&
+                data.length > 0 && (
+                  <div className={isMobileApp() ? 'u-ov-hidden' : ''}>
+                    {data.map(file => (
+                      <File
+                        key={file._id}
+                        attributes={file}
+                        displayedFolder={null}
+                        actions={{}}
+                        isRenaming={false}
+                        onFolderOpen={navigateToFolder}
+                        onFileOpen={navigateToFile}
+                        withSelectionCheckbox={true}
+                        withFilePath={false}
+                        withSharedBadge={true}
+                        isFlatDomain={true}
+                      />
+                    ))}
+                    {hasMore && (
+                      <LoadMore onClick={fetchMore} isLoading={false} />
+                    )}
+                  </div>
+                )}
+            </FileListBodyV2>
+          </FileListv2>
+        </Dropzone>
+      </Main>
+    </SharingProvider>
   )
 }
 
