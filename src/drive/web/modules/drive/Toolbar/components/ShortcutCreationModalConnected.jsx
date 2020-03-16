36a8c8aaa5adac6e9a555d2ca2f71e8f7265de8a
Quentin Valmori <quentin.valmori@gmail.com>
2020-03-16 11:39:18 +0100

    feat: Add shortcut creation

---
+++ src/drive/web/modules/drive/Toolbar/components/ShortcutCreationModalConnected.jsx
@@ -0,0 +1,4 @@
+import toolbarContainer from 'drive/web/modules/drive/Toolbar/toolbar'
+import ShortcutCreationModal from './ShortcutCreationModal'
+
+export default toolbarContainer(ShortcutCreationModal)
