1eaa9c5613739efa0d36eb030912d72aebff4e93
Yannick Lohse <contact@yannick-lohse.fr>
2020-03-18 17:22:19 +0100

    feat: Added a ModalContext

---
+++ src/drive/lib/ModalContext.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useCallback, useContext } from 'react'
+
+export const ModalContext = React.createContext()
+
+export const ModalContextProvider = ({ children }) => {
+  const [modalStack, setModalStack] = useState([])
+  const pushModal = useCallback(modal => {
+    modalStack.push(modal)
+    setModalStack([...modalStack])
+  })
+  const popModal = useCallback(() => {
+    modalStack.pop()
+    setModalStack([...modalStack])
+  })
+
+  return (
+    <ModalContext.Provider value={{ modalStack, pushModal, popModal }}>
+      {children}
+    </ModalContext.Provider>
+  )
+}
+
+export const ModalStack = () => {
+  const { modalStack } = useContext(ModalContext)
+
+  if (modalStack.length === 0) return null
+  else return modalStack[modalStack.length - 1]
+}
