8dbda12bea7f485c6a30c15cedc34ebf5c5e0cdd
Quentin Valmori <quentin.valmori@gmail.com>
2020-04-24 10:00:30 +0200

    test: Better mock (render children) for BarRight

--- jestHelpers/setup.js
+++ jestHelpers/setup.js
@@ -1,4 +1,12 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+global.cozy = {
+  bar: {
+    BarLeft: () => null,
+    BarRight: ({ children }) => children,
+    BarCenter: () => null,
+    setTheme: () => null
+  }
+}
 
 Enzyme.configure({ adapter: new Adapter() })
