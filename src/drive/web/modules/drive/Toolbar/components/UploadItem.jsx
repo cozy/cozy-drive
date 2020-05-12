d6ed0dfea75dbf3320e9f890d11295363dc46dcc
Yannick Lohse <contact@yannick-lohse.fr>
2020-05-12 09:33:32 +0200

    feat: Reload public view after upload

--- src/drive/web/modules/drive/Toolbar/components/UploadItem.jsx
+++ src/drive/web/modules/drive/Toolbar/components/UploadItem.jsx
@@ -30,9 +30,12 @@ const UploadItem = ({ t, isDisabled, onUpload }) => (
   </FileInput>
 )
 
-const mapDispatchToProps = (dispatch, { displayedFolder, sharingState }) => ({
+const mapDispatchToProps = (
+  dispatch,
+  { displayedFolder, sharingState, onUploaded }
+) => ({
   onUpload: files => {
-    dispatch(uploadFiles(files, displayedFolder.id, sharingState))
+    dispatch(uploadFiles(files, displayedFolder.id, sharingState, onUploaded))
   }
 })
 
