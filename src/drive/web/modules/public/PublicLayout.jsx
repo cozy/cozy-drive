5b76d3277b824b392324930105ea76c95381d794
Yannick Lohse <contact@yannick-lohse.fr>
2020-05-27 10:30:07 +0200

    fix: Translate all public alerts

--- src/drive/web/modules/public/PublicLayout.jsx
+++ src/drive/web/modules/public/PublicLayout.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Layout } from 'cozy-ui/transpiled/react/Layout'
 import Alerter from 'cozy-ui/transpiled/react/Alerter'
 import { IconSprite } from 'cozy-ui/transpiled/react'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
-const PublicLayout = ({ t, children }) => (
-  <Layout>
-    <Alerter t={t} />
-    {children}
-    <IconSprite />
-  </Layout>
-)
+const PublicLayout = ({ children }) => {
+  const { t } = useI18n()
+  return (
+    <Layout>
+      <Alerter t={t} />
+      {children}
+      <IconSprite />
+    </Layout>
+  )
+}
 
 export default PublicLayout
