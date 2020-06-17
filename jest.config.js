c16c0eaf1357d394e4593cac3bb8bb70bfae10b4
Patrick Browne <pt.browne@gmail.com>
2020-06-17 16:56:19 +0200

    fix: Use jest.config.js as it is the default for jest
    No need for custom config, this way we can use yarn jest as well
    as yarn test

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  setupFiles: ['<rootDir>/jestHelpers/setup.js'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'styl'],
+  moduleNameMapper: {
+    '.(png|gif|jpe?g)$': '<rootDir>/jestHelpers/mocks/fileMock.js',
+    '.svg$': '<rootDir>/jestHelpers/mocks/iconMock.js',
+    '.styl$': 'identity-obj-proxy',
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '^drive/locales/.*': '<rootDir>/src/drive/locales/en.json',
+    '^photos/locales/.*': '<rootDir>/src/photos/locales/en.json',
+    '^drive/(.*)': '<rootDir>/src/drive/$1',
+    '^models(.*)': '<rootDir>/src/models$1',
+    '^photos/(.*)': '<rootDir>/src/photos/$1',
+    '^sharing(.*)': '<rootDir>/src/sharing$1',
+    '^authentication(.*)': '<rootDir>/src/authentication$1',
+    '^viewer(.*)': '<rootDir>/src/viewer$1',
+    '^react-cozy-helpers(.*)': '<rootDir>/src/lib/react-cozy-helpers$1',
+    '^components(.*)': '<rootDir>/src/components$1',
+    '^test(.*)': '<rootDir>/test/$1',
+    '^folder-references(.*)': '<rootDir>/src/folder-references$1',
+    '^lib(.*)': '<rootDir>/src/lib$1',
+    'react-pdf/dist/pdf.worker.entry.js':
+      '<rootDir>/jestHelpers/mocks/pdfjsWorkerMock.js',
+    '^cozy-client$': 'cozy-client/dist/index.js'
+  },
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  transform: {
+    '^.+\\.(js|jsx)?$': '<rootDir>/test/jestLib/babel-transformer.js',
+    '^.+\\.webapp$': '<rootDir>/test/jestLib/json-transformer.js'
+  },
+  transformIgnorePatterns: ['node_modules/(?!cozy-ui)/'],
+  testEnvironment: 'jest-environment-jsdom-sixteen',
+  testMatch: ['**/(*.)(spec|test).js?(x)'],
+  globals: {
+    __APP_SLUG__: 'drive',
+    __TARGET__: 'browser',
+    __DEVELOPMENT__: true
+  }
+}
