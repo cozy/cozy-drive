a519b88e7e8e23147c44123dbf47a52624d288da
Patrick Browne <pt.browne@gmail.com>
2020-06-17 13:13:23 +0200

    fix: Default folder id to root

--- src/drive/web/modules/selectors.js
+++ src/drive/web/modules/selectors.js
@@ -1,5 +1,10 @@
 export const getCurrentFolderId = rootState => {
-  return rootState.router.params.folderId
+  if (rootState.router.params.folderId) {
+    return rootState.router.params.folderId
+  } else if (rootState.router.location.pathname == '/folder') {
+    return 'io.cozy.files.root-dir'
+  }
+  return null
 }
 
 export const getCurrentFileId = rootState => {
