279442aee34e26135a5e9ae4cb0d0bf2ad9f4302
Patrick Browne <pt.browne@gmail.com>
2020-06-22 12:06:57 +0200

    refactor: Use cozy client in feedback form

---
+++ src/drive/mobile/modules/settings/components/FeedbackForm.spec.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import CozyClient from 'cozy-client'
+
+import FeedbackForm, { FeedbackForm as DumbFeedbackForm } from './FeedbackForm'
+import AppLike from 'test/components/AppLike'
+import JobCollection from 'cozy-stack-client/dist/JobCollection'
+
+describe('FeedbackForm', () => {
+  const setup = () => {
+    const client = new CozyClient({})
+    const root = mount(
+      <AppLike client={client}>
+        <FeedbackForm onClose={jest.fn()} />
+      </AppLike>
+    )
+    const component = root.find(DumbFeedbackForm)
+    return { root, component, client }
+  }
+
+  it('should correctly send an email', () => {
+    const { component, client } = setup()
+    const createMock = jest.fn()
+    client.collection = jest.fn(() => ({
+      create: createMock
+    }))
+    const form = component.find('form')
+    form.simulate('submit')
+    expect(client.collection).toHaveBeenCalledWith('io.cozy.jobs')
+    expect(createMock).toHaveBeenCalledWith(
+      'sendmail',
+      expect.objectContaining({
+        subject: 'Feedback on Cozy Drive'
+      })
+    )
+  })
+})
