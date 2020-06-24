754c3559c3c09786cc8ffb16bd53ac6b98e9ea1e
Patrick Browne <pt.browne@gmail.com>
2020-06-24 09:36:50 +0200

    test: Fix typo

--- src/drive/web/modules/navigation/Breadcrumb/getFolderPath.spec.js
+++ src/drive/web/modules/navigation/Breadcrumb/getFolderPath.spec.js
@@ -6,24 +6,24 @@ describe('Computing a folders path', () => {
     const location = '/drive'
 
     it('should handle the path for the root folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: ROOT_DIR_ID,
         parent: false
       }
-      const path = getFolderPath(curentFolder, location, false, [])
+      const path = getFolderPath(currentFolder, location, false, [])
       expect(path.length).toEqual(1)
-      expect(path[0]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder)
     })
 
     it('should add the root folder if not present in the path', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: false
       }
-      const path = getFolderPath(curentFolder, location, false, [])
+      const path = getFolderPath(currentFolder, location, false, [])
       expect(path.length).toEqual(2)
       expect(path[0].id).toEqual(ROOT_DIR_ID)
-      expect(path[1]).toEqual(curentFolder)
+      expect(path[1]).toEqual(currentFolder)
     })
 
     it('should be ok when no displayed folder is provided', () => {
@@ -34,35 +34,35 @@ describe('Computing a folders path', () => {
     })
 
     it('should add the parent to the path', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456'
         }
       }
-      const path = getFolderPath(curentFolder, location, false, [])
+      const path = getFolderPath(currentFolder, location, false, [])
 
       expect(path.length).toEqual(3)
       expect(path[0].id).toEqual(ROOT_DIR_ID)
-      expect(path[1]).toEqual(curentFolder.parent)
-      expect(path[2]).toEqual(curentFolder)
+      expect(path[1]).toEqual(currentFolder.parent)
+      expect(path[2]).toEqual(currentFolder)
     })
 
     it("should add the parent's parent to the path", () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456',
           dir_id: '789'
         }
       }
-      const path = getFolderPath(curentFolder, location, false, [])
+      const path = getFolderPath(currentFolder, location, false, [])
 
       expect(path.length).toEqual(4)
       expect(path[0].id).toEqual(ROOT_DIR_ID)
-      expect(path[1].id).toEqual(curentFolder.parent.dir_id)
-      expect(path[2]).toEqual(curentFolder.parent)
-      expect(path[3]).toEqual(curentFolder)
+      expect(path[1].id).toEqual(currentFolder.parent.dir_id)
+      expect(path[2]).toEqual(currentFolder.parent)
+      expect(path[3]).toEqual(currentFolder)
     })
   })
 
@@ -70,28 +70,28 @@ describe('Computing a folders path', () => {
     const location = '/drive'
 
     it('should not add the root folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: false
       }
-      const path = getFolderPath(curentFolder, location, true, [])
+      const path = getFolderPath(currentFolder, location, true, [])
       expect(path.length).toEqual(1)
-      expect(path[0]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder)
     })
 
     it('should not add the parents parent', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456',
           dir_id: '789'
         }
       }
-      const path = getFolderPath(curentFolder, location, true, [])
+      const path = getFolderPath(currentFolder, location, true, [])
 
       expect(path.length).toEqual(2)
-      expect(path[0]).toEqual(curentFolder.parent)
-      expect(path[1]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder.parent)
+      expect(path[1]).toEqual(currentFolder)
     })
   })
 
@@ -99,64 +99,64 @@ describe('Computing a folders path', () => {
     const currentView = 'trash'
 
     it('should handle the trash root folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: TRASH_DIR_ID
       }
-      const path = getFolderPath(curentFolder, currentView, false, [])
+      const path = getFolderPath(currentFolder, currentView, false, [])
 
       expect(path.length).toEqual(1)
-      expect(path[0]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder)
     })
 
     it('should add the fake trash folder as root', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: false
       }
-      const path = getFolderPath(curentFolder, currentView, false, [])
+      const path = getFolderPath(currentFolder, currentView, false, [])
       expect(path.length).toEqual(2)
       expect(path[0].id).toEqual(TRASH_DIR_ID)
-      expect(path[1]).toEqual(curentFolder)
+      expect(path[1]).toEqual(currentFolder)
     })
 
     it('should not add the root parent to the path', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: ROOT_DIR_ID
         }
       }
-      const path = getFolderPath(curentFolder, currentView, false, [])
+      const path = getFolderPath(currentFolder, currentView, false, [])
       expect(path.length).toEqual(2)
       expect(path[0].id).toEqual(TRASH_DIR_ID)
-      expect(path[1]).toEqual(curentFolder)
+      expect(path[1]).toEqual(currentFolder)
     })
 
     it('should not add the root grand parent to the path', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456',
           dir_id: ROOT_DIR_ID
         }
       }
-      const path = getFolderPath(curentFolder, currentView, false, [])
+      const path = getFolderPath(currentFolder, currentView, false, [])
       expect(path.length).toEqual(3)
       expect(path[0].id).toEqual(TRASH_DIR_ID)
-      expect(path[1]).toEqual(curentFolder.parent)
-      expect(path[2]).toEqual(curentFolder)
+      expect(path[1]).toEqual(currentFolder.parent)
+      expect(path[2]).toEqual(currentFolder)
     })
   })
 
   describe('in the recent view', () => {
     it('should not add the root folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: false
       }
-      const path = getFolderPath(curentFolder, '/recent', true, [])
+      const path = getFolderPath(currentFolder, '/recent', true, [])
       expect(path.length).toEqual(1)
-      expect(path[0]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder)
     })
   })
 
@@ -164,52 +164,52 @@ describe('Computing a folders path', () => {
     const currentView = 'sharings'
 
     it('should not add the root folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: false
       }
-      const path = getFolderPath(curentFolder, currentView, true, [])
+      const path = getFolderPath(currentFolder, currentView, true, [])
       expect(path.length).toEqual(1)
-      expect(path[0]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder)
     })
 
     it('should add the parent of a regular folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456'
         }
       }
-      const path = getFolderPath(curentFolder, currentView, true, [])
+      const path = getFolderPath(currentFolder, currentView, true, [])
       expect(path.length).toEqual(2)
-      expect(path[0]).toEqual(curentFolder.parent)
-      expect(path[1]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder.parent)
+      expect(path[1]).toEqual(currentFolder)
     })
 
     it('should not add the parent of a shared folder', () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456'
         }
       }
-      const path = getFolderPath(curentFolder, currentView, true, ['123'])
+      const path = getFolderPath(currentFolder, currentView, true, ['123'])
       expect(path.length).toEqual(1)
-      expect(path[0]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder)
     })
 
     it("should not add the parent of a shared folder's parent", () => {
-      const curentFolder = {
+      const currentFolder = {
         id: '123',
         parent: {
           id: '456',
           dir_id: '789'
         }
       }
-      const path = getFolderPath(curentFolder, currentView, true, ['456'])
+      const path = getFolderPath(currentFolder, currentView, true, ['456'])
       expect(path.length).toEqual(2)
-      expect(path[0]).toEqual(curentFolder.parent)
-      expect(path[1]).toEqual(curentFolder)
+      expect(path[0]).toEqual(currentFolder.parent)
+      expect(path[1]).toEqual(currentFolder)
     })
   })
 })
