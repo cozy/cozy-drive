e8e8d728a15b47688c575f3b550c4c68006c36b5
Patrick Browne <pt.browne@gmail.com>
2020-06-26 18:05:13 +0200

    test: Add test for Dropzone

---
+++ src/drive/web/modules/upload/Dropzone.spec.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import flag from 'cozy-flags'
+import { setupFolderContent, mockCozyClientRequestQuery } from 'test/setup'
+import Dropzone, { Dropzone as DumbDropzone } from './Dropzone'
+import AppLike from 'test/components/AppLike'
+import { uploadFiles } from 'drive/web/modules/navigation/duck'
+
+jest.mock('drive/web/modules/navigation/duck', () => ({
+  uploadFiles: jest.fn().mockReturnValue({
+    type: 'FAKE_UPLOAD_FILES'
+  })
+}))
+
+mockCozyClientRequestQuery()
+
+describe('Dropzone', () => {
+  beforeEach(() => {
+    flag('drive.client-migration.enabled', true)
+  })
+
+  afterEach(() => {
+    flag('drive.client-migration.enabled', null)
+  })
+
+  it('should dispatch the uploadFiles action', async () => {
+    const displayedFolder = {
+      id: 'directory-foobar0'
+    }
+    const { store, client } = await setupFolderContent({
+      folderId: 'directory-foobar0'
+    })
+    const root = mount(
+      <AppLike client={client} store={store}>
+        <Dropzone displayedFolder={displayedFolder}/>
+      </AppLike>
+    )
+
+    const dropzone = root.find(DumbDropzone)
+    const files = []
+    dropzone.props().uploadFiles(files)
+    expect(uploadFiles).toHaveBeenCalledWith(
+      files,
+      'directory-foobar0',
+      expect.objectContaining({
+        refresh: expect.any(Function)
+      })
+    )
+  })
+})
