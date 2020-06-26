e10662584e2bd92a928e9ef8b4ddcedcd5688b53
Quentin Valmori <quentin.valmori@gmail.com>
2020-06-26 11:40:39 +0200

    fix: Viewer for recent View

---
+++ src/drive/web/modules/views/Recent/FilesViewerRecent.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useQuery } from 'cozy-client'
+import { useRouter } from 'drive/lib/RouterContext'
+import { buildRecentQuery } from 'drive/web/modules/queries'
+import { getCurrentFileId } from 'drive/web/modules/selectors'
+import { connect } from 'react-redux'
+import FilesViewer, {
+  FilesViewerLoading
+} from 'drive/web/modules/viewer/FilesViewerV2'
+const FilesViewerWithQuery = props => {
+  const filesQuery = buildRecentQuery()
+  const results = useQuery(filesQuery.definition, filesQuery.options)
+  const { router } = useRouter()
+  if (results.data) {
+    const viewableFiles = results.data
+    return (
+      <FilesViewer
+        {...props}
+        files={viewableFiles}
+        filesQuery={results}
+        onClose={() => router.push('/recent')}
+        onChange={fileId =>
+          router.push({
+            pathname: '/recent/file/' + fileId
+          })
+        }
+      />
+    )
+  } else {
+    return <FilesViewerLoading />
+  }
+}
+
+const mapStateToProps = state => ({
+  fileId: getCurrentFileId(state)
+})
+
+export default connect(mapStateToProps)(FilesViewerWithQuery)
