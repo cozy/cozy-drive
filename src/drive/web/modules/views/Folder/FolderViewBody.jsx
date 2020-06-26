236a348766200b562bed5452c1cb6356655eb2d8
Yannick Lohse <contact@yannick-lohse.fr>
2020-06-26 14:06:51 +0200

    feat: Navigate in trash view

---
+++ src/drive/web/modules/views/Folder/FolderViewBody.jsx
@@ -0,0 +1,102 @@
+import React, { useCallback, useContext } from 'react'
+
+import { ThumbnailSizeContext } from 'drive/lib/ThumbnailSizeContext'
+
+import { FileListv2 } from 'drive/web/modules/filelist/FileList'
+import { ConnectedFileListBodyV2 as FileListBodyV2 } from 'drive/web/modules/filelist/FileListBody'
+import AddFolder from 'drive/web/modules/filelist/AddFolder'
+import FileListHeader from 'drive/web/modules/filelist/FileListHeader'
+import MobileFileListHeader from 'drive/web/modules/filelist/MobileFileListHeader'
+import Oops from 'components/Error/Oops'
+import { EmptyDrive } from 'components/Error/Empty'
+import FileListRowsPlaceholder from 'drive/web/modules/filelist/FileListRowsPlaceholder'
+import { isMobileApp } from 'cozy-device-helper'
+import LoadMore from 'drive/web/modules/filelist/LoadMoreV2'
+import { FileWithSelection as File } from 'drive/web/modules/filelist/File'
+import { useFolderSort } from 'drive/web/modules/navigation/duck'
+import SelectionBar from 'drive/web/modules/selection/SelectionBar'
+
+const FolderViewBody = ({
+  folderId,
+  queryResults,
+  actions,
+  canSort,
+  navigateToFolder,
+  navigateToFile
+}) => {
+  const { isBigThumbnail, toggleThumbnailSize } = useContext(
+    ThumbnailSizeContext
+  )
+  const [sortOrder, setSortOrder] = useFolderSort(folderId)
+
+  const changeSortOrder = useCallback((folderId_legacy, attribute, order) =>
+    setSortOrder({ sortAttribute: attribute, sortOrder: order })
+  )
+
+  const isInError = queryResults.some(query => query.fetchStatus === 'error')
+
+  const hasDataToShow =
+    !isInError &&
+    queryResults.some(query => query.data && query.data.length > 0)
+  const isLoading = queryResults.some(
+    query => query.fetchStatus === 'loading' && !query.lastUpdate
+  )
+
+  const isEmpty = !isLoading && !hasDataToShow
+
+  return (
+    <>
+      <SelectionBar actions={actions} />
+      <FileListv2>
+        <MobileFileListHeader
+          folderId={null}
+          canSort={canSort}
+          sort={sortOrder}
+          onFolderSort={changeSortOrder}
+          thumbnailSizeBig={isBigThumbnail}
+          toggleThumbnailSize={toggleThumbnailSize}
+        />
+        <FileListHeader
+          folderId={null}
+          canSort={canSort}
+          sort={sortOrder}
+          onFolderSort={changeSortOrder}
+          thumbnailSizeBig={isBigThumbnail}
+          toggleThumbnailSize={toggleThumbnailSize}
+        />
+        <FileListBodyV2 selectionModeActive={false}>
+          <AddFolder />
+          {isInError && <Oops />}
+          {isLoading && <FileListRowsPlaceholder />}
+          {isEmpty && <EmptyDrive canUpload={true} />}
+          {hasDataToShow && (
+            <div className={isMobileApp() ? 'u-ov-hidden' : ''}>
+              {queryResults.map(query => (
+                <>
+                  {query.data.map(file => (
+                    <File
+                      key={file._id}
+                      attributes={file}
+                      displayedFolder={null}
+                      onFolderOpen={navigateToFolder}
+                      onFileOpen={navigateToFile}
+                      withSelectionCheckbox={true}
+                      withFilePath={false}
+                      withSharedBadge={true}
+                      isFlatDomain={true}
+                      thumbnailSizeBig={isBigThumbnail}
+                      actions={actions}
+                    />
+                  ))}
+                  {query.hasMore && <LoadMore fetchMore={query.fetchMore} />}
+                </>
+              ))}
+            </div>
+          )}
+        </FileListBodyV2>
+      </FileListv2>
+    </>
+  )
+}
+
+export default FolderViewBody
