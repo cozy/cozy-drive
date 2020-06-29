719303c15802813d5a78d232d838c8d405cb1e69
Yannick Lohse <contact@yannick-lohse.fr>
2020-06-29 11:28:47 +0200

    fix: Support opening viewer from subfolders

--- src/drive/web/modules/views/Sharings/FilesViewerSharings.jsx
+++ src/drive/web/modules/views/Sharings/FilesViewerSharings.jsx
@@ -2,28 +2,38 @@ import React from 'react'
 import { useQuery } from 'cozy-client'
 import { useRouter } from 'drive/lib/RouterContext'
 import { buildSharingsQuery } from 'drive/web/modules/queries'
-import { getCurrentFileId } from 'drive/web/modules/selectors'
+import {
+  getCurrentFileId,
+  getCurrentFolderId
+} from 'drive/web/modules/selectors'
 import { connect } from 'react-redux'
 import FilesViewer, {
   FilesViewerLoading
 } from 'drive/web/modules/viewer/FilesViewerV2'
 import withSharedDocumentIds from './withSharedDocumentIds'
 
-const FilesViewerWithQuery = ({ sharedDocumentIds, ...props }) => {
+const FilesViewerWithQuery = ({
+  sharedDocumentIds,
+  currentFolderId,
+  ...props
+}) => {
   const filesQuery = buildSharingsQuery(sharedDocumentIds)
   const results = useQuery(filesQuery.definition, filesQuery.options)
   const { router } = useRouter()
   if (results.data) {
     const viewableFiles = results.data.filter(f => f.type !== 'directory')
+    const basePath = currentFolderId
+      ? `/sharings/${currentFolderId}`
+      : '/sharings'
     return (
       <FilesViewer
         {...props}
         files={viewableFiles}
         filesQuery={results}
-        onClose={() => router.push('/sharings')}
+        onClose={() => router.push(basePath)}
         onChange={fileId =>
           router.push({
-            pathname: '/sharings/file/' + fileId
+            pathname: `${basePath}/file/${fileId}`
           })
         }
       />
@@ -34,7 +44,8 @@ const FilesViewerWithQuery = ({ sharedDocumentIds, ...props }) => {
 }
 
 const mapStateToProps = state => ({
-  fileId: getCurrentFileId(state)
+  fileId: getCurrentFileId(state),
+  currentFolderId: getCurrentFolderId(state)
 })
 
 const FilesViewerWithFolderId = connect(mapStateToProps)(FilesViewerWithQuery)
