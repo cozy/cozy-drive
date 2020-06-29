27dcf43bfef0f82c84f0e93ca99e75a663b1229e
Patrick Browne <pt.browne@gmail.com>
2020-06-29 10:44:47 +0200

    feat: Add dependencies to useCallback

--- src/drive/web/modules/views/Drive/index.jsx
+++ src/drive/web/modules/views/Drive/index.jsx
@@ -100,13 +100,19 @@ const DriveView = ({
     foldersResult.fetchStatus === 'pending' ||
     filesResult.fetchStatus === 'pending'
 
-  const navigateToFolder = useCallback(folderId => {
-    router.push(`/folder/${folderId}`)
-  })
+  const navigateToFolder = useCallback(
+    folderId => {
+      router.push(`/folder/${folderId}`)
+    },
+    [router]
+  )
 
-  const navigateToFile = useCallback(file => {
-    router.push(`/folder/${currentFolderId}/file/${file.id}`)
-  })
+  const navigateToFile = useCallback(
+    file => {
+      router.push(`/folder/${currentFolderId}/file/${file.id}`)
+    },
+    [router, currentFolderId]
+  )
 
   const { hasWriteAccess } = useContext(SharingContext)
   const client = useClient()
