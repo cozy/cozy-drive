d965c80d9eb7caf6ecb5dcb68910f894da7623c6
Yannick Lohse <contact@yannick-lohse.fr>
2020-07-02 17:36:34 +0200

    test: Test public view rendering

---
+++ src/drive/web/modules/views/testUtils.jsx
@@ -0,0 +1,24 @@
+import { generateFile } from 'test/generate'
+
+export const generateFileFixtures = ({
+  nbFiles = 5,
+  path = '/test',
+  dir_id = 'io.cozy.files.root-dir',
+  updated_at = '2020-05-14T10:33:31.365224+02:00'
+}) => {
+  const filesFixture = Array(nbFiles)
+    .fill(null)
+    .map((x, i) => generateFile({ i, type: 'file', path, dir_id, updated_at }))
+
+  return filesFixture
+}
+
+export const getByTextWithMarkup = (getByText, text) => {
+  getByText((content, node) => {
+    const hasText = node => node.textContent === text
+    const childrenDontHaveText = Array.from(node.children).every(
+      child => !hasText(child)
+    )
+    return hasText(node) && childrenDontHaveText
+  })
+}
