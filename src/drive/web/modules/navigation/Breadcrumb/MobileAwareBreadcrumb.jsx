eabd692ac95f7c607d67eeca1628b87cf797a9ec
Yannick Lohse <contact@yannick-lohse.fr>
2020-07-03 17:22:17 +0200

    fix: Back button in breadcrumb

--- src/drive/web/modules/navigation/Breadcrumb/MobileAwareBreadcrumb.jsx
+++ src/drive/web/modules/navigation/Breadcrumb/MobileAwareBreadcrumb.jsx
@@ -1,12 +1,12 @@
 /* global cozy */
-import React from 'react'
+import React, { useCallback } from 'react'
 import { withBreakpoints } from 'cozy-ui/transpiled/react'
 import RouterBreadCrumb from 'drive/web/modules/navigation/Breadcrumb/RouterBreadcrumb'
 import RouterPreviousButton from 'drive/web/modules/navigation/Breadcrumb/RouterPreviousButton'
 import BreadCrumb from 'drive/web/modules/navigation/Breadcrumb/Breadcrumb'
 import PreviousButton from 'drive/web/modules/navigation/Breadcrumb/PreviousButton'
 
-const MobileAwareBreadcrumb = props => {
+const LegacyMobileAwareBreadcrumb = props => {
   const { BarCenter, BarLeft } = cozy.bar
 
   return props.breakpoints.isMobile ? (
@@ -25,16 +25,24 @@ const MobileAwareBreadcrumb = props => {
   )
 }
 
-export default withBreakpoints()(MobileAwareBreadcrumb)
+export default withBreakpoints()(LegacyMobileAwareBreadcrumb)
 
-export const MobileAwareBreadcrumbV2 = withBreakpoints()(props => {
+export const MobileAwareBreadcrumb = props => {
   const { BarCenter, BarLeft } = cozy.bar
+  const { onBreadcrumbClick, path } = props
+  const navigateBack = useCallback(
+    () => {
+      const parentFolder = path[path.length - 2]
+      onBreadcrumbClick(parentFolder)
+    },
+    [onBreadcrumbClick, path]
+  )
 
   return props.breakpoints.isMobile ? (
     <div>
       {props.path.length >= 2 && (
         <BarLeft>
-          <PreviousButton {...props} />
+          <PreviousButton onClick={navigateBack} />
         </BarLeft>
       )}
       <BarCenter>
@@ -44,4 +52,6 @@ export const MobileAwareBreadcrumbV2 = withBreakpoints()(props => {
   ) : (
     <BreadCrumb {...props} />
   )
-})
+}
+
+export const MobileAwareBreadcrumbV2 = withBreakpoints()(MobileAwareBreadcrumb)
