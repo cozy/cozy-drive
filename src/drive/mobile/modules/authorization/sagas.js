297c74fe27e88e4e64bb276c3d534f6902eecd15
Yannick Lohse <contact@yannick-lohse.fr>
2020-07-06 15:56:44 +0200

    feat: Disable replication for old pouch

--- src/drive/mobile/modules/authorization/sagas.js
+++ src/drive/mobile/modules/authorization/sagas.js
@@ -1,3 +1,4 @@
+import flag from 'cozy-flags'
 import { setOffline } from 'drive/mobile/modules/settings/duck'
 import { startReplication } from 'drive/mobile/modules/replication/sagas'
 import { setClient, setToken } from './duck'
@@ -8,5 +9,5 @@ export const saveCredentials = (client, token) => dispatch => {
   dispatch(setOffline(true))
   // TODO: not ideal to startReplication here, we should be explicit when we start it
   // but we need to restart it when renewing authorization (see below)
-  dispatch(startReplication())
+  if (!flag('drive.client-migration.enabled')) dispatch(startReplication())
 }
