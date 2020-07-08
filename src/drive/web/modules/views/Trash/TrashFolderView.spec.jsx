e242e5f693149de6ba2e0312997396bc2ea6f0a3
Yannick Lohse <contact@yannick-lohse.fr>
2020-07-08 12:10:32 +0200

    test: Refactor trash test

--- src/drive/web/modules/views/Trash/TrashFolderView.spec.jsx
+++ src/drive/web/modules/views/Trash/TrashFolderView.spec.jsx
@@ -4,21 +4,15 @@ import { render } from '@testing-library/react'
 import { Router, hashHistory, Route } from 'react-router'
 
 import { setupStoreAndClient } from 'test/setup'
-import { generateFile } from 'test/generate'
 import AppLike from 'test/components/AppLike'
 
+import { generateFileFixtures } from '../testUtils'
 import TrashFolderView from './TrashFolderView'
 
 jest.mock('cozy-client/dist/hooks/useQuery', () => jest.fn())
 
 describe('TrashFolderView', () => {
-  const setup = ({
-    nbFiles = 5,
-    totalCount,
-    path = '/test',
-    dir_id = 'io.cozy.files.trash-dir',
-    updated_at = '2020-05-14T10:33:31.365224+02:00'
-  } = {}) => {
+  const setup = () => {
     const { store, client } = setupStoreAndClient()
 
     client.plugins.realtime = {
@@ -29,60 +23,52 @@ describe('TrashFolderView', () => {
     client.stackClient.fetchJSON = jest
       .fn()
       .mockReturnValue({ data: [], rows: [] })
-    const filesFixture = Array(nbFiles)
-      .fill(null)
-      .map((x, i) =>
-        generateFile({ i, type: 'file', path, dir_id, updated_at })
-      )
-    const foldersFixture = Array(nbFiles)
-      .fill(null)
-      .map((x, i) =>
-        generateFile({
-          i,
-          type: 'directory',
-          path,
-          dir_id,
-          updated_at,
-          prefix: 'folder'
-        })
-      )
-    useQuery
-      .mockReturnValueOnce({
-        data: filesFixture,
-        count: totalCount || filesFixture.length
-      })
-      .mockReturnValueOnce({
-        data: foldersFixture,
-        count: totalCount || foldersFixture.length
-      })
-      .mockReturnValue({
-        data: [],
-        count: 0
-      })
 
     const rendered = render(
       <AppLike client={client} store={store}>
         <Router history={hashHistory}>
           <Route path="/" component={TrashFolderView} />
           <Route path="/folder/:id" component={TrashFolderView} />
-          <Route path="/folder/file/:id" component={() => null} />
         </Router>
       </AppLike>
     )
     return { ...rendered, client }
   }
 
-  it('test the Trash view', () => {
+  it('renders the Trash view', () => {
     const nbFiles = 1
     const path = '/trash'
     const dir_id = 'io.cozy.files.trash-dir'
     const updated_at = '2020-05-14T10:33:31.365224+02:00'
-    const { getByText } = setup({
+    const filesFixture = generateFileFixtures({
       nbFiles,
       path,
       dir_id,
       updated_at
     })
+    const foldersFixture = generateFileFixtures({
+      nbFiles,
+      path,
+      dir_id,
+      updated_at,
+      type: 'directory',
+      prefix: 'folder'
+    })
+    useQuery
+      .mockReturnValueOnce({
+        data: filesFixture,
+        count: filesFixture.length
+      })
+      .mockReturnValueOnce({
+        data: foldersFixture,
+        count: foldersFixture.length
+      })
+      .mockReturnValue({
+        data: [],
+        count: 0
+      })
+
+    const { getByText } = setup()
 
     // Check if we display the folder and the file
     getByText(`folder0`)
--- src/drive/web/modules/views/testUtils.jsx
+++ src/drive/web/modules/views/testUtils.jsx
@@ -4,11 +4,13 @@ export const generateFileFixtures = ({
   nbFiles = 5,
   path = '/test',
   dir_id = 'io.cozy.files.root-dir',
-  updated_at = '2020-05-14T10:33:31.365224+02:00'
+  updated_at = '2020-05-14T10:33:31.365224+02:00',
+  type = 'file',
+  prefix
 }) => {
   const filesFixture = Array(nbFiles)
     .fill(null)
-    .map((x, i) => generateFile({ i, type: 'file', path, dir_id, updated_at }))
+    .map((x, i) => generateFile({ i, type, path, dir_id, updated_at, prefix }))
 
   return filesFixture
 }
