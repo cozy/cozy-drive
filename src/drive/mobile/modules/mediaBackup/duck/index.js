40726f04e90c8bfc83411949e3ee212c0043cfd4
Quentin Valmori <quentin.valmori@gmail.com>
2020-07-09 16:39:31 +0200

    refactor: Extract few methods to write tests

--- src/drive/mobile/modules/mediaBackup/duck/index.js
+++ src/drive/mobile/modules/mediaBackup/duck/index.js
@@ -1,35 +1,19 @@
-import throttle from 'lodash/throttle'
 import {
   getPhotos,
-  uploadLibraryItem,
   isAuthorized,
   requestAuthorization
 } from 'drive/mobile/lib/media'
-import { getTranslateFunction } from 'drive/mobile/lib/i18n'
 import { isWifi } from 'drive/mobile/lib/network'
 import { logException } from 'drive/lib/reporter'
 import { setBackupImages } from 'drive/mobile/modules/settings/duck'
-
+import { getUploadDir, uploadPhoto } from './files'
 import {
   MEDIA_UPLOAD_START,
   MEDIA_UPLOAD_END,
   MEDIA_UPLOAD_ABORT,
-  MEDIA_UPLOAD_SUCCESS,
   MEDIA_UPLOAD_CANCEL,
-  MEDIA_UPLOAD_QUOTA,
-  CURRENT_UPLOAD,
-  CURRENT_UPLOAD_PROGRESS
+  CURRENT_UPLOAD
 } from './reducer'
-import {
-  getReferencedFolders,
-  getOrCreateFolderWithReference,
-  REF_PHOTOS,
-  REF_BACKUP
-} from 'folder-references'
-import logger from 'lib/logger'
-import { fixMagicFolderName } from './fixBackupFolderNamesBug'
-
-const ERROR_CODE_TOO_LARGE = 413
 
 export const cancelMediaBackup = () => ({ type: MEDIA_UPLOAD_CANCEL })
 
@@ -41,53 +25,6 @@ const currentMediaUpload = (media, uploadCounter, totalUpload) => ({
     total: totalUpload
   }
 })
-
-const getUploadDir = async client => {
-  const uploadedFolders = await getReferencedFolders(client, REF_BACKUP)
-  if (uploadedFolders.length >= 1) {
-    //Let's fix the bug introduced between 1.18.18 and 1.18.24
-    await fixMagicFolderName(client, uploadedFolders[0])
-    // There can be more than one referenced folder in case of consecutive delete/restores. We always want to return the most recently used.
-    return uploadedFolders[0]
-  } else {
-    const t = getTranslateFunction()
-    const mediaFolderName = t('mobile.settings.media_backup.media_folder')
-    const uploadFolderName = t('mobile.settings.media_backup.backup_folder')
-    const legacyUploadFolderName = t(
-      'mobile.settings.media_backup.legacy_backup_folder'
-    )
-
-    await getOrCreateFolderWithReference(
-      client,
-      `/${mediaFolderName}`,
-      REF_PHOTOS
-    )
-
-    try {
-      const { data: legacyFolder } = await client
-        .collection('io.cozy.files')
-        .statByPath(`/${mediaFolderName}/${legacyUploadFolderName}/plop`)
-      await getOrCreateFolderWithReference(
-        client,
-        `/${mediaFolderName}/${legacyUploadFolderName}`,
-        REF_BACKUP
-      )
-      return legacyFolder
-    } catch (err) {
-      if (err.status === 404) {
-        // the legacy folder doesn't exist, so we create the new one
-        const uploadFolder = await getOrCreateFolderWithReference(
-          client,
-          `/${mediaFolderName}/${uploadFolderName}`,
-          REF_BACKUP
-        )
-        return uploadFolder
-      } else {
-        throw err
-      }
-    }
-  }
-}
 
 const canBackup = (isManualBackup, getState) => {
   const backupOnAnyNetworkType = !getState().mobile.settings.wifiOnly
@@ -116,16 +53,13 @@ export const startMediaBackup = (isManualBackup = false) => async (
       await dispatch(setBackupImages(false))
     }
   }
-
   if (canBackup(isManualBackup, getState)) {
     try {
       const photosOnDevice = await getPhotos()
       const alreadyUploaded = getState().mobile.mediaBackup.uploaded
-
       const photosToUpload = photosOnDevice.filter(
         photo => !alreadyUploaded.includes(photo.id)
       )
-
       const totalUpload = photosToUpload.length
       if (totalUpload > 0) {
         const {
@@ -146,7 +80,7 @@ export const startMediaBackup = (isManualBackup = false) => async (
         }
       }
 
-      client.fetchJSON('POST', '/settings/synchonized')
+      client.getStackClient().fetchJSON('POST', '/settings/synchonized')
     } catch (e) {
       dispatch({ type: MEDIA_UPLOAD_ABORT })
       if (!e.message.match(/Failed to fetch/))
@@ -157,59 +91,6 @@ export const startMediaBackup = (isManualBackup = false) => async (
   }
 
   dispatch({ type: MEDIA_UPLOAD_END })
-}
-
-const mediaUploadSucceed = ({ id }) => ({
-  type: MEDIA_UPLOAD_SUCCESS,
-  id
-})
-
-const uploadPhoto = (dirName, dirID, photo, client) => async dispatch => {
-  try {
-    const path = dirName + '/' + photo.fileName
-    await client.collection('io.cozy.files').statByPath(path)
-
-    dispatch(mediaUploadSucceed(photo))
-    return
-  } catch (_) {
-    //logger.log('_')
-  } // if an exception is throw, the file doesn't exist yet and we can safely upload it
-
-  const MILLISECOND = 1
-  const SECOND = 1000 * MILLISECOND
-  const MINUTE = 60 * SECOND
-  const maxBackupTime = 5 * MINUTE
-  const timeout = setTimeout(() => {
-    logger.info(JSON.stringify(photo))
-    logException(`Backup duration exceeded ${maxBackupTime} milliseconds`)
-  }, maxBackupTime)
-  try {
-    const onProgressUpdate = progress => {
-      dispatch({ type: CURRENT_UPLOAD_PROGRESS, progress })
-    }
-
-    const onThumbnailGenerated = () => {}
-
-    await uploadLibraryItem(
-      dirID,
-      photo,
-      throttle(onProgressUpdate, 500),
-      onThumbnailGenerated
-    )
-    clearTimeout(timeout)
-    dispatch(mediaUploadSucceed(photo))
-  } catch (err) {
-    if (err.code && parseInt(err.code) === ERROR_CODE_TOO_LARGE) {
-      clearTimeout(timeout)
-      dispatch({ type: MEDIA_UPLOAD_QUOTA })
-    } else {
-      logger.warn('startMediaBackup upload item error')
-      logger.warn(JSON.stringify(err))
-      logger.info(JSON.stringify(photo))
-      const reason = err.message ? err.message : JSON.stringify(err)
-      logException('Backup error: ' + reason, null, ['backup error'])
-    }
-  }
 }
 
 export const backupImages = (backupImages, force = false) => async (
---
+++ src/drive/mobile/modules/mediaBackup/duck/index.spec.js
@@ -0,0 +1,142 @@
+import { startMediaBackup } from './index'
+import { createMockClient } from 'cozy-client/dist/mock'
+import { isAuthorized, getPhotos } from 'drive/mobile/lib/media'
+jest.mock('drive/mobile/lib/media', () => ({
+  ...jest.requireActual('drive/mobile/lib/media'),
+  isAuthorized: jest.fn(),
+  getPhotos: jest.fn()
+}))
+
+import { setBackupImages } from 'drive/mobile/modules/settings/duck'
+jest.mock('drive/mobile/modules/settings/duck', () => ({
+  ...jest.requireActual('drive/mobile/modules/settings/duck'),
+  setBackupImages: jest.fn()
+}))
+
+import { getUploadDir, uploadPhoto } from './files'
+jest.mock('./files', () => ({
+  ...jest.requireActual('./files'),
+  getUploadDir: jest.fn(),
+  uploadPhoto: jest.fn()
+}))
+import {
+  MEDIA_UPLOAD_START,
+  CURRENT_UPLOAD,
+  MEDIA_UPLOAD_ABORT
+} from './reducer'
+
+describe('StartMediaBackup', () => {
+  const client = createMockClient({})
+
+  const mockedDispatch = jest.fn()
+  const mockedGetState = jest.fn()
+  const mockedState = {
+    mobile: {
+      settings: {
+        backupImages: true,
+        wifiOnly: false
+      },
+      mediaBackup: {
+        uploaded: [1]
+      }
+    }
+  }
+  const mockedPhotoOnDevice = [
+    {
+      id: 2
+    }
+  ]
+  const uploadDirIr = 'uploadId'
+  const uploadDirPath = '/upload'
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    getUploadDir.mockResolvedValue({
+      _id: uploadDirIr,
+      attributes: {
+        path: uploadDirPath
+      }
+    })
+  })
+  it('starts the mediabackup with one photo to upload', async () => {
+    getPhotos.mockResolvedValue(mockedPhotoOnDevice)
+    mockedGetState.mockReturnValue(mockedState)
+    isAuthorized.mockResolvedValue(true)
+    const isManualBackup = true
+    const req = startMediaBackup(isManualBackup)
+
+    await req(mockedDispatch, mockedGetState, { client })
+    expect(mockedDispatch).toHaveBeenNthCalledWith(1, {
+      type: MEDIA_UPLOAD_START
+    })
+    expect(setBackupImages).not.toHaveBeenCalled()
+    expect(mockedDispatch).toHaveBeenNthCalledWith(2, {
+      type: CURRENT_UPLOAD,
+      media: {
+        id: 2
+      },
+      messageData: {
+        current: 0,
+        total: 1
+      }
+    })
+    expect(uploadPhoto).toHaveBeenCalledWith(
+      uploadDirPath,
+      uploadDirIr,
+      {
+        id: 2
+      },
+      client
+    )
+    expect(client.getStackClient().fetchJSON).toHaveBeenCalledWith(
+      'POST',
+      '/settings/synchonized'
+    )
+  })
+
+  it('should not upload if there is nothing to upload', async () => {
+    getPhotos.mockResolvedValue([])
+    mockedGetState.mockReturnValue(mockedState)
+    isAuthorized.mockResolvedValue(true)
+
+    const isManualBackup = true
+    const req = startMediaBackup(isManualBackup)
+    await req(mockedDispatch, mockedGetState, { client })
+    expect(mockedDispatch).toHaveBeenNthCalledWith(1, {
+      type: MEDIA_UPLOAD_START
+    })
+
+    expect(uploadPhoto).not.toHaveBeenCalled()
+    expect(client.getStackClient().fetchJSON).toHaveBeenCalledWith(
+      'POST',
+      '/settings/synchonized'
+    )
+  })
+
+  it('stops the backup if settings says no', async () => {
+    getPhotos.mockResolvedValue([])
+    mockedGetState.mockReturnValue({
+      mobile: {
+        settings: {
+          backupImages: false,
+          wifiOnly: false
+        },
+        mediaBackup: {
+          uploaded: [1]
+        }
+      }
+    })
+    isAuthorized.mockResolvedValue(true)
+
+    const isManualBackup = false
+    const req = startMediaBackup(isManualBackup)
+    await req(mockedDispatch, mockedGetState, { client })
+    expect(mockedDispatch).toHaveBeenNthCalledWith(1, {
+      type: MEDIA_UPLOAD_START
+    })
+    expect(mockedDispatch).toHaveBeenNthCalledWith(2, {
+      type: MEDIA_UPLOAD_ABORT
+    })
+    expect(uploadPhoto).not.toHaveBeenCalled()
+  })
+})
