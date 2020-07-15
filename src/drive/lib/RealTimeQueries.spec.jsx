d2babf44a99cb14c9b0e3c734b728fa176563890
Yannick Lohse <contact@yannick-lohse.fr>
2020-07-15 16:29:01 +0200

    fix: Send normalized docs to cozy-client

--- src/drive/lib/RealTimeQueries.spec.jsx
+++ src/drive/lib/RealTimeQueries.spec.jsx
@@ -26,37 +26,59 @@ describe('RealTimeQueries', () => {
       expect(client.plugins.realtime.subscribe).toHaveBeenCalledTimes(3)
     )
 
-    realtimeCallbacks['created']({ id: 'mock-created' })
-    expect(client.dispatch).toHaveBeenCalledWith({
-      definition: {
-        document: { id: 'mock-created' },
-        mutationType: 'CREATE_DOCUMENT'
-      },
-      mutationId: 1,
-      response: { data: { _type: 'io.cozy.files', id: 'mock-created' } },
-      type: 'RECEIVE_MUTATION_RESULT'
-    })
+    realtimeCallbacks['created']({ id: 'mock-created', type: 'file' })
+    expect(client.dispatch).toHaveBeenCalledWith(
+      expect.objectContaining({
+        definition: {
+          document: { id: 'mock-created', type: 'file' },
+          mutationType: 'CREATE_DOCUMENT'
+        },
+        mutationId: 1,
+        response: {
+          data: {
+            _type: 'io.cozy.files',
+            type: 'file',
+            id: 'mock-created',
+            _id: 'mock-created'
+          }
+        },
+        type: 'RECEIVE_MUTATION_RESULT'
+      })
+    )
 
-    realtimeCallbacks['updated']({ id: 'mock-updated' })
+    realtimeCallbacks['updated']({ id: 'mock-updated', type: 'file' })
     expect(client.dispatch).toHaveBeenCalledWith({
       definition: {
-        document: { id: 'mock-updated' },
+        document: { id: 'mock-updated', type: 'file' },
         mutationType: 'UPDATE_DOCUMENT'
       },
       mutationId: 2,
-      response: { data: { _type: 'io.cozy.files', id: 'mock-updated' } },
+      response: {
+        data: {
+          _type: 'io.cozy.files',
+          type: 'file',
+          id: 'mock-updated',
+          _id: 'mock-updated'
+        }
+      },
       type: 'RECEIVE_MUTATION_RESULT'
     })
 
-    realtimeCallbacks['deleted']({ id: 'mock-deleted' })
+    realtimeCallbacks['deleted']({ id: 'mock-deleted', type: 'file' })
     expect(client.dispatch).toHaveBeenCalledWith({
       definition: {
-        document: { id: 'mock-deleted', _deleted: true },
+        document: { id: 'mock-deleted', type: 'file', _deleted: true },
         mutationType: 'DELETE_DOCUMENT'
       },
       mutationId: 3,
       response: {
-        data: { _deleted: true, _type: 'io.cozy.files', id: 'mock-deleted' }
+        data: {
+          _deleted: true,
+          _type: 'io.cozy.files',
+          type: 'file',
+          id: 'mock-deleted',
+          _id: 'mock-deleted'
+        }
       },
       type: 'RECEIVE_MUTATION_RESULT'
     })
