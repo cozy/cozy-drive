800dc7a6a19fa73b95968afb6d54f73007cafd36
Yannick Lohse <contact@yannick-lohse.fr>
2020-07-22 14:48:09 +0200

    test: Switch test to react testing lib

--- src/drive/web/modules/drive/Toolbar/components/MoreMenu.spec.jsx
+++ src/drive/web/modules/drive/Toolbar/components/MoreMenu.spec.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { act } from 'react-dom/test-utils'
-import { ActionMenuItem } from 'cozy-ui/transpiled/react/ActionMenu'
+import { render, fireEvent, configure } from '@testing-library/react'
 import { setupFolderContent, mockCozyClientRequestQuery } from 'test/setup'
 import { downloadFiles } from 'drive/web/modules/actions/utils'
-import MoreMenu from 'drive/web/modules/drive/Toolbar/components/MoreMenu'
+import MoreMenu from './MoreMenu'
 import AppLike from 'test/components/AppLike'
-import { MoreButton } from 'components/Button'
 
 jest.mock('drive/web/modules/actions/utils', () => ({
   downloadFiles: jest.fn().mockResolvedValue()
 }))
 
-const sleep = duration => new Promise(resolve => setTimeout(resolve, duration))
-
 mockCozyClientRequestQuery()
+
+configure({ testIdAttribute: 'data-test-id' })
 
 describe('MoreMenu', () => {
   const setup = async () => {
@@ -23,7 +20,7 @@ describe('MoreMenu', () => {
       folderId
     })
 
-    const root = mount(
+    const result = render(
       <AppLike client={client} store={store}>
         <MoreMenu
           isDisabled={false}
@@ -33,31 +30,17 @@ describe('MoreMenu', () => {
       </AppLike>
     )
 
-    await sleep(1)
+    const { getByTestId } = result
+    fireEvent.click(getByTestId('more-button'))
 
-    // Open the menu
-    act(() => {
-      root
-        .find(MoreButton)
-        .props()
-        .onClick()
-    })
-    root.update()
-
-    return { root, store, client }
+    return { ...result, store, client }
   }
 
   describe('DownloadButton', () => {
     it('should work', async () => {
-      const { root } = await setup()
+      const { getByText } = await setup()
 
-      const actionMenuItem = root.findWhere(node => {
-        return (
-          node.type() === ActionMenuItem && node.text() == 'Download folder'
-        )
-      })
-      actionMenuItem.props().onClick()
-      await sleep(0)
+      fireEvent.click(getByText('Download folder'))
       expect(downloadFiles).toHaveBeenCalled()
     })
   })
