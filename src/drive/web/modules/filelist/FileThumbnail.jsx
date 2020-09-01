e6e3a38ad35afc5aadfc9b8dc7f4a17fa8315278
Yannick Lohse <contact@yannick-lohse.fr>
2020-09-01 15:22:42 +0200

    refactor: FileIcon renders icons for all types of files
    Badges are handled higher up in the tree

--- src/drive/web/modules/filelist/FileThumbnail.jsx
+++ src/drive/web/modules/filelist/FileThumbnail.jsx
@@ -2,35 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import withBreakpoints from 'cozy-ui/transpiled/react/helpers/withBreakpoints'
-import { ImageLoader } from 'components/Image'
 import { SharedBadge } from 'cozy-sharing'
 import FileIcon from 'drive/web/modules/filelist/FileIcon'
 import styles from 'drive/styles/filelist.styl'
+import Badge from 'cozy-ui/transpiled/react/Badge'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import get from 'lodash/get'
 
 const FileThumbnail = ({ file, breakpoints: { isMobile }, size }) => {
-  const isImage = file.class === 'image'
+  const isRegularShortcut = file.class === 'shortcut'
+  const isSimpleFile = !isRegularShortcut
   return (
     <div
       className={cx(styles['fil-content-cell'], styles['fil-file-thumbnail'], {
         'u-pl-0': !isMobile
       })}
     >
-      {isImage ? (
-        <ImageLoader
-          file={file}
-          size="small"
-          render={src => (
-            <img
-              src={src}
-              width={size || 32}
-              height={size || 32}
-              className={styles['fil-file-thumbnail-image']}
-            />
-          )}
-          renderFallback={() => <FileIcon file={file} size={size} />}
-        />
-      ) : (
-        <FileIcon file={file} size={size} />
+      {isSimpleFile && <FileIcon file={file} size={size} />}
+      {isRegularShortcut && (
+        <Badge
+          badgeContent={<Icon icon="link" size={10} />}
+          anchorOrigin={{ vertical: 'bottom' }}
+          variant={'qualifier'}
+        >
+          <FileIcon file={file} size={size} />
+        </Badge>
       )}
       {/**
        * @todo
