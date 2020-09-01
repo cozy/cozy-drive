1e7997a9673d77c6dffe2a8e22fc9ab03d35fac2
Yannick Lohse <contact@yannick-lohse.fr>
2020-09-01 15:14:26 +0200

    refactor: FileIconShortcut only renders icon
    …and not the badge

--- src/drive/web/modules/filelist/FileIconShortcut.jsx
+++ src/drive/web/modules/filelist/FileIconShortcut.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
 import { withClient, useFetchShortcut } from 'cozy-client'
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import Badge from 'cozy-ui/transpiled/react/Badge'
 import palette from 'cozy-ui/transpiled/react/palette'
 
 const FileIconShortcut = ({ file, size, client }) => {
   const { shortcutImg } = useFetchShortcut(client, file.id)
   return (
-    <Badge
-      content={<Icon icon="link" size={10} />}
-      type="normal"
-      alignment="bottom-right"
-      size="medium"
-    >
-      <>
-        <div style={{ display: shortcutImg ? 'block' : 'none' }}>
-          <img src={shortcutImg} width={size} height={size} />
-        </div>
-        <div
-          style={{
-            display: !shortcutImg ? 'block' : 'none'
-          }}
-        >
-          <Icon icon="globe" size={size} color={palette.coolGrey} />
-        </div>
-      </>
-    </Badge>
+    <>
+      <div style={{ display: shortcutImg ? 'block' : 'none' }}>
+        <img src={shortcutImg} width={size} height={size} />
+      </div>
+      <div
+        style={{
+          display: !shortcutImg ? 'block' : 'none'
+        }}
+      >
+        <Icon icon="globe" size={size} color={palette.coolGrey} />
+      </div>
+    </>
   )
 }
 
