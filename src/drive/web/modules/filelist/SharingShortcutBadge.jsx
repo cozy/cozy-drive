ef6db0c3d96fabe41793fa52f5ff2a40297bd07e
Yannick Lohse <contact@yannick-lohse.fr>
2020-09-10 10:44:57 +0200

    fix: Handle shortcut to shared folders icon

--- src/drive/web/modules/filelist/SharingShortcutBadge.jsx
+++ src/drive/web/modules/filelist/SharingShortcutBadge.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import get from 'lodash/get'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import getMimeTypeIcon from 'drive/lib/getMimeTypeIcon'
-import { isDirectory } from 'drive/web/modules/drive/files'
+import { DOCTYPE_FILES } from 'drive/lib/doctypes'
 import FileIconShortcut from 'drive/web/modules/filelist/FileIconShortcut'
 
 const SharingShortcutBadge = ({ file, size }) => {
   const actualFileMime = get(file, 'metadata.target.mime')
+  const doctype = get(file, 'metadata.target._type')
   const isShortcut = actualFileMime === 'application/internet-shortcut'
+  const isDirectory = actualFileMime === '' && doctype === DOCTYPE_FILES
 
   return isShortcut ? (
     <FileIconShortcut file={file} size={size} />
   ) : (
     <Icon
-      icon={getMimeTypeIcon(isDirectory(file), file.name, actualFileMime)}
+      icon={getMimeTypeIcon(isDirectory, file.name, actualFileMime)}
       size={size}
     />
   )
