2954a70a1b9803ad4389add30a7e7df3f40424f9
Quentin Valmori <quentin.valmori@gmail.com>
2020-09-28 17:15:31 +0200

    fix: useRouter instead of props

--- src/drive/web/modules/drive/Toolbar/delete/delete.jsx
+++ src/drive/web/modules/drive/Toolbar/delete/delete.jsx
@@ -3,11 +3,13 @@ import React, { useCallback } from 'react'
 import { connect } from 'react-redux'
 import { showModal } from 'react-cozy-helpers'
 import { withRouter } from 'react-router'
+import { useRouter } from 'drive/lib/RouterContext'
 
 import DeleteConfirm from '../../DeleteConfirm'
 import toolbarContainer from '../toolbar'
 
-const EnhancedDeleteConfirm = ({ folder, router, ...rest }) => {
+const EnhancedDeleteConfirm = ({ folder, ...rest }) => {
+  const { router } = useRouter()
   const navigateToParentFolder = useCallback(
     () => router.push(`/folder/${folder.dir_id}`),
     [router, folder]
@@ -24,16 +26,10 @@ const EnhancedDeleteConfirm = ({ folder, router, ...rest }) => {
 
 export { EnhancedDeleteConfirm }
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
+const mapDispatchToProps = dispatch => ({
   trashFolder: folder =>
     dispatch(
-      showModal(
-        <EnhancedDeleteConfirm
-          folder={folder}
-          dispatch={dispatch}
-          router={ownProps.router}
-        />
-      )
+      showModal(<EnhancedDeleteConfirm folder={folder} dispatch={dispatch} />)
     )
 })
 
