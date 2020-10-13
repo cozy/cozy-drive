e119e11d6a578d9ed190261778d9061fdb2d3b38
Quentin Valmori <quentin.valmori@gmail.com>
2020-10-13 18:12:54 +0200

    refactor: ShareMenuItem use18n

--- src/drive/web/modules/drive/ShareMenuItem.jsx
+++ src/drive/web/modules/drive/ShareMenuItem.jsx
@@ -3,29 +3,35 @@ import PropTypes from 'prop-types'
 import { SharedDocument, SharedRecipients } from 'cozy-sharing'
 import { ActionMenuItem } from 'cozy-ui/transpiled/react/ActionMenu'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import styles from 'drive/styles/actionmenu.styl'
 
-const ShareMenuItem = ({ docId, onClick }, { t }) => (
-  <SharedDocument docId={docId}>
-    {({ isSharedWithMe }) => (
-      <ActionMenuItem
-        className={'u-flex-items-center u-pos-relative'}
-        onClick={onClick}
-        left={<Icon icon="share" />}
-        right={
-          <SharedRecipients
-            className={styles['fil-actionmenu-recipients']}
-            docId={docId}
-            size="small"
-          />
-        }
-      >
-        {isSharedWithMe ? t('Files.share.sharedWithMe') : t('Files.share.cta')}
-      </ActionMenuItem>
-    )}
-  </SharedDocument>
-)
+const ShareMenuItem = ({ docId, onClick }) => {
+  const { t } = useI18n()
+  return (
+    <SharedDocument docId={docId}>
+      {({ isSharedWithMe }) => (
+        <ActionMenuItem
+          className={'u-flex-items-center u-pos-relative'}
+          onClick={onClick}
+          left={<Icon icon="share" />}
+          right={
+            <SharedRecipients
+              className={styles['fil-actionmenu-recipients']}
+              docId={docId}
+              size="small"
+            />
+          }
+        >
+          {isSharedWithMe
+            ? t('Files.share.sharedWithMe')
+            : t('Files.share.cta')}
+        </ActionMenuItem>
+      )}
+    </SharedDocument>
+  )
+}
 
 ShareMenuItem.contextTypes = {
   t: PropTypes.func.isRequired
