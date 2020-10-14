32ed553b508ab94cdf414ae090d2014f5a510e5d
Quentin Valmori <quentin.valmori@gmail.com>
2020-10-14 10:25:32 +0200

    refactor: Clean the actions before to be easier to understand

--- src/drive/web/modules/actionmenu/ActionsItems.jsx
+++ src/drive/web/modules/actionmenu/ActionsItems.jsx
@@ -14,21 +14,32 @@ export const ActionsItems = ({ actions, file, onClose }) => {
       !actionDefinition.displayCondition([file])
     )
   })
-
-  return displayableActions.map((actionObject, i) => {
+  // We need to clean Actions since action has a displayable
+  // conditions and we can't know from the begining what the
+  // behavior will be. For instance, we can't know that
+  // hr will be the latest action in the sharing views for a
+  // folder.
+  // Or we can't know that we'll have two following hr if the
+  // display condition for the actions between are true or false
+  let cleanedActions = []
+  displayableActions.map((actionObject, i) => {
     const actionName = Object.keys(actionObject)[0]
-    const actionDefinition = Object.values(actionObject)[0]
-
     if (previousAction === actionName) {
       previousAction = actionName
       return null
     } else {
       previousAction = actionName
     }
-
     if (actionName === 'hr' && i + 1 === displayableActions.length) {
       return null
     }
+    cleanedActions.push(actionObject)
+  })
+
+  return cleanedActions.map((actionObject, i) => {
+    const actionName = Object.keys(actionObject)[0]
+    const actionDefinition = Object.values(actionObject)[0]
+
     if (actionName === 'hr') return <hr key={'hr' + i} />
     const Component = actionDefinition.Component
     const action = actionDefinition.action
