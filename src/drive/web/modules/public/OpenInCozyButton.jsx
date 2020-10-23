ea6aed13763a2feb4e57878d863953e5042900f7
Quentin Valmori <quentin.valmori@gmail.com>
2020-10-23 18:48:42 +0200

    refactor: Use useI18n

--- src/drive/web/modules/public/OpenInCozyButton.jsx
+++ src/drive/web/modules/public/OpenInCozyButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { ButtonLink } from 'cozy-ui/transpiled/react'
+import { ButtonLink, useI18n } from 'cozy-ui/transpiled/react'
 
-const OpenInCozyButton = ({ t, size = 'normal', href = '' }) => (
-  <ButtonLink
-    href={href}
-    size={size}
-    label={t('toolbar.menu_open_cozy')}
-    icon={'to-the-cloud'}
-  />
-)
+const OpenInCozyButton = ({ size = 'normal', href = '' }) => {
+  const { t } = useI18n()
+  return (
+    <ButtonLink
+      href={href}
+      size={size}
+      label={t('toolbar.menu_open_cozy')}
+      icon={'to-the-cloud'}
+    />
+  )
+}
 
 export default OpenInCozyButton
