e82f3b10fd23022101019d852d06475fb910f1f9
Quentin Valmori <quentin.valmori@gmail.com>
2020-10-23 18:51:26 +0200

    feat: DownloadFilesButton can handle the dl by itself

--- src/drive/web/modules/public/DownloadButton.jsx
+++ src/drive/web/modules/public/DownloadButton.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-import { Button } from 'cozy-ui/transpiled/react'
+import PropTypes from 'prop-types'
+
+import { useClient } from 'cozy-client'
+import { Button, useI18n } from 'cozy-ui/transpiled/react'
+
+import { downloadFiles } from 'drive/web/modules/actions/utils'
 
 const DownloadButton = ({ onDownload, ...props }) => (
   <Button onClick={() => onDownload()} icon="download" {...props} />
 )
 
 export default DownloadButton
+
+export const DownloadFilesButton = ({ files }) => {
+  const { t } = useI18n()
+  const client = useClient()
+  return (
+    <DownloadButton
+      label={t('toolbar.menu_download')}
+      data-test-id="fil-public-download"
+      onDownload={() => {
+        downloadFiles(client, files)
+      }}
+      theme="secondary"
+    />
+  )
+}
+DownloadFilesButton.propTypes = {
+  files: PropTypes.array.isRequired
+}
