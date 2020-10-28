473c48be0df1fc580a39008d8358b174c951083a
Quentin Valmori <quentin.valmori@gmail.com>
2020-10-28 14:01:30 +0100

    fix: Use Class component for FileOpener
    In a few cases, useRef() / ref.current is empty and never
    re-render. Since this is not the first time you have issues
    on this, I rewrite the component in Class since it works well.
    
    I'll try to create an isolated reproductible case

--- src/drive/web/modules/filelist/FileOpener.jsx
+++ src/drive/web/modules/filelist/FileOpener.jsx
@@ -1,63 +1,56 @@
-import React, { useRef, useEffect } from 'react'
+import React, { Component } from 'react'
 import Hammer from 'hammerjs'
 
 import { enableTouchEvents } from './File'
 import styles from './fileopener.styl'
 
-const FileOpener = ({
-  file,
-  children,
-  disabled,
-  actionMenuVisible,
-  toggle,
-  open,
-  selectionModeActive,
-  isRenaming
-}) => {
-  const linkRef = useRef(null)
+class FileOpener extends Component {
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef()
+  }
 
-  const current = linkRef.current
-
-  useEffect(
-    () => {
-      let gesturesHandler = null
-      if (current !== null) {
-        gesturesHandler = new Hammer.Manager(current)
-        gesturesHandler.add(new Hammer.Tap({ event: 'singletap' }))
-        gesturesHandler.add(new Hammer.Press({ event: 'onpress' }))
-        gesturesHandler.on('onpress singletap', ev => {
-          if (actionMenuVisible || disabled) return
-          if (enableTouchEvents(ev)) {
-            ev.preventDefault() // prevent a ghost click
-            if (ev.type === 'onpress' || selectionModeActive) {
-              ev.srcEvent.stopImmediatePropagation()
-              toggle(ev.srcEvent)
-            } else {
-              ev.srcEvent.stopImmediatePropagation()
-              if (!isRenaming) open(ev.srcEvent, file)
-            }
-          }
-        })
-      }
-      return () => gesturesHandler && gesturesHandler.destroy()
-    },
-    [
-      current,
-      actionMenuVisible,
-      disabled,
+  componentDidMount() {
+    const {
       file,
+      disabled,
+      actionMenuVisible,
+      toggle,
       open,
       selectionModeActive,
-      toggle,
       isRenaming
-    ]
-  )
+    } = this.props
+    this.gesturesHandler = new Hammer.Manager(this.myRef.current)
+    this.gesturesHandler.add(new Hammer.Tap({ event: 'singletap' }))
+    this.gesturesHandler.add(new Hammer.Press({ event: 'onpress' }))
+    this.gesturesHandler.on('onpress singletap', ev => {
+      if (actionMenuVisible || disabled) return
+      if (enableTouchEvents(ev)) {
+        ev.preventDefault() // prevent a ghost click
+        if (ev.type === 'onpress' || selectionModeActive) {
+          ev.srcEvent.stopImmediatePropagation()
+          toggle(ev.srcEvent)
+        } else {
+          ev.srcEvent.stopImmediatePropagation()
+          if (!isRenaming) open(ev.srcEvent, file)
+        }
+      }
+    })
+  }
 
-  return (
-    <span className={styles['file-opener']} ref={linkRef} id={file.id}>
-      {children}
-    </span>
-  )
+  componentWillUnmount() {
+    this.gesturesHandler && this.gesturesHandler.destroy()
+  }
+
+  render() {
+    const { file, children } = this.props
+
+    return (
+      <span className={styles['file-opener']} ref={this.myRef} id={file.id}>
+        {children}
+      </span>
+    )
+  }
 }
 
 export default FileOpener
