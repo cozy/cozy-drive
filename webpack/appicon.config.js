4dc7a1b36a27a77c4c12f0e9172324734b7f8f96
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-11-10 10:22:11 +0100

    fix: Add favicon for Public pages

--- webpack/appicon.config.js
+++ webpack/appicon.config.js
@@ -9,6 +9,19 @@ module.exports = {
         from: `src/${app}/targets/vendor/assets/app-icon.svg`,
         to: 'public/app-icon.svg'
       },
+      {
+        from: `src/${app}/targets/vendor/assets/favicon*`,
+        to: 'public/',
+        flatten: true
+      },
+      {
+        from: `src/${app}/targets/vendor/assets/apple-touch-icon.png`,
+        to: 'public/apple-touch-icon.png'
+      },
+      {
+        from: `src/${app}/targets/vendor/assets/safari-pinned-tab.svg`,
+        to: 'public/safari-pinned-tab.svg'
+      },
       { from: `src/${app}/targets/vendor/assets`, ignore: ['.gitkeep'] }
     ])
   ]
