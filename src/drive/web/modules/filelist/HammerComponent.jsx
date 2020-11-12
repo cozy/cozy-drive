f836abe1c4e5bf0e1284410f9b5f54949913e9a4
Quentin Valmori <quentin.valmori@gmail.com>
2020-11-12 11:51:22 +0100

    docs: Add documentation about why HammerComponent is needed

--- src/drive/web/modules/filelist/HammerComponent.jsx
+++ src/drive/web/modules/filelist/HammerComponent.jsx
@@ -3,6 +3,16 @@ import Hammer from '@egjs/hammerjs'
 import propagating from 'propagating-hammerjs'
 import styles from './fileopener.styl'
 
+/**
+ * This component is necessary if you want to use an onClick
+ * and stop its propagation when you are down the tree of
+ * a component that already * has a GestureHandler managed by Hammer.
+ * It is impossible to tell Hammer to ignore the event.
+ * The only way is to use Hammer also for the onClick and to use a lib
+ * that wrap Hammer in order to have access to a stopPropagation that works.
+ *
+ * Hammer is used on the File to manage the longPress to display the selection.
+ */
 class HammerComponent extends Component {
   constructor(props) {
     super(props)
