9a461c112e676c97ed5826f7c9b69d2c93c4234d
Quentin Valmori <quentin.valmori@gmail.com>
2020-11-16 10:58:56 +0100

    feat: Don't display the banner til we know what to display

--- src/drive/web/modules/public/PublicToolbar.jsx
+++ src/drive/web/modules/public/PublicToolbar.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import logger from 'lib/logger'
-import { withClient } from 'cozy-client'
+import { useClient } from 'cozy-client'
 
 import { getQueryParameter } from 'react-cozy-helpers'
 
@@ -30,68 +30,79 @@ const isShortcutCreatedOnTheRecipientCozy = included => {
   }
   return false
 }
-class PublicToolbar extends React.Component {
-  state = {
-    discoveryLink: null,
-    isSharingShortcutCreated: false
-  }
 
-  componentDidMount() {
-    if (window.location.pathname === '/preview') this.loadSharingDiscoveryLink()
-  }
+const PublicToolbar = ({ hasWriteAccess, refreshFolderContent, files }) => {
+  const [discoveryLink, setDiscoveryLink] = useState()
+  const [isSharingShortcutCreated, setIsSharingSharingcutCreated] = useState(
+    false
+  )
+  const [sharing, setSharing] = useState()
+  const [loading, setLoading] = useState(true)
+  const client = useClient()
 
-  async loadSharingDiscoveryLink() {
-    try {
-      const { client } = this.props
-      const response = await client
-        .collection('io.cozy.permissions')
-        .getOwnPermissions()
+  useEffect(
+    () => {
+      const loadSharingDiscoveryLink = async () => {
+        setLoading(true)
+        try {
+          const response = await client
+            .collection('io.cozy.permissions')
+            .getOwnPermissions()
 
-      const isSharingShortcutCreated = isShortcutCreatedOnTheRecipientCozy(
-        response.included
-      )
-      const sourceId = response.data.attributes.source_id
-      const sharingId = sourceId.split('/')[1]
-      const { sharecode } = getQueryParameter()
+          const isSharingShortcutCreated = isShortcutCreatedOnTheRecipientCozy(
+            response.included
+          )
+          const sourceId = response.data.attributes.source_id
+          const sharingId = sourceId.split('/')[1]
+          const { sharecode } = getQueryParameter()
 
-      const link = client
-        .collection('io.cozy.sharings')
-        .getDiscoveryLink(sharingId, sharecode)
+          const link = client
+            .collection('io.cozy.sharings')
+            .getDiscoveryLink(sharingId, sharecode)
 
-      const { data: sharing } = await client
-        .getStackClient()
-        .fetchJSON('GET', `/sharings/${sharingId}`)
-      this.setState({ discoveryLink: link, isSharingShortcutCreated, sharing })
-    } catch (err) {
-      logger.warn('Failed to load sharing discovery link', err)
-    }
-  }
+          const { data: sharing } = await client
+            .getStackClient()
+            .fetchJSON('GET', `/sharings/${sharingId}`)
+          setDiscoveryLink(link)
+          setIsSharingSharingcutCreated(isSharingShortcutCreated)
+          setSharing(sharing)
+        } catch (e) {
+          logger.warn('Failed to load sharing discovery link', e)
+        } finally {
+          setLoading(false)
+        }
+      }
+      if (window.location.pathname === '/preview') {
+        loadSharingDiscoveryLink()
+      }
+    },
+    [client]
+  )
+  return (
+    <>
+      {loading && null}
+      {!loading &&
+        !discoveryLink && (
+          <PublicToolbarByLink
+            files={files}
+            hasWriteAccess={hasWriteAccess}
+            refreshFolderContent={refreshFolderContent}
+          />
+        )}
 
-  render() {
-    const { hasWriteAccess, refreshFolderContent, files } = this.props
-    const { discoveryLink, isSharingShortcutCreated, sharing } = this.state
-    // If there is no DiscoveryLink, it means we're on a sharing by link
-    if (!discoveryLink) {
-      return (
-        <PublicToolbarByLink
-          files={files}
-          hasWriteAccess={hasWriteAccess}
-          refreshFolderContent={refreshFolderContent}
-        />
-      )
-    }
-
-    return (
-      <PublicToolbarCozyToCozy
-        onDownload={this.downloadFiles}
-        discoveryLink={discoveryLink}
-        files={files}
-        isSharingShortcutCreated={isSharingShortcutCreated}
-        sharing={sharing}
-      />
-    )
-  }
+      {!loading &&
+        discoveryLink && (
+          <PublicToolbarCozyToCozy
+            discoveryLink={discoveryLink}
+            files={files}
+            isSharingShortcutCreated={isSharingShortcutCreated}
+            sharing={sharing}
+          />
+        )}
+    </>
+  )
 }
+
 PublicToolbar.propTypes = {
   files: PropTypes.array.isRequired,
   // hasWriteAccess is only required if we're in a sharing by link
@@ -100,4 +111,4 @@ PublicToolbar.propTypes = {
   refreshFolderContent: PropTypes.func
 }
 
-export default withClient(PublicToolbar)
+export default PublicToolbar
