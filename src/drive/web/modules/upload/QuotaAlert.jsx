20108a36ca235c73c97630c177cfa96aa4d19efb
Quentin Valmori <quentin.valmori@gmail.com>
2020-11-17 08:18:33 +0100

    fix: Use QuotaAlert from UI

--- src/drive/web/modules/upload/QuotaAlert.jsx
+++ src/drive/web/modules/upload/QuotaAlert.jsx
@@ -1,42 +1,8 @@
 import React from 'react'
-import get from 'lodash/get'
-import Modal from 'cozy-ui/transpiled/react/Modal'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
-import { withClient } from 'cozy-client'
-import withInstance from './withInstance'
-import { isMobileApp } from 'cozy-device-helper'
+import {default as UIQuotaAlert} from 'cozy-ui/transpiled/react/QuotaAlert'
 
-export const buildPremiumLink = (uuid, managerUrl) =>
-  `${managerUrl}/cozy/instances/${uuid}/premium`
-
-const QuotaAlert = ({ t, onClose, client }) => {
-  let uuid, managerUrl
-  /**
-   * We do the request only on the web since Apple
-   * and Google have a retriscted policy for the
-   * inApp purchase...
-   */
-  if (!isMobileApp()) {
-    const instanceInfo = withInstance(client)
-    uuid = get(instanceInfo, 'instance.data.attributes.uuid')
-    managerUrl = get(instanceInfo, 'context.data.attributes.manager_url')
-  }
-
-  return (
-    <Modal
-      title={t('quotaalert.title')}
-      description={t('quotaalert.desc')}
-      secondaryText={t('quotaalert.confirm')}
-      secondaryAction={onClose}
-      primaryText={uuid && managerUrl ? t('quotaalert.increase') : undefined}
-      primaryAction={() =>
-        uuid && managerUrl
-          ? window.open(buildPremiumLink(uuid, managerUrl), 'self')
-          : onClose
-      }
-      dismissAction={onClose}
-    />
-  )
+const QuotaAlert = ({ onClose }) => {
+ return  <UIQuotaAlert onClose={onClose} />
 }
 
-export default withClient(translate()(QuotaAlert))
+export default QuotaAlert
