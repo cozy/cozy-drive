9f7a3fa77733e87bd3e7a967e9ac3e73f7da3207
Quentin Valmori <quentin.valmori@gmail.com>
2020-11-17 08:35:14 +0100

    fix: Lint

--- src/drive/web/modules/upload/QuotaAlert.jsx
+++ src/drive/web/modules/upload/QuotaAlert.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {default as UIQuotaAlert} from 'cozy-ui/transpiled/react/QuotaAlert'
+import { default as UIQuotaAlert } from 'cozy-ui/transpiled/react/QuotaAlert'
 
 const QuotaAlert = ({ onClose }) => {
- return  <UIQuotaAlert onClose={onClose} />
+  return <UIQuotaAlert onClose={onClose} />
 }
 
 export default QuotaAlert
