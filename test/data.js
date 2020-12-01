87fdb79de96ed7f365a9fce02534a3b6c74bfbf5
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-12-01 14:54:00 +0100

    feat: Disable row elements when folder/file is in sync
    disable element when "in move" too

---
+++ test/data.js
@@ -0,0 +1,128 @@
+export const folder = {
+  id: 'cc0a6981d593205dcefe29dcb2021b80',
+  _id: 'cc0a6981d593205dcefe29dcb2021b80',
+  _type: 'io.cozy.files',
+  type: 'directory',
+  attributes: {
+    type: 'directory',
+    name: 'Folder Name',
+    dir_id: 'io.cozy.files.root-dir',
+    created_at: '2020-11-03T12:31:43.027718+01:00',
+    updated_at: '2020-11-03T12:31:43.027718+01:00',
+    tags: [],
+    path: '/Folder Name',
+    cozyMetadata: {
+      doctypeVersion: '1',
+      metadataVersion: 1,
+      createdAt: '2020-11-03T12:31:43.028779+01:00',
+      createdByApp: 'drive',
+      updatedAt: '2020-11-03T12:31:43.028779+01:00',
+      updatedByApps: [
+        {
+          slug: 'drive',
+          date: '2020-11-03T12:31:43.028779+01:00',
+          instance: 'http://cozy.tools:8080/'
+        }
+      ],
+      createdOn: 'http://cozy.tools:8080/'
+    }
+  },
+  meta: {
+    rev: '1-99e787c29e88dc5a8a2a7aafc4f682ed'
+  },
+  links: {
+    self: '/files/cc0a6981d593205dcefe29dcb2021b80'
+  },
+  name: 'Folder Name',
+  dir_id: 'io.cozy.files.root-dir',
+  created_at: '2020-11-03T12:31:43.027718+01:00',
+  updated_at: '2020-11-03T12:31:43.027718+01:00',
+  tags: [],
+  path: '/Folder Name',
+  cozyMetadata: {
+    doctypeVersion: '1',
+    metadataVersion: 1,
+    createdAt: '2020-11-03T12:31:43.028779+01:00',
+    createdByApp: 'drive',
+    updatedAt: '2020-11-03T12:31:43.028779+01:00',
+    updatedByApps: [
+      {
+        slug: 'drive',
+        date: '2020-11-03T12:31:43.028779+01:00',
+        instance: 'http://cozy.tools:8080/'
+      }
+    ],
+    createdOn: 'http://cozy.tools:8080/'
+  },
+  old_versions: {
+    target: {
+      id: 'cc0a6981d593205dcefe29dcb2021b80',
+      _id: 'cc0a6981d593205dcefe29dcb2021b80',
+      _type: 'io.cozy.files',
+      type: 'directory',
+      attributes: {
+        type: 'directory',
+        name: 'Folder Name',
+        dir_id: 'io.cozy.files.root-dir',
+        created_at: '2020-11-03T12:31:43.027718+01:00',
+        updated_at: '2020-11-03T12:31:43.027718+01:00',
+        tags: [],
+        path: '/Folder Name',
+        cozyMetadata: {
+          doctypeVersion: '1',
+          metadataVersion: 1,
+          createdAt: '2020-11-03T12:31:43.028779+01:00',
+          createdByApp: 'drive',
+          updatedAt: '2020-11-03T12:31:43.028779+01:00',
+          updatedByApps: [
+            {
+              slug: 'drive',
+              date: '2020-11-03T12:31:43.028779+01:00',
+              instance: 'http://cozy.tools:8080/'
+            }
+          ],
+          createdOn: 'http://cozy.tools:8080/'
+        }
+      },
+      meta: {
+        rev: '1-99e787c29e88dc5a8a2a7aafc4f682ed'
+      },
+      links: {
+        self: '/files/cc0a6981d593205dcefe29dcb2021b80'
+      },
+      name: 'Folder Name',
+      dir_id: 'io.cozy.files.root-dir',
+      created_at: '2020-11-03T12:31:43.027718+01:00',
+      updated_at: '2020-11-03T12:31:43.027718+01:00',
+      tags: [],
+      path: '/Folder Name',
+      cozyMetadata: {
+        doctypeVersion: '1',
+        metadataVersion: 1,
+        createdAt: '2020-11-03T12:31:43.028779+01:00',
+        createdByApp: 'drive',
+        updatedAt: '2020-11-03T12:31:43.028779+01:00',
+        updatedByApps: [
+          {
+            slug: 'drive',
+            date: '2020-11-03T12:31:43.028779+01:00',
+            instance: 'http://cozy.tools:8080/'
+          }
+        ],
+        createdOn: 'http://cozy.tools:8080/'
+      }
+    },
+    name: 'old_versions',
+    doctype: 'io.cozy.files.versions'
+  }
+}
+
+export const actionsMenu = [
+  {
+    item: {
+      icon: 'item',
+      Component: jest.fn().mockReturnValue('ActionsMenuItem'),
+      action: jest.fn()
+    }
+  }
+]
