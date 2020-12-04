cb02b2c9644fcdb6437f996352c00e5af743bdf5
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-12-04 11:37:55 +0100

    refactor: Use TableHead, TableHeader, TableRow instead of div

--- src/drive/web/modules/filelist/FileListHeader.jsx
+++ src/drive/web/modules/filelist/FileListHeader.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import classNames from 'classnames'
+import cx from 'classnames'
+
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Button from 'cozy-ui/transpiled/react/Button'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import {
+  TableHead,
+  TableHeader,
+  TableRow
+} from 'cozy-ui/transpiled/react/Table'
+
 import iconList from 'drive/assets/icons/icon-list.svg'
 import iconListMin from 'drive/assets/icons/icon-list-min.svg'
 import HeaderCell from './HeaderCell'
@@ -21,78 +28,77 @@ const FileListHeader = ({
   const { t } = useI18n()
   const actualSort = sort || DEFAULT_SORT
   return (
-    <div
-      className={classNames(
-        styles['fil-content-head'],
-        styles['fil-content-row-head']
-      )}
-    >
-      <div
-        className={classNames(
-          styles['fil-content-header'],
-          styles['fil-content-file-select']
-        )}
-      />
-      {SORTABLE_ATTRIBUTES.map(({ label, attr, css, defaultOrder }, index) => {
-        if (!canSort) {
-          return <HeaderCell key={index} label={label} css={css} />
-        }
-        const isActive = actualSort && actualSort.attribute === attr
-        return (
-          <HeaderCell
-            key={`key_cell_${index}`}
-            label={label}
-            attr={attr}
-            css={css}
-            defaultOrder={defaultOrder}
-            order={isActive ? actualSort.order : null}
-            onSort={(attr, order) => onFolderSort(folderId, attr, order)}
-          />
-        )
-      })}
-      <div
-        className={classNames(
-          styles['fil-content-header'],
-          styles['fil-content-size']
-        )}
-      >
-        {t('table.head_size')}
-      </div>
-      <div
-        className={classNames(
-          styles['fil-content-header'],
-          styles['fil-content-header-status']
-        )}
-      >
-        {t('table.head_status')}
-      </div>
-      <div
-        className={classNames(
-          styles['fil-content-header'],
-          styles['fil-content-header-action']
-        )}
-      >
-        {/** in order to not display this button in a MoveModal for instance */}
-        {canSort && (
-          <Button
-            theme={'action'}
-            onClick={() => {
-              toggleThumbnailSize()
-            }}
-            label={t('table.head_thumbnail_size')}
-            extension="narrow"
-            icon={
-              <Icon
-                icon={thumbnailSizeBig ? iconListMin : iconList}
-                size={17}
-                label={t('table.head_thumbnail_size')}
-              />
+    <TableHead className={styles['fil-content-head']}>
+      <TableRow className={styles['fil-content-row-head']}>
+        <TableHeader
+          className={cx(
+            styles['fil-content-header'],
+            styles['fil-content-file-select']
+          )}
+        />
+        {SORTABLE_ATTRIBUTES.map(
+          ({ label, attr, css, defaultOrder }, index) => {
+            if (!canSort) {
+              return <HeaderCell key={index} label={label} css={css} />
             }
-            iconOnly
-          />
+            const isActive = actualSort && actualSort.attribute === attr
+            return (
+              <HeaderCell
+                key={`key_cell_${index}`}
+                label={label}
+                attr={attr}
+                css={css}
+                defaultOrder={defaultOrder}
+                order={isActive ? actualSort.order : null}
+                onSort={(attr, order) => onFolderSort(folderId, attr, order)}
+              />
+            )
+          }
         )}
-      </div>
-    </div>
+        <TableHeader
+          className={cx(
+            styles['fil-content-header'],
+            styles['fil-content-size']
+          )}
+        >
+          {t('table.head_size')}
+        </TableHeader>
+        <TableHeader
+          className={cx(
+            styles['fil-content-header'],
+            styles['fil-content-header-status']
+          )}
+        >
+          {t('table.head_status')}
+        </TableHeader>
+        <TableHeader
+          className={cx(
+            styles['fil-content-header'],
+            styles['fil-content-header-action']
+          )}
+        >
+          {/** in order to not display this button in a MoveModal for instance */}
+          {canSort && (
+            <Button
+              theme={'action'}
+              onClick={() => {
+                toggleThumbnailSize()
+              }}
+              label={t('table.head_thumbnail_size')}
+              extension="narrow"
+              icon={
+                <Icon
+                  icon={thumbnailSizeBig ? iconListMin : iconList}
+                  size={17}
+                  label={t('table.head_thumbnail_size')}
+                />
+              }
+              iconOnly
+            />
+          )}
+        </TableHeader>
+      </TableRow>
+    </TableHead>
   )
 }
 
