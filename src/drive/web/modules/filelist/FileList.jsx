870092d9a9c3894a617b8f26dc3a4127f02d35bf
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-12-04 11:17:08 +0100

    refactor: Use Table instead of div

--- src/drive/web/modules/filelist/FileList.jsx
+++ src/drive/web/modules/filelist/FileList.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { Table } from 'cozy-ui/transpiled/react/Table'
+
 import styles from 'drive/styles/filelist.styl'
 
 export const FileList = ({ children }) => (
-  <div className={styles['fil-content-table']} role="table">
+  <Table className={styles['fil-content-table']} role="table">
     {children}
-  </div>
+  </Table>
 )
