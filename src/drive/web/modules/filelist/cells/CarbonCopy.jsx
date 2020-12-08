4fb858fec4a42a08f47f087ce6b07967a3695c56
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-12-08 14:17:02 +0100

    feat: Add tooltips on certification cells

--- src/drive/web/modules/filelist/cells/CarbonCopy.jsx
+++ src/drive/web/modules/filelist/cells/CarbonCopy.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import get from 'lodash/get'
 import cx from 'classnames'
 
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { TableCell } from 'cozy-ui/transpiled/react/Table'
 import AppIcon from 'cozy-ui/transpiled/react/AppIcon'
 import CheckIcon from 'cozy-ui/transpiled/react/Icons/Check'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import Tooltip from 'cozy-ui/transpiled/react/Tooltip'
+import Typography from 'cozy-ui/transpiled/react/Typography'
 
 import styles from 'drive/styles/filelist.styl'
 
@@ -20,7 +23,9 @@ const CarbonCopyIcon = ({ file }) => {
 }
 
 const CarbonCopy = ({ file }) => {
+  const { t } = useI18n()
   const hasDataToshow = get(file, 'metadata.carbonCopy')
+  const connectorName = get(file, 'cozyMetadata.uploadedBy.slug')
 
   return (
     <TableCell
@@ -29,7 +34,26 @@ const CarbonCopy = ({ file }) => {
         styles['fil-content-certification']
       )}
     >
-      {hasDataToshow ? <CarbonCopyIcon file={file} /> : '—'}
+      {hasDataToshow ? (
+        <Tooltip
+          title={
+            <div className="u-p-half">
+              <Typography variant="body1">
+                {t('table.row_carbonCopy.title')}
+              </Typography>
+              <Typography variant="caption" color="textSecondary">
+                {t('table.row_carbonCopy.caption', { connectorName })}
+              </Typography>
+            </div>
+          }
+        >
+          <span>
+            <CarbonCopyIcon file={file} />
+          </span>
+        </Tooltip>
+      ) : (
+        '—'
+      )}
     </TableCell>
   )
 }
--- src/drive/web/modules/filelist/cells/ElectronicSafe.jsx
+++ src/drive/web/modules/filelist/cells/ElectronicSafe.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import get from 'lodash/get'
 import cx from 'classnames'
 
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { TableCell } from 'cozy-ui/transpiled/react/Table'
 import AppIcon from 'cozy-ui/transpiled/react/AppIcon'
+import Tooltip from 'cozy-ui/transpiled/react/Tooltip'
+import Typography from 'cozy-ui/transpiled/react/Typography'
 
 import styles from 'drive/styles/filelist.styl'
 
 const ElectronicSafe = ({ file }) => {
+  const { t } = useI18n()
+
   const hasDataToshow = get(file, 'metadata.electronicSafe')
   const connectorName = get(file, 'cozyMetadata.uploadedBy.slug')
 
@@ -18,7 +23,26 @@ const ElectronicSafe = ({ file }) => {
         styles['fil-content-certification']
       )}
     >
-      {hasDataToshow ? <AppIcon app={connectorName} /> : '—'}
+      {hasDataToshow ? (
+        <Tooltip
+          title={
+            <div className="u-p-half">
+              <Typography variant="body1">
+                {t('table.row_electronicSafe.title')}
+              </Typography>
+              <Typography variant="caption" color="textSecondary">
+                {t('table.row_electronicSafe.caption', { connectorName })}
+              </Typography>
+            </div>
+          }
+        >
+          <span>
+            <AppIcon app={connectorName} />
+          </span>
+        </Tooltip>
+      ) : (
+        '—'
+      )}
     </TableCell>
   )
 }
