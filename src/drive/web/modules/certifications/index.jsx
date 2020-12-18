2d72e6af638d5a892f333570e739554465597632
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-12-18 11:46:08 +0100

    refactor: Extract additional columns specs

--- src/drive/web/modules/certifications/index.jsx
+++ src/drive/web/modules/certifications/index.jsx
@@ -1,7 +1,29 @@
-import { useQuery } from 'cozy-client'
+import {
+  CarbonCopy as CarbonCopyCell,
+  ElectronicSafe as ElectronicSafeCell
+} from 'drive/web/modules/filelist/cells'
+import {
+  CarbonCopy as CarbonCopyHeader,
+  ElectronicSafe as ElectronicSafeHeader
+} from 'drive/web/modules/filelist/headers'
 
-export const MakeConditionWithQuery = ({ query }) => {
-  const queryResult = useQuery(query.definition, query.options)
-
-  return queryResult.fetchStatus === 'loaded' && queryResult.data.length > 0
+export const extraColumnsSpecs = {
+  carbonCopy: {
+    query: ({ queryBuilder, currentFolderId, sharedDocumentIds, attribute }) =>
+      queryBuilder({ currentFolderId, sharedDocumentIds, attribute }),
+    condition: ({ conditionBuilder, files, attribute }) =>
+      conditionBuilder({ files, attribute }),
+    label: 'carbonCopy',
+    HeaderComponent: CarbonCopyHeader,
+    CellComponent: CarbonCopyCell
+  },
+  electronicSafe: {
+    query: ({ queryBuilder, currentFolderId, sharedDocumentIds, attribute }) =>
+      queryBuilder({ currentFolderId, sharedDocumentIds, attribute }),
+    condition: ({ conditionBuilder, files, attribute }) =>
+      conditionBuilder({ files, attribute }),
+    label: 'electronicSafe',
+    HeaderComponent: ElectronicSafeHeader,
+    CellComponent: ElectronicSafeCell
+  }
 }
