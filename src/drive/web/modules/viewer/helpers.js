025e2d69c332cafbdf8b8fa158bda87031dbb2cd
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-01-19 10:06:22 +0100

    feat: Add some Viewer helpers

---
+++ src/drive/web/modules/viewer/helpers.js
@@ -0,0 +1,11 @@
+import has from 'lodash/has'
+
+export const hasCertifications = ({ file }) =>
+  has(file, 'metadata.carbonCopy') || has(file, 'metadata.electronicSafe')
+
+export const isInfoPanel = file => {
+  // TODO add rules for connectors
+
+  // for now we just need to return result for certifications
+  return hasCertifications({ file })
+}
