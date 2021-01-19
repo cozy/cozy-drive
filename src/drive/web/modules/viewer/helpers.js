e8246f0c8ea69584dd9c12fef801dd70eec43e14
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-01-19 12:12:19 +0100

    fix: Simplify FilesViewer Panel props and helpers

--- src/drive/web/modules/viewer/helpers.js
+++ src/drive/web/modules/viewer/helpers.js
@@ -3,7 +3,7 @@ import has from 'lodash/has'
 export const hasCertifications = ({ file }) =>
   has(file, 'metadata.carbonCopy') || has(file, 'metadata.electronicSafe')
 
-export const isInfoPanel = file => {
+export const showPanel = ({ file }) => {
   // TODO add rules for connectors
 
   // for now we just need to return result for certifications
