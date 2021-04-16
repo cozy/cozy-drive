9998a640d8a0eab70beed0649493d0981abd000b
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-04-16 12:30:25 +0200

    fix: FlagSwitcher in PublicLayout

--- src/drive/web/modules/public/PublicLayout.jsx
+++ src/drive/web/modules/public/PublicLayout.jsx
@@ -4,14 +4,14 @@ import Alerter from 'cozy-ui/transpiled/react/Alerter'
 import { IconSprite } from 'cozy-ui/transpiled/react'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { RouterContextProvider } from 'drive/lib/RouterContext'
-import flag, { FlagSwitcher } from 'cozy-flags'
+import FlagSwitcher from 'cozy-flags/dist/FlagSwitcher'
 
 const PublicLayout = ({ children, ...otherProps }) => {
   const { t } = useI18n()
   return (
     <RouterContextProvider {...otherProps}>
       <Layout>
-        {flag('switcher') && <FlagSwitcher />}
+        <FlagSwitcher />
         <Alerter t={t} />
         {children}
         <IconSprite />
