b295b3da5ca1ffc0972afda774971904779512cd
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-05-04 16:13:26 +0200

    feat: Add sharingBanner in Public view

--- src/drive/web/modules/views/OnlyOffice/Title.jsx
+++ src/drive/web/modules/views/OnlyOffice/Title.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
 import { DialogTitle } from 'cozy-ui/transpiled/react/Dialog'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
 
+import SharingBanner from 'drive/web/modules/public/SharingBanner'
+import { useSharingInfos } from 'drive/web/modules/public/useSharingInfos'
+import { OnlyOfficeContext } from 'drive/web/modules/views/OnlyOffice'
 import Toolbar from 'drive/web/modules/views/OnlyOffice/Toolbar'
 
 const useStyles = makeStyles(() => ({
   root: {
-    display: 'flex',
-    alignItems: 'center',
-    height: '48px',
-    padding: '0 1rem'
+    width: 'calc(100% - 1rem)',
+    height: '3rem'
   }
 }))
 
 const Title = () => {
+  const { isPublic } = useContext(OnlyOfficeContext)
+  const sharingInfos = useSharingInfos()
   const styles = useStyles()
 
   return (
@@ -23,12 +26,13 @@ const Title = () => {
       <DialogTitle
         data-testid="onlyoffice-title"
         disableTypography
-        className="u-ellipsis"
+        className="u-ellipsis u-flex u-flex-items-center u-p-0 u-pr-1"
         classes={styles}
       >
         <Toolbar />
       </DialogTitle>
       <Divider />
+      {isPublic && <SharingBanner sharingInfos={sharingInfos} />}
     </>
   )
 }
