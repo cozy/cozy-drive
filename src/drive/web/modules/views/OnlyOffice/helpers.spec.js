5f174450802cc79709e931db92b5e7135995ab3c
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-06-01 17:57:02 +0200

    feat: isOnlyOfficeEditorSupported uses now hasSharedParent props

--- src/drive/web/modules/views/OnlyOffice/helpers.spec.js
+++ src/drive/web/modules/views/OnlyOffice/helpers.spec.js
@@ -1,5 +1,47 @@
 import helpers, { isOnlyOfficeEditorSupported } from './helpers'
 
+const casesToTest = [
+  {
+    // the doc is not shared
+    isShared: false,
+    isSharedWithMe: false,
+    hasSharedParent: false
+  },
+  {
+    // the doc is shared by being the owner
+    isShared: true,
+    isSharedWithMe: false,
+    hasSharedParent: false
+  },
+  {
+    // the doc is shared but without being the owner
+    isShared: true,
+    isSharedWithMe: true,
+    hasSharedParent: false
+  },
+  {
+    // the doc is inside a shared folder
+    isShared: false,
+    isSharedWithMe: false,
+    hasSharedParent: true
+  }
+]
+
+const executeTests = (file, expectations) => {
+  casesToTest.forEach(
+    ({ isShared, isSharedWithMe, hasSharedParent }, index) => {
+      expect(
+        isOnlyOfficeEditorSupported({
+          file,
+          isShared,
+          isSharedWithMe,
+          hasSharedParent
+        })
+      ).toBe(expectations[index])
+    }
+  )
+}
+
 describe('isOnlyOfficeEditorSupported', () => {
   afterEach(() => {
     jest.clearAllMocks()
@@ -9,133 +51,33 @@ describe('isOnlyOfficeEditorSupported', () => {
     const file = { type: 'file', class: 'image', name: 'doc.png' }
 
     it('should return false no matter if it is shared', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: false,
-          isSharedWithMe: false
-        })
-      ).toBeFalsy()
-
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: false
-        })
-      ).toBeFalsy()
-
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: true
-        })
-      ).toBeFalsy()
+      const expectations = [false, false, false, false]
+      executeTests(file, expectations)
     })
 
     it('should return false no matter if an only office server is configured', () => {
       jest.spyOn(helpers, 'isOnlyOfficeEnabled').mockReturnValue(true)
 
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: false,
-          isSharedWithMe: false
-        })
-      ).toBeFalsy()
-
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: false
-        })
-      ).toBeFalsy()
-
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: true
-        })
-      ).toBeFalsy()
+      const expectations = [false, false, false, false]
+      executeTests(file, expectations)
     })
   })
 
-  describe('For an only office doc, without any only office server configured', () => {
+  describe('For an only office doc', () => {
     const file = { type: 'file', class: 'slide', name: 'doc.pptx' }
 
-    beforeEach(() => {
+    it('should return the expectation without any only office server configured', () => {
       jest.spyOn(helpers, 'isOnlyOfficeEnabled').mockReturnValue(false)
+
+      const expectations = [false, false, true, true]
+      executeTests(file, expectations)
     })
 
-    it('should return false if the doc is not shared', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: false,
-          isSharedWithMe: false
-        })
-      ).toBeFalsy()
-    })
-
-    it('should return false if the doc is shared by being the owner', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: false
-        })
-      ).toBeFalsy()
-    })
-
-    it('should return true if the doc is shared but without being the owner', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: true
-        })
-      ).toBeTruthy()
-    })
-  })
-
-  describe('For an only office doc, with an only office server configured', () => {
-    const file = { type: 'file', class: 'slide', name: 'doc.pptx' }
-
-    beforeEach(() => {
+    it('should return the expectation with an only office server configured', () => {
       jest.spyOn(helpers, 'isOnlyOfficeEnabled').mockReturnValue(true)
-    })
 
-    it('should return true if the doc is not shared', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: false,
-          isSharedWithMe: false
-        })
-      ).toBeTruthy()
-    })
-
-    it('should return true if the doc is shared by being the owner', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: false
-        })
-      ).toBeTruthy()
-    })
-
-    it('should return true if the doc is shared but without being the owner', () => {
-      expect(
-        isOnlyOfficeEditorSupported({
-          file,
-          isShared: true,
-          isSharedWithMe: true
-        })
-      ).toBeTruthy()
+      const expectations = [true, true, true, true]
+      executeTests(file, expectations)
     })
   })
 })
