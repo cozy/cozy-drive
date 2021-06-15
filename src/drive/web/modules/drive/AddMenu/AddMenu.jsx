617c236c447c39f014d2480608ba1193559d5613
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-06-15 16:04:05 +0200

    fix: ScanWrapper should be outside of the AddMenu
    to be still present after closing the menu

--- src/drive/web/modules/drive/AddMenu/AddMenu.jsx
+++ src/drive/web/modules/drive/AddMenu/AddMenu.jsx
@@ -6,7 +6,6 @@ import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import ActionMenu from 'cozy-ui/transpiled/react/ActionMenu'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 
-import ScanWrapper from 'drive/web/modules/drive/Toolbar/components/ScanWrapper'
 import AddFolderItem from 'drive/web/modules/drive/Toolbar/components/AddFolderItem'
 import CreateNoteItem from 'drive/web/modules/drive/Toolbar/components/CreateNoteItem'
 import CreateShortcut from 'drive/web/modules/drive/Toolbar/components/CreateShortcut'
@@ -63,23 +62,21 @@ const AddMenu = ({
   refreshFolderContent
 }) => {
   return (
-    <ScanWrapper>
-      <ActionMenu
-        anchorElRef={anchorRef}
-        onClose={handleClose}
-        autoclose={true}
-        popperOptions={{
-          strategy: 'fixed'
-        }}
-      >
-        <ActionMenuContent
-          isDisabled={isDisabled}
-          canCreateFolder={canCreateFolder}
-          canUpload={canUpload}
-          refreshFolderContent={refreshFolderContent}
-        />
-      </ActionMenu>
-    </ScanWrapper>
+    <ActionMenu
+      anchorElRef={anchorRef}
+      onClose={handleClose}
+      autoclose={true}
+      popperOptions={{
+        strategy: 'fixed'
+      }}
+    >
+      <ActionMenuContent
+        isDisabled={isDisabled}
+        canCreateFolder={canCreateFolder}
+        canUpload={canUpload}
+        refreshFolderContent={refreshFolderContent}
+      />
+    </ActionMenu>
   )
 }
 
