1dc6a79d2c8b0eecb8fc437193c999fe2fdfb3d1
agirard <merkkur39@gmail.com>
2021-06-21 16:21:27 +0200

    fix: Fix view when loading files

--- src/drive/web/modules/views/Sharings/index.spec.jsx
+++ src/drive/web/modules/views/Sharings/index.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { render, fireEvent, act } from '@testing-library/react'
-import { Router, hashHistory, Route, Redirect, IndexRoute } from 'react-router'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import { Router, hashHistory, Route, Redirect } from 'react-router'
 import FileHistory from 'components/FileHistory'
 
 import { setupStoreAndClient } from 'test/setup'
@@ -28,8 +28,12 @@ jest.mock('drive/web/modules/views/hooks', () => ({
   ...jest.requireActual('drive/web/modules/views/hooks'),
   useFilesQueryWithPath: jest.fn()
 }))
+jest.mock('cozy-client/dist/utils', () => ({
+  ...jest.requireActual('cozy-client/dist/utils'),
+  hasQueryBeenLoaded: jest.fn().mockReturnValue(true)
+}))
 
-const setup = () => {
+const setup = (allLoaded = true) => {
   const { store, client } = setupStoreAndClient()
 
   client.plugins.realtime = {
@@ -45,11 +49,11 @@ const setup = () => {
     <AppLike client={client} store={store}>
       <Router history={hashHistory}>
         <Redirect from="/" to="/sharings" />
-        <Route path="sharings">
-          <IndexRoute component={SharingsView} />
-          <Route component={SharingsView}>
-            <Route path="file/:fileId/revision" component={FileHistory} />
-          </Route>
+        <Route
+          path="sharings"
+          component={props => <SharingsView {...props} allLoaded={allLoaded} />}
+        >
+          <Route path="file/:fileId/revision" component={FileHistory} />
         </Route>
       </Router>
     </AppLike>
@@ -58,67 +62,95 @@ const setup = () => {
 }
 
 describe('Sharings View', () => {
-  it('tests the sharings view', async () => {
-    const nbFiles = 2
-    const path = '/test'
-    const dir_id = 'dirIdParent'
-    const updated_at = '2020-05-14T10:33:31.365224+02:00'
+  const nbFiles = 2
+  const path = '/test'
+  const dir_id = 'dirIdParent'
+  const updated_at = '2020-05-14T10:33:31.365224+02:00'
 
-    const filesFixture = generateFileFixtures({
-      nbFiles,
-      path,
-      dir_id,
-      updated_at
+  const filesFixture = generateFileFixtures({
+    nbFiles,
+    path,
+    dir_id,
+    updated_at
+  })
+
+  const filesFixtureWithPath = {
+    data: filesFixture.map(f => {
+      return {
+        ...f,
+        displayedPath: path
+      }
     })
+  }
 
-    const filesFixtureWithPath = {
-      data: filesFixture.map(f => {
-        return {
-          ...f,
-          displayedPath: path
-        }
-      })
-    }
+  it('should display placeholder when all files are not loaded', async () => {
+    const { container } = setup(false)
+
+    await waitFor(() => {
+      expect(
+        container.querySelector('.fil-content-file-placeholder')
+      ).not.toBeNull()
+    })
+  })
+
+  it('should not display placeholder when all files are loaded', async () => {
     useFilesQueryWithPath.mockReturnValue(filesFixtureWithPath)
 
-    const { getByText, findByText } = setup({
-      nbFiles,
-      path,
-      dir_id,
-      updated_at
+    const { container } = setup(true)
+
+    await waitFor(() => {
+      expect(
+        container.querySelector('.fil-content-file-placeholder')
+      ).toBeNull()
     })
-    const sleep = duration =>
-      new Promise(resolve => setTimeout(resolve, duration))
-    await act(async () => {
-      await sleep(100)
+  })
+
+  it('tests the sharings view', async () => {
+    useFilesQueryWithPath.mockReturnValue(filesFixtureWithPath)
+
+    const { getByText, findByText } = setup()
+
+    // // Display placeholder when all files are not loaded
+    // expect(
+    //   container.querySelector('.fil-content-file-placeholder')
+    // ).not.toBeNull()
+
+    // await act(async () => {
+    //   await new Promise(resolve => setTimeout(resolve, 100))
+    // })
+
+    // // Do not display placeholder when all files are loaded
+    // expect(container.querySelector('.fil-content-file-placeholder')).toBeNull()
+
+    await waitFor(() => {
+      // Get the HTMLElement containing the filename if exist. If not throw
+      const el0 = getByText(`foobar0`)
+      // Check if the filename is displayed with the extension. If not throw
+      getByTextWithMarkup(getByText, `foobar0.pdf`)
+      //get the FileRow element
+      const fileRow0 = el0.closest('.fil-content-row')
+      //check if the date is right
+      expect(fileRow0.getElementsByTagName('time')[0].dateTime).toEqual(
+        updated_at
+      )
+      //check the path to the parent's folder
+      const linkElement0 = fileRow0.getElementsByClassName('fil-file-path')[0]
+      expect(linkElement0.textContent).toEqual(path)
+
+      expect(linkElement0.href.endsWith(`#/folder/${dir_id}`)).toBe(true)
+
+      //check if the ActionMenu is displayed
+      fireEvent.click(fileRow0.getElementsByTagName('button')[0])
+      const el1 = getByText(`foobar1`)
+      const parentDiv1 = el1.closest('.fil-file')
+      expect(
+        parentDiv1.getElementsByClassName('fil-file-path')[0].textContent
+      ).toEqual(path)
+
+      // navigates  to the history view
+      const historyItem = getByText('History')
+      fireEvent.click(historyItem)
     })
-    // Get the HTMLElement containing the filename if exist. If not throw
-    const el0 = getByText(`foobar0`)
-    // Check if the filename is displayed with the extension. If not throw
-    getByTextWithMarkup(getByText, `foobar0.pdf`)
-    //get the FileRow element
-    const fileRow0 = el0.closest('.fil-content-row')
-    //check if the date is right
-    expect(fileRow0.getElementsByTagName('time')[0].dateTime).toEqual(
-      updated_at
-    )
-    //check the path to the parent's folder
-    const linkElement0 = fileRow0.getElementsByClassName('fil-file-path')[0]
-    expect(linkElement0.textContent).toEqual(path)
-
-    expect(linkElement0.href.endsWith(`#/folder/${dir_id}`)).toBe(true)
-
-    //check if the ActionMenu is displayed
-    fireEvent.click(fileRow0.getElementsByTagName('button')[0])
-    const el1 = getByText(`foobar1`)
-    const parentDiv1 = el1.closest('.fil-file')
-    expect(
-      parentDiv1.getElementsByClassName('fil-file-path')[0].textContent
-    ).toEqual(path)
-
-    // navigates  to the history view
-    const historyItem = getByText('History')
-    fireEvent.click(historyItem)
 
     await expect(findByText('FileHistory stub')).resolves.toBeTruthy()
   })
