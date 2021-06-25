eab7efb75c9748048878b9fb3efc3b9f88db43d6
Quentin Valmori <quentin.valmori@gmail.com>
2021-06-25 12:14:34 +0200

    refactor: Use CozyClient instead of clientjs

--- src/drive/web/modules/services/components/URLGetter.jsx
+++ src/drive/web/modules/services/components/URLGetter.jsx
@@ -1,22 +1,18 @@
-/* global cozy */
 import React from 'react'
-
-const getFileDownloadUrl = async id => {
-  const link = await cozy.client.files.getDownloadLinkById(id)
-  return `${cozy.client._url}${link}`
-}
-
+import { withClient } from 'cozy-client'
 class URLGetter extends React.Component {
   componentDidMount() {
     this.getURL()
   }
 
   async getURL() {
-    const { service } = this.props
+    const { service, client } = this.props
 
     try {
       const { id } = service.getData()
-      const url = await getFileDownloadUrl(id)
+      const url = await client
+        .collection('io.cozy.files')
+        .getDownloadLinkById(id)
       service.terminate({ url })
     } catch (error) {
       service.throw(error)
@@ -24,4 +20,4 @@ class URLGetter extends React.Component {
   }
 }
 
-export default URLGetter
+export default withClient(URLGetter)
