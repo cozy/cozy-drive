dabef0f7ec03e709fd12f584b514cfa600c4dcc4
Quentin Valmori <quentin.valmori@gmail.com>
2021-07-05 15:02:51 +0200

    fix: Pass the right arguments to the cozy-bar
    We decided to use data-cozy={{}} to get access to
    the variables injected by the cozy-stack.
    
    By removing data-cozy-*, cozy-bar was lost since
    the bar will look into cozy-data-appslug to detect
    the slug of the app. Since the bar can't do that
    anymore, we need to pass all the args during the
    init phase.

--- src/drive/targets/public/index.jsx
+++ src/drive/targets/public/index.jsx
@@ -41,7 +41,9 @@ const initCozyBar = (data, client) => {
       iconPath: data.app.icon,
       lang: data.locale,
       replaceTitleOnMobile: true,
-      isPublic: true
+      isPublic: true,
+      appSlug: data.app.slug,
+      appNamePrefix: data.app.prefix
     })
   }
 }
