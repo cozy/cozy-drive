43ff65c54feb48ad051502cf05d494571c88c8f5
paultranvan <paul@cozycloud.cc>
2021-12-03 17:13:04 +0100

    fix: Native mobile upload
    The native mobile upload was broken for encrypted directories because
    we did not try to unlock the vault. Thus, it only worked when the vault
    was already unlocked.
    Note this will be useful for the move as well.

--- src/drive/web/modules/move/FileList.jsx
+++ src/drive/web/modules/move/FileList.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { DumbFile as File } from 'drive/web/modules/filelist/File'
+import { VaultUnlocker } from 'cozy-keys-lib'
+import { ROOT_DIR_ID } from 'drive/constants/config'
+import { hasEncryptionRef } from 'drive/lib/encryption'
+
+const isEncryptedFolder = folder => {
+  return hasEncryptionRef(folder)
+}
 
 const isInvalidMoveTarget = (subjects, target) => {
   const isASubject = subjects.find(subject => subject._id === target._id)
@@ -9,31 +16,49 @@ const isInvalidMoveTarget = (subjects, target) => {
   return isAFile || isASubject
 }
 
-const FileList = ({ targets, files, navigateTo }) => (
-  <>
-    {files.map(file => (
-      <File
-        key={file.id}
-        disabled={isInvalidMoveTarget(targets, file)}
-        styleDisabled={isInvalidMoveTarget(targets, file)}
-        attributes={file}
-        displayedFolder={null}
-        actions={null}
-        isRenaming={false}
-        onFolderOpen={id => navigateTo(files.find(f => f.id === id))}
-        onFileOpen={null}
-        withSelectionCheckbox={false}
-        withFilePath={false}
-        withSharedBadge
+const FileList = ({ targets, files, folder, navigateTo }) => {
+  const [shouldUnlock, setShouldUnlock] = useState(true)
+  const isEncFolder = isEncryptedFolder(folder)
+
+  if (isEncFolder && shouldUnlock) {
+    return (
+      <VaultUnlocker
+        onDismiss={() => {
+          setShouldUnlock(false)
+          return navigateTo(ROOT_DIR_ID)
+        }}
+        onUnlock={() => setShouldUnlock(false)}
       />
-    ))}
-  </>
-)
+    )
+  } else {
+    return (
+      <>
+        {files.map(file => (
+          <File
+            key={file.id}
+            disabled={isInvalidMoveTarget(targets, file)}
+            styleDisabled={isInvalidMoveTarget(targets, file)}
+            attributes={file}
+            displayedFolder={null}
+            actions={null}
+            isRenaming={false}
+            onFolderOpen={id => navigateTo(files.find(f => f.id === id))}
+            onFileOpen={null}
+            withSelectionCheckbox={false}
+            withFilePath={false}
+            withSharedBadge
+          />
+        ))}
+      </>
+    )
+  }
+}
 
 FileList.propTypes = {
   targets: PropTypes.array.isRequired,
   files: PropTypes.array.isRequired,
-  navigateTo: PropTypes.func.isRequired
+  navigateTo: PropTypes.func.isRequired,
+  folder: PropTypes.object
 }
 
 export default FileList
