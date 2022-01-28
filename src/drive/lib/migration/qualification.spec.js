7fc3abcadb5c5a1988593f5b305208229109efe4
Pierre Cozy <pierre.trolle@cozycloud.cc>
2022-01-28 15:17:21 +0100

    feat(deps): Upgrade cozy-client to 27.14.2

--- src/drive/lib/migration/qualification.spec.js
+++ src/drive/lib/migration/qualification.spec.js
@@ -62,38 +62,6 @@ describe('qualification migration', () => {
     })
   })
 
-  it('should get the new qualification for a registration file qualified by cozy-scanner', () => {
-    const file = {
-      metadata: {
-        label: 'registration'
-      }
-    }
-    const qualif = getFileRequalification(file)
-    expect(qualif).toEqual({
-      label: 'vehicle_registration',
-      purpose: 'attestation',
-      sourceCategory: 'gov',
-      sourceSubCategory: 'transport',
-      subjects: ['right', 'identity']
-    })
-  })
-
-  it('should get the new qualification for a insurance_card file qualified by cozy-scanner', () => {
-    const file = {
-      metadata: {
-        label: 'insurance_card'
-      }
-    }
-    const qualif = getFileRequalification(file)
-    expect(qualif).toEqual({
-      label: 'national_health_insurance_card',
-      purpose: 'attestation',
-      sourceCategory: 'gov',
-      sourceSubCategory: 'health',
-      subjects: ['identity', 'insurance']
-    })
-  })
-
   it('should get the new qualification for a file qualified by a konnector', () => {
     const file = {
       metadata: {
