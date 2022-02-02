cd9a57f5ee4f02afd7ada978f1c713a7715da818
Pierre Cozy <pierre.trolle@cozycloud.cc>
2022-02-02 18:07:15 +0100

    fix(callToAction): Use rel=noreferrer to fix security fix
    Using target="_blank" without rel="noreferrer" (which implies
    rel="noopener") is a security risk in older browsers:
    see https://mathiasbynens.github.io/rel-noopener/#recommendations

---
+++ src/drive/web/modules/viewer/CallToAction.spec.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import CallToAction from './CallToAction'
+import localforage from 'localforage'
+import { NOVIEWER_DESKTOP_CTA } from 'components/pushClient'
+
+jest.mock('localforage')
+jest.mock('drive/config/config.json', () => ({
+  promoteDesktop: { isActivated: true }
+}))
+jest.mock('components/pushClient', () => ({
+  isClientAlreadyInstalled: jest.fn().mockResolvedValueOnce(false),
+  isLinux: jest.fn(),
+  NOVIEWER_DESKTOP_CTA: 'noviewer_desktop_cta'
+}))
+
+describe('CallToAction', () => {
+  it('should get item noviewer desktop from localforage', async () => {
+    // Given
+    localforage.getItem = jest.fn().mockResolvedValueOnce(false)
+
+    // When
+    await render(<CallToAction t={jest.fn()} />)
+
+    // Then
+    expect(localforage.getItem).toHaveBeenCalledWith(NOVIEWER_DESKTOP_CTA)
+  })
+
+  it('should use rel="noreferrer" (which implies rel="noopener", because it is a security risk', async done => {
+    // Given
+    localforage.getItem = jest.fn().mockResolvedValueOnce(false)
+
+    // When
+    const { container } = await render(<CallToAction t={jest.fn()} />)
+
+    // Then
+    setTimeout(() => {
+      expect(container.querySelector('a[target="_blank"]')).toHaveAttribute(
+        'rel',
+        'noreferrer'
+      )
+      done()
+    }, 1)
+  })
+})
