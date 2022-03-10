4251819e81d703c9f10b13a7215ea11d5d1f78fb
paultranvan <paul@cozycloud.cc>
2022-03-10 17:20:54 +0100

    test: Add encryption test

---
+++ src/drive/lib/encryption.spec.js
@@ -0,0 +1,27 @@
+import { getEncryptionKeyFromDirId } from './encryption'
+import CozyClient from 'cozy-client'
+import flag from 'cozy-flags'
+
+jest.mock('cozy-flags', () => jest.fn())
+
+describe('encryption', () => {
+  const client = new CozyClient({})
+  beforeEach(() => {
+    client.query = jest.fn().mockResolvedValue({
+      data: {
+        key: 'super-secret-key'
+      }
+    })
+  })
+  it('should return nothing when flag is not set', async () => {
+    const key = await getEncryptionKeyFromDirId(client, null)
+    expect(key).toBeNull()
+  })
+
+  it('should return encryption key when flag is set', async () => {
+    flag.mockReturnValue('drive.enable-encryption')
+
+    const key = await getEncryptionKeyFromDirId(client, null)
+    expect(key).toBe('super-secret-key')
+  })
+})
