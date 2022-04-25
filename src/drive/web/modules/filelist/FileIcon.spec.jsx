748fc05dc2f6b1e01f5eaf7790330037f9c184ad
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-04-25 14:40:55 +0200

    test(file-image): test flag feature

---
+++ src/drive/web/modules/filelist/FileIcon.spec.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import FileIcon from './FileIcon'
+
+jest.mock('cozy-flags', () => () => true)
+// eslint-disable-next-line react/display-name
+jest.mock('cozy-ui/transpiled/react/FileImageLoader', () => () => (
+  <div data-testid="FileImageLoader" />
+))
+
+describe('FileIcon', () => {
+  it('should return file image loader when file is image', () => {
+    // Given
+    const file = { class: 'image' }
+
+    // When
+    const { getByTestId } = render(<FileIcon file={file} />)
+
+    // Then
+    expect(getByTestId('FileImageLoader')).toBeInTheDocument()
+  })
+
+  it('should return file image loader when file is pdf', () => {
+    // Given
+    const file = { class: 'pdf' }
+
+    // When
+    const { getByTestId } = render(<FileIcon file={file} />)
+
+    // Then
+    expect(getByTestId('FileImageLoader')).toBeInTheDocument()
+  })
+})
