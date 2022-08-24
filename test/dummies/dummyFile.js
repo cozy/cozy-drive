0e565abc5c28958f518b55b0f8d3da77d5f731f4
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-08-24 15:33:08 +0200

    fix(search): Improve speed of search suggestions
    By preventing every notes fetching url, loading only once a note is clicked

---
+++ test/dummies/dummyFile.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+const { IOCozyFile } = require('cozy-client/dist/types')
+
+/**
+ * Create a dummy file, with overridden value of given param
+ *
+ * @param {Partial<IOCozyFile>} [file={}] - optional file with value to keep
+ * @returns {IOCozyFile} a dummy file
+ */
+export const dummyFile = file => ({
+  _id: 'id-file',
+  _type: 'doctype-file',
+  name: 'name',
+  id: 'id-file',
+  icon: 'icon',
+  path: '/path',
+  type: 'directory',
+  ...file
+})
+
+/**
+ * Create a dummy note, with overridden value of given param
+ *
+ * @param {Partial<IOCozyFile>} [note={}]
+ * @returns {IOCozyFile} a dummy note
+ */
+export const dummyNote = note => ({
+  ...dummyFile(),
+  type: 'file',
+  metadata: {
+    content: '',
+    schema: '',
+    title: '',
+    version: ''
+  },
+  ...note
+})
