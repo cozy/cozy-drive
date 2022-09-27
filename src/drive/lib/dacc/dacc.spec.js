727d29625e61e2b6b1c99db1a989f40c1597d945
paultranvan <paul@cozycloud.cc>
2022-09-27 11:46:06 +0200

    test: Add tests for trashed files and excluded slugs

--- src/drive/lib/dacc/dacc.spec.js
+++ src/drive/lib/dacc/dacc.spec.js
@@ -2,64 +2,100 @@ import {
   aggregateFilesSize,
   aggregateNonExcludedSlugs
 } from 'drive/lib/dacc/dacc'
-import { queryFilesByDate } from 'drive/lib/dacc/query'
+import { queryAllDocsWithFields } from 'drive/lib/dacc/query'
 
 jest.mock('drive/lib/dacc/query')
 
-const mockedFilesQueryResponse = {
-  data: [
-    {
+const mockedFilesQueryResponse = [
+  {
+    doc: {
+      type: 'file',
       size: 1048576,
       cozyMetadata: {
         createdByApp: 'drive'
       }
-    },
-    {
+    }
+  },
+  {
+    doc: {
+      type: 'file',
       size: 3145728,
       cozyMetadata: {
         createdByApp: 'drive'
       }
-    },
-    {
+    }
+  },
+  {
+    doc: {
+      type: 'file',
       size: 2097152,
       cozyMetadata: {
         createdByApp: 'edf'
       }
-    },
-    {
+    }
+  },
+  {
+    doc: {
+      type: 'file',
       size: 8388608,
       cozyMetadata: {
         createdByApp: 'maif'
       }
-    },
-    {
+    }
+  },
+  {
+    doc: {
+      type: 'file',
       size: 6291456,
       cozyMetadata: {
         createdByApp: 'maif-vie'
       }
     }
-  ],
-  next: false
-}
+  },
+  {
+    doc: {
+      type: 'file',
+      trashed: true,
+      size: 2290000,
+      cozyMetadata: {
+        createdByApp: 'maif-vie'
+      }
+    }
+  }
+]
 
-describe('dacc service', () => {
+describe('aggregateFilesSize', () => {
   beforeEach(() => {
-    queryFilesByDate.mockResolvedValue(mockedFilesQueryResponse)
+    queryAllDocsWithFields.mockResolvedValue(mockedFilesQueryResponse)
   })
   it('should aggregate sizes by slug', async () => {
     const sizesBySlug = await aggregateFilesSize(null, '2022-01-01')
     expect(Object.keys(sizesBySlug)).toEqual([
+      'trashed',
       'drive',
       'edf',
       'maif',
       'maif-vie'
     ])
-    expect(sizesBySlug['drive']).toEqual(4.194304)
-    expect(sizesBySlug['edf']).toEqual(2.097152)
-    expect(sizesBySlug['maif']).toEqual(8.388608)
-    expect(sizesBySlug['maif-vie']).toEqual(6.291456)
+    expect(sizesBySlug['drive']).toEqual(4.19)
+    expect(sizesBySlug['edf']).toEqual(2.1)
+    expect(sizesBySlug['maif']).toEqual(8.39)
+    expect(sizesBySlug['maif-vie']).toEqual(6.29)
+    expect(sizesBySlug['trashed']).toEqual(2.29)
   })
 
+  it('should aggregate all sizes but excluded slug', async () => {
+    const sizesBySlug = await aggregateFilesSize(null, '2022-01-01', {
+      excludedSlug: 'maif',
+      nonExcludedGroupLabel: 'not-maif'
+    })
+    const expectedValue =
+      Math.round((sizesBySlug['drive'] + sizesBySlug['edf']) * 100) / 100
+    expect(sizesBySlug['not-maif']).toEqual(expectedValue)
+  })
+})
+
+describe('aggregateNonExcludedSlugs', () => {
   it('should aggregate all sizes but excluded slug', async () => {
     const sizesBySlug = await aggregateFilesSize(null, '2022-01-01')
     const totalSize = aggregateNonExcludedSlugs(sizesBySlug, 'maif')
