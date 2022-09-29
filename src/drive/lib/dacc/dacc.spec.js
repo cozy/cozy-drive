583dac2f2d71bd22417e661f3812b87f0c90f7fc
paultranvan <paul@cozycloud.cc>
2022-09-29 17:03:29 +0200

    fix: Handle files with missing uploadedAt

--- src/drive/lib/dacc/dacc.spec.js
+++ src/drive/lib/dacc/dacc.spec.js
@@ -12,7 +12,8 @@ const mockedFilesQueryResponse = [
       type: 'file',
       size: 1048576,
       cozyMetadata: {
-        createdByApp: 'drive'
+        createdByApp: 'drive',
+        uploadedAt: '2021-01-01'
       }
     }
   },
@@ -20,6 +21,16 @@ const mockedFilesQueryResponse = [
     doc: {
       type: 'file',
       size: 3145728,
+      cozyMetadata: {
+        createdByApp: 'drive',
+        uploadedAt: '2021-01-01'
+      }
+    }
+  },
+  {
+    doc: {
+      type: 'file',
+      size: 4567892,
       cozyMetadata: {
         createdByApp: 'drive'
       }
@@ -30,7 +41,8 @@ const mockedFilesQueryResponse = [
       type: 'file',
       size: 2097152,
       cozyMetadata: {
-        createdByApp: 'edf'
+        createdByApp: 'edf',
+        uploadedAt: '2021-01-01'
       }
     }
   },
@@ -39,7 +51,8 @@ const mockedFilesQueryResponse = [
       type: 'file',
       size: 8388608,
       cozyMetadata: {
-        createdByApp: 'maif'
+        createdByApp: 'maif',
+        uploadedAt: '2021-01-01'
       }
     }
   },
@@ -48,7 +61,8 @@ const mockedFilesQueryResponse = [
       type: 'file',
       size: 6291456,
       cozyMetadata: {
-        createdByApp: 'maif-vie'
+        createdByApp: 'maif-vie',
+        uploadedAt: '2021-01-01'
       }
     }
   },
@@ -58,7 +72,8 @@ const mockedFilesQueryResponse = [
       trashed: true,
       size: 2290000,
       cozyMetadata: {
-        createdByApp: 'maif-vie'
+        createdByApp: 'maif-vie',
+        uploadedAt: '2021-01-01'
       }
     }
   }
@@ -69,7 +84,7 @@ describe('aggregateFilesSize', () => {
     queryAllDocsWithFields.mockResolvedValue(mockedFilesQueryResponse)
   })
   it('should aggregate sizes by slug', async () => {
-    const sizesBySlug = await aggregateFilesSize(null, '2022-01-01')
+    const sizesBySlug = await aggregateFilesSize(null, new Date('2022-01-01'))
     expect(Object.keys(sizesBySlug)).toEqual([
       'trashed',
       'drive',
@@ -85,7 +100,7 @@ describe('aggregateFilesSize', () => {
   })
 
   it('should aggregate all sizes but excluded slug', async () => {
-    const sizesBySlug = await aggregateFilesSize(null, '2022-01-01', {
+    const sizesBySlug = await aggregateFilesSize(null, new Date('2022-01-01'), {
       excludedSlug: 'maif',
       nonExcludedGroupLabel: 'not-maif'
     })
@@ -93,17 +108,38 @@ describe('aggregateFilesSize', () => {
       Math.round((sizesBySlug['drive'] + sizesBySlug['edf']) * 100) / 100
     expect(sizesBySlug['not-maif']).toEqual(expectedValue)
   })
+
+  it('should skip docs not file or without uploadedAt', async () => {
+    queryAllDocsWithFields.mockResolvedValueOnce([
+      {
+        doc: {
+          type: 'file',
+          size: 4567892,
+          cozyMetadata: {
+            createdByApp: 'drive'
+          }
+        }
+      },
+      {
+        doc: {
+          type: 'directory'
+        }
+      }
+    ])
+    const sizesBySlug = await aggregateFilesSize(null, new Date('2022-01-01'))
+    expect(sizesBySlug).toEqual({ trashed: 0 })
+  })
 })
 
 describe('aggregateNonExcludedSlugs', () => {
   it('should aggregate all sizes but excluded slug', async () => {
-    const sizesBySlug = await aggregateFilesSize(null, '2022-01-01')
+    const sizesBySlug = await aggregateFilesSize(null, new Date('2022-01-01'))
     const totalSize = aggregateNonExcludedSlugs(sizesBySlug, 'maif')
     expect(totalSize).toEqual(sizesBySlug['drive'] + sizesBySlug['edf'])
   })
 
   it('should aggregate nothing when excluded slug is empty', async () => {
-    const sizesBySlug = await aggregateFilesSize(null, '2022-01-01')
+    const sizesBySlug = await aggregateFilesSize(null, new Date('2022-01-01'))
     const totalSize = aggregateNonExcludedSlugs(sizesBySlug, '')
     expect(totalSize).toEqual(0)
   })
