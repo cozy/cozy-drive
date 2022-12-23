d7040a1fe5fd0614b992a44f055ee909248cd20d
Théo Poizat <hello@zatteo.com>
2022-12-23 15:49:21 +0100

    feat: Remove unused getCurrentViewFetchStatus function

--- src/drive/web/modules/selectors.spec.js
+++ src/drive/web/modules/selectors.spec.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import { mount } from 'enzyme'
 import {
   getFolderContent,
   getDisplayedFolder,
-  getCurrentViewFetchStatus,
   getCurrentFolderId,
   getCurrentFileId
 } from './selectors'
@@ -12,8 +9,6 @@ import {
   setupStoreAndClient,
   mockCozyClientRequestQuery
 } from 'test/setup'
-import AppLike from 'test/components/AppLike'
-import FolderContent from 'test/components/FolderContent'
 import { ROOT_DIR_ID, TRASH_DIR_ID } from 'drive/constants/config'
 
 jest.mock('drive/web/modules/navigation/AppRoute', () => ({ routes: [] }))
@@ -58,45 +53,6 @@ describe('getDisplayedFolder', () => {
   })
 })
 
-const sleep = duration => new Promise(resolve => setTimeout(resolve, duration))
-
-describe('getCurrentViewFetchStatus', () => {
-  it('should return the fetch status', async () => {
-    // TODO: Warning: An update to %s inside a test was not wrapped in act(...).
-    jest.spyOn(console, 'error').mockImplementation()
-
-    const folderId = 'directory-foobar0'
-    const initialStoreState = {
-      router: {
-        params: {
-          folderId: folderId
-        }
-      }
-    }
-
-    const { client, store } = setupStoreAndClient({ initialStoreState })
-
-    const sortOrder = {
-      attribute: 'name',
-      order: 'desc'
-    }
-    mount(
-      <AppLike store={store} client={client}>
-        <FolderContent folderId={folderId} sortOrder={sortOrder} />
-      </AppLike>
-    )
-
-    const state = store.getState()
-    const status = getCurrentViewFetchStatus(state)
-    expect(status).toEqual('loading')
-
-    await sleep(1)
-
-    const state2 = store.getState()
-    const status2 = getCurrentViewFetchStatus(state2)
-    expect(status2).toEqual('loaded')
-  })
-})
 describe('getCurrentFolderId', () => {
   it('test the getCurrentFolderId behavior', () => {
     expect(getCurrentFolderId('')).toBeNull()
