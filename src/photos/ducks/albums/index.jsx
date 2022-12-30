4efbd11ce20c2150285f5b1f576556f869272286
cballevre <celestin.ballevre@protonmail.com>
2022-12-30 16:53:13 +0100

    fix(Photos): Props.type for AlbumPhotos

--- src/photos/ducks/albums/index.jsx
+++ src/photos/ducks/albums/index.jsx
@@ -143,7 +143,7 @@ export const AlbumPhotosWithLoader =
           updateAlbum={updateAlbum}
           deleteAlbum={deleteAlbum}
           removePhotos={removePhotos}
-          hasMore={album.photos.hasMore}
+          hasMore={album.photos.hasMore !== undefined}
           fetchMore={album.photos.fetchMore.bind(album.photos)}
           lastFetch={lastFetch}
         >
