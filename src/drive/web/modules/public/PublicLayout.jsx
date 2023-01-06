736c036ecac6bcb7d94b6633a6741cd22eee4692
cballevre <celestin.ballevre@protonmail.com>
2023-01-06 15:56:41 +0100

    refactor(PublicLayout): Remove RouterContextProvider

--- src/drive/web/modules/public/PublicLayout.jsx
+++ src/drive/web/modules/public/PublicLayout.jsx
@@ -3,23 +3,20 @@ import { Layout } from 'cozy-ui/transpiled/react/Layout'
 import Alerter from 'cozy-ui/transpiled/react/Alerter'
 import Sprite from 'cozy-ui/transpiled/react/Icon/Sprite'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
-import { RouterContextProvider } from 'drive/lib/RouterContext'
 import FlagSwitcher from 'cozy-flags/dist/FlagSwitcher'
 import UploadQueue from 'drive/web/modules/upload/UploadQueue'
 
-const PublicLayout = ({ children, ...otherProps }) => {
+const PublicLayout = ({ children }) => {
   const { t } = useI18n()
   return (
-    <RouterContextProvider {...otherProps}>
-      <Layout>
-        <FlagSwitcher />
-        <Alerter t={t} />
-        <UploadQueue />
+    <Layout>
+      <FlagSwitcher />
+      <Alerter t={t} />
+      <UploadQueue />
 
-        {children}
-        <Sprite />
-      </Layout>
-    </RouterContextProvider>
+      {children}
+      <Sprite />
+    </Layout>
   )
 }
 
