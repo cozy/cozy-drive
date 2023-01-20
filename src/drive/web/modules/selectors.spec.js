df9e1f07434ca7ae35a85225191bc82a128686d6
Théo Poizat <hello@zatteo.com>
2023-01-20 16:36:38 +0100

    test: Remove useless tests from old selectors

--- src/drive/web/modules/selectors.spec.js
+++ src/drive/web/modules/selectors.spec.js
@@ -1,15 +1,9 @@
-import {
-  getFolderContent,
-  getDisplayedFolder,
-  getCurrentFolderId,
-  getCurrentFileId
-} from './selectors'
+import { getFolderContent } from './selectors'
 import {
   setupFolderContent,
   setupStoreAndClient,
   mockCozyClientRequestQuery
 } from 'test/setup'
-import { ROOT_DIR_ID, TRASH_DIR_ID } from 'drive/constants/config'
 
 jest.mock('drive/web/modules/navigation/AppRoute', () => ({ routes: [] }))
 
@@ -30,46 +24,5 @@ describe('getFolderContent', () => {
     const state = store.getState()
     const files = getFolderContent(state, folderId)
     expect(files.length).toBe(13)
-  })
-})
-
-describe('getDisplayedFolder', () => {
-  it('should return the currently displayed folder as a io.cozy.file', async () => {
-    const folderId = 'directory-foobar0'
-    const { store } = await setupFolderContent({
-      folderId,
-      initialStoreState: {
-        router: {
-          params: {
-            folderId: folderId
-          }
-        }
-      }
-    })
-    const state = store.getState()
-    const folder = getDisplayedFolder(state)
-    expect(folder._id).toEqual(folderId)
-    expect(folder.name).toEqual('foobar0')
-  })
-})
-
-describe('getCurrentFolderId', () => {
-  it('test the getCurrentFolderId behavior', () => {
-    expect(getCurrentFolderId('')).toBeNull()
-    expect(
-      getCurrentFolderId({ router: { location: { pathname: '/trash' } } })
-    ).toEqual(TRASH_DIR_ID)
-    expect(
-      getCurrentFolderId({ router: { location: { pathname: '/folder' } } })
-    ).toEqual(ROOT_DIR_ID)
-    expect(
-      getCurrentFolderId({ router: { params: { folderId: 'folderID' } } })
-    ).toEqual('folderID')
-  })
-})
-
-describe('getCurrentFileId', () => {
-  it('will not crash if there is no router state', () => {
-    expect(getCurrentFileId('')).toBeNull()
   })
 })
