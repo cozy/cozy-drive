660d17a7df8ef3e6fa25924030c6a2d04b5ccb00
Théo Poizat <hello@zatteo.com>
2023-02-10 23:31:21 +0100

    refactor(Unlink): Use navigate hook instead router

--- src/drive/mobile/modules/settings/components/Unlink.jsx
+++ src/drive/mobile/modules/settings/components/Unlink.jsx
@@ -1,54 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { withClient } from 'cozy-client'
+import { useClient } from 'cozy-client'
 import { connect } from 'react-redux'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useNavigate } from 'react-router-dom'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import SettingCategory, { ELEMENT_BUTTON } from './SettingCategory'
 import {
   unlink,
   getClientSettings
 } from 'drive/mobile/modules/authorization/duck'
 
-export class Unlink extends Component {
-  render() {
-    const { t, unlink, clientSettings, client } = this.props
-    return (
-      <div>
-        <SettingCategory
-          title={t('mobile.settings.unlink.title')}
-          elements={[
-            {
-              type: ELEMENT_BUTTON,
-              description: t('mobile.settings.unlink.description'),
-              text: t('mobile.settings.unlink.button'),
-              theme: 'danger-outline',
-              onClick: () => unlink(client, clientSettings)
-            }
-          ]}
-        />
-      </div>
-    )
-  }
+export const Unlink = ({ unlink, clientSettings }) => {
+  const { t } = useI18n()
+  const client = useClient()
+  const navigate = useNavigate()
+  return (
+    <div>
+      <SettingCategory
+        title={t('mobile.settings.unlink.title')}
+        elements={[
+          {
+            type: ELEMENT_BUTTON,
+            description: t('mobile.settings.unlink.description'),
+            text: t('mobile.settings.unlink.button'),
+            theme: 'danger-outline',
+            onClick: () => unlink(client, clientSettings, navigate)
+          }
+        ]}
+      />
+    </div>
+  )
 }
 Unlink.propTypes = {
-  t: PropTypes.func,
   unlink: PropTypes.func,
-  clientSettings: PropTypes.object,
-  router: PropTypes.object
+  clientSettings: PropTypes.object
 }
 
 const mapStateToProps = state => ({
   clientSettings: getClientSettings(state)
 })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  unlink: (client, clientSettings) => {
+const mapDispatchToProps = dispatch => ({
+  unlink: (client, clientSettings, navigate) => {
     dispatch(unlink(client, clientSettings))
-    ownProps.router.replace('/onboarding')
+    navigate('/onboarding', { replace: true })
   }
 })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(translate()(withClient(Unlink)))
+export default connect(mapStateToProps, mapDispatchToProps)(Unlink)
