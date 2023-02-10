efe8d8331acc9fa0815905bc68ca2423a71a0f4a
Théo Poizat <hello@zatteo.com>
2023-02-10 22:31:33 +0100

    test: Fix InsideRegularFolder test with hooks mock
    I also homogenized test cases.

--- src/drive/web/modules/drive/Toolbar/components/InsideRegularFolder.spec.jsx
+++ src/drive/web/modules/drive/Toolbar/components/InsideRegularFolder.spec.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import InsideRegularFolder from './InsideRegularFolder'
+import { useCurrentFolderId, useDisplayedFolder } from 'drive/hooks'
 
-jest.mock('drive/hooks', () => ({
-  useCurrentFolderId: jest.fn().mockReturnValue('id1'),
-  useDisplayedFolder: jest.fn().mockReturnValue({ id: 'id2' })
-}))
+jest.mock('drive/hooks')
 
 describe('InsideRegularFolder', () => {
   it('should return null when insideRegularFolder undefined', () => {
+    // Given
+    useCurrentFolderId.mockReturnValue()
+
     // When
-    const { container } = render(<InsideRegularFolder />)
+    const { container } = render(
+      <InsideRegularFolder>
+        <div />
+      </InsideRegularFolder>
+    )
 
     // Then
     expect(container).toBeEmptyDOMElement()
   })
 
-  it('should return children when insideRegularFolder defined', () => {
+  it('should return children when insideRegularFolder true', () => {
+    // Given
+    useCurrentFolderId.mockReturnValue('current-folder')
+    useDisplayedFolder.mockReturnValue({ id: 'displayed-folder' })
+
     // When
     const { container } = render(
-      <InsideRegularFolder insideRegularFolder>
+      <InsideRegularFolder>
         <div />
       </InsideRegularFolder>
     )
