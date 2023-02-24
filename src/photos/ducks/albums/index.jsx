217e6eb5b113aa3188bae91dd83407cb13eb5ff6
Théo Poizat <hello@zatteo.com>
2023-02-24 17:15:37 +0100

    fix: useParams imported from react-router instead of react-router-dom

--- src/photos/ducks/albums/index.jsx
+++ src/photos/ducks/albums/index.jsx
@@ -10,7 +10,7 @@ import AddToAlbumModal from './components/AddToAlbumModal'
 import Loading from '../../components/Loading'
 
 import { DOCTYPE_ALBUMS } from 'drive/lib/doctypes'
-import { useParams } from 'react-router'
+import { useParams } from 'react-router-dom'
 import { buildAlbumsQuery } from '../../queries/queries'
 
 const ALBUMS_QUERY = client =>
