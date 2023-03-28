03ebb6008e51b2089dd32cfc8448b6a939774da6
cballevre <celestin.ballevre@protonmail.com>
2023-03-28 15:56:55 +0200

    refactor: Extract router from public index.jsx into an AppRouter

---
+++ src/drive/targets/public/components/AppRouter.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+import { models } from 'cozy-client'
+
+import PublicLayout from 'drive/web/modules/public/PublicLayout'
+import PublicFolderView from 'drive/web/modules/views/Public'
+import LightFileViewer from 'drive/web/modules/public/LightFileViewer'
+import FileHistory from 'components/FileHistory'
+import OnlyOfficePaywallView from 'drive/web/modules/views/OnlyOffice/OnlyOfficePaywallView'
+import { redirectToOnlyOfficePaywall } from 'drive/web/modules/views/OnlyOffice/helpers'
+import OnlyOfficeView from 'drive/web/modules/views/OnlyOffice'
+import OnlyOfficeCreateView from 'drive/web/modules/views/OnlyOffice/Create'
+import { isOfficeEnabled } from 'drive/web/modules/views/OnlyOffice/helpers'
+import ExternalRedirect from 'drive/web/modules/navigation/ExternalRedirect'
+
+const AppRouter = ({
+  isReadOnly,
+  username,
+  isOnlyOfficeDocShared,
+  sharedDocumentId,
+  data
+}) => {
+  const isFile = data && data.type === 'file'
+
+  return (
+    <Router history={hashHistory}>
+      <Route component={PublicLayout}>
+        {isOfficeEnabled() && (
+          <>
+            <Route
+              path="onlyoffice/:fileId"
+              component={props => (
+                <OnlyOfficeView
+                  {...props}
+                  isPublic={true}
+                  isReadOnly={isReadOnly}
+                  username={username}
+                  isFromSharing={isOnlyOfficeDocShared}
+                  isInSharedFolder={!isFile}
+                />
+              )}
+            >
+              <Route
+                path="paywall"
+                component={props => (
+                  <OnlyOfficePaywallView {...props} isPublic={true} />
+                )}
+              />
+            </Route>
+            <Route
+              path="onlyoffice/:fileId/fromCreate"
+              component={props => (
+                <OnlyOfficeView
+                  {...props}
+                  isPublic={true}
+                  isReadOnly={isReadOnly}
+                  isInSharedFolder={!isFile}
+                />
+              )}
+            />
+            <Route
+              onEnter={redirectToOnlyOfficePaywall}
+              path="onlyoffice/create/:folderId/:fileClass"
+              component={OnlyOfficeCreateView}
+            />
+            {models.file.shouldBeOpenedByOnlyOffice(data) && (
+              <Redirect from="/" to={`onlyoffice/${data.id}`} />
+            )}
+          </>
+        )}
+
+        {isFile && (
+          <Route
+            path="/"
+            component={() => <LightFileViewer files={[data]} />}
+          />
+        )}
+
+        {!isFile && (
+          <>
+            <Redirect from="/files/:folderId" to="/folder/:folderId" />
+            <Route path="folder(/:folderId)" component={PublicFolderView}>
+              <Route path="file/:fileId/revision" component={FileHistory} />
+              <Route
+                path="paywall"
+                component={props => (
+                  <OnlyOfficePaywallView {...props} isPublic={true} />
+                )}
+              />
+            </Route>
+
+            <Route path="external/:fileId" component={ExternalRedirect} />
+            <Redirect from="/*" to={`folder/${sharedDocumentId}`} />
+          </>
+        )}
+      </Route>
+    </Router>
+  )
+}
+
+export default AppRouter
