399a96980e464cf6d6f9e60cbbe0a756f6b0cd45
Crash-- <quentin.valmori@gmail.com>
2023-04-11 20:11:06 +0200

    fix: Scroll to top
     Since we are not able to restore the scroll correctly,
     and force the scroll to top every time we change the
     current folder. This is to avoid this kind of weird
     behavior:
     - If I go to a sub-folder, if this subfolder has a lot
     of data and I scrolled down until the bottom. If I go
     back, then my folder will also be scrolled down.
    
     This is an ugly hack, yeah.

--- src/drive/web/modules/views/Folder/FolderViewBody.jsx
+++ src/drive/web/modules/views/Folder/FolderViewBody.jsx
@@ -47,9 +47,33 @@ const FolderViewBody = ({
   refreshFolderContent = null,
   extraColumns
 }) => {
-  const { router } = useRouter()
   const { isDesktop } = useBreakpoints()
+  const { router } = useRouter()
   const client = useClient()
+  /**
+   *  Since we are not able to restore the scroll correctly,
+   * and force the scroll to top every time we change the
+   * current folder. This is to avoid this kind of weird
+   * behavior:
+   * - If I go to a sub-folder, if this subfolder has a lot
+   * of data and I scrolled down until the bottom. If I go
+   * back, then my folder will also be scrolled down.
+   *
+   * This is an ugly hack, yeah.
+   * */
+  useEffect(() => {
+    if (isDesktop) {
+      const scrollable = document.querySelectorAll(
+        '[data-testid=fil-content-body]'
+      )[0]
+      if (scrollable) {
+        scrollable.scroll({ top: 0 })
+      }
+    } else {
+      window.scroll({ top: 0 })
+    }
+  }, [currentFolderId, isDesktop])
+
   const { isBigThumbnail, toggleThumbnailSize } =
     useContext(ThumbnailSizeContext)
   const { sharingsValue } = useContext(AcceptingSharingContext)
