4877b8497a60548df8bebbc08d09ee68c9f2344f
cballevre <celestin.ballevre@protonmail.com>
2023-04-18 12:00:38 +0200

    feat: Update cozy-ui from 82.13.0 to 83.1.0
    This update provides a newer version of react-pdf to fix some issues with PDF rendering

--- src/drive/web/modules/viewer/PublicViewer.jsx
+++ src/drive/web/modules/viewer/PublicViewer.jsx
@@ -1,6 +1,6 @@
 // A viewer with a custom pdfjs worker that will be available on the public pages of the app
 import { Viewer } from 'cozy-ui/transpiled/react'
-import createWorker from 'react-pdf/dist/pdf.worker.entry.js'
+import createWorker from 'react-pdf/dist/esm/pdf.worker.entry'
 import { pdfjs } from 'react-pdf'
 
 pdfjs.GlobalWorkerOptions.workerPort = createWorker()
