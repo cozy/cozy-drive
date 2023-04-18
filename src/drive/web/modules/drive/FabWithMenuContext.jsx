5945982d85d41bd4f06de4eea6de56a8f01c1e62
cballevre <celestin.ballevre@protonmail.com>
2023-04-18 11:51:19 +0200

    feat: Extend the FAB for add actions to the top of the page

--- src/drive/web/modules/drive/FabWithMenuContext.jsx
+++ src/drive/web/modules/drive/FabWithMenuContext.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react'
 
 import PlusIcon from 'cozy-ui/transpiled/react/Icons/Plus'
-
-import Fab from 'drive/web/modules/drive/Fab'
-import { AddMenuContext } from 'drive/web/modules/drive/AddMenu/AddMenuProvider'
-import Icon from 'cozy-ui/transpiled/react/Icon'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import { ExtendableFab } from 'cozy-ui/transpiled/react/Fab'
+
+import { AddMenuContext } from 'drive/web/modules/drive/AddMenu/AddMenuProvider'
+import { useFabStyles } from 'drive/web/modules/drive/helpers'
 
 const FabWithMenuContext = ({ noSidebar }) => {
   const { t } = useI18n()
@@ -19,21 +19,25 @@ const FabWithMenuContext = ({ noSidebar }) => {
     a11y
   } = useContext(AddMenuContext)
 
+  const styles = useFabStyles({
+    bottom: noSidebar ? '1rem' : 'calc(var(--sidebarHeight) + 1rem)'
+  })
+
   return (
     <div
       ref={anchorRef ? anchorRef : undefined}
       onClick={isOffline ? handleOfflineClick : undefined}
     >
-      <Fab
-        bottom={noSidebar ? '1rem' : 'calc(var(--sidebarHeight) + 1rem)'}
-        aria-label={t('button.add')}
-        disabled={isDisabled || isOffline}
+      <ExtendableFab
         color="primary"
+        label={t('button.add')}
+        icon={PlusIcon}
+        className={styles.root}
+        disabled={isDisabled || isOffline}
+        follow={window}
         onClick={handleToggle}
         {...a11y}
-      >
-        <Icon icon={PlusIcon} />
-      </Fab>
+      />
     </div>
   )
 }
--- src/drive/web/modules/views/OnlyOffice/ReadOnlyFab.jsx
+++ src/drive/web/modules/views/OnlyOffice/ReadOnlyFab.jsx
@@ -5,11 +5,12 @@ import RenameIcon from 'cozy-ui/transpiled/react/Icons/Rename'
 import CheckIcon from 'cozy-ui/transpiled/react/Icons/Check'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Fab from 'cozy-ui/transpiled/react/Fab'
 
-import Fab from 'drive/web/modules/drive/Fab'
 import { useRouter } from 'drive/lib/RouterContext'
 import { OnlyOfficeContext } from 'drive/web/modules/views/OnlyOffice'
 import { canWriteOfficeDocument } from 'drive/web/modules/views/OnlyOffice/helpers'
+import { useFabStyles } from 'drive/web/modules/drive/helpers'
 
 const ReadOnlyFab = () => {
   const { router } = useRouter()
@@ -31,12 +32,20 @@ const ReadOnlyFab = () => {
     ? t('OnlyOffice.actions.edit')
     : t('OnlyOffice.actions.validate')
 
-  const fabProps = isMobile
-    ? { 'aria-label': label }
-    : { variant: 'extended', right: '30px', bottom: '55px' }
+  const fabProps = isMobile ? { 'aria-label': label } : { variant: 'extended' }
+
+  const styles = useFabStyles({
+    right: isMobile ? '1rem' : '30px',
+    bottom: isMobile ? '1rem' : '55px'
+  })
 
   return (
-    <Fab color="primary" onClick={handleClick} {...fabProps}>
+    <Fab
+      className={styles.root}
+      color="primary"
+      onClick={handleClick}
+      {...fabProps}
+    >
       <Icon
         icon={isEditorModeView ? RenameIcon : CheckIcon}
         className={!isMobile ? 'u-mr-half' : ''}
