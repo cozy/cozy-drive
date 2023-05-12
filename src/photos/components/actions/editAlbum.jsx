a42a1906c53c570327ccbb9aec76e1360b1d57c7
cballevre <celestin.ballevre@protonmail.com>
2023-05-12 17:59:19 +0200

    refactor(AlbumToolbar): Use ActionsMenu instead Menu

---
+++ src/photos/components/actions/editAlbum.jsx
@@ -0,0 +1,26 @@
+import React, { forwardRef } from 'react'
+
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import ActionMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
+import AlbumAddIcon from 'cozy-ui/transpiled/react/Icons/AlbumAdd'
+
+const editAlbum = router => () => ({
+  name: 'editAlbum',
+  action: () => router.push(`${router.location.pathname}/edit`),
+  Component: forwardRef(function EditAlbum(props, ref) {
+    const { t } = useI18n()
+    return (
+      <ActionMenuItem {...props} ref={ref}>
+        <ListItemIcon>
+          <Icon icon={AlbumAddIcon} />
+        </ListItemIcon>
+        <ListItemText primary={t('Toolbar.menu.add_photos')} />
+      </ActionMenuItem>
+    )
+  })
+})
+
+export default editAlbum
--- src/photos/components/actions/index.js
+++ src/photos/components/actions/index.js
@@ -1,1 +1,7 @@
 export { default as newAlbum } from './newAlbum'
+export { default as download } from './download'
+export { default as shareAlbum } from './shareAlbum'
+export { default as deleteAlbum } from './deleteAlbum'
+export { default as selectItems } from './selectItems'
+export { default as renameAlbum } from './renameAlbum'
+export { default as editAlbum } from './editAlbum'
