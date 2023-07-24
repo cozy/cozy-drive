f95a9f3ebaf3d11b211c26397b7300e9f952deee
Théo Poizat <hello@zatteo.com>
2023-07-24 13:39:45 +0200

    feat(backup): Update wording and what is shown in desktop

--- src/photos/ducks/backup/components/BackupDescription.jsx
+++ src/photos/ducks/backup/components/BackupDescription.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
-import { useBreakpoints } from 'cozy-ui/transpiled/react'
 
 const BackupDescription = () => {
   const { t } = useI18n()
-  const { isMobile } = useBreakpoints()
 
   return (
     <>
-      <Typography className="u-mt-1-half" align={isMobile ? 'center' : 'left'}>
+      <Typography className="u-mt-1-half" align={'center'}>
         {t('Backup.description.first')}
       </Typography>
-      <Typography className="u-mt-1" align={isMobile ? 'center' : 'left'}>
+      <Typography className="u-mt-1" align={'center'}>
         {t('Backup.description.second')}
       </Typography>
     </>
