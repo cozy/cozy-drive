264e68c561d7c94f38f917f6b41c65356c1e0b60
Théo Poizat <hello@zatteo.com>
2023-07-27 12:21:23 +0200

    fix(backup): Add upload illus for backup and use it

--- src/photos/ducks/backup/components/BackupHeader.jsx
+++ src/photos/ducks/backup/components/BackupHeader.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
-import PhotoSync from 'photos/assets/illustrations/cozy-photo-sync.svg'
+import PhotoUpload from 'photos/assets/illustrations/cozy-photo-upload.svg'
 
 const BackupHeader = () => {
   const { t } = useI18n()
 
   return (
     <div className="u-flex u-flex-column u-flex-items-center">
-      <img src={PhotoSync} style={{ width: '8rem' }} aria-hidden />
+      <img src={PhotoUpload} style={{ width: '8rem' }} aria-hidden />
       <Typography variant="h4" className="u-mt-1">
         {t('Backup.header.title')}
       </Typography>
