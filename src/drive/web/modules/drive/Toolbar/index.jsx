4fdf8bad9f9a3a1f3624e2c8b67a8e71a38e60aa
cballevre <celestin.ballevre@protonmail.com>
2023-07-28 09:07:26 +0200

    fix(Toolbar): Render for each properties
    Since migrating to react-router v6, the component is not rendered every time, which creates inconsistencies in the sharing modal.  This is because we select the properties that would trigger rendering and the new properties from the react-router migration were not selected. I've decided to remove this check to avoid similar errors in the future. I took the opportunity to convert the component to functional

--- src/drive/web/modules/drive/Toolbar/index.jsx
+++ src/drive/web/modules/drive/Toolbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import { useNavigate, useLocation, useParams } from 'react-router-dom'
@@ -20,107 +20,93 @@ import { BarRightWithProvider } from 'components/Bar'
 
 import styles from 'drive/styles/toolbar.styl'
 
-class Toolbar extends Component {
-  static contextTypes = {
-    store: PropTypes.object.isRequired
+const Toolbar = ({
+  folderId,
+  disabled,
+  canUpload,
+  canCreateFolder,
+  hasWriteAccess
+}) => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  const params = useParams()
+  const displayedFolder = useDisplayedFolder()
+  const { isMobile } = useBreakpoints()
+  const { showSelectionBar, isSelectionBarVisible } = useSelectionContext()
+
+  const isDisabled = disabled || isSelectionBarVisible
+
+  if (disabled) {
+    return null
   }
 
-  static defaultProps = {
-    canUpload: false,
-    canCreateFolder: false,
-    hasWriteAccess: false
-  }
-
-  shouldComponentUpdate(nextProps) {
-    if (
-      nextProps.disabled !== this.props.disabled ||
-      nextProps.isSelectionBarVisible !== this.props.isSelectionBarVisible ||
-      nextProps.canUpload !== this.props.canUpload ||
-      nextProps.canCreateFolder !== this.props.canCreateFolder ||
-      nextProps.hasWriteAccess !== this.props.hasWriteAccess ||
-      nextProps.isShared !== this.props.isShared ||
-      nextProps.breakpoints.isMobile !== this.props.breakpoints.isMobile
-    ) {
-      return true
-    }
-    return false
-  }
-
-  render() {
-    const {
-      disabled,
-      canUpload,
-      canCreateFolder,
-      hasWriteAccess,
-      breakpoints: { isMobile },
-      navigate,
-      pathname,
-      params,
-      folderId,
-      displayedFolder,
-      showSelectionBar,
-      isSelectionBarVisible
-    } = this.props
-
-    const isDisabled = disabled || isSelectionBarVisible
-    if (disabled) {
-      return null
-    }
-
-    return (
-      <div
-        data-testid="fil-toolbar-files"
-        className={cx(styles['fil-toolbar-files'], 'u-flex-items-center')}
-        role="toolbar"
+  return (
+    <div
+      data-testid="fil-toolbar-files"
+      className={cx(styles['fil-toolbar-files'], 'u-flex-items-center')}
+      role="toolbar"
+    >
+      <InsideRegularFolder
+        displayedFolder={displayedFolder}
+        folderId={folderId}
       >
-        <InsideRegularFolder
-          displayedFolder={displayedFolder}
-          folderId={folderId}
-        >
-          <SharedRecipients />
-        </InsideRegularFolder>
-        <InsideRegularFolder
-          displayedFolder={displayedFolder}
-          folderId={folderId}
-        >
-          <ShareButton isDisabled={isDisabled} />
-        </InsideRegularFolder>
+        <SharedRecipients />
+      </InsideRegularFolder>
+      <InsideRegularFolder
+        displayedFolder={displayedFolder}
+        folderId={folderId}
+      >
+        <ShareButton isDisabled={isDisabled} />
+      </InsideRegularFolder>
 
-        {hasWriteAccess && (
-          <AddMenuProvider
+      {hasWriteAccess && (
+        <AddMenuProvider
+          canCreateFolder={canCreateFolder}
+          canUpload={canUpload}
+          disabled={isDisabled}
+          navigate={navigate}
+          params={params}
+          displayedFolder={displayedFolder}
+          isSelectionBarVisible={isSelectionBarVisible}
+        >
+          <AddButton />
+        </AddMenuProvider>
+      )}
+
+      <BarRightWithProvider>
+        {isMobile && <SearchButton navigate={navigate} pathname={pathname} />}
+        <SharingProvider doctype="io.cozy.files" documentType="Files">
+          <MoreMenu
+            isDisabled={isDisabled}
+            hasWriteAccess={hasWriteAccess}
             canCreateFolder={canCreateFolder}
             canUpload={canUpload}
-            disabled={isDisabled}
             navigate={navigate}
             params={params}
+            folderId={folderId}
             displayedFolder={displayedFolder}
+            showSelectionBar={showSelectionBar}
             isSelectionBarVisible={isSelectionBarVisible}
-          >
-            <AddButton />
-          </AddMenuProvider>
-        )}
+            pathname={pathname}
+          />
+        </SharingProvider>
+      </BarRightWithProvider>
+    </div>
+  )
+}
 
-        <BarRightWithProvider store={this.context.store}>
-          {isMobile && <SearchButton navigate={navigate} pathname={pathname} />}
-          <SharingProvider doctype="io.cozy.files" documentType="Files">
-            <MoreMenu
-              isDisabled={isDisabled}
-              hasWriteAccess={hasWriteAccess}
-              canCreateFolder={canCreateFolder}
-              canUpload={canUpload}
-              navigate={navigate}
-              params={params}
-              folderId={folderId}
-              displayedFolder={displayedFolder}
-              showSelectionBar={showSelectionBar}
-              isSelectionBarVisible={isSelectionBarVisible}
-              pathname={pathname}
-            />
-          </SharingProvider>
-        </BarRightWithProvider>
-      </div>
-    )
-  }
+Toolbar.propTypes = {
+  folderId: PropTypes.string,
+  disabled: PropTypes.bool,
+  canUpload: PropTypes.bool,
+  canCreateFolder: PropTypes.bool,
+  hasWriteAccess: PropTypes.bool
+}
+
+Toolbar.defaultProps = {
+  canUpload: false,
+  canCreateFolder: false,
+  hasWriteAccess: false
 }
 
 /**
@@ -131,32 +117,20 @@ class Toolbar extends Component {
  */
 const ToolbarWithSharingContext = props => {
   const folderId = useCurrentFolderId()
-  const navigate = useNavigate()
-  const { pathname } = useLocation()
-  const params = useParams()
-  const displayedFolder = useDisplayedFolder()
-  const breakpoints = useBreakpoints()
-  const { showSelectionBar, isSelectionBarVisible } = useSelectionContext()
-
-  const toolbarProps = {
-    ...props,
-    navigate,
-    pathname,
-    params,
-    displayedFolder,
-    folderId,
-    breakpoints,
-    showSelectionBar,
-    isSelectionBarVisible
-  }
 
   return !folderId ? (
-    <Toolbar {...toolbarProps} />
+    <Toolbar {...props} />
   ) : (
     <SharedDocument docId={folderId}>
       {sharingProps => {
         const { hasWriteAccess } = sharingProps
-        return <Toolbar {...toolbarProps} hasWriteAccess={hasWriteAccess} />
+        return (
+          <Toolbar
+            hasWriteAccess={hasWriteAccess}
+            folderId={folderId}
+            {...props}
+          />
+        )
       }}
     </SharedDocument>
   )
