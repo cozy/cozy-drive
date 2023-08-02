8713bc05c6cd28514a3d7b7e86a951e81d8c8eda
cballevre <celestin.ballevre@protonmail.com>
2023-08-02 17:47:55 +0200

    fix(MoveSharedFolderInsideAnotherModal): Display the list of shared entries

--- src/drive/web/modules/move/MoveSharedFolderInsideAnotherModal.jsx
+++ src/drive/web/modules/move/MoveSharedFolderInsideAnotherModal.jsx
@@ -5,6 +5,8 @@ import { useQuery } from 'cozy-client'
 import { ConfirmDialog } from 'cozy-ui/react/CozyDialogs'
 import Buttons from 'cozy-ui/transpiled/react/Buttons'
 import { useI18n } from 'cozy-ui/transpiled/react'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import { useSharingContext } from 'cozy-sharing'
 
 import { buildOnlyFolderQuery } from 'drive/web/modules/queries'
 import { getEntriesName } from 'drive/web/modules/move/helpers'
@@ -20,7 +22,7 @@ const MoveSharedFolderInsideAnotherModal = ({
   onConfirm
 }) => {
   const { t } = useI18n()
-
+  const { byDocId } = useSharingContext()
   const folderQuery = buildOnlyFolderQuery(folderId)
   const { fetchStatus, data } = useQuery(
     folderQuery.definition,
@@ -28,14 +30,35 @@ const MoveSharedFolderInsideAnotherModal = ({
   )
 
   if (fetchStatus === 'loaded') {
+    const sharedEntries = entries.filter(
+      ({ _id }) => byDocId[_id] !== undefined
+    )
+
     return (
       <ConfirmDialog
         open
         title={t('Move.sharedFolderInsideAnother.title')}
-        content={t('Move.sharedFolderInsideAnother.content', {
-          source: getEntriesName(entries, t),
-          destination: data.name
-        })}
+        content={
+          <>
+            <Typography variant="body1" className="u-mb-half">
+              {t('Move.sharedFolderInsideAnother.content_1')}
+            </Typography>
+            <Typography variant="body1" className="u-mb-half">
+              {t('Move.sharedFolderInsideAnother.content_2', {
+                source: getEntriesName(entries, t),
+                destination: data.name,
+                smart_count: entries.length
+              })}
+            </Typography>
+            {entries.length > 1 ? (
+              <ul>
+                {sharedEntries.map(({ _id, name }) => (
+                  <li key={_id}>{name} </li>
+                ))}
+              </ul>
+            ) : null}
+          </>
+        }
         actions={
           <>
             <Buttons
