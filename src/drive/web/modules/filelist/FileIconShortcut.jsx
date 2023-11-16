6effff13038444b0eb27d332618a7cf6579fc60c
cballevre <celestin.ballevre@protonmail.com>
2023-11-16 10:09:57 +0100

    fix(FileIconShortcut): Manage not-found error
    This change displays the fallback when the image is not found. This can happen because useFetchShorcut can return images from the registry

--- src/drive/web/modules/filelist/FileIconShortcut.jsx
+++ src/drive/web/modules/filelist/FileIconShortcut.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useClient, useFetchShortcut } from 'cozy-client'
 import Icon from 'cozy-ui/transpiled/react/Icon'
@@ -8,14 +8,23 @@ import GlobeIcon from 'cozy-ui/transpiled/react/Icons/Globe'
 const FileIconShortcut = ({ file, size = 32 }) => {
   const client = useClient()
   const { shortcutImg } = useFetchShortcut(client, file.id)
+  const [isBroken, setBroken] = useState(null)
+
   return (
     <>
-      <div style={{ display: shortcutImg ? 'block' : 'none' }}>
-        <img src={shortcutImg} width={size} height={size} />
+      <div style={{ display: shortcutImg && !isBroken ? 'block' : 'none' }}>
+        <img
+          src={shortcutImg}
+          width={size}
+          height={size}
+          onError={() => {
+            setBroken(true)
+          }}
+        />
       </div>
       <div
         style={{
-          display: !shortcutImg ? 'block' : 'none'
+          display: !shortcutImg || isBroken ? 'block' : 'none'
         }}
       >
         <Icon icon={GlobeIcon} size={size} color={palette.coolGrey} />
