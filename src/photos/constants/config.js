502db1443893c013409fd13261fdb67e4cc089e4
cballevre <celestin.ballevre@protonmail.com>
2023-12-12 19:06:45 +0100

    feat: Remove photos folder

--- src/photos/constants/config.js
+++
@@ -1,1 +0,0 @@
-export const HOME_LINK_HREF = 'https://manager.cozycloud.cc/cozy/create'
--- src/photos/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import upload from '../ducks/upload'
-import uiReducer from 'react-cozy-helpers'
-
-export default {
-  upload,
-  ui: uiReducer
-}
