4148f94287c960526ccd6c11f08d21b798994e5d
cballevre <celestin.ballevre@protonmail.com>
2023-12-13 17:46:44 +0100

    fix: Asset import for Drive
    The application name is not shared in the environment variable, so the assets are no longer copied. This commit fix that by removing everything related to photos

--- webpack/appicon.config.js
+++ webpack/appicon.config.js
@@ -1,28 +1,26 @@
 const CopyPlugin = require('copy-webpack-plugin')
-const isDrive = process.env.COZY_APP_SLUG === 'drive'
 
-const app = isDrive ? 'drive' : 'photos'
 module.exports = {
   plugins: [
     new CopyPlugin([
       {
-        from: `src/${app}/targets/vendor/assets/app-icon.svg`,
+        from: `src/drive/targets/vendor/assets/app-icon.svg`,
         to: 'public/app-icon.svg'
       },
       {
-        from: `src/${app}/targets/vendor/assets/favicon*`,
+        from: `src/drive/targets/vendor/assets/favicon*`,
         to: 'public/',
         flatten: true
       },
       {
-        from: `src/${app}/targets/vendor/assets/apple-touch-icon.png`,
+        from: `src/drive/targets/vendor/assets/apple-touch-icon.png`,
         to: 'public/apple-touch-icon.png'
       },
       {
-        from: `src/${app}/targets/vendor/assets/safari-pinned-tab.svg`,
+        from: `src/drive/targets/vendor/assets/safari-pinned-tab.svg`,
         to: 'public/safari-pinned-tab.svg'
       },
-      { from: `src/${app}/targets/vendor/assets`, ignore: ['.gitkeep'] }
+      { from: `src/drive/targets/vendor/assets`, ignore: ['.gitkeep'] }
     ])
   ]
 }
