f882432d8009deb5c962a7953a45eb3f44a98d09
cballevre <celestin.ballevre@protonmail.com>
2023-12-13 17:46:44 +0100

    fix: Asset import for Drive
    The application name is not shared in the environment variable, so the assets are no longer copied. This commit fix that by removing everything related to photos

--- app.config.js
+++ app.config.js
@@ -14,16 +14,13 @@ configurationFiles.push(
   require('cozy-scripts/config/webpack.config.css-modules')
 )
 
-const isDrive = process.env.COZY_APP_SLUG === 'drive'
-const isPhotos = process.env.COZY_APP_SLUG === 'photos'
+configurationFiles.push(require('./webpack/drive.config.js'))
 
-if (isDrive) configurationFiles.push(require('./webpack/drive.config.js'))
-
-if (isDrive && target === 'browser') {
+if (target === 'browser') {
   configurationFiles.push(require('./webpack/assets.config.js'))
 }
 
-if (isDrive && target === 'mobile')
+if (target === 'mobile')
   configurationFiles.push(require('./webpack/mobile.config.js'))
 
 if (target !== 'mobile')
@@ -64,21 +61,5 @@ const extraConfig = {
   ]
 }
 configurationFiles.push(extraConfig)
-
-if (
-  isPhotos &&
-  configurationFiles[0].multiple &&
-  configurationFiles[0].multiple.services
-) {
-  // FIXME: Will be handled correctly by next major version of cozy-scripts
-  configurationFiles[0].multiple.services.__mergeStrategy.strategy[
-    'resolve.modules'
-  ] = 'replace'
-  configurationFiles[0].multiple.services.resolve = {
-    ...configurationFiles[0].multiple.services.resolve,
-    modules: [SRC_DIR, 'node_modules']
-  }
-  // DO NOT REUSE THIS HACK CODE ABOVE
-}
 
 module.exports = configurationFiles
