63fdfd892b4729d9028962531069814e4445b32b
cballevre <celestin.ballevre@protonmail.com>
2023-12-28 18:18:25 +0100

    feat: Add an AddFolder without redux state
    This allows the existing logic to continue to be effective. All while allowing it to be used in the move modal without there being any conflict between the two.

--- src/drive/web/modules/filelist/AddFolder.jsx
+++ src/drive/web/modules/filelist/AddFolder.jsx
@@ -1,88 +1,38 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import cx from 'classnames'
+import { connect, useDispatch } from 'react-redux'
 import compose from 'lodash/flowRight'
 
 import { withClient } from 'cozy-client'
-import { translate } from 'cozy-ui/transpiled/react/providers/I18n'
-import withBreakpoints from 'cozy-ui/transpiled/react/helpers/withBreakpoints'
 import Alerter from 'cozy-ui/transpiled/react/deprecated/Alerter'
-import { TableRow, TableCell } from 'cozy-ui/transpiled/react/Table'
 
-import FilenameInput from 'drive/web/modules/filelist/FilenameInput'
 import {
   isTypingNewFolderName,
   hideNewFolderInput,
   isEncryptedFolder
 } from 'drive/web/modules/filelist/duck'
 import { createFolder } from 'drive/web/modules/navigation/duck'
-import FileThumbnail from 'drive/web/modules/filelist/FileThumbnail'
-import { Empty as EmptyCell } from 'drive/web/modules/filelist/cells'
-
-import styles from 'drive/styles/filelist.styl'
+import { AddFolderRow } from 'drive/web/modules/filelist/AddFolderRow'
 
 const AddFolder = ({
-  f,
   visible,
   isEncrypted,
   onSubmit,
   onAbort,
-  extraColumns,
-  breakpoints: { isMobile }
-}) =>
-  !visible ? null : (
-    <TableRow className={styles['fil-content-row']}>
-      <TableCell
-        className={cx(
-          styles['fil-content-cell'],
-          styles['fil-content-file-select']
-        )}
-      />
-      <TableCell
-        className={cx(
-          styles['fil-content-cell'],
-          styles['fil-file-thumbnail'],
-          {
-            'u-pl-0': !isMobile
-          }
-        )}
-      >
-        <FileThumbnail file={{ type: 'directory' }} isEncrypted={isEncrypted} />
-      </TableCell>
-      <TableCell
-        className={cx(styles['fil-content-cell'], styles['fil-content-file'])}
-      >
-        <FilenameInput onSubmit={onSubmit} onAbort={onAbort} />
-      </TableCell>
-      {!isMobile && (
-        <>
-          <TableCell
-            className={cx(
-              styles['fil-content-cell'],
-              styles['fil-content-date']
-            )}
-          >
-            <time dateTime="">{f(Date.now(), 'MMM D, YYYY')}</time>
-          </TableCell>
-          <EmptyCell className={styles['fil-content-size']} />
-          {extraColumns &&
-            extraColumns.map(column => (
-              <EmptyCell
-                key={column.label}
-                className={styles['fil-content-narrow']}
-              />
-            ))}
-          <EmptyCell className={styles['fil-content-status']} />
-        </>
-      )}
-      <TableCell
-        className={cx(
-          styles['fil-content-cell'],
-          styles['fil-content-file-action']
-        )}
-      />
-    </TableRow>
+  extraColumns
+}) => {
+  if (!visible) {
+    return null
+  }
+
+  return (
+    <AddFolderRow
+      isEncrypted={isEncrypted}
+      onSubmit={onSubmit}
+      onAbort={onAbort}
+      extraColumns={extraColumns}
+    />
   )
+}
 
 const mapStateToProps = state => ({
   visible: isTypingNewFolderName(state),
@@ -102,8 +52,10 @@ const createFolderAfterSubmit =
         }
       )
     ).then(() => {
-      if (ownProps.refreshFolderContent) ownProps.refreshFolderContent()
-      return dispatch(hideNewFolderInput())
+      // eslint-disable-next-line promise/always-return
+      if (ownProps.afterSubmit) {
+        ownProps.afterSubmit()
+      }
     })
   }
 
@@ -113,15 +65,45 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
     if (accidental) {
       Alerter.info('alert.folder_abort')
     }
-    dispatch(hideNewFolderInput())
+    if (ownProps.afterAbort) {
+      ownProps.afterAbort()
+    }
   }
 })
 
-export { AddFolder }
-
-export default compose(
+const AddFolderWithoutState = compose(
   withClient,
-  translate(),
-  connect(mapStateToProps, mapDispatchToProps),
-  withBreakpoints()
+  connect(null, mapDispatchToProps)
 )(AddFolder)
+
+const AddFolderWithState = compose(
+  withClient,
+  connect(mapStateToProps, mapDispatchToProps)
+)(AddFolder)
+
+const AddFolderWithAfter = ({ refreshFolderContent, ...props }) => {
+  const dispatch = useDispatch()
+
+  const handleAfterSubmit = () => {
+    if (refreshFolderContent) {
+      refreshFolderContent()
+    }
+    dispatch(hideNewFolderInput())
+  }
+
+  const handleAfterAbort = () => {
+    dispatch(hideNewFolderInput())
+  }
+
+  return (
+    <AddFolderWithState
+      afterSubmit={handleAfterSubmit}
+      afterAbort={handleAfterAbort}
+      {...props}
+    />
+  )
+}
+
+export { AddFolder, AddFolderWithoutState }
+
+export default AddFolderWithAfter
