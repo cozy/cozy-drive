3e0917b52069f28c475ad79ecf6790a7dfca8e30
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-01-11 10:39:33 +0100

    feat: Upgrade cozy-ui 100.1.0 to 101.1.0

--- jestHelpers/setup.js
+++ jestHelpers/setup.js
@@ -1,15 +1,6 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-// To avoid the errors while creating theme (since no CSS stylesheet
-// defining CSS variables is injected during tests)
-// Material-UI: the color provided to augmentColor(color) is invalid.
-// The color object needs to have a `main` property or a `500` property.
-jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
-  ...jest.requireActual('cozy-ui/transpiled/react/utils/color'),
-  getCssVariableValue: () => '#fff'
-}))
-
 global.cozy = {}
 
 jest.mock('cozy-bar/transpiled', () => ({
