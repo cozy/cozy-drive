2b304be4afd0bc44da8fb86820871b15a7de4a7d
cballevre <celestin.ballevre@protonmail.com>
2024-01-22 16:27:56 +0100

    chore: chore: Cleaning Downcloud keys for Travis deployment

--- scripts/signing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_SECURE_ENV_VARS" != "false" ]; then
-  mkdir -p src/targets/mobile/keys/android/;
-  cp /tmp/cozy-drive-release-key.jks src/targets/mobile/keys/android/cozy-drive-release-key.jks;
-  cp /tmp/key.json src/targets/mobile/keys/android/key.json;
-fi
