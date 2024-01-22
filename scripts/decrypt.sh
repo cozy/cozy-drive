cf34a3db45831aecde65e160e0630562fe684cb3
cballevre <celestin.ballevre@protonmail.com>
2024-01-22 16:27:56 +0100

    chore: chore: Cleaning Downcloud keys for Travis deployment

--- scripts/decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_SECURE_ENV_VARS" != "false" ]; then
-  openssl aes-256-cbc -K $encrypted_b1733cb50f7f_key -iv $encrypted_b1733cb50f7f_iv -in ci-files.tar.enc -out /tmp/ci-files.tar -d;
-  tar xvf /tmp/ci-files.tar -C /tmp;
-fi
