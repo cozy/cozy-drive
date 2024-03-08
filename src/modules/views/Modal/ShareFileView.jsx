cda1ff4460acb3336d2a56f8cf89f0361a6af6bd
Antonin Cezard <acezard@gmail.com>
2024-03-08 15:00:15 +0100

    fix: Refactor ShareFileView component
    - There were two unhandled cases resulting
    in infinite loader
    - 404 sharing
    - succesful self removal from a sharing
    - we handle these cases by using the success navigation to parent

--- src/modules/views/Modal/ShareFileView.jsx
+++ src/modules/views/Modal/ShareFileView.jsx
@@ -14,19 +14,31 @@ const ShareFileView = () => {
   const fileQuery = buildFileByIdQuery(fileId)
   const fileResult = useQuery(fileQuery.definition, fileQuery.options)
 
-  if (hasQueryBeenLoaded(fileResult) && fileResult.data) {
-    const onClose = () => {
-      navigate('..', { replace: true })
-    }
+  const handleExit = () => {
+    navigate('..', { replace: true })
+  }
 
+  if (hasQueryBeenLoaded(fileResult) && fileResult.data) {
     return (
       <ShareModal
         document={fileResult.data}
         documentType="Files"
         sharingDesc={fileResult.data.name}
-        onClose={onClose}
+        onClose={handleExit}
       />
     )
+  }
+
+  // After successfully removing self from a shared file, the file is not found anymore but the query is considered loaded
+  // We check if the data is null, meaning the sharing has been removed
+  if (hasQueryBeenLoaded(fileResult) && !fileResult.data) {
+    handleExit()
+  }
+
+  // Accessing the URL of a file that doesn't exist anymore (or never existed)
+  // e.g. /folder/io.cozy.files.shared-with-me-dir/file/someidresolvingto404/share
+  if (fileResult.fetchStatus === 'failed') {
+    handleExit()
   }
 
   return <LoaderModal />
