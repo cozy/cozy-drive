cde2ee385ea1421877d96921b2c0a2f578fbc2a3
cballevre <celestin.ballevre@protonmail.com>
2024-05-13 15:29:07 +0200

    feat(nextcloud): Add toolbar

---
+++ src/modules/nextcloud/components/NextcloudToolbar.jsx
@@ -0,0 +1,27 @@
+import cx from 'classnames'
+import React from 'react'
+
+import { makeActions } from 'cozy-ui/transpiled/react/ActionsMenu/Actions'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+import { MoreMenu } from 'components/MoreMenu'
+import { selectable } from 'modules/actions/selectable'
+import { useSelectionContext } from 'modules/selection/SelectionProvider'
+
+const NextcloudToolbar = ({ docs }) => {
+  const { t } = useI18n()
+  const { showSelectionBar } = useSelectionContext()
+
+  const moreActions = makeActions([selectable], { t, showSelectionBar })
+
+  return (
+    <div
+      className={cx('u-flex', 'u-flex-items-center', 'u-ml-auto')}
+      role="toolbar"
+    >
+      <MoreMenu actions={moreActions} docs={docs} disabled={false} />
+    </div>
+  )
+}
+
+export { NextcloudToolbar }
