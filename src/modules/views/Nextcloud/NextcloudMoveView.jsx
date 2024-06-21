08a997b6fef3c05716d439b4f13811d36a42051d
cballevre <celestin.ballevre@protonmail.com>
2024-06-21 17:21:06 +0200

    refactor(NextcloudMoveView): Improve readability

--- src/modules/views/Nextcloud/NextcloudMoveView.jsx
+++ src/modules/views/Nextcloud/NextcloudMoveView.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
   Navigate,
   useLocation,
@@ -7,10 +7,10 @@ import {
   useSearchParams
 } from 'react-router-dom'
 
-import { hasQueryBeenLoaded } from 'cozy-client'
-
+import { computeNextcloudRootFolder } from 'components/FolderPicker/helpers'
 import { LoaderModal } from 'components/LoaderModal'
 import { getParentPath } from 'lib/path'
+import { hasDataLoaded } from 'lib/queries'
 import MoveModal from 'modules/move/MoveModal'
 import { useNextcloudFolder } from 'modules/nextcloud/hooks/useNextcloudFolder'
 import { useNextcloudInfos } from 'modules/nextcloud/hooks/useNextcloudInfos'
@@ -23,51 +23,53 @@ const NextcloudMoveView = () => {
   const path = useNextcloudPath()
   const navigate = useNavigate()
 
-  const { rootFolderName } = useNextcloudInfos({ sourceAccount })
+  const { instanceName } = useNextcloudInfos({ sourceAccount })
   const { nextcloudResult } = useNextcloudFolder({
     sourceAccount,
     path
   })
+
   const { nextcloudResult: nextcloudParentResult } = useNextcloudFolder({
     sourceAccount,
     path: getParentPath(path)
   })
 
+  /**
+   * Nextcloud don't have route to get parent folder
+   * so we need to fetch the content of his parent folder to get current folder data
+   */
+  const currentFolder = useMemo(() => {
+    if (path === '/') {
+      return computeNextcloudRootFolder({
+        sourceAccount,
+        instanceName
+      })
+    }
+
+    if (hasDataLoaded(nextcloudParentResult)) {
+      return (nextcloudParentResult?.data ?? []).find(
+        file => file.path === path
+      )
+    }
+
+    return undefined
+  }, [path, nextcloudParentResult, sourceAccount, instanceName])
+
+  const newPath = getParentPath(pathname) + `?${searchParams.toString()}`
+
   const hasFileIds = state?.fileIds != undefined
   if (!hasFileIds) {
-    const newPath = getParentPath(pathname) + `?${searchParams.toString()}`
     return <Navigate to={newPath} replace />
   }
 
-  if (
-    hasQueryBeenLoaded(nextcloudResult) &&
-    nextcloudResult.data &&
-    (path === '/' ||
-      (hasQueryBeenLoaded(nextcloudParentResult) && nextcloudParentResult.data))
-  ) {
+  if (hasDataLoaded(nextcloudResult) && currentFolder) {
     const onClose = () => {
-      const newPath = getParentPath(pathname) + `?${searchParams.toString()}`
       navigate(newPath, { replace: true })
     }
 
     const entries = nextcloudResult.data.filter(({ _id }) =>
       state.fileIds.includes(_id)
     )
-
-    var currentFolder = {
-      _id: 'io.cozy.remote.nextcloud.files.root-dir',
-      _type: 'io.cozy.remote.nextcloud.files',
-      name: rootFolderName,
-      path: '/',
-      cozyMetadata: {
-        sourceAccount: sourceAccount
-      }
-    }
-    if (path !== '/') {
-      currentFolder = (nextcloudParentResult?.data ?? []).find(
-        file => file.path === path
-      )
-    }
 
     return (
       <MoveModal
