3942acbbbc8a0d12f00d9346cb4dd051287489a5
cballevre <celestin.ballevre@protonmail.com>
2024-06-21 12:21:59 +0200

    refactor: Extract unlocker for encrypted folder into a component

--- src/modules/move/FileList.jsx
+++ src/modules/move/FileList.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
-
-import { VaultUnlocker } from 'cozy-keys-lib'
+import React from 'react'
 
 import { ROOT_DIR_ID } from 'constants/config'
-import { isEncryptedFolder } from 'lib/encryption'
 import { DumbFile as File } from 'modules/filelist/File'
+import { FolderUnlocker } from 'modules/folder/components/FolderUnlocker'
 
 const isInvalidMoveTarget = (subjects, target) => {
   const isASubject = subjects.find(subject => subject._id === target._id)
@@ -14,42 +12,31 @@ const isInvalidMoveTarget = (subjects, target) => {
 }
 
 const FileList = ({ targets, files, folder, navigateTo }) => {
-  const [shouldUnlock, setShouldUnlock] = useState(true)
-  const isEncFolder = isEncryptedFolder(folder)
-
-  if (isEncFolder && shouldUnlock) {
-    return (
-      <VaultUnlocker
-        onDismiss={() => {
-          setShouldUnlock(false)
-          return navigateTo(ROOT_DIR_ID)
-        }}
-        onUnlock={() => setShouldUnlock(false)}
-      />
-    )
-  } else {
-    return (
-      <>
-        {files.map(file => (
-          <File
-            key={file.id}
-            disabled={isInvalidMoveTarget(targets, file)}
-            styleDisabled={isInvalidMoveTarget(targets, file)}
-            attributes={file}
-            displayedFolder={null}
-            actions={null}
-            isRenaming={false}
-            onFolderOpen={({ id }) => navigateTo(files.find(f => f.id === id))}
-            onFileOpen={() => {}}
-            withSelectionCheckbox={false}
-            withFilePath={false}
-            withSharedBadge
-            disableSelection={true}
-          />
-        ))}
-      </>
-    )
+  const handleFolderUnlockerDismiss = () => {
+    navigateTo(ROOT_DIR_ID)
   }
+
+  return (
+    <FolderUnlocker folder={folder} onDismiss={handleFolderUnlockerDismiss}>
+      {files.map(file => (
+        <File
+          key={file.id}
+          disabled={isInvalidMoveTarget(targets, file)}
+          styleDisabled={isInvalidMoveTarget(targets, file)}
+          attributes={file}
+          displayedFolder={null}
+          actions={null}
+          isRenaming={false}
+          onFolderOpen={({ id }) => navigateTo(files.find(f => f.id === id))}
+          onFileOpen={() => {}}
+          withSelectionCheckbox={false}
+          withFilePath={false}
+          withSharedBadge
+          disableSelection={true}
+        />
+      ))}
+    </FolderUnlocker>
+  )
 }
 
 FileList.propTypes = {
