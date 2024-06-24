281dece1f5403c09d44b6926f45d67c710546ed3
Théo Poizat <hello@zatteo.com>
2024-06-24 14:02:03 +0200

    feat: Update cozy-ui to 110.1.0
    To better support dark mode (flagship app and dark mode button in
    cozy-settings).

--- src/modules/views/Folder/FolderView.jsx
+++ src/modules/views/Folder/FolderView.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ModalManager } from 'react-cozy-helpers'
 
+import { RealTimeQueries } from 'cozy-client'
+
 import { NotFound } from 'components/Error/NotFound'
 import FilesRealTimeQueries from 'components/FilesRealTimeQueries'
 import { ModalStack } from 'lib/ModalContext'
@@ -9,6 +11,7 @@ import Main from 'modules/layout/Main'
 const FolderView = ({ children, isNotFound }) => (
   <Main>
     <FilesRealTimeQueries />
+    <RealTimeQueries doctype="io.cozy.settings" />
     <ModalStack />
     <ModalManager />
     {isNotFound ? <NotFound /> : children}
