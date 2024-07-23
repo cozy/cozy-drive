e337b82cb82b09667773d794e4d6db4799e5637d
cballevre <celestin.ballevre@protonmail.com>
2024-07-23 14:23:27 +0200

    feat: Add hook to manage entries

---
+++ src/modules/nextcloud/hooks/useNextcloudEntries.tsx
@@ -0,0 +1,57 @@
+import { useLocation, useParams } from 'react-router-dom'
+
+import { NextcloudFile } from 'cozy-client/types/types'
+
+import { hasDataLoaded } from 'lib/queries'
+import { useNextcloudFolder } from 'modules/nextcloud/hooks/useNextcloudFolder'
+import { useNextcloudPath } from 'modules/nextcloud/hooks/useNextcloudPath'
+
+interface useNextcloudEntriesReturn {
+  isLoading: boolean
+  entries?: NextcloudFile[]
+  hasEntries: boolean
+}
+
+const useNextcloudEntries = ({
+  insideTrash = false
+} = {}): useNextcloudEntriesReturn => {
+  const { state } = useLocation() as {
+    state?: { fileIds?: string[] }
+  }
+
+  const { sourceAccount } = useParams()
+  const path = useNextcloudPath({
+    insideTrash
+  })
+
+  const { nextcloudResult } = useNextcloudFolder({
+    sourceAccount,
+    path,
+    insideTrash
+  })
+
+  if (!state?.fileIds) {
+    return {
+      isLoading: false,
+      hasEntries: false
+    }
+  }
+
+  if (hasDataLoaded(nextcloudResult)) {
+    const entries = nextcloudResult.data.filter(({ _id }) =>
+      state.fileIds.includes(_id)
+    )
+    return {
+      isLoading: false,
+      hasEntries: entries.length > 0,
+      entries
+    }
+  }
+
+  return {
+    isLoading: true,
+    hasEntries: true
+  }
+}
+
+export { useNextcloudEntries }
--- src/modules/views/Nextcloud/NextcloudDeleteView.jsx
+++ src/modules/views/Nextcloud/NextcloudDeleteView.jsx
@@ -2,29 +2,20 @@ import React from 'react'
 import {
   Navigate,
   useLocation,
-  useParams,
   useNavigate,
   useSearchParams
 } from 'react-router-dom'
 
 import { LoaderModal } from 'components/LoaderModal'
 import { getParentPath } from 'lib/path'
-import { hasDataLoaded } from 'lib/queries'
 import { NextcloudDeleteConfirm } from 'modules/nextcloud/components/NextcloudDeleteConfirm'
-import { useNextcloudFolder } from 'modules/nextcloud/hooks/useNextcloudFolder'
-import { useNextcloudPath } from 'modules/nextcloud/hooks/useNextcloudPath'
+import { useNextcloudEntries } from 'modules/nextcloud/hooks/useNextcloudEntries'
 
 const NextcloudDeleteView = () => {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
-  const { sourceAccount } = useParams()
-  const path = useNextcloudPath()
-  const { state, pathname } = useLocation()
-
-  const { nextcloudResult } = useNextcloudFolder({
-    sourceAccount,
-    path
-  })
+  const { pathname } = useLocation()
+  const { entries, hasEntries, isLoading } = useNextcloudEntries()
 
   const newPath = getParentPath(pathname) + `?${searchParams.toString()}`
 
@@ -32,14 +23,11 @@ const NextcloudDeleteView = () => {
     navigate(newPath, { replace: true })
   }
 
-  if (!state?.fileIds) {
+  if (!hasEntries) {
     return <Navigate to={newPath} replace />
   }
 
-  if (hasDataLoaded(nextcloudResult)) {
-    const entries = nextcloudResult.data.filter(({ _id }) =>
-      state.fileIds.includes(_id)
-    )
+  if (entries && !isLoading) {
     return <NextcloudDeleteConfirm files={entries} onClose={handleClose} />
   }
 
