8586999e48ad5458b8d4ad19092ffc79b5fbd207
cballevre <celestin.ballevre@protonmail.com>
2024-09-05 09:41:30 +0200

    feat: Show time on last update with a tooltip

--- src/modules/filelist/cells/LastUpdate.jsx
+++ src/modules/filelist/cells/LastUpdate.jsx
@@ -2,16 +2,24 @@ import cx from 'classnames'
 import React from 'react'
 
 import { TableCell } from 'cozy-ui/transpiled/react/deprecated/Table'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import styles from 'styles/filelist.styl'
 
-const _LastUpdate = ({ date, formatted = '—' }) => (
-  <TableCell
-    className={cx(styles['fil-content-cell'], styles['fil-content-date'])}
-  >
-    <time dateTime={date}>{formatted}</time>
-  </TableCell>
-)
+const _LastUpdate = ({ date, formatted = '—' }) => {
+  const { f, t } = useI18n()
+
+  return (
+    <TableCell
+      className={cx(styles['fil-content-cell'], styles['fil-content-date'])}
+      {...(formatted !== '—' && {
+        title: f(date, t('LastUpdate.titleFormat'))
+      })}
+    >
+      <time dateTime={date}>{formatted}</time>
+    </TableCell>
+  )
+}
 
 const LastUpdate = React.memo(_LastUpdate)
 
