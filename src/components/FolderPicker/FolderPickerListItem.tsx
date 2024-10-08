b86d3e4228192ab7fde8fc4b8b203753e2ca3b01
cballevre <celestin.ballevre@protonmail.com>
2024-10-08 17:00:36 +0200

    refactor(FolderPicker): Use ListItem instead File
    This decouples the display of the file table from that of the FolderPicker modal. This will allow you to change the lines as Link in the table in order to take advantage of the default mechanisms of browsers

---
+++ src/components/FolderPicker/FolderPickerListItem.tsx
@@ -0,0 +1,72 @@
+import { filesize } from 'filesize'
+import React, { FC } from 'react'
+
+import { isDirectory } from 'cozy-client/dist/models/file'
+import Divider from 'cozy-ui/transpiled/react/Divider'
+import ListItem from 'cozy-ui/transpiled/react/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+import type { File } from 'components/FolderPicker/types'
+import FileThumbnail from 'modules/filelist/icons/FileThumbnail'
+
+import styles from 'styles/folder-picker.styl'
+
+interface FolderPickerListItemProps {
+  file: File
+  disabled?: boolean
+  onClick: (file: File) => void
+  showDivider?: boolean
+}
+
+const FolderPickerListItem: FC<FolderPickerListItemProps> = ({
+  file,
+  disabled = false,
+  onClick,
+  showDivider = false
+}) => {
+  const { f, t } = useI18n()
+  const { isMobile } = useBreakpoints()
+  const gutters = isMobile ? 'default' : 'double'
+
+  const handleClick = (): void => {
+    onClick(file)
+  }
+
+  const formattedUpdatedAt = f(file.updated_at, t('table.row_update_format'))
+  const formattedSize = file.size
+    ? filesize(file.size, { base: 10 })
+    : undefined
+  const secondaryText = !isDirectory(file)
+    ? `${formattedUpdatedAt}${formattedSize ? ` - ${formattedSize}` : ''}`
+    : undefined
+
+  return (
+    <>
+      <ListItem
+        button
+        onClick={handleClick}
+        disabled={disabled}
+        gutters={gutters}
+      >
+        <ListItemIcon className="u-pos-relative">
+          <FileThumbnail
+            file={file}
+            showSharedBadge
+            componentsProps={{
+              sharedBadge: {
+                className: styles['icon-shared']
+              }
+            }}
+          />
+        </ListItemIcon>
+        <ListItemText primary={file.name} secondary={secondaryText} />
+      </ListItem>
+      {showDivider && <Divider />}
+    </>
+  )
+}
+
+export { FolderPickerListItem }
