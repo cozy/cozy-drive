019f3237f0a095c6a9dedc093d298cff139228c4
cballevre <celestin.ballevre@protonmail.com>
2024-10-08 17:00:36 +0200

    refactor(FolderPicker): Use ListItem instead File
    This decouples the display of the file table from that of the FolderPicker modal. This will allow you to change the lines as Link in the table in order to take advantage of the default mechanisms of browsers

--- src/components/FolderPicker/FolderPickerContentLoader.tsx
+++ src/components/FolderPicker/FolderPickerContentLoader.tsx
@@ -1,8 +1,10 @@
 import React, { ReactNode } from 'react'
 
+import ListItemSkeleton from 'cozy-ui/transpiled/react/Skeletons/ListItemSkeleton'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+
 import { EmptyDrive } from 'components/Error/Empty'
 import Oops from 'components/Error/Oops'
-import FileListRowsPlaceholder from 'modules/filelist/FileListRowsPlaceholder'
 
 interface FolderPickerContentLoaderProps {
   fetchStatus: string
@@ -15,7 +17,22 @@ const FolderPickerContentLoader: React.FC<FolderPickerContentLoaderProps> = ({
   hasNoData,
   children
 }) => {
-  if (fetchStatus === 'loading') return <FileListRowsPlaceholder />
+  const { isMobile } = useBreakpoints()
+  const gutters = isMobile ? 'default' : 'double'
+
+  if (fetchStatus === 'loading')
+    return (
+      <>
+        {Array.from({ length: 8 }, (_, index) => (
+          <ListItemSkeleton
+            key={`key_file_placeholder_${index}`}
+            gutters={gutters}
+            hasSecondary
+            divider={index !== 7}
+          />
+        ))}
+      </>
+    )
   else if (fetchStatus === 'failed') return <Oops />
   else if (fetchStatus === 'loaded' && hasNoData)
     return <EmptyDrive canUpload={false} />
