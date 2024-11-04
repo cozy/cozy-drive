10a02718cb29186ab5f853a8280158dad6ace949
cballevre <celestin.ballevre@protonmail.com>
2024-11-04 15:30:28 +0100

    fix: Correct index offset due to directory being taken into account

--- src/modules/views/Public/PublicFileViewer.jsx
+++ src/modules/views/Public/PublicFileViewer.jsx
@@ -16,18 +16,16 @@ const PublicFileViewer = () => {
   const currentFolderId = useCurrentFolderId()
 
   const filesResult = usePublicFilesQuery(currentFolderId)
-  const files = filesResult.data
+  const viewableFiles = filesResult.data.filter(f => f.type !== 'directory')
 
   const currentIndex = useMemo(() => {
-    return files.findIndex(f => f.id === fileId)
-  }, [files, fileId])
+    return viewableFiles.findIndex(f => f.id === fileId)
+  }, [viewableFiles, fileId])
   const hasCurrentIndex = useMemo(() => currentIndex != -1, [currentIndex])
   const viewerIndex = useMemo(
     () => (hasCurrentIndex ? currentIndex : 0),
     [hasCurrentIndex, currentIndex]
   )
-
-  const viewableFiles = files.filter(f => f.type !== 'directory')
 
   const handleChange = ({ _id }) => {
     navigate(`../${_id}`, {
