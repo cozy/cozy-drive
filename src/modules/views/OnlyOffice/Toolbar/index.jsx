c43fe536efc871596a3e7e6084337d6a5623cc3a
AlexisG <merkkur39@gmail.com>
2024-12-02 15:09:57 +0100

    feat(OO): Change actions of the toolbar
    And their display conditions.

--- src/modules/views/OnlyOffice/Toolbar/index.jsx
+++ src/modules/views/OnlyOffice/Toolbar/index.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 
+import { makeActions } from 'cozy-ui/transpiled/react/ActionsMenu/Actions'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import FilesRealTimeQueries from 'components/FilesRealTimeQueries'
+import { HOME_LINK_HREF } from 'constants/config'
 import { useRedirectLink } from 'hooks/useRedirectLink'
+import { openExternalLink } from 'modules/actions'
+import { OpenExternalLinkButton } from 'modules/public/OpenExternalLinkButton'
+import PublicToolbarMoreMenu from 'modules/public/PublicToolbarMoreMenu'
 import { useOnlyOfficeContext } from 'modules/views/OnlyOffice/OnlyOfficeProvider'
 import BackButton from 'modules/views/OnlyOffice/Toolbar/BackButton'
 import FileIcon from 'modules/views/OnlyOffice/Toolbar/FileIcon'
 import FileName from 'modules/views/OnlyOffice/Toolbar/FileName'
 import HomeIcon from 'modules/views/OnlyOffice/Toolbar/HomeIcon'
 import HomeLinker from 'modules/views/OnlyOffice/Toolbar/HomeLinker'
-import ReadOnly from 'modules/views/OnlyOffice/Toolbar/ReadOnly'
 import Separator from 'modules/views/OnlyOffice/Toolbar/Separator'
 import Sharing from 'modules/views/OnlyOffice/Toolbar/Sharing'
 import { useFileWithPath } from 'modules/views/hooks'
 
-const Toolbar = () => {
+const Toolbar = ({ sharingInfos }) => {
   const { isMobile } = useBreakpoints()
-  const { fileId, isPublic, isReadOnly, isEditorReady } = useOnlyOfficeContext()
+  const { t } = useI18n()
+  const { fileId, isPublic, isEditorReady } = useOnlyOfficeContext()
+  const { discoveryLink, isSharingShortcutCreated, loading } = sharingInfos
 
   const { data: fileWithPath } = useFileWithPath(fileId)
   const { redirectBack, canRedirect } = useRedirectLink({ isPublic })
@@ -27,6 +34,16 @@ const Toolbar = () => {
   const handleOnClick = () => {
     redirectBack()
   }
+  // Check if the share shortcut has not yet been added
+  const isShareNotAdded = !loading && !isSharingShortcutCreated
+
+  // discoveryLink exists only in cozy to cozy sharing
+  const link = discoveryLink || HOME_LINK_HREF
+  const actions = makeActions([openExternalLink], {
+    t,
+    link,
+    isSharingShortcutCreated
+  })
 
   return (
     <>
@@ -50,7 +67,17 @@ const Toolbar = () => {
         )}
         <FileName fileWithPath={fileWithPath} isPublic={isPublic} />
       </div>
-      {isReadOnly && <ReadOnly />}
+      {isPublic && !isMobile && isShareNotAdded && (
+        <OpenExternalLinkButton
+          link={link}
+          isSharingShortcutCreated={isSharingShortcutCreated}
+        />
+      )}
+
+      {isPublic && (
+        <PublicToolbarMoreMenu files={[fileWithPath]} actions={actions} />
+      )}
+
       {!isPublic && isEditorReady && <Sharing fileWithPath={fileWithPath} />}
     </>
   )
