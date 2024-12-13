19365c2f5c3890555d265d96ab72cbc6a7f7329f
Théo Poizat <hello@zatteo.com>
2024-12-13 09:35:54 +0100

    feat: Use search from dataproxy

--- src/modules/search/components/AppBarSearch.jsx
+++ src/modules/search/components/AppBarSearch.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 import { BarSearch } from 'cozy-bar'
+import { AssistantDesktop } from 'cozy-dataproxy-lib'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
-import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
-
-import BarSearchAutosuggest from 'modules/search/components/BarSearchAutosuggest'
 
 const AppBarSearch = () => {
-  const { t } = useI18n()
   const { isMobile } = useBreakpoints()
 
-  return <BarSearch>{!isMobile && <BarSearchAutosuggest t={t} />}</BarSearch>
+  return (
+    <BarSearch>
+      {!isMobile && (
+        <AssistantDesktop
+          componentsProps={{ SearchBarDesktop: { size: 'small' } }}
+        />
+      )}
+    </BarSearch>
+  )
 }
 
 export default AppBarSearch
