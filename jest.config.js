306ed271a29e6412790968446ff44f73f75bbde7
Ldoppea <yannick.chiron@gmail.com>
2025-01-07 13:47:45 +0100

    feat: Remove mobile target

--- jest.config.js
+++ jest.config.js
@@ -42,8 +42,5 @@ module.exports = {
     url: 'http://cozy.localhost:8080/'
   },
   testMatch: ['**/(*.)(spec|test).[jt]s?(x)'],
-  globals: {
-    __TARGET__: 'browser'
-  },
   reporters: ['default', '<rootDir>/jestHelpers/ConsoleUsageReporter.js']
 }
