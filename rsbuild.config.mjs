c7447ca775a216d62f33b6ccbaebb6957e04c3d4
Ldoppea <yannick.chiron@gmail.com>
2025-01-15 14:56:53 +0100

    feat: Configure WebWorker for react-pdf
    By migrating from webpack to rsbuild we lost this resolution override
    that is needed by react-pdf for generating PDFs previews using
    WebWorkers
    
    More details:
    https://github.com/cozy/cozy-libs/blob/a00baa1723bd7d37591fe95dcc1921d4e2c24f87/packages/cozy-viewer/src/Readme.md?plain=1#L299-L335

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from '@rsbuild/core'
+import { defineConfig, mergeRsbuildConfig } from '@rsbuild/core'
 import { getRsbuildConfig } from 'rsbuild-config-cozy-app'
 
 const config = getRsbuildConfig({
@@ -7,4 +7,12 @@ const config = getRsbuildConfig({
   hasPublic: true
 })
 
-export default defineConfig(config)
+const mergedConfig = mergeRsbuildConfig(config, {
+  resolve: {
+    alias: {
+      'react-pdf$': 'react-pdf/dist/esm/entry.webpack'
+    }
+  }
+})
+
+export default defineConfig(mergedConfig)
