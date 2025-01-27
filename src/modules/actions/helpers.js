f0ccd00ed7890fbac30112b76eb0d3c4f169f2a4
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-01-27 15:17:05 +0100

    refactor: Actions helpers can now manage absolute and relative path

--- src/modules/actions/helpers.js
+++ src/modules/actions/helpers.js
@@ -2,7 +2,10 @@ import { joinPath } from 'lib/path'
 
 export const navigateToModal = ({ navigate, pathname, files, path }) => {
   const file = Array.isArray(files) ? files[0] : files
-  return navigate(joinPath(pathname, `file/${file.id}/${path}`))
+
+  return navigate(
+    pathname ? joinPath(pathname, `file/${file.id}/${path}`) : `v/${path}`
+  )
 }
 
 export const navigateToModalWithMultipleFile = ({
@@ -12,8 +15,11 @@ export const navigateToModalWithMultipleFile = ({
   path,
   search
 }) => {
-  navigate(
-    { pathname: joinPath(pathname, path), search: search ? `?${search}` : '' },
+  return navigate(
+    {
+      pathname: pathname ? joinPath(pathname, path) : `v/${path}`,
+      search: search ? `?${search}` : ''
+    },
     {
       state: { fileIds: files.map(file => file.id) }
     }
