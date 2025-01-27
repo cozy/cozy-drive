4b3c90a0645d53e4eb2415a6a4631279c434a0d2
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-01-27 15:20:55 +0100

    feat: Add MoreMenu for the Viewer

---
+++ src/modules/viewer/MoreMenu.jsx
@@ -0,0 +1,48 @@
+import cx from 'classnames'
+import React, { useState, useRef } from 'react'
+
+import ActionsMenu from 'cozy-ui/transpiled/react/ActionsMenu'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
+import DotsIcon from 'cozy-ui/transpiled/react/Icons/Dots'
+import { useBreakpoints } from 'cozy-ui/transpiled/react/providers/Breakpoints'
+
+import { useMoreMenuActions } from 'hooks/useMoreMenuActions'
+
+const MoreMenu = ({ file }) => {
+  const [showMenu, setShowMenu] = useState(false)
+  const { isDesktop } = useBreakpoints()
+  const anchorRef = useRef()
+  const actions = useMoreMenuActions(file)
+
+  if (file.trashed) return null
+
+  return (
+    <>
+      <IconButton
+        ref={anchorRef}
+        variant="secondary"
+        className={cx({ 'u-white': isDesktop })}
+        onClick={() => setShowMenu(v => !v)}
+      >
+        <Icon icon={DotsIcon} />
+      </IconButton>
+      {showMenu && (
+        <ActionsMenu
+          open
+          ref={anchorRef}
+          docs={[file]}
+          actions={actions}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right'
+          }}
+          autoClose
+          onClose={() => setShowMenu(false)}
+        />
+      )}
+    </>
+  )
+}
+
+export default MoreMenu
