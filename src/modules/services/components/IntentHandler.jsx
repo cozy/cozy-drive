000327b5b87f1a535029352051d94ea5adb97682
Ldoppea <yannick.chiron@gmail.com>
2025-01-28 15:35:22 +0100

    feat: Make Intents work with cozy-client instead of cozy-client-js
    In previous commit we removed cozy-client-js so we now want Intents to
    work with cozy-client instead

--- src/modules/services/components/IntentHandler.jsx
+++ src/modules/services/components/IntentHandler.jsx
@@ -1,63 +1,60 @@
-/* global cozy */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { useClient } from 'cozy-client'
+import Intents from 'cozy-interapp'
 
 import Embeder from './Embeder'
 import URLGetter from './URLGetter'
 
-class IntentHandler extends React.Component {
-  constructor(props) {
-    super(props)
+const IntentHandler = ({ intentId }) => {
+  const client = useClient()
 
-    this.state = {
-      component: null,
-      service: null,
-      intent: null
-    }
-  }
+  const [state, setState] = useState({
+    component: null,
+    service: null,
+    intent: null
+  })
 
-  componentDidMount() {
-    this.startService()
-  }
+  const ServiceComponent = state.component
 
-  async startService() {
-    const { intentId } = this.props
+  useEffect(() => {
+    const startService = async () => {
+      let component
+      let service
+      let intent
+      try {
+        const intents = new Intents({ client })
+        service = await intents.createService(intentId, window)
+        intent = service.getIntent()
 
-    let component
-    let service
-    let intent
-    try {
-      service = await cozy.client.intents.createService(intentId, window)
-      intent = service.getIntent()
+        if (
+          intent.attributes.action === 'OPEN' &&
+          intent.attributes.type === 'io.cozy.files'
+        ) {
+          component = Embeder
+        } else if (intent.attributes.action === 'GET_URL') {
+          component = URLGetter
+        }
 
-      if (
-        intent.attributes.action === 'OPEN' &&
-        intent.attributes.type === 'io.cozy.files'
-      ) {
-        component = Embeder
-      } else if (intent.attributes.action === 'GET_URL') {
-        component = URLGetter
+        setState({
+          component,
+          service,
+          intent
+        })
+      } catch (error) {
+        console.error(error)
+        service.throw(error)
       }
-
-      this.setState({
-        component,
-        service,
-        intent
-      })
-    } catch (error) {
-      service.throw(error)
     }
-  }
 
-  render() {
-    const { service, intent } = this.state
-    const ServiceComponent = this.state.component
+    startService()
+  }, [client, intentId])
 
-    return ServiceComponent ? (
-      <ServiceComponent service={service} intent={intent} />
-    ) : (
-      <div className="u-w-100 u-bg-charcoalGrey" />
-    )
-  }
+  return ServiceComponent ? (
+    <ServiceComponent service={state.service} intent={state.intent} />
+  ) : (
+    <div className="u-w-100 u-bg-charcoalGrey" />
+  )
 }
 
 export default IntentHandler
