6dfab2f7b53e13b3afe19154190c4b1576c03c64
Ldoppea <yannick.chiron@gmail.com>
2025-01-28 18:23:27 +0100

    fix: Remove PublicViewer
    Since the react-pdf's webpack resolver is declared in
    `rsbuild.config.mjs` then we don't need to manually enforce a worker in
    PublicViewer
    
    A worker will be used anyway thanks to the webpack resolver

--- src/modules/views/Public/PublicFileViewer.jsx
+++ src/modules/views/Public/PublicFileViewer.jsx
@@ -1,11 +1,13 @@
 import React, { useMemo, useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 
-import { FooterActionButtons, ForwardOrDownloadButton } from 'cozy-viewer'
+import Viewer, {
+  FooterActionButtons,
+  ForwardOrDownloadButton
+} from 'cozy-viewer'
 
 import { FilesViewerLoading } from '@/components/FilesViewerLoading'
 import { useCurrentFolderId } from '@/hooks'
-import PublicViewer from '@/modules/viewer/PublicViewer'
 import usePublicFilesQuery from '@/modules/views/Public/usePublicFilesQuery'
 
 const PublicFileViewer = () => {
@@ -72,7 +74,7 @@ const PublicFileViewer = () => {
   }
 
   return (
-    <PublicViewer
+    <Viewer
       files={viewableFiles}
       currentIndex={viewerIndex}
       isPublic={true}
@@ -82,7 +84,7 @@ const PublicFileViewer = () => {
       <FooterActionButtons>
         <ForwardOrDownloadButton />
       </FooterActionButtons>
-    </PublicViewer>
+    </Viewer>
   )
 }
 
