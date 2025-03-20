4e169b5ea2bbac85f8069a1eeae26753feeaa518
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-03-20 10:36:34 +0100

    feat: Remove useless padding on Main

--- src/modules/layout/Main.jsx
+++ src/modules/layout/Main.jsx
@@ -1,19 +1,12 @@
-import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import { Main as MainUI } from 'cozy-ui/transpiled/react/Layout'
 
-import styles from '@/styles/main.styl'
-
 import PushBanner from '@/components/PushBanner'
 
 const Main = ({ children, isPublic = false }) => (
-  <MainUI
-    className={classNames({
-      [styles['fil-content']]: !isPublic
-    })}
-  >
+  <MainUI>
     <PushBanner isPublic={isPublic} />
     {children}
   </MainUI>
