79f66c181aca8c377088d906c343ea819be04cee
lenhanphung <lenhanphung@gmail.com>
2025-03-24 16:42:36 +0700

    refactor(FileList): Improve header interface for file listing
    Update both desktop and mobile versions of file header
    Modify header cell styling to enhance user experience

--- src/modules/filelist/FileListHeaderDesktop.jsx
+++ src/modules/filelist/FileListHeaderDesktop.jsx
@@ -17,6 +17,7 @@ import styles from '@/styles/filelist.styl'
 import iconListMin from '@/assets/icons/icon-list-min.svg'
 import iconList from '@/assets/icons/icon-list.svg'
 import { SORTABLE_ATTRIBUTES, DEFAULT_SORT } from '@/config/sort'
+import { isTwakeTheme } from 'cozy-ui/transpiled/react/helpers/isTwakeTheme';
 
 const FileListHeaderDesktop = ({
   folderId,
@@ -29,6 +30,7 @@ const FileListHeaderDesktop = ({
 }) => {
   const { t } = useI18n()
   const actualSort = sort || DEFAULT_SORT
+  const isTwake = isTwakeTheme()
 
   return (
     <TableHead className={styles['fil-content-head']}>
@@ -36,13 +38,14 @@ const FileListHeaderDesktop = ({
         <TableHeader
           className={cx(
             styles['fil-content-header'],
-            styles['fil-content-file-select']
+            styles['fil-content-file-select'],
+            { [styles['u-capitalize']]: isTwake }
           )}
         />
         {SORTABLE_ATTRIBUTES.map(
           ({ label, attr, css, defaultOrder }, index) => {
             if (!canSort) {
-              return <HeaderCell key={index} label={label} css={css} />
+              return <HeaderCell key={index} label={label} css={css} className={isTwake ? [styles['u-capitalize']] : ''} />
             }
             const isActive = actualSort && actualSort.attribute === attr
             return (
@@ -54,6 +57,7 @@ const FileListHeaderDesktop = ({
                 defaultOrder={defaultOrder}
                 order={isActive ? actualSort.order : null}
                 onSort={(attr, order) => onFolderSort(folderId, attr, order)}
+                className={isTwake ? [styles['u-capitalize']] : ''}
               />
             )
           }
@@ -61,7 +65,8 @@ const FileListHeaderDesktop = ({
         <TableHeader
           className={cx(
             styles['fil-content-header'],
-            styles['fil-content-size']
+            styles['fil-content-size'],
+            { [styles['u-capitalize']]: isTwake }
           )}
         >
           {t('table.head_size')}
@@ -73,7 +78,8 @@ const FileListHeaderDesktop = ({
         <TableHeader
           className={cx(
             styles['fil-content-header'],
-            styles['fil-content-header-status']
+            styles['fil-content-header-status'],
+            { [styles['u-capitalize']]: isTwake }
           )}
         >
           {t('table.head_status')}
@@ -81,13 +87,15 @@ const FileListHeaderDesktop = ({
         <TableHeader
           className={cx(
             styles['fil-content-header'],
-            styles['fil-content-header-sharing-shortcut']
+            styles['fil-content-header-sharing-shortcut'],
+            { [styles['u-capitalize']]: isTwake }
           )}
         />
         <TableHeader
           className={cx(
             styles['fil-content-header'],
-            styles['fil-content-header-action']
+            styles['fil-content-header-action'],
+            { [styles['u-capitalize']]: isTwake }
           )}
         >
           {/** in order to not display this button in a MoveModal for instance */}
