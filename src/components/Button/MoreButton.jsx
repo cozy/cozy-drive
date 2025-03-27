544f2f8f5808586f848168302a0ea0bbd01493a9
lenhanphung <lenhanphung@gmail.com>
2025-03-27 11:04:31 +0700

    style(UI): Improve action buttons hover style
    Update 3-dots action buttons appearance with rounded hover style next to share button and in table action column

--- src/components/Button/MoreButton.jsx
+++ src/components/Button/MoreButton.jsx
@@ -1,7 +1,7 @@
 import cx from 'classnames'
 import React from 'react'
 
-import Button from 'cozy-ui/transpiled/react/Buttons'
+import CircleButton from 'cozy-ui/transpiled/react/CircleButton'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import DotsIcon from 'cozy-ui/transpiled/react/Icons/Dots'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
@@ -11,16 +11,19 @@ import styles from './index.styl'
 const MoreButton = ({ disabled, onClick, ...props }) => {
   const { t } = useI18n()
   return (
-    <Button
-      data-testid="more-button"
-      className={cx('u-miw-auto', styles['dri-btn--more'])}
-      variant="secondary"
-      disabled={disabled}
-      onClick={onClick}
-      label={<Icon icon={DotsIcon} />}
-      aria-label={t('Toolbar.more')}
-      {...props}
-    />
+    <div>
+      <CircleButton
+        data-testid="more-button"
+        className={cx('u-miw-auto', styles['dri-btn--more'])}
+        disabled={disabled}
+        onClick={onClick}
+        size="small"
+        aria-label={t('Toolbar.more')}
+        {...props}
+      >
+        <Icon icon={DotsIcon} />
+      </CircleButton>
+    </div>
   )
 }
 
