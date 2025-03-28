ec6d719bd404858c26b9c3b0a0ccd9746aef4d95
lenhanphung <lenhanphung@gmail.com>
2025-03-28 16:52:29 +0700

    chore(Merge): Resolve merge conflicts
    Fix conflicts between branches and ensure code consistency

--- src/modules/layout/Layout.jsx
+++ src/modules/layout/Layout.jsx
@@ -5,6 +5,7 @@ import { BarComponent } from 'cozy-bar'
 import CozyDevtools from 'cozy-devtools'
 import flag from 'cozy-flags'
 import FlagSwitcher from 'cozy-flags/dist/FlagSwitcher'
+import { SharedDocument } from 'cozy-sharing'
 import Sprite from 'cozy-ui/transpiled/react/Icon/Sprite'
 import { Layout as LayoutUI } from 'cozy-ui/transpiled/react/Layout'
 import Sidebar from 'cozy-ui/transpiled/react/Sidebar'
@@ -15,7 +16,10 @@ import StorageButton from '@/components/Storage/StorageButton'
 import StorageProgress from '@/components/Storage/StorageProgress'
 import ButtonClient from '@/components/pushClient/Button'
 import SupportUs from '@/components/pushClient/SupportUs'
+import { useDisplayedFolder, useCurrentFolderId } from '@/hooks'
 import { initFlags } from '@/lib/flags'
+import AddMenuProvider from '@/modules/drive/AddMenu/AddMenuProvider'
+import AddButton from '@/modules/drive/Toolbar/components/AddButton'
 import Nav from '@/modules/navigation/Nav'
 import { SelectionProvider } from '@/modules/selection/SelectionProvider'
 import UploadQueue from '@/modules/upload/UploadQueue'
@@ -24,6 +28,8 @@ initFlags()
 
 const Layout = () => {
   const { isMobile } = useBreakpoints()
+  const folderId = useCurrentFolderId()
+  const { displayedFolder } = useDisplayedFolder()
 
   return (
     <LayoutUI>
@@ -33,6 +39,26 @@ const Layout = () => {
       />
       <FlagSwitcher />
       <Sidebar className="u-flex-justify-between">
+        {folderId ? (
+          <SharedDocument docId={folderId}>
+            {sharingProps => {
+              const { hasWriteAccess } = sharingProps
+              return (
+                hasWriteAccess && (
+                  <AddMenuProvider
+                    canCreateFolder={true}
+                    canUpload={true}
+                    disabled={false}
+                    displayedFolder={displayedFolder}
+                    isSelectionBarVisible={false}
+                  >
+                    <AddButton className="u-mh-1-half u-mt-1-half u-miw-4 u-bdrs-6" />
+                  </AddMenuProvider>
+                )
+              )
+            }}
+          </SharedDocument>
+        ) : null}
         <Nav />
         <div>
           {isTwakeTheme() && (
