a4eca76577fbe38750a27a635b178853d121ccba
Théo Poizat <hello@zatteo.com>
2025-03-31 11:23:55 +0200

    fix: Adjust style for MoreButton
    Since we switched to IconButton, there is no need for custom styles.

--- src/components/Button/MoreButton.jsx
+++ src/components/Button/MoreButton.jsx
@@ -5,15 +5,12 @@ import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import DotsIcon from 'cozy-ui/transpiled/react/Icons/Dots'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
-import styles from './index.styl'
-
 const MoreButton = ({ disabled, onClick, ...props }) => {
   const { t } = useI18n()
   return (
     <div>
       <IconButton
         data-testid="more-button"
-        className={styles['dri-btn--more']}
         disabled={disabled}
         onClick={onClick}
         size="small"
