0da66d18242fffc3838f061b7c33f1794b511757
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-04-15 11:48:25 +0200

    feat: Remove logic to change favicon according to opened file

--- src/components/useHead.jsx
+++ src/components/useHead.jsx
@@ -1,7 +1,6 @@
 import { useMemo } from 'react'
 import { useParams } from 'react-router-dom'
 
-import useUpdateDocumentFavicon from '@/modules/views/useUpdateDocumentFavicon'
 import useUpdateDocumentTitle from '@/modules/views/useUpdateDocumentTitle'
 
 const useHead = () => {
@@ -10,7 +9,6 @@ const useHead = () => {
   const isFileOpen = useMemo(() => fileId !== undefined, [fileId])
 
   useUpdateDocumentTitle(isFileOpen ? fileId : folderId)
-  useUpdateDocumentFavicon(isFileOpen ? fileId : folderId)
 }
 
 export default useHead
