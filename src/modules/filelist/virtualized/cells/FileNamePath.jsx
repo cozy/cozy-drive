146a9d8065786cb5ff040b66807ed38348010fa7
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-05-22 12:46:09 +0200

    feat(FileName): Don't load path component if not necessary
    Passing a comp returning null as props is not the same than passing
    directly undefined, because if we test the prop inside the comp,
    first way will return true, second way will return false

--- src/modules/filelist/virtualized/cells/FileNamePath.jsx
+++ src/modules/filelist/virtualized/cells/FileNamePath.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { isDirectory } from 'cozy-client/dist/models/file'
 import MidEllipsis from 'cozy-ui/transpiled/react/MidEllipsis'
 import { useBreakpoints } from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
@@ -9,37 +8,26 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 import styles from '@/styles/filelist.styl'
 
 import CertificationsIcons from '@/modules/filelist/cells/CertificationsIcons.jsx'
-import {
-  getFileNameAndExtension,
-  makeParentFolderPath
-} from '@/modules/filelist/helpers'
+import { getFileNameAndExtension } from '@/modules/filelist/helpers'
 
 const FileNamePath = ({
   attributes,
   withFilePath,
   formattedSize,
-  formattedUpdatedAt
+  formattedUpdatedAt,
+  parentFolderPath
 }) => {
   const { isMobile } = useBreakpoints()
   const { t } = useI18n()
   const { filename, extension } = getFileNameAndExtension(attributes, t)
-  const parentFolderPath = makeParentFolderPath(attributes)
 
   if (!withFilePath) {
-    if (isDirectory(attributes) || !isMobile) {
-      return null
-    }
-
     return (
       <div className={styles['fil-file-infos']}>
         {`${formattedUpdatedAt}${formattedSize ? ` - ${formattedSize}` : ''}`}
         <CertificationsIcons attributes={attributes} />
       </div>
     )
-  }
-
-  if (!parentFolderPath) {
-    return null
   }
 
   if (isMobile) {
