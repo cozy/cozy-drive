59d6d2c7c5523283d4e917566267628e5d6a7f0a
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-06-19 12:53:16 +0200

    feat: Use new right click behavior

--- src/modules/views/Folder/virtualized/Table.jsx
+++ src/modules/views/Folder/virtualized/Table.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 import VirtuosoTable from 'cozy-ui/transpiled/react/Table/Virtualized'
+import TableRowDnD from 'cozy-ui/transpiled/react/Table/Virtualized/Dnd/TableRow'
+import virtuosoComponents from 'cozy-ui/transpiled/react/Table/Virtualized/virtuosoComponents'
 
 import { secondarySort } from '../helpers'
 
+import RightClickFileMenu from '@/components/RightClick/RightClickFileMenu'
 import Cell from '@/modules/filelist/virtualized/cells/Cell'
+
+const TableRow = forwardRef(({ item, context, children, ...props }, ref) => {
+  const isSelected = context?.isSelectedItem(item)
+  const isDisabled = context?.itemsInDropProcess.includes(item._id)
+
+  return (
+    <RightClickFileMenu doc={item} actions={context.actions} {...props}>
+      <TableRowDnD
+        ref={ref}
+        item={item}
+        context={context}
+        selected={isSelected}
+        disabled={isDisabled}
+        hover
+      >
+        {children}
+      </TableRowDnD>
+    </RightClickFileMenu>
+  )
+})
+
+TableRow.displayName = 'TableRow'
+
+const TableRowMemo = React.memo(TableRow)
 
 const Table = ({
   rows,
@@ -21,6 +48,8 @@ const Table = ({
 }) => {
   return (
     <VirtuosoTable
+      context={{ actions }}
+      components={{ ...virtuosoComponents, TableRow: TableRowMemo }}
       rows={rows}
       columns={columns}
       dragProps={dragProps}
