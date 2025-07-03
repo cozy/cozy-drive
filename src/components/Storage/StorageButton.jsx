61fe59739ca8e90e657dc2b692a2b4b2d81f712e
Théo Poizat <hello@zatteo.com>
2025-07-03 07:49:47 +0200

    fix: Redirect StorageButton to premium link instead of hardcoded link

--- src/components/Storage/StorageButton.jsx
+++ src/components/Storage/StorageButton.jsx
@@ -1,14 +1,26 @@
 import cx from 'classnames'
 import React from 'react'
 
+import { useInstanceInfo } from 'cozy-client'
+import {
+  shouldDisplayOffers,
+  buildPremiumLink
+} from 'cozy-client/dist/models/instance'
+import { isFlagshipApp } from 'cozy-device-helper'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import TwakeWorkplaceIcon from 'cozy-ui/transpiled/react/Icons/TwakeWorkplace'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 const StorageButton = ({ className }) => {
-  const { t, lang } = useI18n()
-  const _lang = ['en', 'fr', 'es'].includes(lang) ? lang : 'en'
+  const { t } = useI18n()
+  const instanceInfo = useInstanceInfo()
+  const { isMobile } = useBreakpoints()
+
+  if (!instanceInfo.isLoaded || isFlagshipApp() || isMobile) return null
+
+  if (!shouldDisplayOffers(instanceInfo)) return null
 
   return (
     <Button
@@ -19,7 +31,9 @@ const StorageButton = ({ className }) => {
       size="small"
       height="auto"
       fullWidth
-      onClick={() => window.open(`https://cozy.io/${_lang}/pricing/`)}
+      component="a"
+      target="_blank"
+      href={buildPremiumLink(instanceInfo)}
     />
   )
 }
