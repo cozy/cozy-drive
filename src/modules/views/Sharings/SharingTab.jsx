86b6dcaddea6cb2fbcf3efcfec67be414d11c25c
lethemanh <themanhle1234@gmail.com>
2025-08-01 10:05:38 +0700

    feat: Display shared drive as tab in sharings section ::sparkles:

---
+++ src/modules/views/Sharings/SharingTab.jsx
@@ -0,0 +1,76 @@
+import cx from 'classnames'
+import React from 'react'
+
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import FileTypeFolderIcon from 'cozy-ui/transpiled/react/Icons/FileTypeFolder'
+import FileTypeServerIcon from 'cozy-ui/transpiled/react/Icons/FileTypeServer'
+import Tab from 'cozy-ui/transpiled/react/Tab'
+import Tabs from 'cozy-ui/transpiled/react/Tabs'
+import ToggleButton from 'cozy-ui/transpiled/react/ToggleButton'
+import ToggleButtonGroup from 'cozy-ui/transpiled/react/ToggleButtonGroup'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+import styles from '@/styles/topbar.styl'
+
+const SharingTab = ({ tab, setTab }) => {
+  const tabItems = ['sharings_tab_all', 'sharings_tab_drives']
+  const { isMobile } = useBreakpoints()
+  const { t } = useI18n()
+
+  const handleChange = (_, newValue) => {
+    setTab(newValue)
+  }
+
+  return (
+    <>
+      {isMobile ? (
+        <Tabs
+          indicatorColor="primary"
+          textColor="primary"
+          value={tab}
+          narrowed
+          onChange={handleChange}
+        >
+          {tabItems.map(tabItem => (
+            <Tab key={tabItem} label={t(`toolbar.${tabItem}`)} />
+          ))}
+        </Tabs>
+      ) : (
+        <ToggleButtonGroup
+          variant="rounded"
+          value={tab}
+          exclusive
+          onChange={handleChange}
+          className={cx('u-mb-1-half', styles['fil-topbar'])}
+        >
+          {tabItems.map((tabItem, index) => (
+            <ToggleButton
+              key={tabItem}
+              value={index}
+              rounded
+              className={cx(styles['fil-tab-item'], 'u-ml-0', {
+                [styles['fil-tab-item--selected']]: index === tab
+              })}
+            >
+              <Icon
+                icon={
+                  tabItem === 'sharings_tab_all'
+                    ? FileTypeFolderIcon
+                    : FileTypeServerIcon
+                }
+                className={cx(styles['fil-tab-icon'], 'u-mr-1')}
+              />
+              <Typography className={cx(styles['fil-tab-name'])}>
+                {t(`toolbar.${tabItem}`)}
+              </Typography>
+            </ToggleButton>
+          ))}
+        </ToggleButtonGroup>
+      )}
+    </>
+  )
+}
+
+export default SharingTab
