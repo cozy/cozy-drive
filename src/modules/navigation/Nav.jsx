a53102b8394015762dba53cceedf7506423600a9
lethemanh <themanhle1234@gmail.com>
2025-08-18 10:38:40 +0700

    feat: Remove shared drive list from sidebar :coffin:

--- src/modules/navigation/Nav.jsx
+++ src/modules/navigation/Nav.jsx
@@ -12,13 +12,10 @@ import { FavoriteList } from '@/modules/navigation/FavoriteList'
 import { useNavContext } from '@/modules/navigation/NavContext'
 import { NavItem } from '@/modules/navigation/NavItem'
 import { SharingsNavItem } from '@/modules/navigation/SharingsNavItem'
-import { SharedDriveList } from '@/modules/navigation/components/SharedDriveList'
-import { useSharedDrives } from '@/modules/shareddrives/hooks/useSharedDrives'
 
 export const Nav = () => {
   const clickState = useNavContext()
   const { isDesktop } = useBreakpoints()
-  const { isLoaded: isSharedDriveLoaded, sharedDrives } = useSharedDrives()
 
   return (
     <UINav>
@@ -53,13 +50,6 @@ export const Nav = () => {
         rx={/\/trash(\/.*)?/}
         clickState={clickState}
       />
-      {isDesktop && isSharedDriveLoaded ? (
-        <SharedDriveList
-          clickState={clickState}
-          className="u-mt-half"
-          sharedDrives={sharedDrives}
-        />
-      ) : null}
       {isDesktop ? (
         <FavoriteList clickState={clickState} className="u-mt-half" />
       ) : null}
