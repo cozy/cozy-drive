bc68ff06f2c3eec7b206714d9aa6aab390fdbdef
doubleface <christophe@cozycloud.cc>
2025-09-01 18:16:49 +0200

    feat: Add header to shared folder view

--- src/modules/views/SharedDrive/SharedDriveFolderView.jsx
+++ src/modules/views/SharedDrive/SharedDriveFolderView.jsx
@@ -3,7 +3,10 @@ import { Outlet, useParams } from 'react-router-dom'
 
 import { Content } from 'cozy-ui/transpiled/react/Layout'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
+import { MobileAwareBreadcrumb as Breadcrumb } from '@/modules/breadcrumb/components/MobileAwareBreadcrumb'
+import Toolbar from '@/modules/drive/Toolbar'
 import { SharedDriveFolderBody } from '@/modules/shareddrives/components/SharedDriveFolderBody'
 import { useSharedDriveFolder } from '@/modules/shareddrives/hooks/useSharedDriveFolder'
 import FolderView from '@/modules/views/Folder/FolderView'
@@ -12,6 +15,7 @@ import FolderViewHeader from '@/modules/views/Folder/FolderViewHeader'
 const SharedDriveFolderView = () => {
   const { isMobile } = useBreakpoints()
   const { driveId, folderId } = useParams()
+  const { t } = useI18n()
 
   const { sharedDriveResult } = useSharedDriveFolder({
     driveId,
@@ -25,7 +29,10 @@ const SharedDriveFolderView = () => {
   return (
     <FolderView>
       <Content className={isMobile ? '' : 'u-pt-1'}>
-        <FolderViewHeader>TODO</FolderViewHeader>
+        <FolderViewHeader>
+          <Breadcrumb path={[{ name: t('breadcrumb.title_sharings') }]} />
+          <Toolbar canUpload={false} canCreateFolder={false} />
+        </FolderViewHeader>
         <SharedDriveFolderBody
           folderId={folderId}
           queryResults={queryResults}
