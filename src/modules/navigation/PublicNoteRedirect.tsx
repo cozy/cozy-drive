cd7b44fb3cf1253a3174754f95d71aed976f341c
Théo Poizat <hello@zatteo.com>
2025-09-05 07:44:11 +0200

    feat: Open a shared drive note

--- src/modules/navigation/PublicNoteRedirect.tsx
+++ src/modules/navigation/PublicNoteRedirect.tsx
@@ -14,7 +14,7 @@ import { DummyLayout } from '@/modules/layout/DummyLayout'
 
 const PublicNoteRedirect: FC = () => {
   const { t } = useI18n()
-  const { fileId } = useParams()
+  const { fileId, driveId } = useParams()
   const client = useClient()
 
   const [noteUrl, setNoteUrl] = useState<string | null>(null)
@@ -38,6 +38,7 @@ const PublicNoteRedirect: FC = () => {
             id: fileId
           },
           {
+            driveId,
             pathname
           }
         )
