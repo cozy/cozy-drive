f9e3192b87b3f66199896844ca2c65f0f3e36927
Théo Poizat <hello@zatteo.com>
2025-09-18 11:40:52 +0200

    feat: Display menu on sidebar shared drive with manage access action
    A icon can now appear on hovering a shared drive item in sidebar.
    When clicking this icon, it displays an action menu. This action menu
    for now allows to open a manage access modal.
    
    Opening this manage access modal is done with a redirection. It could
    be improved (so without redirection) later.

--- src/modules/navigation/components/SharedDriveListItem.tsx
+++ src/modules/navigation/components/SharedDriveListItem.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from 'react'
+import cx from 'classnames'
+import React, { FC, useState } from 'react'
 
 import FileTypeServerIcon from 'cozy-ui/transpiled/react/Icons/FileTypeServer'
 import { NavIcon, NavLink, NavItem } from 'cozy-ui/transpiled/react/Nav'
 import Typography from 'cozy-ui/transpiled/react/Typography'
+import { makeStyles } from 'cozy-ui/transpiled/react/styles'
 
 import { FileLink } from '@/modules/navigation/components/FileLink'
 import { useSharedDriveLink } from '@/modules/navigation/hooks/useSharedDriveLink'
+import { SharedDriveListItemMenu } from '@/modules/shareddrives/components/SharedDriveListItemMenu'
 import { SharedDrive } from '@/modules/shareddrives/helpers'
+
+// We need to override the NavItem styles to make the menu icon appear on the
+// right side of the item.
+const useStyles = makeStyles({
+  withMenu: {
+    flex: 0,
+    width: 'calc(100% - 6.5rem)'
+  }
+})
 
 interface SharedDriveListItemProps {
   sharedDrive: SharedDrive
@@ -18,13 +30,23 @@ const SharedDriveListItem: FC<SharedDriveListItemProps> = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   clickState: [lastClicked, setLastClicked]
 }) => {
+  const classes = useStyles()
+
   const { link } = useSharedDriveLink(sharedDrive)
+  const [isMenuAvailable, setIsMenuAvailable] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const toggleMenu = (): void => setIsMenuOpen(!isMenuOpen)
 
   return (
-    <NavItem key={sharedDrive._id}>
+    <NavItem
+      key={sharedDrive._id}
+      className="u-flex"
+      onMouseOver={(): void => setIsMenuAvailable(true)}
+      onMouseLeave={(): void => setIsMenuAvailable(false)}
+    >
       <FileLink
         link={link}
-        className={NavLink.className}
+        className={cx(NavLink.className, isMenuAvailable && classes.withMenu)}
         onClick={(): void => setLastClicked(undefined)}
       >
         <NavIcon icon={FileTypeServerIcon} />
@@ -32,6 +54,13 @@ const SharedDriveListItem: FC<SharedDriveListItemProps> = ({
           {sharedDrive.description}
         </Typography>
       </FileLink>
+      {isMenuAvailable && (
+        <SharedDriveListItemMenu
+          isOpen={isMenuOpen}
+          toggleMenu={toggleMenu}
+          sharedDrive={sharedDrive}
+        />
+      )}
     </NavItem>
   )
 }
---
+++ src/modules/shareddrives/components/actions/manageAccess.js
@@ -0,0 +1,36 @@
+import React, { forwardRef } from 'react'
+
+import ActionsMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import PeopleIcon from 'cozy-ui/transpiled/react/Icons/People'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+
+export const manageAccess = ({ sharedDrive, navigate, t }) => {
+  const label = t('toolbar.menu_manage_access')
+  const icon = PeopleIcon
+
+  return {
+    name: 'manageAccess',
+    label: label,
+    icon,
+    action: () => {
+      const folderId = sharedDrive?.rules[0]?.values[0]
+      if (folderId) {
+        navigate(`folder/${folderId}/share`)
+      } else {
+        navigate('sharings')
+      }
+    },
+    Component: forwardRef(function ManageAccess(props, ref) {
+      return (
+        <ActionsMenuItem {...props} ref={ref}>
+          <ListItemIcon>
+            <Icon icon={icon} />
+          </ListItemIcon>
+          <ListItemText primary={label} />
+        </ActionsMenuItem>
+      )
+    })
+  }
+}
