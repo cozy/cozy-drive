b57bb60532cdd7dd632182ef055e22c6e9f5f1cd
Théo Poizat <hello@zatteo.com>
2025-09-22 18:33:01 +0200

    feat: Display manage access button for owner in MoreMenu

--- src/modules/drive/Toolbar/components/MoreMenu.jsx
+++ src/modules/drive/Toolbar/components/MoreMenu.jsx
@@ -11,6 +11,7 @@ import AddMenuItem from '@/modules/drive/Toolbar/components/AddMenuItem'
 import DownloadButtonItem from '@/modules/drive/Toolbar/components/DownloadButtonItem'
 import InsideRegularFolder from '@/modules/drive/Toolbar/components/InsideRegularFolder'
 import LeaveSharedDriveButtonItem from '@/modules/drive/Toolbar/components/LeaveSharedDriveButtonItem'
+import ManageAccessItem from '@/modules/drive/Toolbar/components/ManageAccessItem'
 import DeleteItem from '@/modules/drive/Toolbar/delete/DeleteItem'
 import SelectableItem from '@/modules/drive/Toolbar/selectable/SelectableItem'
 import ShareItem from '@/modules/drive/Toolbar/share/ShareItem'
@@ -97,6 +98,7 @@ const MoreMenu = ({
             </InsideRegularFolder>
             {isMobile && hasWriteAccess && <AddMenuItem />}
             <SelectableItem onClick={showSelectionBar} />
+            {isSharedDriveOwner && <ManageAccessItem />}
             {hasWriteAccess && (
               <InsideRegularFolder
                 displayedFolder={displayedFolder}
