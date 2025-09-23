3f9b87545f6686117ba3311c8ef60f62f2f5d268
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-09-23 15:42:58 +0200

    feat: Add trash illustration on trash view when trash is empty

--- src/components/Error/Empty.jsx
+++ src/components/Error/Empty.jsx
@@ -9,19 +9,21 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 import styles from './empty.styl'
 
 import FolderEmptyIllu from '@/assets/icons/illu-folder-empty.svg'
+import TrashIllustration from '@/assets/icons/illu-trash-empty.svg'
 
-const EmptyCanvas = ({ canUpload, localeKey, hasTextMobileVersion }) => {
+const EmptyCanvas = ({ type, canUpload, localeKey, hasTextMobileVersion }) => {
   const { t } = useI18n()
   const { isMobile } = useBreakpoints()
 
   const otherProps = { iconSize: isMobile ? 'large' : 'medium' }
+  const IconToShow = type === 'trash' ? TrashIllustration : FolderEmptyIllu
 
   return (
     <Empty
       data-testid="empty-folder"
       icon={
         <div className="u-w-100">
-          <Icon icon={FolderEmptyIllu} size={160} />
+          <Icon icon={IconToShow} size={160} />
         </div>
       }
       title={localeKey ? t(`empty.${localeKey}_title`) : t('empty.title')}
