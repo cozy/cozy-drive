4a13d56540de6cd4222d59bfe5cba1bba0714d66
Théo Poizat <hello@zatteo.com>
2025-09-23 06:23:28 +0200

    refactor: Extract helpers to detected shared drive owner vs recipient

--- src/modules/shareddrives/helpers.ts
+++ src/modules/shareddrives/helpers.ts
@@ -1,4 +1,7 @@
+import { IOCozyFile } from 'cozy-client/types/types'
+
 import type { File, FolderPickerEntry } from '@/components/FolderPicker/types'
+import { SHARED_DRIVES_DIR_ID } from '@/constants/config'
 
 // Temporary type, need to be completed and then put in cozy-client
 export interface SharedDrive {
@@ -35,6 +38,15 @@ export const getFolderIdFromSharing = (
     return undefined
   }
 }
+
+export const isFolderFromSharedDriveRecipient = (folder: IOCozyFile): boolean =>
+  folder && Boolean(folder.driveId)
+
+export const isFolderFromSharedDriveOwner = (folder: IOCozyFile): boolean =>
+  folder &&
+  folder._type == 'io.cozy.files' &&
+  folder.dir_id === SHARED_DRIVES_DIR_ID &&
+  !isFolderFromSharedDriveRecipient(folder)
 
 export const isSharedDriveFolder = (
   file: File | FolderPickerEntry
