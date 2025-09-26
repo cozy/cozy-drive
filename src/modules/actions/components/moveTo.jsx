ed00235fb9b7d44d958c3fa1cfac2d577fa5cd02
Théo Poizat <hello@zatteo.com>
2025-09-26 17:10:29 +0200

    fix: Do not display "Move" actions for share drive
    It makes no sense to move a shared drive.

--- src/modules/actions/components/moveTo.jsx
+++ src/modules/actions/components/moveTo.jsx
@@ -7,6 +7,7 @@ import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
 import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
 
 import { navigateToModalWithMultipleFile } from '@/modules/actions/helpers'
+import { isSharedDriveFolder } from '@/modules/shareddrives/helpers'
 
 const moveTo = ({ t, canMove, pathname, navigate, isMobile, search }) => {
   const icon = MovetoIcon
@@ -18,7 +19,13 @@ const moveTo = ({ t, canMove, pathname, navigate, isMobile, search }) => {
     name: 'moveTo',
     label,
     icon,
-    displayCondition: docs => docs.length > 0 && canMove,
+    displayCondition: docs => {
+      return (
+        docs.length > 0 &&
+        canMove &&
+        docs.every(doc => !isSharedDriveFolder(doc))
+      )
+    },
     action: async files => {
       navigateToModalWithMultipleFile({
         files,
