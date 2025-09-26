0b49269cee8fd4629010f670e46543882b105ef6
Théo Poizat <hello@zatteo.com>
2025-09-26 17:04:22 +0200

    fix: Do not display "Share" actions for share drive
    For owner, we already have "manage access".
    
    For recipient, we do not want to display it.

--- src/modules/actions/share.jsx
+++ src/modules/actions/share.jsx
@@ -10,6 +10,7 @@ import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 
 import { isEncryptedFileOrFolder } from '@/lib/encryption'
 import { navigateToModal } from '@/modules/actions/helpers'
+import { isSharedDriveFolder } from '@/modules/shareddrives/helpers'
 
 const share = ({ t, hasWriteAccess, navigate, pathname, allLoaded }) => {
   const label = t('Files.share.cta')
@@ -24,7 +25,8 @@ const share = ({ t, hasWriteAccess, navigate, pathname, allLoaded }) => {
         allLoaded && // We need to wait for the sharing context to be completely loaded to avoid race conditions
         hasWriteAccess &&
         files?.length === 1 &&
-        !isEncryptedFileOrFolder(files[0])
+        !isEncryptedFileOrFolder(files[0]) &&
+        !isSharedDriveFolder(files[0])
       )
     },
     action: files =>
