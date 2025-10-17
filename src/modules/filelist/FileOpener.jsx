3e87aa2c951a41cb4b8248cdbd38c1c256e58414
Vince Linise <contact@vincelinise.com>
2025-10-17 15:14:18 +0200

    feat: File open from parent iframe shell

--- src/modules/filelist/FileOpener.jsx
+++ src/modules/filelist/FileOpener.jsx
@@ -5,6 +5,7 @@ import styles from './fileopener.styl'
 import { useLongPress } from '@/hooks/useOnLongPress'
 import { FileLink } from '@/modules/navigation/components/FileLink'
 import { useFileLink } from '@/modules/navigation/hooks/useFileLink'
+import { useShell } from '@/hooks/useShell'
 
 const FileOpener = ({
   file,
@@ -16,6 +17,7 @@ const FileOpener = ({
 }) => {
   const rowRef = useRef()
   const { link, openLink } = useFileLink(file)
+  const { runsInShell, selectedFile, openFileInParent } = useShell();
   const handlers = useLongPress({
     file,
     disabled,
@@ -24,6 +26,26 @@ const FileOpener = ({
     toggle,
     onInteractWithFile
   })
+
+  const fileExt = file.name.split(".")[file.name.split(".").length - 1];
+
+  if (runsInShell && file.type === "file") {
+    if (fileExt == "docs-note") {
+      return (
+        <div
+          onClick={() => {
+            openFileInParent(file)
+          }}
+          className={`${styles['file-opener']} ${styles['file-opener__a']}`}
+        >
+          {children}
+        </div>
+      )
+    }
+    else {
+      link.openInNewTab = true;
+    }
+  }
 
   return (
     <FileLink
