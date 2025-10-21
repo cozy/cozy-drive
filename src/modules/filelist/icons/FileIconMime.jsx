127233109709242e69415604dfb12f58010c3dee
doubleface <christophe@cozycloud.cc>
2025-10-21 19:02:48 +0200

    Revert "feat: Display colored folders if any color is defined"
    This reverts commit dfe2ba9ac4544078e19c2cec036148edc60a7cf8.

--- src/modules/filelist/icons/FileIconMime.jsx
+++ src/modules/filelist/icons/FileIconMime.jsx
@@ -6,24 +6,19 @@ import Icon from 'cozy-ui/transpiled/react/Icon'
 
 import { isEncryptedFolder } from '@/lib/encryption'
 import getMimeTypeIcon from '@/lib/getMimeTypeIcon'
-import { CustomizedIcon } from '@/modules/views/Folder/CustomizedIcon'
 
 const FileIconMime = ({ file, size = 32, isEncrypted = false }) => {
   const isDir = isDirectory(file)
   const isDirEncrypted = isEncrypted || (isDirectory && isEncryptedFolder(file)) // use file.ref + file.type
 
-  if (isDir) {
-    return <CustomizedIcon selectedColor={file.decorations.color} size={size} />
-  } else {
-    return (
-      <Icon
-        icon={getMimeTypeIcon(isDir, file.name, file.mime, {
-          isEncrypted: isDirEncrypted
-        })}
-        size={size}
-      />
-    )
-  }
+  return (
+    <Icon
+      icon={getMimeTypeIcon(isDir, file.name, file.mime, {
+        isEncrypted: isDirEncrypted
+      })}
+      size={size}
+    />
+  )
 }
 
 FileIconMime.propTypes = {
