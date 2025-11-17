fa9037c43655c4243f3ff7aa2a68eca712d54a31
doubleface <christophe@cozycloud.cc>
2025-11-17 17:11:08 +0100

    fix: Refresh public view on drag&drop and rename

--- src/modules/views/Folder/virtualized/FolderViewBodyContent.jsx
+++ src/modules/views/Folder/virtualized/FolderViewBodyContent.jsx
@@ -114,7 +114,8 @@ const FolderViewBodyContent = ({
               selectAll,
               registerCancelable,
               sharedPaths,
-              t
+              t,
+              refreshFolderContent
             })
           }}
           fetchMore={fetchMore}
@@ -147,7 +148,8 @@ const FolderViewBodyContent = ({
               selectAll,
               registerCancelable,
               sharedPaths,
-              t
+              t,
+              refreshFolderContent
             })
           }}
           onInteractWithFile={onInteractWithFile}
